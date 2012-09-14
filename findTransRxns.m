c661d733cd61b5b5dd1ca88945baea828e9a6490
Daniel Robert Hyduke <danielhyduke@gmail.com>
2012-09-13 17:23:08 -0700

    added in jd's patch for bug 3566781

--- findTransRxns.m
+++ findTransRxns.m
@@ -1,7 +1,8 @@
-function [transRxns,nonTransRxns] = findTransRxns(model,inclExc)
-%findTransRxns identify all transport reactions in a model
+function [transRxns,nonTransRxns] = findTransRxns(model, inclExc, rxnInds)
+%findTransRxns identify all transport reactions in a model, which are
+%defined as reactions involved with metabolites in more than 1 compartment
 %
-% [transRxns,nonTransRxns] = findTransRxns(model,inclExc)
+% [transRxns,nonTransRxns] = findTransRxns(model,inclExc,rxnInds)
 %
 %INPUT
 % model             COBRA model structure
@@ -9,38 +10,54 @@ function [transRxns,nonTransRxns] = findTransRxns(model,inclExc)
 %OPTIONAL INPUT
 % inclExc           include exchange reactions as transport?
 %                   (Default = false)
+% rxnInds           indices of reactions to test for transport activity.
+%                   (default = test all columns of model.S)
 %
 %OUTPUT
 % transRxns         all transport reactions in the model
 % nonTransRxns      all non-transport reactions in the model
 %
+% Note that: rxnsInds = union(transRxns, nonTransRxns).
+%
 % right now, this function only works with models the compartments [c],
 % [p], and [e]
 %
 % Jeff Orth  8/31/07
+% 
+% Jonathan Dreyfuss on 6/16/12
+% modified the function to work with arbitrary compartments (rather than just
+% with [c], [p], & [e]), to use arrayfun, & to use findExcRxns.
 
 if nargin < 2
     inclExc = false;
 end
+if nargin < 3
+    rxnInds = 1:size(model.S, 2);
+end
 
-isTrans = zeros(1,length(model.rxns));
 
-for i = 1:length(model.rxns)
-    mets = model.mets(find(model.S(:,i)));
-    cMets = regexp(mets,'\[c\]');
-    hasCs = ~isempty([cMets{:}]);
-    pMets = regexp(mets,'\[p\]');
-    hasPs = ~isempty([pMets{:}]);
-    eMets = regexp(mets,'\[e\]');
-    hasEs = ~isempty([eMets{:}]);
-    
-    if (sum([hasCs,hasPs,hasEs]) > 1) || hasEs&&inclExc
-        isTrans(i) = 1;
+if inclExc
+    % findExcRxns returns boolean vector
+    isExc = findExcRxns(model,inclObjFlag,irrevFlag);
+else
+    isExc=zeros(1, length(rxnInds));
+end
+
+isNonexchTrans = zeros(1,length(rxnInds));
+
+% if there are few rxns to test (e.g. <25% of rxns), then could ID the
+% indices of relevant mets, to save time downstream, but not done here.
+
+[baseMetNames,compSymbols,uniqueMetNames,uniqueCompSymbols]=arrayfun(@parseMetNames, model.mets);
+
+for i = rxnInds
+    uniqueCompSymbolsTmp=uniqueCompSymbols(model.S(:,i)~=0);
+    % if there's more than 1 compartment involved, it's a transport rxn
+    if length(unique(uniqueCompSymbolsTmp))>1
+        isNonexchTrans(i) = 1;
     end
 end
 
-transRxns = model.rxns(isTrans==1);
-nonTransRxns = model.rxns(isTrans==0);
-
-
-
+rxnNames=model.rxns(rxnInds);
+transRxns = rxnNames(isNonexchTrans==1 | isExc==1);
+nonTransRxns = setdiff(rxnNames, transRxns);
