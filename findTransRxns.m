59f3988038b2fbebb782968500ddea65c1b0ff95
Daniel Robert Hyduke <danielhyduke@gmail.com>
2012-10-19 12:39:40 -0700

    added artefact 3575876

--- findTransRxns.m
+++ findTransRxns.m
@@ -1,4 +1,5 @@
-function [transRxns,nonTransRxns] = findTransRxns(model, inclExc, rxnInds)
+function [transRxns,nonTransRxns] = findTransRxns(model,inclExc, ... 
+    rxnInds,inclObjAsExc,irrevFlag)
 %findTransRxns identify all transport reactions in a model, which are
 %defined as reactions involved with metabolites in more than 1 compartment
 %
@@ -12,21 +13,20 @@ function [transRxns,nonTransRxns] = findTransRxns(model, inclExc, rxnInds)
 %                   (Default = false)
 % rxnInds           indices of reactions to test for transport activity.
 %                   (default = test all columns of model.S)
+% inclObjAsExc      include objective as an exchange reaction? this is
+%                   passed to findExcRxns. (default = false)
+% irrevFlag         is model irreversible? this is passed to findExcRxns.
+%                   (default=false)
 %
 %OUTPUT
-% transRxns         all transport reactions in the model
-% nonTransRxns      all non-transport reactions in the model
-%
-% Note that: rxnsInds = union(transRxns, nonTransRxns).
+% transRxns         transport reactions in rxnInds
+% nonTransRxns      non-transport reactions in rxnInds
 %
 % right now, this function only works with models the compartments [c],
-% [p], and [e]
-%
-% Jeff Orth  8/31/07
+% [p], and [e]. Jeff Orth, 8/31/07
 % 
-% Jonathan Dreyfuss on 6/16/12
-% modified the function to work with arbitrary compartments (rather than just
-% with [c], [p], & [e]), to use arrayfun, & to use findExcRxns.
+% modified the function to work with arbitrary compartments, to accept 
+% rxnInds, & to use findExcRxns. Jonathan Dreyfuss, 10/9/12
 
 if nargin < 2
     inclExc = false;
@@ -34,30 +34,39 @@ end
 if nargin < 3
     rxnInds = 1:size(model.S, 2);
 end
-
+if nargin < 4
+    inclObjAsExc = false;
+end
+if nargin < 5
+    irrevFlag = false;
+end
 
 if inclExc
     % findExcRxns returns boolean vector
-    isExc = findExcRxns(model,inclObjFlag,irrevFlag);
+    isExc0 = findExcRxns(model,inclObjAsExc,irrevFlag);
+    % subset to rxnInds
+    isExc = isExc0(rxnInds);
 else
-    isExc=zeros(1, length(rxnInds));
+    isExc=zeros(length(rxnInds), 1);
 end
 
-isNonexchTrans = zeros(1,length(rxnInds));
-
-% if there are few rxns to test (e.g. <25% of rxns), then could ID the
-% indices of relevant mets, to save time downstream, but not done here.
-
-[baseMetNames,compSymbols,uniqueMetNames,uniqueCompSymbols]=arrayfun(@parseMetNames, model.mets);
-
-for i = rxnInds
-    uniqueCompSymbolsTmp=uniqueCompSymbols(model.S(:,i)~=0);
+% initialize isNonexchangeTransport rxns vector
+isNonexchTrans = zeros(length(rxnInds), 1);
+% get compartment symbols for each metabolite
+[baseMetNames,compSymbols]=arrayfun(@parseMetNames, model.mets);
+for i = 1:length(rxnInds)
+    rxnIndTmp=rxnInds(i);
+    % get compartment symbols for each rxn
+    compSymbolsTmp=compSymbols(model.S(:,rxnIndTmp)~=0);
     % if there's more than 1 compartment involved, it's a transport rxn
-    if length(unique(uniqueCompSymbolsTmp))>1
+    if length(unique(compSymbolsTmp))>1
         isNonexchTrans(i) = 1;
     end
 end
 
-rxnNames=model.rxns(rxnInds);
-transRxns = rxnNames(isNonexchTrans==1 | isExc==1);
-nonTransRxns = setdiff(rxnNames, transRxns);
+% get rxn abbreviations for all rxns in rxnInds
+rxnAbbrevs=model.rxns(rxnInds);
+% if inclExc==1, exchange rxns will have isExc==1, and should be counted as
+% transport rxns; else, all isExc will be 0.
+transRxns = rxnAbbrevs(isNonexchTrans==1 | isExc==1);
+nonTransRxns = setdiff(rxnAbbrevs, transRxns);
