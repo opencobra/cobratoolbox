fe7ae8f20a98d186b74f9669271a29a3d69d6f40
Ben Heavner <bheavner@gmail.com>
2013-06-10 11:15:10 -0700

    add if tests for char or cell

--- io/parseSBMLNotesField.m
+++ io/parseSBMLNotesField.m
@@ -9,72 +9,86 @@ function [genes,rule,subSystem,grRule,formula,confidenceScore,citation,comment,e
 % Ben Heavner 10 June 2013 - add cell array functionality
 %
 
-if isempty(regexp(notesField,'html:p', 'once'))
-    tag = 'p';
+if ischar(notesField) %if it's a string, use MH's code
+    if isempty(regexp(notesField,'html:p', 'once'))
+        tag = 'p';
+    else
+        tag = 'html:p';
+    end
+
+    subSystem = '';
+    grRule = '';
+    genes = {};
+    rule = '';
+    formula = '';
+    confidenceScore = '';
+    citation = '';
+    ecNumber = '';
+    comment = '';
+    charge = [];
+    Comment = 0;
+
+    [~,fieldList] = regexp(notesField,['<' tag '>.*?</' tag '>'], ...
+        'tokens', 'match');
+
+    for i = 1:length(fieldList)
+        fieldTmp = regexp(fieldList{i},['<' tag '>(.*)</' tag '>'], ...
+            'tokens');
+        fieldStr = fieldTmp{1}{1};
+        if (regexp(fieldStr,'GENE_ASSOCIATION'))
+            gprStr = regexprep(strrep(fieldStr,'GENE_ASSOCIATION:',''), ...
+                '^(\s)+','');
+            grRule = gprStr;
+            [genes,rule] = parseBoolean(gprStr);
+        elseif (regexp(fieldStr,'GENE ASSOCIATION'))
+            gprStr = regexprep(strrep(fieldStr,'GENE ASSOCIATION:',''), ...
+                '^(\s)+','');
+            grRule = gprStr;
+            [genes,rule] = parseBoolean(gprStr);
+        elseif (regexp(fieldStr,'SUBSYSTEM'))
+            subSystem = regexprep(strrep(fieldStr,'SUBSYSTEM:',''), ...
+                '^(\s)+','');
+            subSystem = strrep(subSystem,'S_','');
+            subSystem = regexprep(subSystem,'_+',' ');
+            if (isempty(subSystem))
+                subSystem = 'Exchange';
+            end
+        elseif (regexp(fieldStr,'EC Number'))
+            ecNumber = regexprep(strrep(fieldStr,'EC Number:',''), ...
+                '^(\s)+','');
+        elseif (regexp(fieldStr,'FORMULA'))
+            formula = regexprep(strrep(fieldStr,'FORMULA:',''), ...
+                '^(\s)+','');
+        elseif (regexp(fieldStr,'CHARGE'))
+            charge = str2num(regexprep(strrep(fieldStr,'CHARGE:',''), ...
+                '^(\s)+',''));
+        elseif (regexp(fieldStr,'AUTHORS'))
+            if isempty(citation)
+                citation = strcat(...
+                    regexprep(strrep(fieldStr,'AUTHORS:',''), ...
+                    '^(\s)+',''));
+            else
+                citation = strcat(...
+                    citation, ';', ...
+                    regexprep(strrep(fieldStr,'AUTHORS:',''),'^(\s)+',''));
+            end
+        elseif Comment == 1 && isempty(regexp(fieldStr,'genes:', 'once'))
+            Comment = 0;
+            comment = fieldStr;
+        elseif (regexp(fieldStr,'Confidence'))
+            confidenceScore = regexprep(strrep(fieldStr, ...
+                'Confidence Level:', ''), '^(\s)+', '');
+            Comment = 1;
+        end
+    end
+    
+elseif iscell(str) % if it's a cell array, use BH code
+        
 else
-    tag = 'html:p';
+    errorstr = [...
+        'The str variable passed to parseBoolean must be a string or ' ...
+        'cell array.'];
+    error(errorstr)
 end
 
-subSystem = '';
-grRule = '';
-genes = {};
-rule = '';
-formula = '';
-confidenceScore = '';
-citation = '';
-ecNumber = '';
-comment = '';
-charge = [];
-Comment = 0;
-
-[tmp,fieldList] = regexp(notesField,['<' tag '>.*?</' tag '>'], ...
-    'tokens', 'match');
-
-for i = 1:length(fieldList)
-    fieldTmp = regexp(fieldList{i},['<' tag '>(.*)</' tag '>'], ...
-        'tokens');
-    fieldStr = fieldTmp{1}{1};
-    if (regexp(fieldStr,'GENE_ASSOCIATION'))
-        gprStr = regexprep(strrep(fieldStr,'GENE_ASSOCIATION:',''), ...
-            '^(\s)+','');
-        grRule = gprStr;
-        [genes,rule] = parseBoolean(gprStr);
-    elseif (regexp(fieldStr,'GENE ASSOCIATION'))
-        gprStr = regexprep(strrep(fieldStr,'GENE ASSOCIATION:',''), ...
-            '^(\s)+','');
-        grRule = gprStr;
-        [genes,rule] = parseBoolean(gprStr);
-    elseif (regexp(fieldStr,'SUBSYSTEM'))
-        subSystem = regexprep(strrep(fieldStr,'SUBSYSTEM:',''), ...
-            '^(\s)+','');
-        subSystem = strrep(subSystem,'S_','');
-        subSystem = regexprep(subSystem,'_+',' ');
-        if (isempty(subSystem))
-            subSystem = 'Exchange';
-        end
-    elseif (regexp(fieldStr,'EC Number'))
-        ecNumber = regexprep(strrep(fieldStr,'EC Number:',''), ...
-            '^(\s)+','');
-    elseif (regexp(fieldStr,'FORMULA'))
-        formula = regexprep(strrep(fieldStr,'FORMULA:',''), ...
-            '^(\s)+','');
-    elseif (regexp(fieldStr,'CHARGE'))
-        charge = str2num(regexprep(strrep(fieldStr,'CHARGE:',''), ...
-            '^(\s)+',''));
-    elseif (regexp(fieldStr,'AUTHORS'))
-        if isempty(citation)
-            citation = strcat(regexprep(strrep(fieldStr,'AUTHORS:',''), ...
-                '^(\s)+',''));
-        else
-            citation = strcat(citation, ';', ...
-                regexprep(strrep(fieldStr,'AUTHORS:',''),'^(\s)+',''));
-        end
-    elseif Comment == 1 && isempty(regexp(fieldStr,'genes:', 'once'))
-        Comment = 0;
-        comment = fieldStr;
-    elseif (regexp(fieldStr,'Confidence'))
-        confidenceScore = regexprep(strrep(fieldStr, ...
-            'Confidence Level:', ''), '^(\s)+', '');
-        Comment = 1;
-    end
 end
