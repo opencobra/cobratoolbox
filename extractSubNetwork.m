be7c553c1efff6d7d432968485f4adcd6657cb66
sriki18 <srikiranc@gmail.com>
2014-07-15 23:59:11 +0530

    Add the fields - rxnNames, rules and metCharge
    Adds rxnNames, rules (which contains the binary representations whereas
    grRules contains a string(cell) representation) and metCharge to the
    subnetwork.

--- extractSubNetwork.m
+++ extractSubNetwork.m
@@ -19,6 +19,7 @@ function subModel = extractSubNetwork(model,rxnList,metList)
 % - Replaced rxnNames with rxnList as model.rxnNames is different from
 % model.rxns, and model.rxns is used to select the subnetwork.
 % - Replaced metNames with metList, to avoid similar confusion.
+% - Added the fields - rxnNames, rules and metCharge to subModel.
 
 selRxns = ismember(model.rxns,rxnList);
 subS = model.S(:,selRxns);
@@ -70,4 +71,13 @@ if (isfield(model,'geneNames'))
 end
 if (isfield(model,'subSystems'))
     subModel.subSystems = model.subSystems(selRxns);
+end
+if (isfield(model,'rxnNames'))
+    subModel.rxnNames = model.rxnNames(selRxns);
+end
+if (isfield(model,'rules'))
+    subModel.rules = model.rules(selRxns);
+end
+if (isfield(model,'metCharge'))
+    subModel.metCharge = model.metCharge(selRxns);
 end
