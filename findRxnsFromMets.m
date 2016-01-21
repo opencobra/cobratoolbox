643818d84df8a9546f64720a6c229b96a3c66034
Thomas Pfau <thomas@thomas-precision-t1700.uni.lux>
2016-01-21 07:37:22 +0100

    updated findRxnsFromMets to accurately reflect the verboseFlag description and added options for Property/Value pairs

--- findRxnsFromMets.m
+++ findRxnsFromMets.m
@@ -1,4 +1,4 @@
-function [rxnList, rxnFormulaList] = findRxnsFromMets(model, metList, verbFlag)
+function [rxnList, rxnFormulaList] = findRxnsFromMets(model, metList, varargin)
 %findRxnsFromMets returns a list of reactions in which at least one
 %metabolite listed in metList participates.
 %
@@ -10,25 +10,62 @@ function [rxnList, rxnFormulaList] = findRxnsFromMets(model, metList, verbFlag)
 %
 %OPTIONAL INPUT
 % verbFlag          Print reaction formulas to screen (Default = false)
+% Property/Value    Allowed Properties are: 
+%                   containsAll (True/False) - If true only reactions
+%                   containing all metabolites in metList are returned
+%                   verbFlag (True/False) as Above, will overwrite a
+%                   verbFlag set individually
 %
 %OUTPUTS
 % rxnList           List of reactions
 % rxnFormulaList    Reaction formulas coresponding to rxnList
 %
 %Richard Que (08/12/2010)
-% Almut Heinken (09/25/2015)- made change so formulas are not printed if reaction list
-% is empty.
+%Richard Que (08/12/2010)
+% Almut Heinken (09/25/2015)- made change so formulas are not printed if reaction list 
+%                             is empty.
+% Thomas Pfau (21/1/2016) - Additional Options, and minimal speedup of the indexing, 
+%                           also updated behaviour of verbFlag to accurately reflect description.
+% 
 
-if nargin < 3 || isempty(verbFlag), verbFlag = false; end
+verbFlag = false;
+containsAll = false;
+
+if mod(numel(varargin), 2) == 1 % the first argument has to be verbFlag, the remainder Argument/Value pairs
+    verbFlag = varargin{1};
+    if numel(varargin) > 1
+        varargin = varargin(2:end);
+    end
+end
+
+if numel(varargin) > 1 % we have already checked whether we have a verbFlag, now we can go for Argument value pairs
+    for i=1:2:numel(varargin)
+        key = varargin{i};
+        value = varargin{i+1};
+        switch key
+            case 'containsAll'
+                containsAll = value;
+            case 'verbFlag' 
+                verbFlag = value;
+            otherwise
+                msg = sprintf('Unexpected key %s',key)
+                error(msg);
+        end
+                
+    end
+end
 
 %Find met indicies
-[isMet index] = ismember(metList,model.mets);
-index = index(isMet);
-%expand rxns list for logical indexing
-rxns = repmat(model.rxns,1,length(index));
-%find reactions
-rxnList = unique(rxns(model.S(index,:)'~=0));
-if nargout > 1
+index = ismember(model.mets,metList);
+if containsAll
+   rxnList = model.rxns(sum(model.S(index,:) ~= 0) == numel(metList));   
+else
+    %rxns = repmat(model.rxns,1,length(index));
+    %find reactions i.e. all columns with at least one non zero value
+    rxnList = model.rxns(sum(model.S(index,:)~=0) > 0);
+end
+
+if verbFlag
     if ~isempty(rxnList)
     rxnFormulaList = printRxnFormula(model,rxnList,verbFlag);
     else
