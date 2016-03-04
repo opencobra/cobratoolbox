2f5a68282a0e5fcee974b419668b5719c0577ab0
Will Bryant <w.bryant@imperial.ac.uk>
2016-03-04 16:57:32 +0000

    Allow model not to contain genetic information when imported

--- gapFilling/fastGapFill/submitFastGapFill.m
+++ gapFilling/fastGapFill/submitFastGapFill.m
@@ -123,6 +123,19 @@ else
         model = readMlModel(modelFile);
     elseif regexp(modelFile,'.xml$')
         model = readCbModel(modelFile);
+        % If subSystems is empty, create a dummy subSystems for mergeTwoModels
+        if ~exist('model.subSystems') || length(model.subSystems) ~= length(model.rxnNames)
+            model.subSystems = repmat({''},length(model.rxnNames));
+        end
+        if ~exist('model.genes')
+            model.genes = repmat({'no_gene'},1);
+        end
+        if ~exist('model.rxnGeneMat')
+            model.rxnGeneMat = zeros(length(model.rxnNames),1);
+        end
+        if ~exist('model.grRules')
+            model.grRules = repmat({''},length(model.rxnNames));
+        end
     end
     
     % remove constraints from exchange reactions
