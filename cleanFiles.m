592a19652ed981a5a3bb6574403dc64b303ef91d
Laurent Heirendt <laurent.heirendt@uni.lu>
2016-08-31 14:44:16 +0200

    Implementation of optional user interaction

--- cleanFiles.m
+++ cleanFiles.m
@@ -3,7 +3,6 @@
 % Purpose: Clean log and result files
 % Author: Laurent Heiredt, LCSB
 %
-% Feature request by Marouen - featured key user: optional request for confirmation of deletion of files
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Clear log files from the drivers directory
@@ -13,13 +12,19 @@
 %  unix('sudo rm ../drivers/*.log');
 %end
 
+userInteraction = true;
+
 % Clear previous log files in logFiles/
 D = dir('../fastFVA/logFiles/*.log');
 nbFiles = numel(D);
 if(nbFiles > 0)
     fprintf('\n Number of files in logFiles/: %d\n', nbFiles);
-    m=input('\n Do you want to clean all log files in logFiles/ ? - Y/N [Y]: ','s');
-    if m=='Y' || m =='y'
+
+    if userInteraction
+      m=input('\n Do you want to clean all log files in logFiles/ ? - Y/N [Y]: ','s');
+    end
+
+    if m=='Y' || m =='y' || ~userInteraction
       unix('rm ../fastFVA/logFiles/*.log');
       fprintf(' >> All log files in logFiles/ removed.\n');
     end
@@ -32,8 +37,11 @@ D = dir('../fastFVA/results/*.mat');
 nbFiles = numel(D);
 if(nbFiles > 0)
     fprintf('\n Number of files in results/: %d\n', nbFiles);
-    m=input('\n Do you want to clean al results files in results/ ? - Y/N [Y]: ','s');
-    if m=='Y' || m =='y'
+    
+    if userInteraction
+      m=input('\n Do you want to clean al results files in results/ ? - Y/N [Y]: ','s');
+    end
+    if m=='Y' || m =='y' || ~userInteraction
       unix('rm ../fastFVA/results/*.mat');
       fprintf(' >> All result files in results/ removed.\n');
     end
