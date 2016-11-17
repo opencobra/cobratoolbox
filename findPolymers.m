2db46ee3569f99fff30f2c970cd53e5901f7aa4a
Lemmer EL ASSAL <lemmer.elassal@uni.lu>
2016-11-17 16:44:27 +0100

    New function: getPolymers(model); Outputs boolean vector of metabolites which are polymers.

---
+++ findPolymers.m
@@ -0,0 +1,1 @@
+% findPolymers% Input:% model     COBRA model% Output:% res       boolean vector; 1 = polymer, 0 otherwise% Lemmer El Assal, Nov 2016function res = findPolymers(model)has_X = cellfun(@(x) numAtomsOfElementInFormula(x,'X'),model.metFormulas);has_R = cellfun(@(x) numAtomsOfElementInFormula(x,'R'),model.metFormulas);res = has_X | has_R;end
