8c760f8767efc20e340a432482d7f657c638f524
Ojami <Ojami@users.noreply.github.com>
2016-12-16 17:48:35 +0330

    Update createTissueSpecificModel.m

--- reconstruction/createTissueSpecificModel.m
+++ reconstruction/createTissueSpecificModel.m
@@ -487,7 +487,7 @@ for i = 1:numel(AllRules)
     orFind = textscan(EachRule,'%s','delimiter','or'); orFind = orFind{1};
     orFind(cellfun('isempty',orFind)) = [];
 
-    GPR_temp=regexp(orFind,'\d*','match');
+    GPR_temp=regexp(orFind,'(\d*)[.](\d*)|(\d*)','match');
     corrRxns = [corrRxns;repmat(model.rxns(i),numel(GPR_temp),1)];
     AllGPR = [AllGPR;GPR_temp];
 end
