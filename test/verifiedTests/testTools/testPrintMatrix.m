1913a964d2a336b77d288ea46b44ecd6bd8a23cb
Laurent Heirendt <laurent.heirendt@uni.lu>
2017-01-23 22:58:51 +0100

    Minor changes and moving external tools to external folder

--- test/verifiedTests/testTools/testPrintMatrix.m
+++ test/verifiedTests/testTools/testPrintMatrix.m
@@ -17,7 +17,7 @@ cd([CBTDIR '/test/verifiedTests/testTools'])
 A = rand(10, 10);
 
 % print the matrix on screen with default format
-assert(printMatrix(A) == 1)
+assert(printMatrix(A) == 1);
 
 % print the matrix on screen with short format
 assert(printMatrix(A, '%3.2f\t') == 1);
@@ -26,11 +26,14 @@ assert(printMatrix(A, '%3.2f\t') == 1);
 assert(printMatrix(A, '%3.2f\t', 'testPrintMatrix.txt') == 1);
 
 % remove the generated file
-cd([CBTDIR '/test/verifiedTests/testTools'])
+cd([CBTDIR '/test/verifiedTests/testTools']);
 system('rm testPrintMatrix.txt');
 
 % test for a cell matrix
-A = {0, 1; 2, 3; 4, 5}
+A = {0, 1, 2;
+     2, 3, 4;
+     4, 5, 6;
+     7, 8, 9};
 
 % print the matrix on screen with default format
-assert(printMatrix(A) == 1)
+assert(printMatrix(A) == 1);
