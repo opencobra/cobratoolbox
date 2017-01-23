426ebc2b2528d52555411f07208a9321fa3f189b
Laurent Heirendt <laurent.heirendt@uni.lu>
2017-01-23 19:45:30 +0100

    Verified test of testRank

---
+++ test/verifiedTests/testRank/testRank.m
@@ -0,0 +1,32 @@
+% The COBRAToolbox: testRank.m
+%
+% Purpose:
+%     - tests the computation of the rank of a matrix using the LU solver termed
+%
+% Authors:
+%     - LUSOL developed by Michael A. Saunders
+%     - original test file: Ronan Fleming
+%     - CI integration: Laurent Heirendt
+
+% load the model
+load('iAF1260.mat')
+A = Ecoli.S;
+printLevel = 1;
+
+% calculate the rank with minimum i/o arguments
+rankA = getRankLUSOL(A);
+
+% test the rank
+assert(rankA == 1630);
+
+for printLevel = 0:1
+    % calculate the rank using LUSOL
+    [rankA, p, q] = getRankLUSOL(A, printLevel);
+
+    % test the rank
+    assert(rankA == 1630);
+
+    % compare the norms of the permutations vectors
+    assert(abs(norm(p) - 3.934854424244942e+04) < 1e-6);
+    assert(abs(norm(q) - 6.714114249102409e+04) < 1e-6);
+end
