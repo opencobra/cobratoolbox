038a8817c53604c43c682c857541cb7467f62a6a
Laurent Heirendt <laurent.heirendt@uni.lu>
2017-01-25 15:52:56 +0100

    Minor changes

--- test/verifiedTests/testFBA/testFBA.m
+++ test/verifiedTests/testFBA/testFBA.m
@@ -22,27 +22,22 @@ cd([CBTDIR '/test/verifiedTests/testFBA'])
 % set the tolerance
 tol = 1e-8;
 
-% load the model
-load('testFBAData.mat');
-
 % set the solver
 changeCobraSolver('tomlab_cplex')
+
+% load the model
+load('testFBAData.mat');
 
 % check the optimal solution - BiomassEcoli
 fprintf('\n>> Optimal minimum 1-norm solution\n');
 model = changeObjective(model, {'BiomassEcoli'}, 1);
 solution = optimizeCbModel(model);
-f_values = solution.f;
 
 % testing if f values are within range
-for i = 1:size(f_values)
-    assert(abs(solution.f - solutionStd.f) < tol)
-end
+assert(abs(solution.f - solutionStd.f) < tol)
 
 % testing if c*x == f
-for i = 1:size(f_values)
-    assert(abs(model.c'*solution.x - solution.f) < tol)
-end
+assert(abs(model.c' * solution.x - solution.f) < tol)
 
 % print the flux vector
 printFluxVector(model, solution.x, true, true);
@@ -51,17 +46,12 @@ printFluxVector(model, solution.x, true, true);
 fprintf('\n>> Optimal solution on fructose\n');
 model2 = changeRxnBounds(model, {'EX_glc(e)','EX_fru(e)'}, [0 -9], 'l');
 solution2 = optimizeCbModel(model2);
-f_values = solution.f;
 
 % testing if f values are within range
-for i = 1:size(f_values)
-    assert(abs(solution2.f - solution2Std.f) < tol)
-end
+assert(abs(solution2.f - solution2Std.f) < tol)
 
 % testing if c*x == f
-for i = 1:size(f_values)
-    assert(abs(model2.c'*solution2.x - solution2.f) < tol)
-end
+assert(abs(model2.c' * solution2.x - solution2.f) < tol)
 
 % print the flux vector
 printFluxVector(model2, solution.x, true, true);
@@ -70,33 +60,23 @@ printFluxVector(model2, solution.x, true, true);
 fprintf('\n>> Optimal anaerobic solution\n');
 model3 = changeRxnBounds(model, 'EX_o2(e)', 0, 'l');
 solution3 = optimizeCbModel(model3);
-f_values = solution.f;
 
 % testing if f values are within range
-for i = 1:size(f_values)
-    assert(abs(solution3.f - solution3Std.f) < tol)
-end
+assert(abs(solution3.f - solution3Std.f) < tol)
 
 % testing if c*x == f
-for i = 1:size(f_values)
-    assert(abs(model3.c'*solution3.x - solution3.f) < tol)
-end
+assert(abs(model3.c' * solution3.x - solution3.f) < tol)
 
 % check the optimal ethanol secretion rate solution
 fprintf('\n>> Optimal ethanol secretion rate solution \n');
 model4 = changeObjective(model, 'EX_etoh(e)', 1);
 solution4 = optimizeCbModel(model4);
-f_values = solution.f;
 
 % testing if f values are within range
-for i = 1:size(f_values)
-    assert(abs(solution4.f - solution4Std.f) < tol)
-end
+assert(abs(solution4.f - solution4Std.f) < tol)
 
 % testing if c*x == f
-for i = 1:size(f_values)
-    assert(abs(model4.c'*solution4.x - solution4.f) < tol)
-end
+assert(abs(model4.c' * solution4.x - solution4.f) < tol)
 
 % change the directory
 cd(CBTDIR)
