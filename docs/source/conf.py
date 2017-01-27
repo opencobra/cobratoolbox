192378c9f8ba5c1c57bf9efff812a8b27f9e2dd7
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-01-27 15:11:09 +0100

    switched to restructtured text documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,12 +43,16 @@ primary_domain = 'mat'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.md'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #
