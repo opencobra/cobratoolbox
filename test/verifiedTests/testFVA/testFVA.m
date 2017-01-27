0062020365ae932a354d8e86c1e521d36c06a39b
laurentheirendt <laurent.heirendt@uni.lu>
2017-01-27 16:38:20 +0100

    Changes to testFVA

--- test/verifiedTests/testFVA/testFVA.m
+++ test/verifiedTests/testFVA/testFVA.m
@@ -34,8 +34,11 @@ for k = 1:length(solverPkgs)
     if solverOK == 1
         fprintf('   Testing flux variability analysis using %s ... ', solverPkgs{k});
 
-        % launch 2 workers
-        parpool(2);
+        poolobj = gcp('nocreate'); % If no pool, do not create new one.
+        if isempty(poolobj)
+            % launch 2 workers
+            parpool(2);
+        end
 
         % launch the flux variability analysis
         [minFluxT, maxFluxT] = fluxVariability(model, 90);
