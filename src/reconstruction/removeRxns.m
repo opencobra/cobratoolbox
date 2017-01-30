f1c544b9f58426654bd0d34c747fa49c5417f82c
Laurent Heirendt <laurent.heirendt@uni.lu>
2017-01-30 21:54:11 +0100

    Fixed removal of mets field in removeRxns and minor changes to addReaction

--- src/reconstruction/removeRxns.m
+++ src/reconstruction/removeRxns.m
@@ -19,39 +19,39 @@ function modelOut = removeRxns(model,rxnRemoveList,irrevFlag,metFlag)
 % Fatima Liliana Monteiro and Hulda Haraldsdóttir, November 2016
 
 
-if (nargin < 3)
-  irrevFlag = false;
+if nargin < 3
+    irrevFlag = false;
 end
-if (nargin < 4)
-  metFlag = true;
+if nargin < 4
+    metFlag = true;
 end
 
-[nMets,nRxns] = size(model.S);
-if (isfield(model,'genes'))
+[nMets, nRxns] = size(model.S);
+if (isfield(model, 'genes'))
     nGenes = length(model.genes);
 else
     nGenes = 0;
 end
 
 % Find indices to rxns in the model
-[isValidRxn,removeInd] = ismember(rxnRemoveList,model.rxns);
+[isValidRxn, removeInd] = ismember(rxnRemoveList, model.rxns);
 removeInd = removeInd(isValidRxn);
 
 % Remove reversible tag from the reverse reaction if the reaction to be
 % deleted is reversible
-if (irrevFlag)
-  for i = 1:length(removeInd)
-    remRxnID = removeInd(i);
-    if (model.match(remRxnID) > 0)
-      revRxnID = model.match(remRxnID);
-      model.rev(revRxnID) = 0;
-      model.rxns{revRxnID} = model.rxns{revRxnID}(1:end-2);
+if irrevFlag
+    for i = 1:length(removeInd)
+        remRxnID = removeInd(i);
+        if model.match(remRxnID) > 0
+            revRxnID = model.match(remRxnID);
+            model.rev(revRxnID) = 0;
+            model.rxns{revRxnID} = model.rxns{revRxnID}(1:end-2);
+        end
     end
-  end
 end
 
 % Construct vector to select rxns to be included in the model rapidly
-selectRxns = true(nRxns,1);
+selectRxns = true(nRxns, 1);
 selectRxns(removeInd) = false;
 
 % Construct new model
@@ -62,34 +62,34 @@ rfields = {};
 
 if ~any([nMets nGenes] == nRxns)
     for i = 1:length(mfields)
-        if any(size(model.(mfields{i})) == nRxns)
+        if any(size(model.(mfields{i})) == nRxns) && ~strcmp(mfields{i}, 'mets')
             rfields = [rfields mfields(i)];
         end
     end
 else
-    rfields = {'S' 'c' 'lb' 'ub' 'rxns' 'rules' 'grRules' 'rev' 'subSystems'}';
+    rfields = {'S', 'c', 'lb', 'ub', 'rxns', 'rules', 'grRules', 'rev', 'subSystems'}';
     rfields = [rfields; mfields(strncmp('rxn', mfileds, 3))];
     rfields = intersect(rfields,mfields);
 end
 
 for i = 1:length(rfields)
-   if size(model.(rfields{i}),1) == nRxns
-       modelOut.(rfields{i}) = modelOut.(rfields{i})(selectRxns,:);
-   else size(model.(rfields{i}),2) == nRxns
-       modelOut.(rfields{i}) = modelOut.(rfields{i})(:,selectRxns);
+   if size(model.(rfields{i}), 1) == nRxns
+       modelOut.(rfields{i}) = model.(rfields{i})(selectRxns, :);
+   elseif size(model.(rfields{i}), 2) == nRxns
+       modelOut.(rfields{i}) = model.(rfields{i})(:, selectRxns);
    end
 end
 
 % Reconstruct the match list
-if (irrevFlag)
-  modelOut.match = reassignFwBwMatch(model.match,selectRxns);
-  modelOut.rev(modelOut.match == 0) = false;
+if irrevFlag
+    modelOut.match = reassignFwBwMatch(model.match,selectRxns);
+    modelOut.rev(modelOut.match == 0) = false;
 end
 
 % Remove metabolites that are not used anymore
-if (metFlag)
-  selMets = modelOut.mets(any(sum(abs(modelOut.S),2) == 0,2));
-  if (~isempty(selMets))
-    modelOut = removeMetabolites(modelOut, selMets, false);
-  end
+if metFlag
+    selMets = modelOut.mets(any(sum(abs(modelOut.S), 2) == 0, 2));
+    if ~isempty(selMets)
+      modelOut = removeMetabolites(modelOut, selMets, false);
+    end
 end
