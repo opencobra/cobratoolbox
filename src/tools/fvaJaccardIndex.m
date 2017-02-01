e705d5124f3d4dd980efaa5f079b2ab302113400
laurentheirendt <laurent.heirendt@uni.lu>
2017-02-01 15:38:58 +0100

    Input checks and tests for JaccardIndex function

--- src/tools/fvaJaccardIndex.m
+++ src/tools/fvaJaccardIndex.m
@@ -1,23 +1,48 @@
-function J = fvaJaccardIndex(minFlux,maxFlux)
+function J = fvaJaccardIndex(minFlux, maxFlux)
 % Compare flux ranges by computing the Jaccard index
-% 
-% J = fvaJaccardIndex(minFlux,maxFlux);
-% 
+%
+% J = fvaJaccardIndex(minFlux, maxFlux);
+%
 % INPUTS
 % minFlux ... An n x k matrix of minimum fluxes through n reactions in k
-%             different constraint-based models.
+%             different constraint-based models (k > 1).
 % maxFlux ... An n x k matrix of maximum fluxes through n reactions in k
-%             different constraint-based models.
-% 
+%             different constraint-based models (k > 1).
+%
 % OUTPUTS
 % J       ... An n x 1 vector of Jaccard indices, defined as the
 %             intersection divided by the union of all k flux ranges
-% 
+%
 % Hulda S. Haraldsdottir, 2016/08/22
-fvaIntersect = min(maxFlux,[],2) - max(minFlux,[],2); % Compute intersection
-fvaUnion = max(maxFlux,[],2) - min(minFlux,[],2); % Compute union
-fvaIntersect(fvaIntersect < 0) = 0; % Gap between flux ranges
-fvaIntersect(fvaUnion < 1e-6) = 1; % Fixed flux value
-fvaUnion(fvaUnion < 1e-6) = 1; % Fixed flux value
-J = fvaIntersect./fvaUnion; % Jaccard indices
+% Laurent Heirendt, 2017/02/01
+
+% set the tolerance
+tol = 1e-6;
+
+if size(minFlux, 2) ~= size(maxFlux, 2)
+    error('The size the minFlux and maxFlux matrices is different.');
+
+elseif size(minFlux, 2) <= 1
+    error('In order to calculate a Jaccard index, please input flux vectors of more than only one model (k > 1).');
+
+else
+    % Compute intersection
+    fvaIntersect = min(maxFlux, [], 2) - max(minFlux, [], 2);
+
+    % Compute union
+    fvaUnion = max(maxFlux, [], 2) - min(minFlux, [], 2);
+
+    % Gap between flux ranges
+    fvaIntersect(fvaIntersect < 0) = 0;
+
+    % Fix flux value
+    fvaIntersect(fvaUnion < tol) = 1;
+
+    % Fix flux value
+    fvaUnion(fvaUnion < tol) = 1;
+
+    % Calculate Jaccard indices
+    J = fvaIntersect ./ fvaUnion;
+end
+
 end
