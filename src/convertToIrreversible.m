f2be0931ad4754ab3709e2c938bc2474b4787f89
Marouen Benguebila <marouen.benguebila@uni.lu>
2017-02-03 18:37:42 +0100

    added additional condition on reversibility and support csense vector

--- src/convertToIrreversible.m
+++ src/convertToIrreversible.m
@@ -52,7 +52,8 @@ for i = 1:nRxns
     if (model.ub(i) > 0 && model.lb(i) < 0) && model.rev(i) == false
         model.rev(i) = true;
         warning(cat(2,'Reaction: ',model.rxns{i},' is classified as irreversible, but bounds are positive and negative!'))
-
+    elseif (sign(model.ub(i)) == sign(model.lb(i)) ) && model.rev(i) == true
+        model.rev(i) = false;
     end
    
     % Reaction entirely in the negative direction
@@ -120,6 +121,9 @@ matchRev = columnVector(matchRev(1:cnt));
 modelIrrev.match = matchRev;
 if (isfield(model,'b'))
     modelIrrev.b = model.b;
+end
+if (isfield(model,'csense'))
+    modelIrrev.csense = model.csense;
 end
 if isfield(model,'description')
     modelIrrev.description = [model.description ' irreversible'];
