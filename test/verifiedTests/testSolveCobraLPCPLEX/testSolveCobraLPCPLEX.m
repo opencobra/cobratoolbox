99ddeb1d9ac9374345f9c52d14ff90a50c62fa3f
Marouen Benguebila <marouen.benguebila@uni.lu>
2017-02-08 14:28:29 +0100

    changed solver paths

--- test/verifiedTests/testSolveCobraLPCPLEX/testSolveCobraLPCPLEX.m
+++ test/verifiedTests/testSolveCobraLPCPLEX/testSolveCobraLPCPLEX.m
@@ -12,39 +12,40 @@
 pth = which('initCobraToolbox.m');
 CBTDIR = pth(1:end - (length('initCobraToolbox.m') + 1));
 
-cd([CBTDIR '/test/verifiedTests/testSolveCobraLPCPLEX'])
+cd([CBTDIR filesep 'test' filesep 'verifiedTests' filesep 'testSolveCobraLPCPLEX'])
  
 load testDataSolveCobraLPCPLEX;
-load ecoli_core_model;
+load('ecoli_core_model','model');
 
 tol = 0.01;%set tolerance
 ecoli_blckd_rxn = {'EX_fru(e)','EX_fum(e)','EX_gln_L(e)','EX_mal_L(e)',...
     'FRUpts2','FUMt2_2','GLNabc','MALt2_2'};%blocked rxn in Ecoli
 
 %test TOMLAB
-changeCobraSolver('tomlab_cplex','LP');
 solTest=solveCobraLPCPLEX(model,0,0,0,[],0,'tomlab_cplex')
 %test was performed on objective as solution can vary between machines,
 %solver version etc..
 assert(any(abs(solTest.obj-sol.obj) < tol))
 
 %test ILOG
-changeCobraSolver('ibm_cplex','LP');
 solTest=solveCobraLPCPLEX(model,0,0,0,[],0,'ILOGcomplex');
 assert(any(abs(solTest.obj-sol.obj) < tol))
 
-for k = {'tomlab_cplex' 'ILOGcomplex'}%test solver packages
+%test solver packages
+solverPkgs = {'tomlab_cplex','ibm_cplex','glpk'};
+
+for k =1:length(solverPkgs)
     %test minNorm
-    solTest=solveCobraLPCPLEX(model,0,0,0,[],1e-6,k{1})
+    solTest=solveCobraLPCPLEX(model,0,0,0,[],1e-6,solverPkgs{k});
     assert(isequal(ecoli_blckd_rxn,model.rxns(find(~solTest.full))'))
     assert(any(abs(solTest.obj-sol.obj) < tol))
 
     %test basis generation
-    [solTest,basisTest]=solveCobraLPCPLEX(model,0,1,0,[],0,k{1})
+    [solTest,basisTest]=solveCobraLPCPLEX(model,0,1,0,[],0,solverPkgs{k});
     assert(any(abs(solTest.obj-sol.obj) < tol))
 
     %test basis reuse
-    [solTest]=solveCobraLPCPLEX(basis,0,1,0,[],0,k{1})
+    [solTest]=solveCobraLPCPLEX(basis,0,1,0,[],0,solverPkgs{k});
     assert(any(abs(solTest.obj-sol.obj) < tol))
 end
 % change the directory
