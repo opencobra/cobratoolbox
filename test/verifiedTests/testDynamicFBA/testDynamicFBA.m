3ce2a6dc1aa9bcbe8e8a11d7f5479433e40b2e51
Marouen Benguebila <marouen.benguebila@uni.lu>
2017-02-08 10:36:54 +0100

    took out failing solvers

--- test/verifiedTests/testDynamicFBA/testDynamicFBA.m
+++ test/verifiedTests/testDynamicFBA/testDynamicFBA.m
@@ -24,7 +24,7 @@ Xec = 0.001;%Initial biomass
 dt = 1/100;%Time steps
 time = 1/dt; %Simulation time
 
-for k ={'tomlab_cplex' 'ibm_cplex' 'glpk'}
+for k ={'tomlab_cplex' 'ibm_cplex'}
     solverLPOK = changeCobraSolver(k{1});
     if solverLPOK
         [concentrationMatrixtest,excRxnNamestest,timeVectest,biomassVectest]=dynamicFBA(model,smi,smc,Xec,dt,time);
