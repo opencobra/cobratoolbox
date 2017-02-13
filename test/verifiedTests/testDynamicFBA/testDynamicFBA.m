e3c70c1b9941c9b41aa7f2d8f6945e9174db6e86
laurentheirendt <laurent.heirendt@uni.lu>
2017-02-13 12:26:02 +0100

    Rebase and adapted version of test

--- test/verifiedTests/testDynamicFBA/testDynamicFBA.m
+++ test/verifiedTests/testDynamicFBA/testDynamicFBA.m
@@ -5,41 +5,62 @@
 %
 % Author:
 %     - Marouen BEN GUEBILA - 31/01/2017
-  
-  
+
+% define global paths
+global path_TOMLAB
+
 % define the path to The COBRAToolbox
 pth = which('initCobraToolbox.m');
 CBTDIR = pth(1:end - (length('initCobraToolbox.m') + 1));
-  
-cd([CBTDIR filesep 'test' filesep 'verifiedTests' filesep 'testDynamicFBA'])
-  
+
+initTest([CBTDIR filesep 'test' filesep 'verifiedTests' filesep 'testDynamicFBA'])
+
 load('ecoli_core_model', 'model');
 load testDataDynamicFBA;
 
-smi = {'EX_glc(e)'
-    'EX_ac(e)'};%Exchange reaction for substrate in environment
+smi = {'EX_glc(e)' 'EX_ac(e)'}; % exchange reaction for substrate in environment
 
 smc = [10.8; 0.4]; % Glucose, Acetate concentration (all in mM)
 
-Xec = 0.001;%Initial biomass
-dt = 1/100;%Time steps
-time = 1/dt; %Simulation time
+Xec = 0.001; % initial biomass
+dt = 1.0/100.0; % time steps
+time = 1.0/dt; % simulation time
 
 %define solver packages
-solverPkgs = {'tomlab_cplex','ibm_cplex','glpk'};
+solverPkgs = {'tomlab_cplex'};
 
-for k =1:length(solverPkgs)
-    solverLPOK = changeCobraSolver(solverPkgs{1});
-    if solverLPOK
-        [concentrationMatrixtest,excRxnNamestest,timeVectest,biomassVectest]=dynamicFBA(model,smi,smc,Xec,dt,time);
+% set tolerance
+tol = 1e-8;
 
-        tol = eps(0.5);%set tolerance
-        assert(any(any(abs(concentrationMatrixtest-concentrationMatrix) < tol)))
-        assert(isequal(excRxnNamestest,excRxnNames))
-        assert(isequal(timeVectest,timeVec))
-        assert(any(abs(biomassVectest-biomassVec) < tol))
+for k = 1:length(solverPkgs)
+
+    % add the solver paths (temporary addition for CI)
+    if strcmp(solverPkgs{k}, 'tomlab_cplex')
+        addpath(genpath(path_TOMLAB));
     end
+
+    % change the COBRA solver (LP)
+    solverLPOK = changeCobraSolver(solverPkgs{k});
+
+    if solverLPOK == 1
+        fprintf('   Testing dynamicFBA using %s ... ', solverPkgs{k});
+        [concentrationMatrixtest, excRxnNamestest, timeVectest, biomassVectest] = dynamicFBA(model, smi, smc, Xec, dt, time);
+
+        assert(any(any(abs(concentrationMatrixtest - concentrationMatrix) < tol)))
+        assert(isequal(excRxnNamestest, excRxnNames))
+        assert(isequal(timeVectest, timeVec))
+        assert(any(abs(biomassVectest - biomassVec) < tol))
+    end
+
+    % remove the solver paths (temporary addition for CI)
+    if strcmp(solverPkgs{k}, 'tomlab_cplex')
+        rmpath(genpath(path_TOMLAB));
+    end
+
 end
 
+% close open figure
+close all
+
 % change the directory
-cd(CBTDIR)  
+cd(CBTDIR)
