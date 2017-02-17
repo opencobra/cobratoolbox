a28867e0717cc65a51deb5e8a4b60df8db9f46b1
Lemmer EL ASSAL <lemmer.elassal@uni.lu>
2017-02-17 12:28:45 +0100

    :bug: Included textprogressbar and added to showprogress()

--- src/showprogress.m
+++ src/showprogress.m
@@ -1,12 +1,25 @@
-function fout = showprogress(x,whichbar, varargin)
+% showprogress shows waitbars
+% Inputs
+% x, whichbar, varagin: see waitbar header for explanation
+% Feb 2017, Lemmer El Assal
+
+function fout = showprogress(x, whichbar, varargin)
     global WAITBAR_TYPE;
     fout = [];
     if ~isempty(WAITBAR_TYPE)
         switch WAITBAR_TYPE
             case 1 % graphic waitbar
-                fout = waitbar(x,whichbar,varargin);
-            %case 2 % text
-
+                if nargin > 2
+                    fout = waitbar(x, whichbar, varargin);
+                else
+                    fout = waitbar(x, whichbar);
+                end
+            case 2 % text
+                if x > 0
+                    textprogressbar(x*100);
+                else
+                    textprogressbar(whichbar);
+                end
         end
     end
 end
