619330756de4a92296c7cbc4b18eb505a17325b0
Lemmer EL ASSAL <lemmer.elassal@uni.lu>
2017-02-20 14:50:28 +0100

    Removed commas from textprogressbar

--- external/textprogressbar/textprogressbar.m
+++ external/textprogressbar/textprogressbar.m
@@ -22,14 +22,14 @@ strDotsMaximum      = 10;   %   The total number of dots in a progress bar
 
 %% Main 
 
-if isempty(strCR) && ~ischar(c),
+if isempty(strCR) && ~ischar(c)
     % Progress bar must be initialized with a string
     error('The text progress must be initialized with a string');
-elseif isempty(strCR) && ischar(c),
+elseif isempty(strCR) && ischar(c)
     % Progress bar - initialization
     fprintf('%s',c);
     strCR = -1;
-elseif ~isempty(strCR) && ischar(c),
+elseif ~isempty(strCR) && ischar(c)
     % Progress bar  - termination
     strCR = [];  
     fprintf([c '\n']);
@@ -43,7 +43,7 @@ elseif isnumeric(c)
     strOut = [percentageOut dotOut];
     
     % Print it on the screen
-    if strCR == -1,
+    if strCR == -1
         % Don't do carriage return during first run
         fprintf(strOut);
     else
