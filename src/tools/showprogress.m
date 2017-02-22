35441488399b9df95aec2b6e8728ba29944cc799
Lemmer EL ASSAL <lemmer.elassal@uni.lu>
2017-02-22 14:06:43 +0100

    Fixed showprogress

--- src/tools/showprogress.m
+++ src/tools/showprogress.m
@@ -13,20 +13,22 @@ function fout = showprogress(x, whichbar, varargin)
 %        - Lemmer El Assal (Feb 2017)
 %
     global WAITBAR_TYPE;
-    fout = [];
     if ~isempty(WAITBAR_TYPE)
         switch WAITBAR_TYPE
-            case 1 % graphic waitbar
-                if nargin > 2
-                    fout = waitbar(x, whichbar, varargin);
-                else
-                    fout = waitbar(x, whichbar);
-                end
-            case 2 % text
+            case 0 % silent mode
+                
+            case 1 % text
                 if x > 0
                     textprogressbar(x*100);
                 else
                     textprogressbar(whichbar);
+                end
+                
+            case 2 % graphic waitbar
+                if nargin > 2
+                    fout = waitbar(x, whichbar, varargin);
+                else
+                    fout = waitbar(x, whichbar);
                 end
         end
     end
