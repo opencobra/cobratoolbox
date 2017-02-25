19dd253059961e070ec505cc67f935ab2fd16459
laurentheirendt <laurent.heirendt@uni.lu>
2017-02-25 10:36:22 +0100

    Adaptations of testPrintMatrix

--- test/verifiedTests/testTools/testPrintMatrix.m
+++ test/verifiedTests/testTools/testPrintMatrix.m
@@ -10,21 +10,8 @@
 pth = which('initCobraToolbox.m');
 CBTDIR = pth(1:end-(length('initCobraToolbox.m') + 1));
 
-% define the matrix A
-A = rand(10, 10);
-
-% print the matrix on screen with default format
-assert(printMatrix(A) == 1);
-
-% print the matrix on screen with short format
-assert(printMatrix(A, '%3.2f\t') == 1);
-
-% print the matrix to a file with short format
-assert(printMatrix(A, '%3.2f\t', 'testPrintMatrix.txt') == 1);
-
-% remove the generated file
-cd([CBTDIR, filesep, 'test', filesep, 'verifiedTests', filesep, 'testTools']);
-system(['rm ', CBTDIR, filesep, 'testPrintMatrix.txt']);
+fileName = 'testPrintMatrix.txt';
+nbFormat = '%3.2f\t';
 
 % test for a cell matrix
 A = {0, 1, 2;
@@ -34,6 +21,38 @@ A = {0, 1, 2;
 
 % print the matrix on screen with default format
 assert(printMatrix(A) == 1);
+
+% define the matrix A
+A = rand(10, 10);
+
+% print the matrix on screen with default format
+assert(printMatrix(A) == 1);
+
+% print the matrix on screen with short format
+assert(printMatrix(A, nbFormat) == 1);
+
+% print the matrix to a file with short format
+assert(printMatrix(A, nbFormat, fileName) == 1);
+
+% remove the generated file
+system(['rm ', CBTDIR, filesep, 'test', filesep, 'verifiedTests', filesep, 'testTools', filesep, fileName]);
+
+% test to print to a file and read the data from that same file
+A = ones(6) * 6;
+A = A + (A / 10) + (A / 100);
+nbFormat = '%6.2f\t';
+fileName = 'testFile_printMatrix.txt';
+
+retStatus = printMatrix(A, nbFormat, fileName);
+assert(retStatus == 1);
+
+% read in the data from the newly generated file and test against reference data
+[~, data1, ~] = readMixedData(fileName, 0, 0)
+[~, data2, ~] = readMixedData('ref_test.txt', 0, 0)
+assert(isequal(data1, data2))
+
+% remove the generated file
+system(['rm ', CBTDIR, filesep, 'test', filesep, 'verifiedTests', filesep, 'testTools', filesep, fileName]);
 
 % change the directory
 cd(CBTDIR)
