edd4432fc6e760d0fcfef47a275fb8878aa4e283
Sylvain <sylvain.arreckx@gmail.com>
2017-02-27 17:25:02 +0100

    Update test to standards
    @lemmerelassal, Please follow our instructions to write all your tests!

--- test/verifiedTests/testTools/testParseBoolean.m
+++ test/verifiedTests/testTools/testParseBoolean.m
@@ -8,26 +8,22 @@
 %     - Lemmer El Assal February 2017
 %
 
-% tests the functionality of parseBoolean
+% define the path to The COBRAToolbox
+pth = which('initCobraToolbox.m');
+CBTDIR = pth(1:end-(length('initCobraToolbox.m') + 1));
 
-%save original directory
-oriDir = pwd;
-
-%change to test folder
-mFilePath = mfilename('fullpath');
-cd(mFilePath(1:end-length(mfilename)));
+initTest([CBTDIR, filesep, 'test', filesep, 'verifiedTests', filesep, 'testTools']);
 
 str = '((A&B)|(B&C))&(~D)';
 tokens = '&!()';
 
-ref_elements = {'A','B','|','C','~D'};
+ref_elements = {'A', 'B', '|', 'C', '~D'};
 ref_newRule = '((x(1)&x(2))|(x(2)&x(4)))&(x(5))';
 
-
-[elements,newRule] = parseBoolean(str,tokens);
+[elements, newRule] = parseBoolean(str, tokens);
 
 assert(isequal(ref_elements, elements))
 assert(isequal(ref_newRule, newRule))
 
-%return to original directory
-cd(oriDir);
+% change the directory
+cd(CBTDIR)
