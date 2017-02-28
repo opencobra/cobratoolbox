a7727041040469c330ae6d079470f3a8f4142aa1
Sylvain <sylvain.arreckx@gmail.com>
2017-02-28 16:07:16 +0100

    Fix space after commas

--- test/verifiedTests/testTools/testUnionCell.m
+++ test/verifiedTests/testTools/testUnionCell.m
@@ -19,12 +19,12 @@ load testUnionCell.mat;
 A = {'String1','String2'};
 B = {'String2','String1'};
 
-AB{1} = unioncell(A,1,B,1);
-AB{2} = unioncell(A,1,B,2);
-AB{3} = unioncell(A,2,B,1);
-AB{4} = unioncell(A,2,B,2);
+AB{1} = unioncell(A, 1, B, 1);
+AB{2} = unioncell(A, 1, B, 2);
+AB{3} = unioncell(A, 2, B, 1);
+AB{4} = unioncell(A, 2, B, 2);
 
-assert(isequal(ref_AB,AB))
+assert(isequal(ref_AB, AB))
 
 % change the directory
 cd(CBTDIR)
