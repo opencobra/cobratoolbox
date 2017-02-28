24411686d898c0df34e918eee8a2a4f28d3e5647
laurentheirendt <laurent.heirendt@uni.lu>
2017-02-28 17:02:35 +0100

    Fixed showprogress

--- src/tools/showprogress.m
+++ src/tools/showprogress.m
@@ -5,11 +5,11 @@ function fout = showprogress(x, whichbar, varargin)
 %   x: percentage in integer (e.g.: 1 = 1%, 40 = 40%, etc.)
 %   whichbar: caption
 %   varagin: see waitbar header for explanation
-% 
+%
 % Output:
 %   fout: handle output from waitbar() (WAITBAR_TYPE = 1)
 %
-% .. Author: 
+% .. Author:
 %        - Lemmer El Assal (Feb 2017)
 %
     global WAITBAR_TYPE;
@@ -17,14 +17,14 @@ function fout = showprogress(x, whichbar, varargin)
     if ~isempty(WAITBAR_TYPE)
         switch WAITBAR_TYPE
             case 0 % silent mode
-                
+
             case 1 % text
                 if x > 0
-                    textprogressbar(x*100);
+                    textprogressbar(num2str(x*100));
                 else
                     textprogressbar(whichbar);
                 end
-                
+
             case 2 % graphic waitbar
                 if nargin > 2
                     fout = waitbar(x, whichbar, varargin);
