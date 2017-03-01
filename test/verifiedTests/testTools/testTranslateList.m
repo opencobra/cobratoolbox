227eb83d29d6763402931965387e51ffb90c58b8
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-03-01 23:25:13 +0100

    Move file to the right folder and minor style change.

--- test/verifiedTests/testTools/testTranslateList.m
+++ test/verifiedTests/testTools/testTranslateList.m
@@ -14,11 +14,11 @@ CBTDIR = pth(1:end-(length('initCobraToolbox.m') + 1));
 
 initTest([CBTDIR, filesep, 'test', filesep, 'verifiedTests', filesep, 'testTools']);
 
-list = {'a','b','c'};
-trList1 = {'b','c'};
-trList2 = {'B','C'};
-newList = translateList(list,trList1,trList2);
-assert(isequal(newList,{'a','B','C'}))
+list = {'a', 'b', 'c'};
+trList1 = {'b', 'c'};
+trList2 = {'B', 'C'};
+newList = translateList(list, trList1, trList2);
+assert(isequal(newList,{'a', 'B', 'C'}))
 
 % change the directory
 cd(CBTDIR)
