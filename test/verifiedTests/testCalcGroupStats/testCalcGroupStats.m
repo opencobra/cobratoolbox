bf2835b2116c4841e95f7c1af22e5febbf9fc5ae
Lemmer EL ASSAL <lemmer.elassal@uni.lu>
2017-03-02 09:48:05 +0100

    Added spaces after commas in testCalcGroupStats.m

--- test/verifiedTests/testCalcGroupStats/testCalcGroupStats.m
+++ test/verifiedTests/testCalcGroupStats/testCalcGroupStats.m
@@ -24,13 +24,13 @@ groups = {'test1','test2','test3','test4','test1'};
 ref_groupCnt = [2;1;1;1];
 
 for i = 1:4 
-    [groupStat(:,:,i),groupList,groupCnt] = calcGroupStats(data,groups,statname{i},ref_groupList,1,10);
+    [groupStat(:,:,i), groupList, groupCnt] = calcGroupStats(data, groups, statname{i}, ref_groupList, 1, 10);
     assert(isequal(ref_groupList, groupList));
     assert(isequal(ref_groupCnt, groupCnt));
     %assert(isequal(groupStat,data));
 end
 
-assert(isequal(ref_groupStat,groupStat))
+assert(isequal(ref_groupStat, groupStat))
 
 % change the directory
 cd(CBTDIR)
