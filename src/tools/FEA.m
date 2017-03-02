42eeb980a311a91a085ce1d9d364ff5215f9b053
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-03-02 14:39:49 +0100

    minor style changes.

--- src/tools/FEA.m
+++ src/tools/FEA.m
@@ -26,54 +26,54 @@ if ~ischar(group)
     error('Please provide the group name as string of characters e.g. ''subSystems'' ')
 end
 
-%compute frequency of enriched terms
-[uniquehSubsystemsA, ~, K]=unique(eval(['model.' group]));
+% compute frequency of enriched terms
+[uniquehSubsystemsA, ~, K] = unique(eval(['model.' group]));
 
-%fetch group
+% fetch group
 enRxns = eval(['model.' group '(rxnSet)']);
 m = length(uniquehSubsystemsA);
 allSubsystems = zeros(1, m);
 
-%look for unique occurences
+% look for unique occurences
 [uniquehSubsystems, ~, J] = unique(enRxns);
 occ = histc(J, 1:numel(uniquehSubsystems));
-[l,p] = intersect(uniquehSubsystemsA,uniquehSubsystems);
+[l, p] = intersect(uniquehSubsystemsA, uniquehSubsystems);
 allSubsystems(p) = occ;
 
-%compute total number of reactions per group
+% compute total number of reactions per group
 nRxns = histc(K, 1:numel(uniquehSubsystemsA));  % the number of reactions per susbsystem
 
-%Compute p-values
+% Compute p-values
 gopvalues = hygepdf(allSubsystems', max(nRxns), max(allSubsystems), nRxns);
 
-%take out the zeros for one-sided test
+% take out the zeros for one-sided test
 nonZerInd = find(allSubsystems);
 
-%sort p-values
-[m, rxnInd]= sort(gopvalues);
+% sort p-values
+[m, rxnInd] = sort(gopvalues);
 
-%intersect non zero sets with ordered pvalues
+% intersect non zero sets with ordered pvalues
 [~, nonZeroInd] = intersect(rxnInd, nonZerInd)
 orderedPval = rxnInd(sort(nonZeroInd));
 
-%Build result cell
-%initilize variable
-resultCell = cell(length(orderedPval)+1,5);
-resultCell(1, :)  = {'P-value', 'Adjusted P-value', 'Group', 'Enriched set size', 'Total set size'};
+% Build result cell
+% initilize variable
+resultCell = cell(length(orderedPval) + 1, 5);
+resultCell(1, :) = {'P-value', 'Adjusted P-value', 'Group', 'Enriched set size', 'Total set size'};
 
-%P values
-resultCell(2:end, 1)= num2cell(gopvalues(orderedPval));
+% P values
+resultCell(2:end, 1) = num2cell(gopvalues(orderedPval));
 
-%correct for multiple testing with FDR
+% correct for multiple testing with FDR
 resultCell(2:end, 2) = num2cell(mafdr(cell2mat(resultCell(2:end, 1)), 'BHFDR', true));
 
-%Group name
+% Group name
 resultCell(2:end, 3) = uniquehSubsystemsA(orderedPval);
 
-%Test size
+% Test size
 resultCell(2:end, 4) = num2cell(allSubsystems(orderedPval))';
 
-%Total group size
+% Total group size
 resultCell(2:end, 5) = num2cell(nRxns(orderedPval));
 
 end
