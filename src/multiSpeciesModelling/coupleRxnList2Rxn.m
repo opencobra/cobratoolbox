db53e1ff20cdef27ffacba51990dcbd37e2b0892
laurentheirendt <laurent.heirendt@uni.lu>
2017-03-07 19:44:28 +0100

    Linting of new functions

--- src/multiSpeciesModelling/coupleRxnList2Rxn.m
+++ src/multiSpeciesModelling/coupleRxnList2Rxn.m
@@ -27,7 +27,6 @@ function [modelCoupled]=coupleRxnList2Rxn(model,rxnList,rxnC,c,u)
 % Output:
 % modelCoupled coupled model
 %
-%
 % Sept 2011 AH/IT
 % May 2012: Added a warning if the coupled reaction is not in model. AH
 
@@ -36,71 +35,62 @@ if ~exist('rxnList', 'var') || isempty(rxnList)
 end
 
 if exist('c', 'var')
-    c = c*ones(length(rxnList),1); %moved by fede...commented in the original position
+    c = c * ones(length(rxnList), 1);
 end
 
 if ~exist('c', 'var') || isempty(c)
-    c = 1000*ones(length(rxnList),1);
+    c = 1000 * ones(length(rxnList), 1);
 end
 
-% if exist('c', 'var')
-%     c = c*ones(length(rxnList),1);
-% end
-
 if exist('u', 'var')
-    u = u*ones(length(rxnList),1); % moved by fede...commented in the original position
+    u = u * ones(length(rxnList), 1);
 end
 
 if ~exist('u', 'var') || isempty(u)
-    u = 0.01*ones(length(rxnList),1);
+    u = 0.01 * ones(length(rxnList), 1);
 end
-
-% if exist('u', 'var')
-%     u = u*ones(length(rxnList),1);
-% end
 
 % model.csense
 if ~isfield(model, 'csense') || isempty(model.csense)
     % assume all constraints are equality
-    model.csense(1:length(model.b),1) = 'E';
+    model.csense(1:length(model.b), 1) = 'E';
 end
 
 % find index for rxnC
-rxnCID = find(ismember(model.rxns,rxnC));
+rxnCID = find(ismember(model.rxns, rxnC));
 
-% 
-if isempty(find(ismember(model.rxns,rxnC)))
+if isempty(find(ismember(model.rxns, rxnC)))
     fprintf('Reaction not in model!');
-    modelCoupled=[];
-else modelCoupled=model;
-    if ~isfield(modelCoupled,'A')
+    modelCoupled = [];
+
+else modelCoupled = model;
+    if ~isfield(modelCoupled, 'A')
         modelCoupled.A = modelCoupled.S;
     end
-    [nRows,nCols]=size(modelCoupled.A);
-    nRows = nRows +1;
-    
-    for i = 1 : length(rxnList)
-        RID = find(ismember(modelCoupled.rxns,rxnList(i)));
-        % for every reaction
-        % update modelCoupled.A
+    [nRows, nCols] = size(modelCoupled.A);
+    nRows = nRows + 1;
+
+    for i = 1:length(rxnList)
+        RID = find(ismember(modelCoupled.rxns, rxnList(i)));
+        % for every reaction, update modelCoupled.A
         % add 1 to position of RID, and -c to position of rxnCID, creates new row in A
-        modelCoupled.A(nRows,RID) = 1;
-        modelCoupled.A(nRows,rxnCID) = -c(i);
-        modelCoupled.b(nRows,1) = u(i);
-        modelCoupled.mets(nRows,1) = strcat('slack_',rxnList(i));
+        modelCoupled.A(nRows, RID) = 1;
+        modelCoupled.A(nRows, rxnCID) = - c(i);
+        modelCoupled.b(nRows, 1) = u(i);
+        modelCoupled.mets(nRows, 1) = strcat('slack_', rxnList(i));
         modelCoupled.csense(nRows) = 'L';
         nRows = nRows +1;
+
         % for reversible reactions we add a "mirror" constraint
         if modelCoupled.lb(RID) < 0
             modelCoupled.A(nRows,RID) = 1;
             modelCoupled.A(nRows,rxnCID) = c(i);
-            modelCoupled.b(nRows,1) = -u(i);
-            modelCoupled.mets(nRows,1) = strcat('slack_',rxnList(i),'_R');
+            modelCoupled.b(nRows,1) = - u(i);
+            modelCoupled.mets(nRows,1) = strcat('slack_', rxnList(i), '_R');
             modelCoupled.csense(nRows) = 'G';
             nRows = nRows +1;
         end
     end
-    
+
     modelCoupled.csense = modelCoupled.csense';
 end
-
--- src/multiSpeciesModelling/createMultipleSpeciesModel.m
+++ src/multiSpeciesModelling/createMultipleSpeciesModel.m
@@ -18,7 +18,7 @@ function [modelJoint] = createMultipleSpeciesModel(models,nameTagsModels,modelHo
 % models     cell array of COBRA models(at least one).
 % format: models{1,1}=model 1, models{2,1}= model 2...
 % OPTIONAL INPUTS
-% nameTagsModels   cell array of tags for reaction/metabolite abbreviation 
+% nameTagsModels   cell array of tags for reaction/metabolite abbreviation
 % corresponding to each model
 % format: nameTagsModels{1,1}='name tag 1', models{2,1}= 'name tag 2'...
 % modelHost         COBRA model for host
@@ -35,175 +35,171 @@ if isempty(models)
    error('Please enter at least one model!')
 end
 
-modelNumber=size(models,1);
-if nargin <2 || isempty(nameTagsModels)
+modelNumber = size(models, 1);
+
+if nargin < 2 || isempty(nameTagsModels)
     % assign default name tags for microbes
-    for i=1:modelNumber
-        nameTagsModels{i,1}=strcat('model',num2str(i),'_');
+    for i = 1:modelNumber
+        nameTagsModels{i, 1} = strcat('model', num2str(i), '_');
     end
 else
-    if size(nameTagsModels,1) ~= modelNumber
+    if size(nameTagsModels, 1) ~= modelNumber
         error('Number of name tags and joint models needs to be identical!')
     end
 end
 
-if nargin ==3
+if nargin == 3
     % assign default name tag for host
-    nameTagHost='Host_';
+    nameTagHost = 'Host_';
 end
 
 %% define some variables
 eTag = 'u';
 exTag = 'e';
 % find exchange reactions for models, but leaves demand and sink reactions
-% Do this for all models to be added
-% remove exchange reactions from host while leaving demand and sink
-% reactions
+% do this for all models to be added, remove exchange reactions from host while leaving demand and sink reactions
 
-for i=1:modelNumber
+for i = 1:modelNumber
     % a new model each turn
-    model=models{i,1};
+    model = models{i, 1};
     % find exchange reactions and external metabolites
     exmod = model.rxns(strmatch('EX', model.rxns));
     %remove all previously defined exchange reactions
     model = removeRxns(model, exmod);
     % make sure the exchange reactions and changed model are saved under correct name
-    modelStorage(i,1)=model;
+    modelStorage(i, 1) = model;
 end
 
-if nargin >=3
+if nargin >= 3
     %% with a host
     exmod = modelHost.rxns(strmatch('EX', modelHost.rxns));
 
-%     modelHost = removeRxns(modelHost,ExRH);
-   
-%     ExRH = modelHost.rxns(selExcH);
-%     ExRH(strmatch('sink',modelHost.rxns(selExcH)))=[];
-%     ExRH(strmatch('DM',modelHost.rxns(selExcH)))=[];
+    % modelHost = removeRxns(modelHost,ExRH);
+    % ExRH = modelHost.rxns(selExcH);
+    % ExRH(strmatch('sink',modelHost.rxns(selExcH)))=[];
+    % ExRH(strmatch('DM',modelHost.rxns(selExcH)))=[];
 
-%% create a new extracellular space for host
-
-    for i = 1 : length(modelHost.mets)
-        if ~isempty(strfind(modelHost.mets{i},'[e]'))
+    % create a new extracellular space for host
+    for i = 1:length(modelHost.mets)
+        if ~isempty(strfind(modelHost.mets{i}, '[e]'))
             % find all reactions associated - copy and rename
-            ERxnind = find(modelHost.S(i,:));
-            ERxnForm = printRxnFormula(modelHost,modelHost.rxns(ERxnind),false);
-            ERxnForm = regexprep(ERxnForm,'\[e\]','\[b\]');
-            for j = 1 : length(ERxnForm)
-                
+            ERxnind = find(modelHost.S(i, :));
+            ERxnForm = printRxnFormula(modelHost, modelHost.rxns(ERxnind), false);
+            ERxnForm = regexprep(ERxnForm, '\[e\]', '\[b\]');
+            for j = 1:length(ERxnForm)
                 [modelHost,rxnIDexists] = addReaction(modelHost,...
-                    strcat(modelHost.rxns{ERxnind(j)},'b'),ERxnForm{j},[],modelHost.rev(ERxnind(j)),...
-                    modelHost.lb(ERxnind(j)),modelHost.ub(ERxnind(j)),modelHost.c(ERxnind(j)),'Host exchange','','','',false);
+                                                      strcat(modelHost.rxns{ERxnind(j)}, 'b'), ERxnForm{j}, [], modelHost.rev(ERxnind(j)), ...
+                                                      modelHost.lb(ERxnind(j)), modelHost.ub(ERxnind(j)), modelHost.c(ERxnind(j)), 'Host exchange', '', '', '', false);
             end
         end
     end
-    % remove exchange reactions from host while leaving demand and sink
-% reactions
-modelHost = removeRxns(modelHost, exmod);
 
-%% create intercellular space
-% will need to find all extracellular metabolites and duplicate reactions
-% if a host model was input
-% create the shared compartment for the microbes
-    model=modelStorage(1,1);
-    nameTag=nameTagsModels{1,1};
-    [modelJoint,MexGJoint] = createInterSpace(model,nameTag,eTag,exTag);
-    
-  % if more than one microbe was input
-     if modelNumber >1
-        for i=1:modelNumber
-            model=modelStorage(i,1);
-            nameTag=nameTagsModels{i,1};
-            [model,MexG] = createInterSpace(model,nameTag,eTag,exTag);
+    % remove exchange reactions from host while leaving demand and sink reactions
+    modelHost = removeRxns(modelHost, exmod);
+
+    %% create intercellular space
+    % will need to find all extracellular metabolites and duplicate reactions
+    % if a host model was input, create the shared compartment for the microbes
+    model = modelStorage(1, 1);
+    nameTag = nameTagsModels{1, 1};
+    [modelJoint, MexGJoint] = createInterSpace(model, nameTag, eTag, exTag);
+
+    % if more than one microbe was input
+    if modelNumber > 1
+        for i = 1:modelNumber
+            model = modelStorage(i, 1);
+            nameTag = nameTagsModels{i, 1};
+            [model, MexG] = createInterSpace(model, nameTag, eTag, exTag);
             % make sure the changed model is saved under correct name
-            modelStorage(i,1)=model;
+            modelStorage(i, 1) = model;
             MexGJoint = union(MexG, MexGJoint);
         end
      end
-    
-    [modelHost,MexGHost] = createInterSpace(modelHost,nameTagHost,eTag,exTag);
+
+    [modelHost,MexGHost] = createInterSpace(modelHost, nameTagHost, eTag, exTag);
     MexGJoint = union(MexGJoint, MexGHost);
-    
+
     %% merge the models
     % if more than one microbe was input
-    if modelNumber >1
-        for i=2:modelNumber
-            model=modelStorage(i,1);
-            [modelJoint] = mergeTwoModels(modelJoint,model,1,0);
+    if modelNumber > 1
+        for i = 2:modelNumber
+            model = modelStorage(i, 1);
+            [modelJoint] = mergeTwoModels(modelJoint, model, 1, 0);
         end
     end
-    [modelJoint] = mergeTwoModels(modelJoint,modelHost,1,0);
-    
-    modelJoint = addExchangeRxn(modelJoint,unique(MexGJoint));
+    [modelJoint] = mergeTwoModels(modelJoint,modelHost, 1, 0);
+
+    modelJoint = addExchangeRxn(modelJoint, unique(MexGJoint));
 
 else
     %% without a host
      % create the shared compartment for the microbes
-    model=modelStorage(1,1);
-    nameTag=nameTagsModels{1,1};
-    [modelJoint,MexGJoint] = createInterSpace(model,nameTag,eTag,exTag);
-     if modelNumber >1
-        for i=2:modelNumber
-            model=modelStorage(i,1);
-            nameTag=nameTagsModels{i,1};
-            [model,MexG] = createInterSpace(model,nameTag,eTag,exTag);
-            modelStorage(i,1)=model;
+    model = modelStorage(1, 1);
+    nameTag = nameTagsModels{1, 1};
+    [modelJoint, MexGJoint] = createInterSpace(model, nameTag, eTag, exTag);
+     if modelNumber > 1
+        for i = 2:modelNumber
+            model = modelStorage(i, 1);
+            nameTag = nameTagsModels{i, 1};
+            [model, MexG] = createInterSpace(model, nameTag, eTag, exTag);
+            modelStorage(i, 1)=model;
             MexGJoint = union(MexG, MexGJoint);
         end
      end
-    
-     if modelNumber >1
-        for i=2:modelNumber
-            model=modelStorage(i,1);
-            [modelJoint] = mergeTwoModels(modelJoint,model,1);
+
+     if modelNumber > 1
+        for i = 2:modelNumber
+            model = modelStorage(i, 1);
+            [modelJoint] = mergeTwoModels(modelJoint, model, 1);
         end
      end
-    
-    modelJoint = addExchangeRxn(modelJoint,unique(MexGJoint));
+
+    modelJoint = addExchangeRxn(modelJoint, unique(MexGJoint));
 end
 end
+
 %%
 function [modelNew,MexG] = createInterSpace(model, nameTag, eTag, exTag)
 % create intercellular space
-% will need to find all extracellular metabolites and duplicate reactions
-% using them
-%cnt = 1;
+% will need to find all extracellular metabolites and duplicate reactions using them
+
 ExR = [];
 SpR = [];
 modelNew = model;
 cnt = 1;
 
 % add name tag to all metabolites and reactions in model
-modelNew.mets = strcat(nameTag,model.mets);
-modelNew.rxns = strcat(nameTag,model.rxns);
-for i = 1 : length(modelNew.mets)
-    if ~isempty(strfind(modelNew.mets{i},strcat('[',exTag,']')))
+modelNew.mets = strcat(nameTag, model.mets);
+modelNew.rxns = strcat(nameTag, model.rxns);
+
+for i = 1:length(modelNew.mets)
+    if ~isempty(strfind(modelNew.mets{i}, strcat('[', exTag, ']')))
         % add diffusion reactions into extracellular space
         Mex = modelNew.mets{i};
-        MexG{cnt} = regexprep(modelNew.mets{i},strcat('\[',exTag,'\]'),strcat('\[',eTag,'\]'));
-        
+        MexG{cnt} = regexprep(modelNew.mets{i}, strcat('\[', exTag, '\]'), strcat('\[', eTag, '\]'));
+
         % remove nameTag from metabolites
-        MexG{cnt} = regexprep(MexG{cnt},nameTag,'');
-        [modelNew,rxnIDexists] = addReaction...
-            (modelNew,strcat(nameTag,'IEX_',MexG{cnt},'tr'),{Mex MexG{cnt}},[-1 1],1,...
-            -1000,1000,0,'Transport, intercellular','','',false,false);
-        
+        MexG{cnt} = regexprep(MexG{cnt}, nameTag, '');
+        [modelNew, rxnIDexists] = addReaction(modelNew, ...
+                                              strcat(nameTag, 'IEX_', MexG{cnt}, 'tr'), {Mex MexG{cnt}}, [-1 1], 1, ...
+                                              -1000, 1000, 0, 'Transport, intercellular', '', '', false, false);
         cnt = cnt + 1;
-    elseif ~isempty(strfind(modelNew.mets{i},'biomass[c]'))
+
+    elseif ~isempty(strfind(modelNew.mets{i}, 'biomass[c]'))
         % add diffusion reactions into extracellular space
         Mex = modelNew.mets{i};
-        MexG{cnt} = regexprep(modelNew.mets{i},strcat('\[',exTag,'\]'),strcat('\[',eTag,'\]'));
-        
+        MexG{cnt} = regexprep(modelNew.mets{i}, strcat('\[', exTag, '\]'), strcat('\[', eTag, '\]'));
+
         % remove nameTag from metabolites
-        MexG{cnt} = regexprep(MexG{cnt},nameTag,'');
-        [modelNew,rxnIDexists] = addReaction...
-            (modelNew,strcat(nameTag,'IEX_',MexG{cnt},'tr'),{Mex MexG{cnt}},[-1 1],1,...
-            -1000,1000,0,'Transport, intercellular','','',false,false);
-        
+        MexG{cnt} = regexprep(MexG{cnt}, nameTag, '');
+        [modelNew, rxnIDexists] = addReaction(modelNew,...
+                                              strcat(nameTag, 'IEX_', MexG{cnt}, 'tr'), {Mex MexG{cnt}}, [-1 1], 1, ...
+                                              -1000, 1000, 0, 'Transport, intercellular', '', '', false, false);
+
         cnt = cnt + 1;
-    else continue;
+    else
+        continue;
     end
 end
 % add exchange reactions
 end
-
