99c81d933243287af829ea86398635c5c71bc44a
laurentheirendt <laurent.heirendt@uni.lu>
2017-03-07 15:05:03 +0100

    Extension of test for parseBoolean

--- test/verifiedTests/testTools/testParseBoolean.m
+++ test/verifiedTests/testTools/testParseBoolean.m
@@ -25,5 +25,23 @@ ref_newRule = '((x(1)&x(2))|(x(2)&x(4)))&(x(5))';
 assert(isequal(ref_elements, elements))
 assert(isequal(ref_newRule, newRule))
 
+%
+cellArr = {str};
+
+ref_elements1 = {'A', 'B', 'C', 'D'};
+ref_newRule1 = '((A & x(2)) | (B & x(3))) & (~x(4))';
+
+[elements1, newRule1, rxnGeneMat] = parseBoolean(cellArr);
+
+for i = 1:length(elements1)
+    assert(isequal(elements1{i}, ref_elements1{i}))
+end
+
+assert(strcmp(ref_newRule1, newRule1) == 1)
+
+assert(rxnGeneMat(1,2) == 1)
+assert(rxnGeneMat(1,3) == 1)
+assert(rxnGeneMat(1,4) == 1)
+
 % change the directory
 cd(CBTDIR)
