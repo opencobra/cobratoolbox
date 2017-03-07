c1f83ee9eed8a94003c6bf12083f7cd3675472d9
laurentheirendt <laurent.heirendt@uni.lu>
2017-03-07 18:54:39 +0100

    Multiple solver support for testCreateMultipleSpeciesModel

--- test/verifiedTests/testMultiSpeciesModelling/testCreateMultipleSpeciesModel.m
+++ test/verifiedTests/testMultiSpeciesModelling/testCreateMultipleSpeciesModel.m
@@ -14,10 +14,11 @@
 global path_TOMLAB
 global path_GUROBI
 
-%% test the code for creating multi-species model
+% define the path to The COBRAToolbox
+pth = which('initCobraToolbox.m');
+CBTDIR = pth(1:end - (length('initCobraToolbox.m') + 1));
 
-% test model creation and biomass production
-solverOK = changeCobraSolver('glpk', 'LP');
+initTest([CBTDIR, filesep, 'test', filesep, 'verifiedTests', filesep, 'testMultiSpeciesModelling']);
 
 % load the model
 load('ecoli_core_model.mat', 'model');
@@ -28,107 +29,141 @@ tol = 1e-6;
 % also as "host"
 host = model;
 
-% loop through the test cases
-for k = 1:3
-    % define an empty cell
-    microbeModels = {};
+% define the solver packages to be used to run this test
+solverPkgs = {'glpk', 'gurobi6', 'tomlab_cplex'};
 
-    if k == 1  % test joining of one microbe model with/without host
-        microbeModels{1, 1} = model;
+for k = 1:length(solverPkgs)
 
-    elseif k == 2  % test joining of two microbes with/without host
-        for p = 1:2
-            microbeModels{p, 1} = model;
-        end
-    elseif k == 3  % test joining of three  microbes with/without host
-        for p = 1:3
-            microbeModels{p, 1} = model;
-        end
-
-        % use custom nametags
-        microbeNameTags = {'ecoli1_'; 'ecoli2_'; 'ecoli3_'};
-        hostNameTag = 'modelHost_';
+    % add the solver paths (temporary addition for CI)
+    if strcmp(solverPkgs{k}, 'tomlab_cplex')
+        addpath(genpath(path_TOMLAB));
+    elseif strcmp(solverPkgs{k}, 'gurobi6')
+        addpath(genpath(path_GUROBI));
     end
 
+    % change the COBRA solver (LP)
+    solverOK = changeCobraSolver(solverPkgs{k});
 
-    if k == 1 || k == 2
-        % only one microbe entered
-        [modelJoint] = createMultipleSpeciesModel(microbeModels);
+    if solverOK == 1
+        % loop through the test cases
+        for j = 1:3
+            fprintf('   Testing createMultipleSpeciesModel using %s ... ', solverPkgs{k});
 
-        % one microbe model with host
-        [modelJointHost] = createMultipleSpeciesModel(microbeModels, [], host);
-    else
-        % three microbe models with host
-        [modelJointHost] = createMultipleSpeciesModel(microbeModels, microbeNameTags, host, hostNameTag);
+            % define an empty cell
+            microbeModels = {};
 
-        % three microbe models without host
-        [modelJoint] = createMultipleSpeciesModel(microbeModels, microbeNameTags);
-    end
+            if j == 1  % test joining of one microbe model with/without host
+                microbeModels{1, 1} = model;
 
-    for i = 1:k
-        if k == 1 || k == 2
-            tmpTag = 'model';
-        else
-            tmpTag = 'ecoli';
-        end
+            elseif j == 2  % test joining of two microbes with/without host
+                for p = 1:2
+                    microbeModels{p, 1} = model;
+                end
 
-        assert(length(model.rxns) == length(strmatch(strcat(tmpTag, num2str(i), '_'), modelJoint.rxns)))
-        assert(length(model.mets) == length(strmatch(strcat(tmpTag, num2str(i), '_'), modelJoint.mets)))
-        assert(length(model.rxns) == length(strmatch(strcat(tmpTag, num2str(i), '_'), modelJointHost.rxns)))
-        assert(length(model.mets) == length(strmatch(strcat(tmpTag, num2str(i), '_'), modelJointHost.mets)))
+            elseif j == 3  % test joining of three  microbes with/without host
+                for p = 1:3
+                    microbeModels{p, 1} = model;
+                end
 
-        if k == 2 || k == 3
-            % test the biomass production of modelJoint
-            modelJoint = changeObjective(modelJoint, strcat(tmpTag, num2str(i), '_', 'Biomass_Ecoli_core_w_GAM'));
-            FBA = optimizeCbModel(modelJoint, 'max');
-            assert(FBA.f > tol);
+                % use custom nametags
+                microbeNameTags = {'ecoli1_'; 'ecoli2_'; 'ecoli3_'};
+                hostNameTag = 'modelHost_';
+            end
 
-            % test the biomass production of modelJointHost
-            modelJointHost = changeObjective(modelJointHost, strcat(tmpTag, num2str(i), '_', 'Biomass_Ecoli_core_w_GAM'));
-            FBA = optimizeCbModel(modelJointHost, 'max');
-            assert(FBA.f > tol);
-        end
-    end
 
-    % count the number of extracellular reactions in host to determine number
-    % of body fluid compartment reactions added
-    exRxns = {};
-    rxnCnt = 1;
-    metCnt = 1;
+            if j == 1 || j == 2
+                % only one microbe entered
+                [modelJoint] = createMultipleSpeciesModel(microbeModels);
 
-    for i = 1:length(host.mets)
-        if ~isempty(strfind(host.mets{i}, '[e]'))
-            exMets{metCnt, 1} = host.mets{i};
-            metCnt = metCnt + 1;
+                % one microbe model with host
+                [modelJointHost] = createMultipleSpeciesModel(microbeModels, [], host);
+            else
+                % three microbe models with host
+                [modelJointHost] = createMultipleSpeciesModel(microbeModels, microbeNameTags, host, hostNameTag);
 
-            % find all reactions associated - copy and rename
-            ERxnind = find(host.S(i, :));
-            for j = 1:length(ERxnind)
-                exRxns{rxnCnt, 1} = host.rxns{ERxnind(j), 1};
-                rxnCnt = rxnCnt + 1;
+                % three microbe models without host
+                [modelJoint] = createMultipleSpeciesModel(microbeModels, microbeNameTags);
+            end
+
+            for i = 1:j
+                if j == 1 || j == 2
+                    tmpTag = 'model';
+                else
+                    tmpTag = 'ecoli';
+                end
+
+                assert(length(model.rxns) == length(strmatch(strcat(tmpTag, num2str(i), '_'), modelJoint.rxns)))
+                assert(length(model.mets) == length(strmatch(strcat(tmpTag, num2str(i), '_'), modelJoint.mets)))
+                assert(length(model.rxns) == length(strmatch(strcat(tmpTag, num2str(i), '_'), modelJointHost.rxns)))
+                assert(length(model.mets) == length(strmatch(strcat(tmpTag, num2str(i), '_'), modelJointHost.mets)))
+
+                if j == 2 || j == 3
+                    % test the biomass production of modelJoint
+                    modelJoint = changeObjective(modelJoint, strcat(tmpTag, num2str(i), '_', 'Biomass_Ecoli_core_w_GAM'));
+                    FBA = optimizeCbModel(modelJoint, 'max');
+                    assert(FBA.f > tol);
+
+                    % test the biomass production of modelJointHost
+                    modelJointHost = changeObjective(modelJointHost, strcat(tmpTag, num2str(i), '_', 'Biomass_Ecoli_core_w_GAM'));
+                    FBA = optimizeCbModel(modelJointHost, 'max');
+                    assert(FBA.f > tol);
+                end
+            end
+
+            % count the number of extracellular reactions in host to determine number
+            % of body fluid compartment reactions added
+            exRxns = {};
+            rxnCnt = 1;
+            metCnt = 1;
+
+            for ii = 1:length(host.mets)
+                if ~isempty(strfind(host.mets{ii}, '[e]'))
+                    exMets{metCnt, 1} = host.mets{ii};
+                    metCnt = metCnt + 1;
+
+                    % find all reactions associated - copy and rename
+                    ERxnind = find(host.S(ii, :));
+                    for jj = 1:length(ERxnind)
+                        exRxns{rxnCnt, 1} = host.rxns{ERxnind(jj), 1};
+                        rxnCnt = rxnCnt + 1;
+                    end
+                end
+            end
+
+            exRxns = unique(exRxns);
+            exch = strmatch('EX_', exRxns);
+
+            % now compare total number of reactions
+            % NOTE: some large host reconstructions (e.g., Recon2) are too complex and/or already have lumen compartment-in this j, the test won't work
+
+            if j == 1 || j == 2
+                assert(length(exch) == length(strmatch('Host_EX', modelJointHost.rxns)));
+                assert(length(host.rxns) + length(exRxns) == length(strmatch('Host_', modelJointHost.rxns)));
+                assert(length(host.mets) + length(exMets) == length(strmatch('Host_', modelJointHost.mets)));
+
+            elseif j == 3
+                assert(length(exch) == length(strmatch('modelHost_EX', modelJointHost.rxns)))
+                assert(length(host.rxns) + length(exRxns) == length(strmatch('modelHost_', modelJointHost.rxns)))
+                assert(length(host.mets) + length(exMets) == length(strmatch('modelHost_', modelJointHost.mets)))
+
+                % test host biomass
+                modelJointHost = changeObjective(modelJointHost, 'modelHost_Biomass_Ecoli_core_w_GAM');
+                FBA = optimizeCbModel(modelJointHost, 'max');
+                assert(FBA.f > tol)
             end
         end
     end
 
-    exRxns = unique(exRxns);
-    exch = strmatch('EX_', exRxns);
+    % output a success message
+    fprintf('Done.\n');
 
-    % now compare total number of reactions
-    % NOTE: some large host reconstructions (e.g., Recon2) are too complex and/or already have lumen compartment-in this case, the test won't work
-
-    if k == 1 || k == 2
-        assert(length(exch) == length(strmatch('Host_EX', modelJointHost.rxns)));
-        assert(length(host.rxns) + length(exRxns) == length(strmatch('Host_', modelJointHost.rxns)));
-        assert(length(host.mets) + length(exMets) == length(strmatch('Host_', modelJointHost.mets)));
-
-    elseif k == 3
-        assert(length(exch) == length(strmatch('modelHost_EX',modelJointHost.rxns)))
-        assert(length(host.rxns) + length(exRxns) == length(strmatch('modelHost_', modelJointHost.rxns)))
-        assert(length(host.mets) + length(exMets) == length(strmatch('modelHost_', modelJointHost.mets)))
-
-        % test host biomass
-        modelJointHost = changeObjective(modelJointHost, 'modelHost_Biomass_Ecoli_core_w_GAM');
-        FBA = optimizeCbModel(modelJointHost, 'max');
-        assert(FBA.f > tol)
+    % remove the solver paths (temporary addition for CI)
+    if strcmp(solverPkgs{k}, 'tomlab_cplex')
+        rmpath(genpath(path_TOMLAB));
+    elseif strcmp(solverPkgs{k}, 'gurobi6')
+        rmpath(genpath(path_GUROBI));
     end
 end
+
+% change the directory
+cd(CBTDIR)
