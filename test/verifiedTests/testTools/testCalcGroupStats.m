33c86d838dad9b8cccfc73d75f283f36c360806d
laurentheirendt <laurent.heirendt@uni.lu>
2017-03-07 11:52:11 +0100

    Adaptations to test for calcGroupstats

--- test/verifiedTests/testTools/testCalcGroupStats.m
+++ test/verifiedTests/testTools/testCalcGroupStats.m
@@ -12,22 +12,21 @@
 pth = which('initCobraToolbox.m');
 CBTDIR = pth(1:end-(length('initCobraToolbox.m') + 1));
 
-initTest([CBTDIR, filesep, 'test', filesep, 'verifiedTests', filesep, 'testTools']);
+cd([CBTDIR, filesep, 'test', filesep, 'verifiedTests', filesep, 'testTools']);
 
 % load reference data - too complex to embed
-load ref.mat;
+load('refData_calcGroupStats.mat');
 
-data = ones(5)*6;
+data = 6 * ones(5);
 statname = {'mean', 'std', 'median', 'count'};
 groups = {'test1', 'test2', 'test3', 'test4', 'test1'};
 
 ref_groupCnt = [2; 1; 1; 1];
 
-for i = 1:4 
+for i = 1:4
     [groupStat(:,:,i), groupList, groupCnt] = calcGroupStats(data, groups, statname{i}, ref_groupList, 1, 10);
     assert(isequal(ref_groupList, groupList));
     assert(isequal(ref_groupCnt, groupCnt));
-    %assert(isequal(groupStat,data));
 end
 
 assert(isequal(ref_groupStat, groupStat))
