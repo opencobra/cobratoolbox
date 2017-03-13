673674fdc494753f23c982bed575d84d386074f6
laurentheirendt <laurent.heirendt@uni.lu>
2017-03-13 10:04:26 +0100

    Changes to launchTests for CI

--- test/launchTests.m
+++ test/launchTests.m
@@ -4,8 +4,10 @@ global path_ILOG_CPLEX
 global path_TOMLAB
 
 % do not change the paths below
-addpath(genpath('/var/lib/jenkins/MOcov'));
-addpath(genpath('/var/lib/jenkins/jsonlab'));
+if strcmpi(getenv('CI'), 'jenkins')
+    addpath(genpath('/var/lib/jenkins/MOcov'));
+    addpath(genpath('/var/lib/jenkins/jsonlab'));
+end
 
 % include the root folder and all subfolders
 addpath(genpath(pwd))
@@ -25,6 +27,10 @@ end
 % run the official initialisation script
 initCobraToolbox
 
+if strcmpi(getenv('CI'), 'jenkins')
+    WAITBAR_TYPE = 0;
+end
+
 % run the official testsuite
 %testAll
 
@@ -40,41 +46,43 @@ try
     % run the tests in the subfolder verifiedTests/ recursively
     result = runtests('./test/', 'Recursively', true, 'BaseFolder', '*verified*');
 
-    % write coverage based on profile('info')
-    mocov('-cover','src',...
-          '-profile_info',...
-          '-cover_json_file','coverage.json',...
-          '-cover_method', 'profile');
-
     sumFailed = 0;
     sumIncomplete = 0;
 
-    for i = 1:size(result,2)
-        sumFailed = sumFailed + result(i).Failed;
-        sumIncomplete = sumIncomplete + result(i).Incomplete;
+    if strcmpi(getenv('CI'), 'jenkins')
+        % write coverage based on profile('info')
+        mocov('-cover','src',...
+              '-profile_info',...
+              '-cover_json_file','coverage.json',...
+              '-cover_method', 'profile');
+
+        for i = 1:size(result,2)
+            sumFailed = sumFailed + result(i).Failed;
+            sumIncomplete = sumIncomplete + result(i).Incomplete;
+        end
+
+        % load the coverage file
+        data = loadjson('coverage.json', 'SimplifyCell', 1);
+
+        sf = data.source_files;
+        clFiles = zeros(length(sf), 1);
+        tlFiles = zeros(length(sf), 1);
+
+        for i = 1:length(sf)
+            clFiles(i) = nnz(sf(i).coverage);
+            tlFiles(i) = length(sf(i).coverage);
+        end
+
+        % average the values for each file
+        cl = sum(clFiles);
+        tl = sum(tlFiles);
+
+        % print out the coverage
+        fprintf('Covered Lines: %i, Total Lines: %i, Coverage: %f%%.\n', cl, tl, cl/tl * 100);
     end
-
-    % load the coverage file
-    data = loadjson('coverage.json', 'SimplifyCell', 1);
-
-    sf = data.source_files;
-    clFiles = zeros(length(sf), 1);
-    tlFiles = zeros(length(sf), 1);
-
-    for i = 1:length(sf)
-        clFiles(i) = nnz(sf(i).coverage);
-        tlFiles(i) = length(sf(i).coverage);
-    end
-
-    % average the values for each file
-    cl = sum(clFiles);
-    tl = sum(tlFiles);
 
     % print out a summary table
     table(result)
-
-    % print out the coverage as requested by gitlab
-    fprintf('Covered Lines: %i, Total Lines: %i, Coverage: %f%%.\n', cl, tl, cl/tl * 100);
 
     if sumFailed > 0 || sumIncomplete > 0
         exit_code = 1;
