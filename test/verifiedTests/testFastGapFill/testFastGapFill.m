0c9d4af9bb7f5af7d8bbee1a45f0d569303b5cb3
laurentheirendt <laurent.heirendt@uni.lu>
2017-03-13 19:08:07 +0100

    Initial version of testFastGapFill
    New version of test .xml file

--- test/verifiedTests/testFastGapFill/testFastGapFill.m
+++ test/verifiedTests/testFastGapFill/testFastGapFill.m
@@ -1,72 +1,53 @@
-function fgffail = testFastGapFill()
-%testFastGapFill tests the functionality of FastGapFill
+% The COBRAToolbox: testFastGapFill.m
 %
-% fgffail = testFastGapFill()
+% Purpose:
+%     - testFastGapFill tests the fastGapFill function
 %
-% OUTPUT
-% 
-% fgffail   - integer value 0 if success and 1 if failure
+% Author:
+%     - CI: integration: Laurent Heirendt - March 2017
 
-%Move to testing folder
-oriFolder = pwd;
-test_folder = what('testFastGapFill');
-cd(test_folder(1).path);
+global path_ILOG_CPLEX
+
+% define the path to The COBRAToolbox
+pth = which('initCobraToolbox.m');
+CBTDIR = pth(1:end - (length('initCobraToolbox.m') + 1));
+
+initTest([CBTDIR filesep 'test' filesep 'verifiedTests' filesep 'testFastGapFill'])
+
+% ILOG CPLEX libraray must be included
+addpath(genpath(path_ILOG_CPLEX))
 
 %Specify test files
-modelFile='fgf_test_model.xml';
-dbFile='fgf_test_rxn_db.lst';
-dictFile='fgf_test_dict.tsv';
-listCompartments={'[c]'};
+modelFile = 'fgf_test_model.xml';
+dbFile = 'fgf_test_rxn_db.lst';
+dictFile = 'fgf_test_dict.tsv';
+listCompartments = {'[c]'};
 
 fprintf('Testing FastGapFill ...\n');
 
 %Test DB import
-dbfail=0;
-try
-    evalc('U_model = createUniversalReactionModel2(dbFile,[],true);');
-catch
-    fprintf('Creation of universal reaction model failed\n');
-    dbfail=1;
-end
-if exist('U_model','var')
-    rxnCount = length(U_model.rxns);
-    metCount = length(U_model.mets);
-    if rxnCount ~= 3 || metCount ~= 5
-        fprintf('Universal reaction model imported incorrectly\n');
-        dbfail=1;
-    end
-end
+U_model = createUniversalReactionModel2(dbFile, []);
+rxnCount = length(U_model.rxns);
+metCount = length(U_model.mets);
+assert(rxnCount == 3 && metCount == 5);
 
 %Test dict conversion
-dictfail=0;
-if dbfail == 0
-    file_handle = fopen(dictFile);
-    u = textscan(file_handle,'%s\t%s');
-    dictionary = {};
-    for i = 1:length(u{1})
-        dictionary{i,1} = u{1}{i};
-        dictionary{i,2} = u{2}{i};
-    end
-    fclose(file_handle);
-    try
-        evalc('translated_model = transformKEGG2Model(U_model,dictionary);');
-    catch
-        fprintf('Creation of translated universal model failed\n')
-        dictfail=1;
-    end
-    if exist('translated_model','var')
-        if sum(cellfun(@(s) ~isempty(strfind('A[c]', s)), translated_model.mets)) ~= 1
-                fprintf('Universal model translated incorrectly\n')
-                dictfail=1;
-        end
-    end
-else
-    dictfail=1;
+file_handle = fopen(dictFile);
+u = textscan(file_handle,'%s\t%s');
+dictionary = {};
+for i = 1:length(u{1})
+    dictionary{i,1} = u{1}{i};
+    dictionary{i,2} = u{2}{i};
 end
-    
+fclose(file_handle);
+
+translated_model = transformKEGG2Model(U_model, dictionary);
+
+assert(sum(cellfun(@(s) ~isempty(strfind('A[c]', s)), translated_model.mets)) == 1);
+
 %Test SUX creation
-suxfail=0;
-evalc('modelFull = readCbModel(modelFile);');
+modelFull = readCbModel(modelFile);
+changeCobraSolver('glpk');
 if ~exist('modelFull.subSystems') || length(modelFull.subSystems) ~= length(modelFull.rxnNames)
     modelFull.subSystems = repmat({''},length(modelFull.rxnNames));
 end
@@ -79,63 +60,22 @@ end
 if ~exist('modelFull.grRules')
     modelFull.grRules = repmat({''},length(modelFull.rxnNames));
 end
-evalc('[modelConsistent, ~] = identifyBlockedRxns(modelFull,1e-4);');
+[modelConsistent, ~] = identifyBlockedRxns(modelFull, 1e-4);
 
-if dictfail == 0
-    try
-        evalc('MatricesSUX=generateSUXComp(modelConsistent,dictionary,dbFile,[],listCompartments);');
-    catch
-        fprintf('Creation of SUX model failed\n');
-        suxfail=1;
-    end
-    if exist('MatricesSUX','var')
-        rxnCount = length(MatricesSUX.rxns);
-        metCount = length(MatricesSUX.mets);
-        if rxnCount ~= 23 || metCount ~= 14
-            fprintf('SUX model created incorrectly\n');
-            suxfail=1;
-        end
-    end
-else
-    suxfail=1;
-end
+MatricesSUX = generateSUXComp(modelConsistent, dictionary, dbFile, [], listCompartments);
+
+rxnCount = length(MatricesSUX.rxns);
+metCount = length(MatricesSUX.mets);
+assert(rxnCount == 23 && metCount == 14);
+
+changeCobraSolver('glpk');
 
 %Test full FastGapFill
-fgffail=0;
-if (~dbfail && ~dictfail && ~suxfail)
-    try
-        evalc('[AddedRxns]=submitFastGapFill(modelFile,dbFile,dictFile,[],[],true,[],[],listCompartments);');
-    catch
-        fprintf('Exception caused FastGapFill to exit.\n');
-        fgffail=1;
-    end
-    %Look for RXN1013 in solution, which fills test model gap
-    if exist('AddedRxns','var')
-        if sum(cellfun(@(s) ~isempty(strfind('RXN1013', s)), AddedRxns.rxns)) ~= 1
-            fprintf('FastGapFill failed to find gap solution.\n')
-            fgffail=1;
-        end
-    end
-else
-    fgffail=1;
-end
+[AddedRxns] = submitFastGapFill(modelFile, dbFile, dictFile, [], 'test_sampleWeights.tsv', true, [], [], listCompartments);
 
-if fgffail == 0
-    fprintf('success.\n')
-else
-    fprintf('failure.\n')
-end
+assert(sum(cellfun(@(s) ~isempty(strfind('RXN1013', s)), AddedRxns.rxns)) == 1)
 
-try
-    delete matlab.mat;
-catch
-end
-try
-    delete KEGGMatrix.mat;
-catch
-end
+delete KEGGMatrix.mat;
 
-cd(oriFolder);
-
-end
-
+% change the directory
+cd(CBTDIR)
