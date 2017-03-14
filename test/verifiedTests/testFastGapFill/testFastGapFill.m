d12fc211c31890126a722442834313081a7caa76
laurentheirendt <laurent.heirendt@uni.lu>
2017-03-14 11:44:32 +0100

    Changes for multiple solvers in test for fastGapFill

--- test/verifiedTests/testFastGapFill/testFastGapFill.m
+++ test/verifiedTests/testFastGapFill/testFastGapFill.m
@@ -7,15 +7,13 @@
 %     - CI: integration: Laurent Heirendt - March 2017
 
 global path_ILOG_CPLEX
+global path_TOMLAB
 
 % define the path to The COBRAToolbox
 pth = which('initCobraToolbox.m');
 CBTDIR = pth(1:end - (length('initCobraToolbox.m') + 1));
 
 initTest([CBTDIR filesep 'test' filesep 'verifiedTests' filesep 'testFastGapFill'])
-
-% ILOG CPLEX libraray must be included
-addpath(genpath(path_ILOG_CPLEX))
 
 %Specify test files
 modelFile = 'fgf_test_model.xml';
@@ -68,16 +66,56 @@ rxnCount = length(MatricesSUX.rxns);
 metCount = length(MatricesSUX.mets);
 assert(rxnCount == 23 && metCount == 14);
 
-changeCobraSolver('glpk');
+%test solver packages
+solverPkgs = {'tomlab_cplex', 'ILOGsimple', 'ILOGcomplex'};
 
-%Test full FastGapFill
-[AddedRxns] = submitFastGapFill(modelFile, dbFile, dictFile, [], 'test_sampleWeights.tsv', true, [], [], listCompartments);
+for k = 1:length(solverPkgs)
 
-assert(sum(cellfun(@(s) ~isempty(strfind('RXN1013', s)), AddedRxns.rxns)) == 1)
+    % add the solver paths (temporary addition for CI)
+    if strcmp(solverPkgs{k}, 'tomlab_cplex')
+        addpath(genpath(path_TOMLAB));
+    elseif strcmp(solverPkgs{k}, 'ILOGsimple') || strcmp(solverPkgs{k}, 'ILOGcomplex')
+        addpath(genpath(path_ILOG_CPLEX));
+    end
 
-delete KEGGMatrix.mat;
+    if ~verLessThan('matlab','8') && ( strcmp(solverPkgs{k}, 'ILOGcomplex')) %2016b %strcmp(solverPkgs{k}, 'ILOGsimple') ||
+        fprintf(['\n IBM ILOG CPLEX - ', solverPkgs{k}, ' - is incompatible with this version of MATLAB, please downgrade or change solver\n'])
+    else
+        fprintf('   Running testFastGapFill using %s ... ', solverPkgs{k});
 
-rmpath(genpath(path_ILOG_CPLEX))
+        solverOK  = changeCobraSolver(solverPkgs{k});
+
+        if solverOK
+
+            % FASTCORE functions must have the CPLEX library included in order to run
+            if ~strcmp(solverPkgs{k}, 'ILOGsimple') && ~strcmp(solverPkgs{k}, 'ILOGcomplex')
+                addpath(genpath(path_ILOG_CPLEX));
+            end
+
+            %Test full FastGapFill
+            [AddedRxns] = submitFastGapFill(modelFile, dbFile, dictFile, [], 'test_sampleWeights.tsv', true, [], [], listCompartments);
+
+            assert(sum(cellfun(@(s) ~isempty(strfind('RXN1013', s)), AddedRxns.rxns)) == 1)
+
+            delete KEGGMatrix.mat;
+
+            % FASTCORE functions must have the CPLEX library included in order to run
+            if ~strcmp(solverPkgs{k}, 'ILOGsimple') && ~strcmp(solverPkgs{k}, 'ILOGcomplex')
+                rmpath(genpath(path_ILOG_CPLEX));
+            end
+        end
+
+        % print a success message
+        fprintf('Done.\n')
+    end
+
+    % remove the solver paths (temporary addition for CI)
+    if strcmp(solverPkgs{k}, 'tomlab_cplex')
+        rmpath(genpath(path_TOMLAB));
+    elseif strcmp(solverPkgs{k}, 'ILOGsimple') || strcmp(solverPkgs{k}, 'ILOGcomplex')
+        rmpath(genpath(path_ILOG_CPLEX));
+    end
+end
 
 % change the directory
 cd(CBTDIR)
