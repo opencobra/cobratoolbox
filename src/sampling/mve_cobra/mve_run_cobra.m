f65ab17ba5cb7d1a570d6a17cd419cf550ebacdc
Ben Cousins <bcousins3@gatech.edu>
2017-03-14 12:40:10 -0700

    Updated MVE function calls to not call external/ folder

--- src/sampling/mve_cobra/mve_run_cobra.m
+++ src/sampling/mve_cobra/mve_run_cobra.m
@@ -22,14 +22,14 @@ maxiter = 150; tol1 = 1.e-8; tol2 = 1.E-6;
 t0 = tic;
 
 if nargin < 3
-    mve_chkdata(A,b);
-    [msg,x0] = mve_presolve(A,b,maxiter,tol1);
+    mve_chkdata_cobra(A,b);
+    [msg,x0] = mve_presolve_cobra(A,b,maxiter,tol1);
     fprintf('  End of Presolve ......\n');
     if msg(1) ~= 's', disp(msg); return; end
-    [x,E2] = mve_solver(A,b,x0,maxiter,tol2,reg); %Ben
+    [x,E2] = mve_solver_cobra(A,b,x0,maxiter,tol2,reg); %Ben
 else
     mve_chkdata(A,b,x0);
-    [x,E2,converged] = mve_solver(A,b,x0,maxiter,tol2,reg); %Ben
+    [x,E2,converged] = mve_solver_cobra(A,b,x0,maxiter,tol2,reg); %Ben
 end
 E = chol(nearestSPD(E2)); %Ben
 E = E';
