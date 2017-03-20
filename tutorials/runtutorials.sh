77a74617b1e811c4b236de20b00ad1aaccb5a1cb
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-03-20 23:30:30 +0100

    Fix issue with Travis instructions for running tutorials.

--- tutorials/runtutorials.sh
+++ tutorials/runtutorials.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
-/mnt/data/MATLAB/$MATLAB_VER/bin/./matlab -nodesktop -nosplash < tutorials/launchTutorials.m
-CODE=$?
-exit $CODE
+#!/bin/bash
+if [ "$CI_PROJECT_NAME" = "COBRAToolbox-branches-auto" ] || [ "$CI_PROJECT_NAME" = "COBRAToolbox-branches-manual" ];
+then
+    /mnt/data/MATLAB/$MATLAB_VER/bin/./matlab -nodesktop -nosplash < tutorials/launchTutorials.m
+    CODE=$?
+    exit $CODE
+fi
