090623572013601252da56542906e8a88a7c8383
Lemmer <lemmer.elassal@me.com>
2017-03-24 13:58:32 +0100

    Restructuring tests

--- test/verifiedTests/testPrint/testPrintConstraints.m
+++ test/verifiedTests/testPrint/testPrintConstraints.m
@@ -8,11 +8,12 @@
 %     - Lemmer El Assal March 2017
 %
 
-% define the path to The COBRAToolbox
-pth = which('initCobraToolbox.m');
-CBTDIR = pth(1:end-(length('initCobraToolbox.m') + 1));
+% save the current path
+currentDir = pwd;
 
-cd([CBTDIR, filesep, 'test', filesep, 'verifiedTests', filesep, 'testPrint']);
+% initialize the test
+initTest(fileparts(which(mfilename)));
+
 
 load('ecoli_core_model', 'model');
 
@@ -27,4 +28,4 @@ text2 = fileread('printConstraints.txt');
 assert(isequal(text1,text2));
 
 % change the directory
-cd(CBTDIR)
+cd(currentDir)
