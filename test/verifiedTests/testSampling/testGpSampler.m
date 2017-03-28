438a41520f5a962cc359252f0da1aacbd071e86b
laurentheirendt <laurent.heirendt@uni.lu>
2017-03-28 09:43:41 +0200

    Multi-solver support gpSampler

--- test/verifiedTests/testSampling/testGpSampler.m
+++ test/verifiedTests/testSampling/testGpSampler.m
@@ -1,33 +1,66 @@
-% The COBRAToolbox: testgpSampler.m
+% The COBRAToolbox: testGpSampler.m
 %
 % Purpose:
-%     - tests the newSampler function using the E. coli Core Model
+%     - tests the gpSampler function using the E. coli Core Model
 %
+
+% define global paths
+global path_TOMLAB
+global path_GUROBI
 
 % save the current path
 currentDir = pwd;
 
 % initialize the test
-cd(fileparts(which(mfilename)));
+initTest(fileparts(which(mfilename)));
 
-changeCobraSolver('glpk');
-
+% load the model
 load('ecoli_core_model.mat', 'model');
 
+% define the number of sample points
 samplePoints = [5, 190];
 
-for i = 1:length(samplePoints)
-    % call sampler
-    [sampleStructOut, mixedFrac] = gpSampler(model, samplePoints(i), [], 2);
+% define the solver packages to be used to run this test
+solverPkgs = {'gurobi6', 'tomlab_cplex', 'glpk'};
 
-    % check
-    [errorsA, errorsLUB, stuckPoints] = verifyPoints(sampleStructOut);
+for k = 1:length(solverPkgs)
+    % add the solver paths (temporary addition for CI)
+    if strcmp(solverPkgs{k}, 'tomlab_cplex')
+        addpath(genpath(path_TOMLAB));
+    elseif strcmp(solverPkgs{k}, 'gurobi6')
+        addpath(genpath(path_GUROBI));
+    end
 
-    tmpErrorsA = ~any(errorsA);
+    % set the solver
+    solverOK = changeCobraSolver(solverPkgs{k});
 
-    assert(tmpErrorsA(1) && tmpErrorsA(2))
-    assert(~any(errorsLUB))
-    assert(~any(stuckPoints))
+    if solverOK == 1
+        fprintf('   Testing readSBML using %s ... \n', solverPkgs{k});
+
+        for i = 1:length(samplePoints)
+            % call sampler
+            [sampleStructOut, mixedFrac] = gpSampler(model, samplePoints(i), [], 2);
+
+            % check
+            [errorsA, errorsLUB, stuckPoints] = verifyPoints(sampleStructOut);
+
+            tmpErrorsA = ~any(errorsA);
+
+            assert(tmpErrorsA(1) && tmpErrorsA(2))
+            assert(~any(errorsLUB))
+            assert(~any(stuckPoints))
+        end
+
+        % print a line for success of loop i
+        fprintf(' Done.\n');
+    end
+
+    % remove the solver paths (temporary addition for CI)
+    if strcmp(solverPkgs{k}, 'tomlab_cplex')
+        rmpath(genpath(path_TOMLAB));
+    elseif strcmp(solverPkgs{k}, 'gurobi6')
+        rmpath(genpath(path_GUROBI));
+    end
 end
 
 % change the directory
