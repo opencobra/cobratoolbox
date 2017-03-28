8aa56c713034e8c8c058c55ae6c809230db650f0
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-03-28 15:20:43 +0200

    tests for moieties identification.

--- test/verifiedTests/testTopology/testMoieties.m
+++ test/verifiedTests/testTopology/testMoieties.m
@@ -1,36 +1,59 @@
-% Test code to identify conserved moieties in metabolic networks by graph
-% theoretical analysis of atom transition networks.
+% The COBRAToolbox: testMoieties.m
+%
+% Purpose:
+%     - testMoieties tests the functionality of conserved moities tools
+%       and compares it to expected data.
+%     - testMoieties identifies conserved moieties in metabolic networks by graph
+%       theoretical analysis of atom transition networks.
+%
+% Authors:
+%     - Sylvain Arreckx March 2017
+%
 
-load ../../tutorials/moieties/Data/DAS.mat % The dopamine synthesis network
-rxnfileDir = '../../tutorials/moieties/Data/AlternativeAtomMappingFiles/'; % Predicted atom mappings from DREAM (http://selene.princeton.edu/dream/)
+% save the current path
+currentDir = pwd;
+
+% initialize the test
+initTest(fileparts(which(mfilename)));
+
+% Load reference data
+load('refData_moieties.mat')
+
+% Load the dopamine synthesis network
+load('DAS.mat')
+
+% Predicted atom mappings from DREAM (http://selene.princeton.edu/dream/)
+rxnfileDir = ['..' filesep '..' filesep '..' filesep 'tutorials' filesep 'moieties' filesep 'Data' filesep 'AlternativeAtomMappingFiles'];
 
 % Generate atom transition network
-ATN = buildAtomTransitionNetwork(model,rxnfileDir);
+ATN = buildAtomTransitionNetwork(model, rxnfileDir);
+assert(all(all(ATN.A == ATN0.A)), 'Atom transition network does not match reference.')
 
 % Identify conserved moieties
-[L,Lambda,moietyFormulas,moieties2mets,moieties2vectors,atoms2moieties] = identifyConservedMoieties(model,ATN);
+[L, Lambda, moietyFormulas, moieties2mets, moieties2vectors, atoms2moieties] = identifyConservedMoieties(model, ATN);
+assert(all(all(L == L0)), 'Moiety matrix does not match reference.')
+assert(all(all(Lambda == Lambda0)), 'Moiety graph does not match reference.')
 
 % Classify moieties
-types = classifyMoieties(L,model.S);
+types = classifyMoieties(L, model.S);
+assert(all(strcmp(types, types0)), 'Moiety classifications do not match reference.')
 
 % Decompose moiety vectors
-rbool = ismember(model.rxns,ATN.rxns); % True for reactions included in ATN
-mbool = any(model.S(:,rbool),2); % True for metabolites in ATN reactions
-N = model.S(mbool,rbool);
+rbool = ismember(model.rxns, ATN.rxns);  % True for reactions included in ATN
+mbool = any(model.S(:, rbool), 2);  % True for metabolites in ATN reactions
+N = model.S(mbool, rbool);
 
-changeCobraSolver('gurobi6','milp');
-D = decomposeMoietyVectors(L,N);
+solverOK = changeCobraSolver('gurobi6', 'MILP');
+if solverOK
+    D = decomposeMoietyVectors(L, N);
+    assert(all(all(D == D0)), 'Decomposed moiety matrix does not match reference.')
 
-% Estimate chemical formulas of decomposed moieties
-load ../../tutorials/moieties/Data/elementalMatrix.mat % Load elemental matrix
-[decomposedMoietyFormulas,M] = estimateMoietyFormulas(D,E,elements);
+    % Build elemental matrix for the dopamine network
+    [E, elements] = constructElementalMatrix(model.metFormulas, model.metCharges);
 
-% Check results
-load moietyResults.mat % saved results for comparison
+    % Estimate chemical formulas of decomposed moieties
+    [decomposedMoietyFormulas, M] = estimateMoietyFormulas(D, E, elements);
+    assert(all(strcmp(decomposedMoietyFormulas, decomposedMoietyFormulas0)), 'Estimated formulas of decomposed moieties do not match reference.')
+end
 
-assert(all(all(ATN.A == ATN0.A)),'Atom transition network does not match reference.')
-assert(all(all(L == L0)),'Moiety matrix does not match reference.')
-assert(all(all(Lambda == Lambda0)),'Moiety graph does not match reference.')
-assert(all(strcmp(types,types0)),'Moiety classifications do not match reference.')
-assert(all(all(D == D0)),'Decomposed moiety matrix does not match reference.')
-assert(all(strcmp(decomposedMoietyFormulas,decomposedMoietyFormulas0)),'Estimated formulas of decomposed moieties do not match reference.')
+cd(currentDir)
