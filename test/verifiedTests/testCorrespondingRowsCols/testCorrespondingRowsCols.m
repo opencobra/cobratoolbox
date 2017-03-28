2111060683212901d06528a5632a721864b29931
laurentheirendt <laurent.heirendt@uni.lu>
2017-03-28 20:00:17 +0200

    CI integration of testCorrespondingRowsCols

--- test/verifiedTests/testCorrespondingRowsCols/testCorrespondingRowsCols.m
+++ test/verifiedTests/testCorrespondingRowsCols/testCorrespondingRowsCols.m
@@ -1,103 +1,108 @@
-function bool = testCorrespondingRowsCols()
-%tests getCorrespondingRows and getCorrespondingCols
-S = [-1  0  0  0  0;
-      2 -3  0  0  0;
-      0  4 -5  0  0;
-      0  0  6 -7  0;
-      0  0  0  0  8]; 
- 
-  
-printLevel=1;
+% The COBRAToolbox: testCorrespondingRowsCols.m
+%
+% Purpose:
+%     - tests getCorrespondingRows and getCorrespondingCols
+%
+% Author:
+%     - CI integration: Laurent Heirendt
 
-if printLevel
-    display(S)
-end  
 
-if printLevel
-    display('--- row subset for getCorrespondingRows ----')
-end
+% save the current path
+currentDir = pwd;
 
-rowBool=false(size(S,1),1);
-colBool=false(size(S,2),1);
-rowBool(1:5)=1;
-colBool(1:3)=1;
+% initialize the test
+initTest(fileparts(which(mfilename)));
 
-if printLevel
-    display(S(rowBool,colBool))
-end
+% toy model stoichiometric matrix
+S = [-1,  0,  0,  0,  0;
+      2, -3,  0,  0,  0;
+      0,  4, -5,  0,  0;
+      0,  0,  6, -7,  0;
+      0,  0,  0,  0,  8];
 
+display(S);
+fprintf('--- row subset for getCorrespondingRows ----\n')
+
+rowBool = false(size(S, 1), 1);
+colBool = false(size(S, 2), 1);
+rowBool(1:5) = 1;
+colBool(1:3) = 1;
+
+display(S(rowBool, colBool))
+
+% test the exclusive mode
+mode = 'exclusive';
+fprintf('%s\n', mode);
+restrictedRowBool = getCorrespondingRows(S, rowBool, colBool, mode);
+
+display(S(restrictedRowBool, colBool));
+ref_rows_exclusive = [-1,  0,  0;
+                       2, -3,  0;
+                       0,  4, -5];
+assert(isequal(S(restrictedRowBool, colBool), ref_rows_exclusive));
+
+% test the inclusive mode
+mode = 'inclusive';
+fprintf('%s\n', mode);
+restrictedRowBool = getCorrespondingRows(S, rowBool, colBool, mode);
+
+display(S(restrictedRowBool, colBool));
+ref_rows_inclusive = [-1,  0,  0;
+                       2, -3,  0;
+                       0,  4, -5;
+                       0,  0,  6];
+assert(isequal(S(restrictedRowBool, colBool), ref_rows_inclusive));
+
+% test the partial mode
+mode = 'partial';
+fprintf('%s\n', mode);
+
+restrictedRowBool = getCorrespondingRows(S, rowBool, colBool, mode);
+
+display(S(restrictedRowBool,colBool));
+ref_rows_partial = [0, 0, 6];
+assert(isequal(S(restrictedRowBool, colBool), ref_rows_partial));
+
+fprintf('--- col subset for getCorrespondingCols ----\n');
+rowBool = false(size(S, 1), 1);
+colBool = false(size(S, 2), 1);
+rowBool(1:3) = 1;
+colBool(1:5) = 1;
+
+display(S(rowBool, colBool));
+
+% test the exclusive mode
 mode ='exclusive';
-if printLevel
-    display('exclusive')
-end
-restrictedRowBool = getCorrespondingRows(S,rowBool,colBool,mode);
+fprintf('%s\n', mode);
+restrictedColBool = getCorrespondingCols(S, rowBool, colBool, mode);
 
-if printLevel
-    display(S(restrictedRowBool,colBool))
-end
+display(S(rowBool, restrictedColBool));
+ref_cols_exclusive = [-1,  0;
+                       2, -3;
+                       0,  4];
+assert(isequal(S(rowBool, restrictedColBool), ref_cols_exclusive));
 
+% test the inclusive mode
 mode ='inclusive';
-if printLevel
-    display('inclusive')
-end
-restrictedRowBool = getCorrespondingRows(S,rowBool,colBool,mode);
+fprintf('%s\n', mode);
+restrictedColBool = getCorrespondingCols(S,rowBool,colBool,mode);
 
-if printLevel
-    display(S(restrictedRowBool,colBool))
-end
+display(S(rowBool,restrictedColBool));
+ref_cols_inclusive = [-1,  0,  0;
+                       2, -3,  0;
+                       0,  4, -5];
+assert(isequal(S(rowBool, restrictedColBool), ref_cols_inclusive));
 
+% test the partial mode
 mode ='partial';
-if printLevel
-    display('partial')
-end
-restrictedRowBool = getCorrespondingRows(S,rowBool,colBool,mode);
-
-if printLevel
-    display(S(restrictedRowBool,colBool))
-end
-
-
-if printLevel
-    display('--- col subset for getCorrespondingCols ----')
-end
-rowBool=false(size(S,1),1);
-colBool=false(size(S,2),1);
-rowBool(1:3)=1;
-colBool(1:5)=1;
-
-if printLevel
-    display(S(rowBool,colBool))
-end
-
-
-mode ='exclusive';
-if printLevel
-    display('exclusive')
-end
+fprintf('%s\n', mode);
 restrictedColBool = getCorrespondingCols(S,rowBool,colBool,mode);
 
-if printLevel
-    display(S(rowBool,restrictedColBool))
-end
+display(S(rowBool,restrictedColBool));
+ref_cols_partial = [0;
+                    0;
+                   -5];
+assert(isequal(S(rowBool, restrictedColBool), ref_cols_partial));
 
-mode ='inclusive';
-if printLevel
-    display('inclusive')
-end
-restrictedColBool = getCorrespondingCols(S,rowBool,colBool,mode);
-
-if printLevel
-    display(S(rowBool,restrictedColBool))
-end
-
-mode ='partial';
-if printLevel
-    display('partial')
-end
-restrictedColBool = getCorrespondingCols(S,rowBool,colBool,mode);
-
-if printLevel
-    display(S(rowBool,restrictedColBool))
-end
-
-bool=1;
+% change the directory
+cd(currentDir)
