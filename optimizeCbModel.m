f058495030f5334028cee3477260bb433638baec
Karthik Raman <karthik.raman@gmail.com>
2017-03-31 10:38:13 +0530

    fixed missed comment line

--- optimizeCbModel.m
+++ optimizeCbModel.m
@@ -80,7 +80,7 @@ function FBAsolution = optimizeCbModel(model,osenseStr, minNorm, allowLoops, zer
 %                           'lp-'      : L_p norm with p<0
 %                           'lp+'      : L_p norm with 0<p<1
 %                           'all'      : try all approximations and return the best result
-
+%
 %OUTPUT
 % FBAsolution
 %   f         Objective value
