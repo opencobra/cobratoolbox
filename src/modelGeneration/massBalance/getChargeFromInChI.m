fb4a8d46bb00d019b6dff71ff4fa7c40f1d3e26a
Thomas Pfau <thomas.pfau@uni.lu>
2017-05-02 12:31:47 +0200

    Updated the InChI interpretation functions and added a test

--- src/modelGeneration/massBalance/getChargeFromInChI.m
+++ src/modelGeneration/massBalance/getChargeFromInChI.m
@@ -8,89 +8,122 @@ function charge=getChargeFromInChI(InChI)
 % charge
 %
 % Ronan Fleming 23 Sept 09
+% Updated May 2017 Thomas Pfau
 
-k = strfind(InChI, '/q');
-if isempty(k)
-    charge=0;
-else
-    %disp(InChI)
-    %check if it has a composite formula
-    indDots=findstr('.',getFormulaFromInChI(InChI));
-    if isempty(indDots)
-        if strcmp(InChI(k+2),'+')
-            %positive charge
-            sgn=1;
-        else
-            sgn=-1;
-        end
-        if length(InChI)<k+4
-            charge=sgn*str2num(InChI(k+3:k+3));
-        else
-            if strcmp(InChI(k+4),'/')
-                charge=sgn*str2num(InChI(k+3:k+3));
-            else
-                if length(InChI)<k+5
-                    charge=sgn*str2num(InChI(k+3:k+4));
-                else
-                    if strcmp(InChI(k+5),'/')
-                        charge=sgn*str2num(InChI(k+3:k+4));
-                    else
-                        disp(InChI)
-                        error('Charge too high')
-                    end
-                end
-            end
-        end
-    else
-        %todo - cleanup, this code is a bit messy but seems to work
-        totalCharge=0;
-        for d=1:length(indDots)+1
-            while  strcmp(InChI(k+2),';')
-                k=k+1;
-            end
-            if strcmp(InChI(k+2),'+') || strcmp(InChI(k+2),'-')
-                if strcmp(InChI(k+2),'+')
-                    %positive charge
-                    sgn=1;
-                else
-                    sgn=-1;
-                end
-            else
-                if strcmp(InChI(k+2),'/')
-                    break;
-                else
-                    disp(InChI)
-                    sgn=0;
-                    warning(['Not valid charge: ' InChI(k+2)])
-                    break;
-                    %error(InChI(k+2))
-                end
-            end
-            if length(InChI)<k+4
-                charge=sgn*str2num(InChI(k+3:k+3));
-                k=k+2;
-            else
-                if strcmp(InChI(k+4),'/') || strcmp(InChI(k+4),';')
-                    charge=sgn*str2num(InChI(k+3:k+3));
-                    k=k+2;
-                else
-                    if length(InChI)<k+5 
-                        charge=sgn*str2num(InChI(k+3:k+4));
-                        k=k+3;
-                    else
-                        if strcmp(InChI(k+5),'/')  || strcmp(InChI(k+5),';')
-                            charge=sgn*str2num(InChI(k+3:k+4));
-                            k=k+3;
-                        else
-                            disp(InChI)
-                            error('Charge too high')
-                        end
-                    end
-                end
-            end
-            d=d+1;
-            totalCharge=totalCharge+charge;
-        end
-        charge=totalCharge;
-    end
+%InChI Charge is defined in the charge layer and can be modified in the
+%proton layer. If nothing is defined, the compound is uncharged
+
+%First: Discard any "Reconnected" parts, as those don't influence the
+%charges
+InChI = regexprep(InChI,'/r.*','');
+
+%Charge Layer: (either at the end or at the start)
+q_layer = regexp(InChI,'/q(.*?)/|/q(.*?)$','tokens');
+%proton layer
+p_layer = regexp(InChI,'/p(.*?)/|/p(.*?)$','tokens');
+
+charge = 0;
+
+if ~isempty(q_layer)
+    %Get individual charges from splitted reactions. 
+    individualCharges = cellfun(@(x) {strsplit(x{1},';')},q_layer);
+    %And calculate the charge by evaluating the individual components.
+    charge = cellfun(@(x) sum(cellfun(@(y) eval(y) , x)), individualCharges);    
 end
+
+proton_charges = 0;
+if ~isempty(p_layer)
+    individualProtons = cellfun(@(x) {strsplit(x{1},';')},p_layer);
+    proton_charges = cellfun(@(x) sum(cellfun(@(y) eval(y) , x)), individualProtons);
+end
+charge = proton_charges + sum(charge);
+
+
+% k = strfind(InChI, '/q');
+% if isempty(k)
+%     charge=0;
+% else
+%     %disp(InChI)
+%     %check if it has a composite formula
+%     indDots=findstr('.',getFormulaFromInChI(InChI));
+%     if isempty(indDots)
+%         if strcmp(InChI(k+2),'+')
+%             %positive charge
+%             sgn=1;
+%         else
+%             sgn=-1;
+%         end
+%         if length(InChI)<k+4
+%             charge=sgn*str2num(InChI(k+3:k+3));
+%         else
+%             if strcmp(InChI(k+4),'/')
+%                 charge=sgn*str2num(InChI(k+3:k+3));
+%             else
+%                 if length(InChI)<k+5
+%                     charge=sgn*str2num(InChI(k+3:k+4));
+%                 else
+%                     if strcmp(InChI(k+5),'/')
+%                         charge=sgn*str2num(InChI(k+3:k+4));
+%                     else
+%                         disp(InChI)
+%                         error('Charge too high')
+%                     end
+%                 end
+%             end
+%         end
+%     else
+%         %todo - cleanup, this code is a bit messy but seems to work
+%         totalCharge=0;
+%         for d=1:length(indDots)+1
+%             while  strcmp(InChI(k+2),';')
+%                 k=k+1;
+%             end
+%             if strcmp(InChI(k+2),'+') || strcmp(InChI(k+2),'-')
+%                 if strcmp(InChI(k+2),'+')
+%                     %positive charge
+%                     sgn=1;
+%                 else
+%                     sgn=-1;
+%                 end
+%             else
+%                 if strcmp(InChI(k+2),'/')
+%                     break;
+%                 else
+%                     disp(InChI)
+%                     sgn=0;
+%                     warning(['Not valid charge: ' InChI(k+2)])
+%                     break;
+%                     %error(InChI(k+2))
+%                 end
+%             end
+%             if length(InChI)<k+4
+%                 charge=sgn*str2num(InChI(k+3:k+3));
+%                 k=k+2;
+%             else
+%                 if strcmp(InChI(k+4),'/') || strcmp(InChI(k+4),';')
+%                     charge=sgn*str2num(InChI(k+3:k+3));
+%                     k=k+2;
+%                 else
+%                     if length(InChI)<k+5 
+%                         charge=sgn*str2num(InChI(k+3:k+4));
+%                         k=k+3;
+%                     else
+%                         if strcmp(InChI(k+5),'/')  || strcmp(InChI(k+5),';')
+%                             charge=sgn*str2num(InChI(k+3:k+4));
+%                             k=k+3;
+%                         else
+%                             disp(InChI)
+%                             error('Charge too high')
+%                         end
+%                     end
+%                 end
+%             end
+%             d=d+1;
+%             totalCharge=totalCharge+charge;
+%         end
+%         charge=totalCharge;
+%     end
+% end
+
+%Also look at the proton layer (removed protons, change the charge as well)
+
--- src/modelGeneration/massBalance/getFormulaFromInChI.m
+++ src/modelGeneration/massBalance/getFormulaFromInChI.m
@@ -2,3 +2,61 @@ function formula=getFormulaFromInChI(InChI)
 % extract formula from InChI
 [token,rem] = strtok(InChI, '/');
 formula=strtok(rem, '/');
+%This could be a composite formula, so combine it.
+tokens = strsplit(formula,'.');
+
+%The protonation state can also modify the formula! To get it, we remove
+%any reconstruction fields, as they do not influence it.
+InChI = regexprep(InChI,'/r.*','');
+p_layer = regexp(InChI,'/p(.*?)/|/p(.*?)$','tokens');
+
+if ~isempty(p_layer)
+    individualProtons = cellfun(@(x) {strsplit(x{1},';')},p_layer);
+    addedProtons = cellfun(@(x) sum(cellfun(@(y) eval(y) , x)), individualProtons);
+end
+
+
+%Calc the coefs for all formulas
+if (numel(tokens) > 1) || (~isempty(regexp(formula,'(^[0-9]+)'))) || (~isempty(p_layer))
+    CoefLists = cellfun(@(x) calcFormula(x), tokens,'UniformOutput',0);
+    if exist('addedProtons','var')
+        CoefLists = [CoefLists;{{'H';addedProtons}}];
+    end
+    %and now, combine them.
+    Elements = {};
+    Coefficients = [];
+    for i = 1:numel(CoefLists)
+        currentForm = CoefLists{i};
+        Elements = [Elements,setdiff(currentForm(1,:),Elements)];
+        current_coefs = cell2mat(currentForm(2,:));
+        [A,B] = ismember(Elements,currentForm(1,:));
+        %Extend the coefficients if necessary 
+        Coefficients(end+1:numel(Elements)) = 0;
+        Coefficients(A) = Coefficients(A)+current_coefs;        
+    end    
+        
+    Coefs = num2cell(Coefficients);
+    Coefs(cellfun(@(x) x == 1, Coefs)) = {[]};
+    Coefs = cellfun(@(x) num2str(x) , Coefs,'UniformOutput',0);
+    formula = strjoin([Elements , {''}],Coefs);    
+end
+
+    
+end
+
+
+function [CoefList] = calcFormula(Formula)
+multiplier = 1;
+isReplicated = regexp(Formula,'(^[0-9]+)','tokens');
+ElementTokens = regexp(Formula,'([A-Z][a-z]?)([0-9]*)','tokens');    
+Elements = cellfun(@(x) x{1}, ElementTokens,'UniformOutput',0);
+Coefs = cellfun(@(x) str2num(x{2}), ElementTokens,'UniformOutput',0);
+Coefs(cellfun(@isempty, Coefs)) = {1};
+
+if ~isempty(isReplicated)
+    multiplier = str2num(isReplicated{1}{1});
+    Coefs = cellfun(@(x) x*multiplier, Coefs,'UniformOutput',0);
+end
+
+CoefList = [Elements;Coefs];
+end
