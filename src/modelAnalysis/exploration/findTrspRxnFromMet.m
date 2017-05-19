98f8ee72d880e1d5f8f5d4a494e9fad6b57c8a95
Anne <arichelle@ucsd.edu>
2017-05-18 17:14:54 -0700

    new function to find transport reactions for a defined metabolite

---
+++ src/modelAnalysis/exploration/findTrspRxnFromMet.m
@@ -0,0 +1,54 @@
+function [TrspRxns] = findTrspRxnFromMet(model, metList, compFlag)
+% Find transport reactions for defined metabolites. Option available to
+% define the compartment of the transport
+%
+% USAGE:
+%
+%    [TrspRxns] = findTrspRxnFromMet(model, met)
+%
+% INPUT:
+%    model            COBRA model structure
+%    metList          metabolites list
+% OPTIONAL INPUTS:
+%    compFlag         compartment of the transport (e.g. 'c',cytosol)
+%
+% OUTPUTS:
+%    TrspRxns         List of transporters reactions
+
+%
+% .. Author: - Anne Richelle May 2017
+
+
+if nargin < 3
+    compFlag={};
+end
+    
+TrspRxns={};
+for i=1:numel(metList)
+    
+    formulas={};
+    Name_met=metList{i};
+    
+    % Find the reactions involving the metabolite
+    rxnsMets=findRxnsFromMets(model,Name_met);
+    formulas = printRxnFormula(model,rxnsMets,false);
+    
+    for j=1:numel(formulas) 
+        metaboliteList = parseRxnFormula(formulas{j});
+        [baseMetNames,compSymbols,uniqueMetNames,uniqueCompSymbols] = parseMetNames(metaboliteList);
+        
+        if sum(strcmp(baseMetNames,[Name_met(1:end-3)]))== 2 && sum(strcmp(uniqueMetNames,[Name_met(1:end-3)]))== 1
+            if ~isempty(compFlag)
+                if sum(strcmp(uniqueCompSymbols,compFlag))== 1
+                    TrspRxns(end+1)=rxnsMets(j);
+                end
+            else
+                TrspRxns(end+1)=rxnsMets(j);
+            end
+        end
+    end
+end
+
+
+
+
