78e83aa8bc15cde8e12b8dfa108aafdd35a453c7
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-05-23 11:49:38 +0200

    Documentation formatting

--- src/dataIntegration/transcriptomics/MBA/MBA.m
+++ src/dataIntegration/transcriptomics/MBA/MBA.m
@@ -1,64 +1,66 @@
 function tissueModel = MBA(model, medium_set, high_set, tol, core)
-% Use the MBA algorithm ('Jerby et al., 2010') to extract a context
+% Uses the MBA algorithm (`Jerby et al., 2010`) to extract a context
 % specific model using data. MBA algorithm defines high-confidence reactions
 % to ensure activity in the extracted model. Medium confidence reactions
 % are only kept when a certain parsimony trade-off is met. In random order,
 % the algorithm prunes other reactions and removes them if not required to
 % support high- or medium- confidence reactions.
 %
-% INPUTS
+% USAGE:
 %
-%   model               input model (COBRA model structure)
-%   medium_set          list of reactions with medium confidence
-%   high_set            list of reactions with high confidence
+%    tissueModel = MBA(model, medium_set, high_set, tol, core)
 %
-% OPTIONAL INPUTS
-%   tol                 minimum flux threshold for "expressed" reactions
-%                       (default 1e-8)
-%   core                cell with reaction names (strings) that are manually put in
-%                       the high confidence core
+% INPUTS:
+%    model:          input model (COBRA model structure)
+%    medium_set:     list of reactions with medium confidence
+%    high_set:       list of reactions with high confidence
 %
-% OUTPUTS
+% OPTIONAL INPUTS:
+%    tol:            minimum flux threshold for "expressed" reactions
+%                    (default 1e-8)
+%    core:           cell with reaction names (strings) that are manually put in
+%                    the high confidence core
 %
-%   tissueModel         extracted model
+% OUTPUT:
+%    tissueModel:    extracted model
 %
-% 'Jerby et al. (201)). Computational reconstruction of tissue-specific
+% `Jerby et al. (201)). Computational reconstruction of tissue-specific
 % metabolic models: application to human liver metabolism. Mol. Syst. Biol.
-% 6, 401.'
+% 6, 401.`
 %
 % .. Author: - Commented by A. Richelle, May 2017.
-%
+
 if nargin < 4 || isempty(tol)
     tol = 1e-8;
 end
 if nargin < 5 || isempty(core)
     core={};
 end
-    
+
     if ~isempty (core)
         high_set= union(high_set,core);
     end
     NC = setdiff(model.rxns,union(high_set,medium_set));
-    
+
     %MBA
     PM = model;
     removed = {};
-    
+
     param.epsilon=tol;
     param.modeFlag=0;
     param.method='fastcc';
-    
+
     while ~isempty(NC)
         ri = randi([1,numel(NC)],1);
-        r = NC{ri};       
-            
+        r = NC{ri};
+
         PM = removeRxns(PM, r);
         [fluxConsistentMetBool,fluxConsistentRxnBool] = findFluxConsistentSubset(PM,param);
         inactive=PM.rxns(fluxConsistentRxnBool==0);
         eH = intersect(inactive, high_set);
         eM = intersect(inactive, medium_set);
         eX = setdiff(inactive,union(high_set,medium_set));
-        
+
         if numel(eH)==0 && numel(eM) < epsil*numel(eX)
             PM = removeRxns(PM, inactive);
             NC = setdiff(NC,inactive);
