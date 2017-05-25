ba7c737bc5bb8284cea0888be346862aed862676
laurentheirendt <laurent.heirendt@uni.lu>
2017-05-25 16:36:51 +0200

    UNIX compatible version of tutorial II

--- tutorials/metabotools/tutorial_II/runTutorial_II.m
+++ tutorials/metabotools/tutorial_II/runTutorial_II.m
@@ -1,4 +1,4 @@
-% %% Tutorial II - Integration of quantitative metabolomic data 
+% %% Tutorial II - Integration of quantitative metabolomic data
 
 %% Start of the tutorial
 clear
@@ -8,9 +8,8 @@ tol =  1e-6;
 %%% specify your own solver
 solver = '...ADD YOUR SOLVER...'; % can be gurobi or tomlab_cplex
 solverQuant = 'cplex_direct'; % can only be cplex_direct
-path = ' ... ADD YOUR PATH...\'; % output is saved to this location, can be the same as pathToCOBRA 'C: ... \cobratoolbox\metabotools\tutorial_II\';
+pathSaveLocation = ' ... ADD YOUR PATH...\'; % output is saved to this location, can be the same as pathToCOBRA 'C: ... \cobratoolbox\metabotools\tutorial_II\';
 pathToCOBRA =  '.. ADD YOUR PATH TO cobratoolbox...'; % to load tutorial input files ,; e.g., pathToCOBRA = 'D:\..\Documents\GitHub\'
-
 
 %% set and check solver
 solverQOK = changeCobraSolver(solverQuant,'LP');
@@ -38,17 +37,17 @@ end
 
 
 %% load and check tutorial input is loaded correctly
-if isequal(exist([pathToCOBRA '\cobratoolbox\tutorials\metabotools\tutorial_II\starting_model.mat'],'file'),2)% 2 means it's a file.
+if isequal(exist([pathToCOBRA filesep 'cobratoolbox' filesep 'tutorials' filesep 'metabotools' filesep 'tutorial_II' filesep 'starting_model.mat'], 'file'),2)% 2 means it's a file.
     % We have a file!
-    load([pathToCOBRA '\cobratoolbox\tutorials\metabotools\tutorial_II\starting_model.mat']);
+    load([pathToCOBRA filesep 'cobratoolbox' filesep 'tutorials' filesep 'metabotools' filesep 'tutorial_II' filesep 'starting_model.mat']);
     display('The model is loaded.');
-   
-else 
+
+else
     display('Error in pathToCOBRA: The starting_model and tutorial_II_data could not be loaded. Does pathToCOBRA contain typos or additional '\'? ');
-end 
+end
 
 % Check output path and writing permission
-if ~exist(path)==7
+if ~exist(pathSaveLocation)==7
     display('Error in path: Does pathToCOBRA contain typos or additional '\' ?');
 end
 
@@ -57,7 +56,8 @@ display('Warning: If a red matlab error message appears after this message, file
 
 % make and save dummy file to test writing to output directory
 A = rand(1);
-save([path '\' 'A']);
+[pathSaveLocation filesep 'A.mat']
+save([pathSaveLocation filesep 'A.mat'], 'A');
 
 
 display('Continue with tutorial if solver and paths are set correctly.');
@@ -87,29 +87,29 @@ close_exchanges = 0;
 [modelMedium,basisMedium] = setMediumConstraints(starting_model, set_inf, current_inf, medium_composition, met_Conc_mM, cellConc, t, cellWeight, mediumCompounds, mediumCompounds_lb, customizedConstraints, customizedConstraints_ub, customizedConstraints_lb, close_exchanges);
 
 
-clearvars -EXCEPT modelMedium tol solver path solverQuant
+clearvars -EXCEPT pathSaveLocation pathToCOBRA modelMedium tol solver solverQuant
 
 %% Section 2 - Generate an individual exchange profiles for each sample
 
-load([pathToCOBRA '\cobratoolbox\tutorials\metabotools\tutorial_II\tutorial_II_data']);
+[pathToCOBRA filesep 'cobratoolbox' filesep 'tutorials' filesep 'metabotools' filesep 'tutorial_II' filesep 'tutorial_II_data']
+
+load([pathToCOBRA filesep 'cobratoolbox' filesep 'tutorials' filesep 'metabotools' filesep 'tutorial_II' filesep 'tutorial_II_data']);
 model = modelMedium;
 test_max = 500;
 test_min = 0.0001;
 variation = 20;
 
-prepIntegrationQuant(model,metData,exchanges,samples,test_max,test_min,path,tol,variation);
+prepIntegrationQuant(model,metData,exchanges,samples,test_max,test_min,pathSaveLocation,tol,variation);
 
-clearvars -EXCEPT modelMedium samples tol solver path solverQuant
-
-
+clearvars -EXCEPT modelMedium samples tol solver pathSaveLocation solverQuant pathToCOBRA
 
 %% Section 2B - Prepare table to check exchange profiles
 nmets = 70;
-[mapped_exchanges, minMax,mapped_uptake, mapped_secretion] = checkExchangeProfiles(samples, path ,nmets);
+[mapped_exchanges, minMax,mapped_uptake, mapped_secretion] = checkExchangeProfiles(samples, pathSaveLocation ,nmets);
 
-clearvars -EXCEPT modelMedium samples tol solver mapped_exchanges  path solverQuant
+clearvars -EXCEPT pathSaveLocation pathToCOBRA modelMedium samples tol solver mapped_exchanges solverQuant
 
-save ([path 'Result_checkExchangeProfiles']);
+save ([pathSaveLocation filesep 'Result_checkExchangeProfiles']);
 
 
 %% Section 3 - Generate contextualized models
@@ -127,32 +127,31 @@ epsilon = 1e-4;
 
 addExtraExch = {'EX_tdchola(e)','Ex_5hoxindoa[e]'};
 addExtraExch_value = 1;
-[ResultsAllCellLines,OverViewResults] = setQuantConstraints(model,samples,tol,minGrowth,obj,no_secretion,no_uptake,medium,addExtraExch,addExtraExch_value,path,epsilon);
-clearvars -EXCEPT modelMedium samples ResultsAllCellLines OverViewResults tol solver mapped_exchanges path
+[ResultsAllCellLines,OverViewResults] = setQuantConstraints(model,samples,tol,minGrowth,obj,no_secretion,no_uptake,medium,addExtraExch,addExtraExch_value,pathSaveLocation);
+clearvars -EXCEPT pathSaveLocation pathToCOBRA modelMedium samples ResultsAllCellLines OverViewResults tol solver mapped_exchanges
 
 %% Section 4 - Analyze added exchanges
 
 changeCobraSolver(solver,'LP');
 
 [Ex_added_all_unique] = statisticsAddedExchanges(ResultsAllCellLines,samples);
-clearvars -EXCEPT modelMedium samples ResultsAllCellLines OverViewResults Ex_added_all_unique tol solver mapped_exchanges path
+clearvars -EXCEPT modelMedium samples ResultsAllCellLines OverViewResults Ex_added_all_unique tol solver mapped_exchanges pathSaveLocation pathToCOBRA
 
 
 [Added_all] = mkTableOfAddedExchanges(ResultsAllCellLines,samples,Ex_added_all_unique);
 
-save([path 'statistics']);
+save([pathSaveLocation filesep 'statistics']);
 
-clearvars -EXCEPT modelMedium samples ResultsAllCellLines OverViewResults tol solver mapped_exchanges path
-
+clearvars -EXCEPT pathSaveLocation pathToCOBRA modelMedium samples ResultsAllCellLines OverViewResults tol solver mapped_exchanges
 
 
 %% Section 5 - Analyze the sets of essential genes
 
 cutoff= 0.05;
 
-[genes,ResultsAllCellLines,OverViewResults] = analyzeSingleGeneDeletion(ResultsAllCellLines, path, samples, cutoff, OverViewResults);
+[genes,ResultsAllCellLines,OverViewResults] = analyzeSingleGeneDeletion(ResultsAllCellLines, pathSaveLocation, samples, cutoff, OverViewResults);
 
-clearvars -EXCEPT modelMedium samples ResultsAllCellLines OverViewResults Ex_added_all_unique genes tol solver mapped_exchanges  path
+clearvars -EXCEPT pathSaveLocation pathToCOBRA modelMedium samples ResultsAllCellLines OverViewResults Ex_added_all_unique genes tol solver mapped_exchanges
 
 
 %% Section 6 - Check which individual gene-associated reaction makes the model infeasible
@@ -163,7 +162,7 @@ genes_to_test = {'55293.1'};
 
 [FBA_Rxns_KO,ListResults] = checkEffectRxnKO(samples_to_test,fill,genes_to_test,samples,ResultsAllCellLines);
 
-clearvars -EXCEPT modelMedium samples ResultsAllCellLines OverViewResults Ex_added_all_unique genes FBA_Rxns_KO ListResults tol solver mapped_exchanges  path
+clearvars -EXCEPT pathSaveLocation pathToCOBRA modelMedium samples ResultsAllCellLines OverViewResults Ex_added_all_unique genes FBA_Rxns_KO ListResults tol solver mapped_exchanges
 
 
 %% Section 7 - Make intersect and union model
@@ -171,13 +170,13 @@ clearvars -EXCEPT modelMedium samples ResultsAllCellLines OverViewResults Ex_add
 mk_union = 1;
 mk_intersect = 1;
 mk_reactionDiff = 1;
-load([pathToCOBRA '\cobratoolbox\tutorials\metabotools\tutorial_II\starting_model']);
+load([pathToCOBRA filesep 'cobratoolbox' filesep 'tutorials' filesep 'metabotools' filesep 'tutorial_II' filesep 'starting_model']);
 model = starting_model;
 
 [unionModel,intersectModel, diffRxns, diffExRxns] = makeSummaryModels(ResultsAllCellLines,samples, model, mk_union, mk_intersect, mk_reactionDiff);
-clearvars -EXCEPT modelMedium samples ResultsAllCellLines OverViewResults Ex_added_all_unique genes FBA_Rxns_KO ListResults unionModel intersectModel  diffRxns diffExRxns tol solver mapped_exchanges  path model
+clearvars -EXCEPT pathSaveLocation pathToCOBRA modelMedium samples ResultsAllCellLines OverViewResults Ex_added_all_unique genes FBA_Rxns_KO ListResults unionModel intersectModel  diffRxns diffExRxns tol solver mapped_exchanges model
 
-save([path 'summary']);
+save([pathSaveLocation filesep 'summary']);
 
 
 %% Section 8 - Predict differences in metabolite production or consumption
@@ -189,7 +188,7 @@ dir = 1;
 
 % ATP production
 % exclude transport reactions from flux split analysis
-transportRxns = {'ATPtm' 
+transportRxns = {'ATPtm'
     'ATPtn'
     'ATPtx'
     'ATP1ter'
@@ -226,10 +225,7 @@ PHs =[samples maximum_contributing_rxn(:,1)];
 maximum_contributing_flux_ATP = maximum_contributing_flux;
 
 clear ATPprod transportRxns met2test maximum_contributing_rxn
- 
- 
- 
- 
+
 %% Section 8B NADH production
 met2test ={'nadh[c]','nadh[m]','nadh[n]','nadh[x]', 'nadh[r]'};
 
@@ -243,7 +239,7 @@ transportRxns = {'NADHtpu'
 PHs =[PHs maximum_contributing_rxn(:,1)];
 
 
-clear transportRxns met2test  maximum_contributing_rxn
+clear transportRxns met2test maximum_contributing_rxn
 
 
 %% Section 8C FADH2 production
@@ -269,20 +265,20 @@ transportRxns = {'NADPHtru'
 
 met2test ={'nadph[c]','nadph[m]','nadph[n]','nadph[x]', 'nadph[r]'};
 [BMall,ResultsAllCellLines,metRsall, maximum_contributing_rxn, maximum_contributing_flux_NADPH] = predictFluxSplits(model, obj, met2test,samples,ResultsAllCellLines, dir, transportRxns);
-clear transportRxns met2test 
+clear transportRxns met2test
 
 PHs =[PHs maximum_contributing_rxn(:,1)];
 
-save([path 'fluxSplits']);
+save([pathSaveLocation filesep 'fluxSplits']);
 
-%% Section 8E illustrate the phenotypes (PHs) on 3Dplot 
+%% Section 8E illustrate the phenotypes (PHs) on 3Dplot
 
 diff_view = 1;
 fonts = 18;
 
-make3Dplot(PHs, maximum_contributing_flux_ATP, fonts, path, diff_view);
+make3Dplot(PHs, maximum_contributing_flux_ATP, fonts, pathSaveLocation, diff_view);
 
- 
+
 %% Section 9 Perform phase Plane Analysis
 
 mets ={'EX_glc(e)','EX_o2(e)';'EX_gln_L(e)','EX_o2(e)';'EX_lac_L(e)','EX_o2(e)'};
@@ -292,7 +288,7 @@ direct = [-1,-1;-1,-1;1,-1];
 
 [ResultsAllCellLines] = performPPP(ResultsAllCellLines,mets,step_size,samples,step_num,direct);
 
-save([path 'PPP']);
+save([pathSaveLocation filesep 'PPP']);
 
 
 
@@ -302,7 +298,4 @@ label = {'Glucose uptake (fmol/cell/hr)'; 'Oxygen uptake (fmol/cell/hr)';'Growth
 mets ={'EX_glc(e)';'EX_o2(e)'};
 fonts = 12;
 samples = {'IGROV1'};
-illustrate_ppp(ResultsAllCellLines,mets,path,samples,label,fonts,tol);
-
-
-
+illustrate_ppp(ResultsAllCellLines,mets,pathSaveLocation,samples,label,fonts,tol);
