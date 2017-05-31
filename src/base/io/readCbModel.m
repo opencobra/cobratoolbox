92f1c89a457abb6307e8c0542bec9374d637c79b
Thomas Pfau <thomas.pfau@uni.lu>
2017-05-31 11:17:19 +0200

    fixed bug introducing additonal .

--- src/base/io/readCbModel.m
+++ src/base/io/readCbModel.m
@@ -182,7 +182,7 @@ end
 
 if isempty(modelDescription)
     [~,mfile,mextension] = fileparts(fileName);
-    modelDescription = [mfile '.' mextension];
+    modelDescription = [mfile mextension];
 end
 
 switch fileType
