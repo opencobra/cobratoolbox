c411a7b17aa43ea38ac985bde8e84ddabf69cec4
Thomas Pfau <thomas.pfau@uni.lu>
2017-06-08 07:54:09 +0200

    WIP

--- src/reconstruction/refinement/convertToIrreversible.m
+++ src/reconstruction/refinement/convertToIrreversible.m
@@ -1,4 +1,4 @@
-function [modelIrrev, matchRev, rev2irrev, irrev2rev] = convertToIrreversible(model, sRxns)
+function [modelIrrev, matchRev, rev2irrev, irrev2rev] = convertToIrreversible(model, sRxns, leaveInverse)
 % Converts model to irreversible format, either for the entire model or for
 % a defined list of reversible reactions.
 %
@@ -10,20 +10,23 @@ function [modelIrrev, matchRev, rev2irrev, irrev2rev] = convertToIrreversible(mo
 %    model:         COBRA model structure
 %
 % OPTIONAL INPUTS:
-%    sRxns          List of specific reversible reactions to convert to
+%    sRxns:         List of specific reversible reactions to convert to
 %                   irreversible (Default = model.rxns)
+%    leaveInverse:  Do not alter reactions that can only carry negative
+%                   flux, normally, those are flipped and marked with '_r'
 % OUTPUTS:
 %    modelIrrev:    Model in irreversible format
 %    matchRev:      Matching of forward and backward reactions of a reversible reaction
 %    rev2irrev:     Matching from reversible to irreversible reactions
 %    irrev2rev:     Matching from irreversible to reversible reactions
 %
-% Uses the reversible list to construct a new model with reversible
-% reactions separated into forward and backward reactions.  Separated
-% reactions are appended with '_f' and '_b' and the reversible list tracks
-% these changes with a '1' corresponding to separated forward reactions.
-% Reactions entirely in the negative direction will be reversed and
-% appended with '_r'.
+% NOTE:
+%   Uses the reversible list to construct a new model with reversible
+%   reactions separated into forward and backward reactions.  Separated
+%   reactions are appended with '_f' and '_b' and the reversible list tracks
+%   these changes with a '1' corresponding to separated forward reactions.
+%   Reactions entirely in the negative direction will be reversed and
+%   appended with '_r'.
 %
 % .. Authors:
 %       - written by Gregory Hannum 7/9/05
@@ -40,25 +43,25 @@ end
 % %first, get the reversible reactions, which should be converted
 % % Note: reactions which can only carry negative flux, will have an inactive
 % % forward reaction.
-% relReacs = ismember(model.rxns,sRxns) & model.lb < 0;
-% nRevRxns = sum(relReacs);
-% nRxns = numel(model.rxns);
-% rxnIDs = 1:nRxns;
-% irrevRxnIDs = nRxns + (1:nRevRxns);
-% 
-% 
-% %teat special fields: S, lb, ub, rxns
-% model.S(:,end+1:end+nRevRxns) = -model.S(:,relReacs);
-% model.lb(relReacs) = max(0,model.lb(relReacs));
-% model.ub(relReacs) = max(0,model.ub(relReacs));
-% model.lb(end+1,end+nRevRxns) = max(0,-model.lb(relReacs));
-% model.ub(end+1,end+nRevRxns) = max(0,-model.ub(relReacs));
-% model.c(end+1,end+nRevRxns) = model.c(relReacs);
-% RelReacNames = model.rxns(relReacs);
-% model.rxns(relReacs) = strcat(RelReacNames,'_f');
-% model.rxns(end+1,end+nRevRxns) = strcat(RelReacNames,'_b');
-% model.rxns(end+1,end+nRevRxns) = strcat(RelReacNames,'_b');
+relReacs = ismember(model.rxns,sRxns) & model.lb < 0 & model.ub > 0;
+nRevRxns = sum(relReacs);
+nRxns = numel(model.rxns);
+rxnIDs = 1:nRxns;
+irrevRxnIDs = nRxns + (1:nRevRxns);
 
+
+%teat special fields: S, lb, ub, rxns
+model.S(:,end+1:end+nRevRxns) = -model.S(:,relReacs);
+model.lb(relReacs) = max(0,model.lb(relReacs));
+model.ub(relReacs) = max(0,model.ub(relReacs));
+model.lb(end+1,end+nRevRxns) = max(0,-model.lb(relReacs));
+model.ub(end+1,end+nRevRxns) = max(0,-model.ub(relReacs));
+model.c(end+1,end+nRevRxns) = model.c(relReacs);
+RelReacNames = model.rxns(relReacs);
+model.rxns(relReacs) = strcat(RelReacNames,'_f');
+model.rxns(end+1,end+nRevRxns) = strcat(RelReacNames,'_b');
+model.rxns(end+1,end+nRevRxns) = strcat(RelReacNames,'_b');
+model.S(
 
 
 
