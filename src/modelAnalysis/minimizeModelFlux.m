d1faa96239357fb827e8debfa960b5ed12a3c39d
Sylvain <sylvain.arreckx@gmail.com>
2017-06-12 20:12:34 +0200

    changes in MathJax formulae

--- src/modelAnalysis/minimizeModelFlux.m
+++ src/modelAnalysis/minimizeModelFlux.m
@@ -13,45 +13,55 @@ function [MinimizedFlux modelIrrev]= minimizeModelFlux(model, osenseStr, minNorm
 %    minNorm:           {(0), 'one', 'zero', > 0 , n x 1 vector}, where `[m,n]=size(S)`;
 %                       0 - Default, normal LP
 %                       'one'  Minimise the Taxicab Norm using LP.
-%                            .. math::
-%                                 min |v|
-%                                 s.t. S*v = b
-%                                 c'v = f
-%                                 lb <= v <= ub
+%
+%                       .. math::
+%
+%                          min  ~&~ |v| \\
+%                          s.t. ~&~ S v = b \\
+%                               ~&~ c^T v = f \\ 
+%                               ~&~ lb \leq v \leq ub
+%
 %                       A LP solver is required.
 %                       'zero' Minimize the cardinality (zero-norm) of v
-%                            .. math::
-%                                 min ||v||_0
-%                                 s.t. S*v = b
-%                                 c'v = f
-%                                 lb <= v <= ub
+%
+%                       .. math::
+%
+%                          min  ~&~ ||v||_0 \\
+%                          s.t. ~&~ S v = b \\
+%                               ~&~ c^T v = f \\
+%                               ~&~ lb \leq v \leq ub
 %                       The zero-norm is approximated by a non-convex approximation
 %                       Six approximations are available: capped-L1 norm, exponential function
 %                       logarithmic function, SCAD function, L_p norm with p<0, L_p norm with 0<p<1
 %                       Note : capped-L1, exponential and logarithmic function often give
 %                       the best result in term of sparsity.
 %
-%                        .. See "Le Thi et al., DC approximation approaches for sparse optimization,
-%                           European Journal of Operational Research, 2014"
-%                           http://dx.doi.org/10.1016/j.ejor.2014.11.031
-%                           A LP solver is required.
+%                       .. See "Le Thi et al., DC approximation approaches for sparse optimization,
+%                          European Journal of Operational Research, 2014"
+%                          http://dx.doi.org/10.1016/j.ejor.2014.11.031
+%                          A LP solver is required.
 %
 %                       The remaining options work only with a valid QP solver:
 %
 %                       > 0    Minimises the Euclidean Norm of internal fluxes.
 %                       Typically 1e-6 works well.
-%                            .. math::
-%                                 min ||v||
-%                                 s.t. S*v = b
-%                                 c'v = f
-%                                 lb <= v <= ub
+%
+%                       .. math::
+%
+%                          min  ~&~ ||v||      \\
+%                          s.t. ~&~ S v = b    \\
+%                               ~&~ c^T v = f  \\
+%                               ~&~ lb \leq v \leq ub
+%
 %                       `n` x 1   Forms the diagonal of positive definiate
 %                       matrix `F` in the quadratic program
-%                            .. math::
-%                                 min 0.5*v'*F*v
-%                                 st. S*v = b
-%                                 c'*v = f
-%                                 lb <= v <= ub
+%
+%                       .. math::
+%
+%                          min  ~&~ 0.5 v^T F v  \\
+%                          s.t. ~&~ S v = b      \\
+%                               ~&~ c^T v = f    \\
+%                              ~&~ lb \leq v \leq ub
 %
 % OUTPUTS:
 %   MinimizedFlux:      minimum flux possible through the netwok
