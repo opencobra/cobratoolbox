95171b2f0cb957d1140919a4e05855cc646e9807
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-06-26 23:15:58 +0200

    formated docstrings and style

--- src/modelAnalysis/topology/minimalCutSets.m
+++ src/modelAnalysis/topology/minimalCutSets.m
@@ -1,87 +1,73 @@
 function [output] = minimalCutSets(targets, varargin)
-%%%%%%%%%%%%%%%%%%%%%%%%% MinimialCutSet.m %%%%%%%%%%%%%%%%%%%%%%%%%%
-% This code can be used to compute minimal cut sets by the CNA software 
-% package.
-% ------------------------------------------------
-% --> Berge algorithm for calculation of (constrained)
-% minimal cut sets (= hypergraph transversal)
+% Computes minimal cut sets for paths/cycles/elementary modes with Berge algorithm
+% (equivalent to hypergraph transversal) by the CNA software package.
 %
-% Usage: output = minimialCutSets(targets, mcsmax, names, sets2save,
-% earlycheck)   
-% computes cutsets for paths/cycles/elementary modes with Berge algorithm
-% (equivalent to hypergraph transversal)
+% USAGE:
 %
-% INPUT              :
-% targets (mandatory): a binary matrix that row-wise contains the 
-%                      target paths/cycles/elementary modes; a '1' in
-%                      the i-th row and j-th column in targets indicates
-%                      participation of element (reaction) j in mode i;
-% 
-% mcsmax             : maximal size of cutsets to be calculated; must
-%                      be a value grater 0; Inf means no size limit 
-%                      (default: Inf)
+%    output = minimialCutSets(targets, varargin)
 %
-% names              : a char matrix; its rows are names corresponding
-%                      to the columns of 'targets'; used for diagnostics
-%                      in preprocessing (default:[]; the matrix is then
-%                      constructed with 'I1,',I2',....)
+% INPUT:
+%    targets (mandatory): a binary matrix that row-wise contains the
+%                         target paths/cycles/elementary modes; a '1' in
+%                         the i-th row and j-th column in targets indicates
+%                         participation of element (reaction) j in mode i;
 %
-% sets2save          : (default: []) struct array with sets of (desired)
-%                      modes/paths/cycles that should be preserved (not
-%                      be hit by the cut sets computed). Should have the
-%                      following fields:
+%    mcsmax:              maximal size of cutsets to be calculated; must
+%                         be a value grater 0; Inf means no size limit
+%                         (default: Inf)
 %
-%    (i)               sets2save(k).tabl2save = k-th matrix containing 
-%                      row-wise 'desired' sets (desired paths/cycles/
-%                      modes) that should not be hit by the cut sets
-%                      to be computed. be saved A '1' in the i-th row
-%                      and j-th column of sets2save(k) indicates 
-%                      participation of element j in mode i in set k
-%                      of desired modes. These matrics must have the 
-%                      same number of columns (reactions) as 'targets'.
+%    names:               a char matrix; its rows are names corresponding
+%                         to the columns of 'targets'; used for diagnostics
+%                         in preprocessing (default:[]; the matrix is then
+%                         constructed with 'I1,',I2',....)
 %
-%     (ii)             sets2save(k).min2save  = specifies the minimum
-%                      number of desired paths/cycles/modes in
-%                      sets2save(k).tabl2save that should not be
-%                      hit by the cut sets computed.
+%    sets2save:           (default: []) struct array with sets of (desired)
+%                         modes/paths/cycles that should be preserved (not
+%                         be hit by the cut sets computed). Should have the
+%                         following fields:
 %
-% earlycheck         : whether the test checking for the fulfillment
-%                      of constraints in sets2save should be caried 
-%                      out during (1) or after (0) computation 
-%                      of cut sets [default: 1; makes only sense 
-%                      in combination with sets2save] 
+%                         * sets2save(k).tabl2save = k-th matrix containing
+%                           row-wise 'desired' sets (desired paths/cycles/
+%                           modes) that should not be hit by the cut sets
+%                           to be computed. be saved A '1' in the i-th row
+%                           and j-th column of sets2save(k) indicates
+%                           participation of element j in mode i in set k
+%                           of desired modes. These matrics must have the
+%                           same number of columns (reactions) as 'targets'.
 %
+%                         * sets2save(k).min2save = specifies the minimum
+%                           number of desired paths/cycles/modes in
+%                           sets2save(k).tabl2save that should not be
+%                           hit by the cut sets computed.
 %
-% OUTPUT            :
+% earlycheck:             whether the test checking for the fulfillment
+%                         of constraints in sets2save should be caried
+%                         out during (1) or after (0) computation
+%                         of cut sets [default: 1; makes only sense
+%                         in combination with sets2save]
 %
-% cutsets           : matrix that contains the (constrained) cutsets
-%                     row-wise; a '1' means that the reaction
-%                     /interaction is part of the cutset, 0 means the
-%                      element/reaction/interaction is not involved.
-%                      Each cutset hits all modes stored in "targets"
-%                      while it does not hit at least 
-%                     "sets2save(k).min2save" many modes in
-%                     "sets2save(k).tabl2save" 
+% OUTPUT:
+%    cutsets:             matrix that contains the (constrained) cutsets
+%                         row-wise; a '1' means that the reaction
+%                         /interaction is part of the cutset, 0 means the
+%                         element/reaction/interaction is not involved.
+%                         Each cutset hits all modes stored in "targets"
+%                         while it does not hit at least
+%                         "sets2save(k).min2save" many modes in
+%                         "sets2save(k).tabl2save"
 %
-% Code by         : 
-%                      Susan Ghaderi, Systems Biochemistry Group, LCSB,
-%                      University of Luxembourg.
-%
-%
-% Last update     :
-%                       06.06.2017
-%
-% ==================================================================
+% .. Author: Susan Ghaderi, LCSB, 06.06.2017
+
 mcsmax = Inf;
 names = [];
 sets2save = [];
 earlycheck = 1;
 
-%% varargin checking 
-if numel(varargin) > 1 
+%% varargin checking
+if numel(varargin) > 1
     for i = 1:2:numel(varargin)
         key = varargin{i};
-        value = varargin{i+1};
+        value = varargin{i + 1};
         switch key
             case 'mcsmax'
                 mcsmax = value;
@@ -92,20 +78,16 @@ if numel(varargin) > 1
             case 'earlycheck'
                 earlycheck = value;
             otherwise
-                msg = sprintf('Unexpected key %s',key)
+                msg = sprintf('Unexpected key %s', key)
                 error(msg);
         end
 
     end
 end
 %%
-cutsets= CNAcomputeCutsets(targets, mcsmax, names,...
-                           sets2save, earlycheck)
+cutsets = CNAcomputeCutsets(targets, mcsmax, names, ...
+                            sets2save, earlycheck)
 
-%% OUTPUT : 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% OUTPUT :
 output.cutsets = cutsets;
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 end
--- src/modelAnalysis/topology/pathVectors.m
+++ src/modelAnalysis/topology/pathVectors.m
@@ -1,112 +1,101 @@
-
 function [output] = pathVectors(model, directory, varargin)
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% This code can be used to compute elementary mode and extreme pathway 
-% (convex basis) of an arbitrary COBRA model by the CNA software 
+% Computes elementary mode and extreme pathway
+% (convex basis) of an arbitrary COBRA model by the CNA software
 % package.
 %
-% INPUT           :
-% model           :    COBRA model
-% directory       :    A path that CNA model is going to be saved there
-% constraints     :    empty 
-%                      cnap.numr
-%                      many rows and up to 4 columns:
-%                   -  COLUMN1 specifies excluded/enforced reactions: if
-%                      (constraints(i,1)==0) then onlythose modes / rays 
-%                      / points will be computed that do not include 
+% INPUT:
+%    model:          COBRA model
+%    directory:      A path that CNA model is going to be saved there
+%    constraints:    empty
+%                    cnap.numr
+%                    many rows and up to 4 columns:
+%                    - COLUMN1 specifies excluded/enforced reactions: if
+%                      (constraints(i,1)==0) then onlythose modes / rays
+%                      / points will be computed that do not include
 %                      reaction i; constraints(i,1)~=0 and
-%                      constraints(i)~=NaN enforces reaction i, i.e. only  
+%                      constraints(i)~=NaN enforces reaction i, i.e. only
 %                      those modes / rays / points will be computed that
 %                      involve reaction i; for all other reactions choose
-%                      constraint(i,1)=NaN; several reactions may be 
+%                      constraint(i,1)=NaN; several reactions may be
 %                      suppressed/enforced simultaneously
-%                   -  COLUMN2: specifies lower boundaries for the 
+%                    - COLUMN2: specifies lower boundaries for the
 %                      reaction rates (choose NaN if none is active).
 %                      Note that zero boundaries (irreversibilities) are
 %                      better described by cnap.reacMin. In any case, the
-%                      lower boundary eventually considered will be zero 
+%                      lower boundary eventually considered will be zero
 %                      if cnap.reaMin(i)==0 and constraints(i,2)<0.
-%                   -  COLUMN3: specifies upper boundaries for the 
+%                    - COLUMN3: specifies upper boundaries for the
 %                      reaction rates (choose NaN if none is active)
-%                   -  COLUMN4: specifies equalities for the reaction 
-%                      rates (choose NaN if none is active). 
-% mexVersion      :    (default:4)
+%                    - COLUMN4: specifies equalities for the reaction
+%                      rates (choose NaN if none is active).
+% mexVersion:       (default:4)
 %                   1, CNA mex files,
 %                   2, Metatool mex files,
 %                   3, CNA and Metatool mex files
-%                   4, Marco Terzer's EFM tool  
+%                   4, Marco Terzer's EFM tool
 %
-% irrevFlag      :    (default: 1)
+% irrevFlag:        (default: 1)
 %                   0, reversible
 %                   1, irreversible
 %
-% convBasisFlag  :    (Default: 0)
+% convBasisFlag:    (Default: 0)
 %                   0, elementary modes
 %                   1, extreme pathways
 %
-% isoFlag        :    (default: 0)
+% isoFlag:          (default: 0)
 %                   0, not consider isoenzymes
 %                   1, consider isoenzymes
 %
-% cMacro         :    (default: cnap.macroDefault)
-%                      empty
-%                      cnap.macroComposition
-                      
-% printLevel         :    (default: 'All')
+% cMacro:           (default: cnap.macroDefault)
+%                   empty
+%                   cnap.macroComposition
+
+% printLevel:       (default: 'All')
 %                      'None'
 %                      'Iteration'
 %                      'All'
 %                      'Details'
-%     
-% efmToolOptions :    (default: {})
+%
+% efmToolOptions:   (default: {})
 %                      'arithmetic'
 %                      'fractional'
 %                      'compression'
 %                      'off'
-% positivity      : 0, normal convex basis
+% positivity:       0, normal convex basis
 %                   1, positive convex basis
 %
-% OUTPUT          :
-% efm             :    matrix that contains (row-wise) the elementary
-%                      modes (or elemenatry vectors) or a minimal set
-%                      of generators (lineality space + extreme rays/
-%                      points), depending on the chosen scenario. The
-%                      columns  correspond to the reactions; the column
-%                      indices of efms (with respect to the columns in
-%                      cnap.stoichMat) are stored in the returned 
-%                      variable idx (see below; note that columns are
-%                      removed in efms if the corresponding reactions
-%                      are not contained in any mode) %
+% OUTPUT:
+%    efms:          matrix that contains (row-wise) the elementary
+%                   modes (or elemenatry vectors) or a minimal set
+%                   of generators (lineality space + extreme rays/
+%                   points), depending on the chosen scenario. The
+%                   columns  correspond to the reactions; the column
+%                   indices of efms (with respect to the columns in
+%                   cnap.stoichMat) are stored in the returned
+%                   variable idx (see below; note that columns are
+%                   removed in efms if the corresponding reactions
+%                   are not contained in any mode) %
 %
-% rev             :    vector indicating for each mode whether it is 
-%                      reversible(0)/irreversible (1)
+% rev:              vector indicating for each mode whether it is
+%                   reversible(0)/irreversible (1)
 %
-% idx             :    maps the columns in efm onto the column indices
-%                      in cnap.stoichmat, i.e. idx(i) refers to the 
-%                      column number in cnap.stoichmat (and to
-%	                   the row number in cnap.reacID)
+% idx:              maps the columns in efm onto the column indices
+%                   in cnap.stoichmat, i.e. idx(i) refers to the
+%                   column number in cnap.stoichmat (and to
+%	                  the row number in cnap.reacID)
 %
-% ray             :    indicates whether the i-th row (vector) in efm
-%                      is an unbounded (1) or bounded (0) direction 
-%                      of the flux cone / flux polyhedron. Bounded
-%                      directions (such as extreme points) can only
-%                      arise if an inhomogeneous problem was defined
-%                      (see also above for 'constraints').
-% cbmodel         :    If the input model be changed during computations 
-%                      then the new model will be saved as cbmodel (COBRA
-%                      model)
+% ray:              indicates whether the i-th row (vector) in efm
+%                   is an unbounded (1) or bounded (0) direction
+%                   of the flux cone / flux polyhedron. Bounded
+%                   directions (such as extreme points) can only
+%                   arise if an inhomogeneous problem was defined
+%                   (see also above for 'constraints').
+% cbmodel:          If the input model be changed during computations
+%                   then the new model will be saved as cbmodel (COBRA
+%                   model)
 %
-% Code by         : 
-%                      Susan Ghaderi, Systems Biochemistry Group, LCSB,
-%                      University of Luxembourg.
+% .. Author: Susan Ghaderi, LCSB, 06.06.2017
 %
-%
-% Last update     :
-%                       06.06.2017
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% default value of varargin
 
 constraints = [];
 positivity = 0;
@@ -118,11 +107,11 @@ cMacro = 0;
 efmToolOptions = {};
 display = 'None';
 
-%% varargin checking 
-if numel(varargin) > 1 
+%% varargin checking
+if numel(varargin) > 1
     for i = 1:2:numel(varargin)
         key = varargin{i};
-        value = varargin{i+1};
+        value = varargin{i + 1};
         switch key
             case 'constraints'
                 constraints = value;
@@ -143,7 +132,7 @@ if numel(varargin) > 1
             case 'display'
                 display = value;
             otherwise
-                msg = sprintf('Unexpected key %s',key)
+                msg = sprintf('Unexpected key %s', key)
                 error(msg);
         end
 
@@ -151,64 +140,56 @@ if numel(varargin) > 1
 end
 
 %% Error checking
-if mexVersion~=1 && mexVersion~=2 && mexVersion~=3 && mexVersion~=4
+if mexVersion ~= 1 && mexVersion ~= 2 && mexVersion ~= 3 && mexVersion ~= 4
     error('mexversion should be either 1 or 2 or 3 or 4');
 end
 
-if irrevFlag~=0 && irrevFlag~=1
+if irrevFlag ~= 0 && irrevFlag ~= 1
     error('irrev_flag should be either 0 or 1');
 end
 
-if convBasisFlag~=0 && convBasisFlag~=1
+if convBasisFlag ~= 0 && convBasisFlag ~= 1
     error('convbasis_flag should be either 0 or 1');
 end
 
-if isoFlag~=0 && isoFlag~=1
+if isoFlag ~= 0 && isoFlag ~= 1
     error('iso_flag should be either 0 or 1');
 end
 
-if positivity~=0 && positivity~=1
+if positivity ~= 0 && positivity ~= 1
     error('positivity should be either 0 or 1');
 end
 
-%% Convertining reversible reactions into irreversible reactions 
+%% Convertining reversible reactions into irreversible reactions
 if positivity
-       
-    ind = model.rev==1;
+    ind = model.rev == 1;
     rev = [model.rev; model.rev(ind)];
     rxns = strcat(model.rxnNames(ind), '-rev');
     c = [model.c; model.c(ind)];
-    model.S = [model.S, -model.S(:,ind)];
-    model.lb = zeros(length(model.rev)+length(model.rev(ind)),1);
-    model.ub = [model.ub; Inf*ones(size(model.lb(ind),1),1)];
+    model.S = [model.S, -model.S(:, ind)];
+    model.lb = zeros(length(model.rev) + length(model.rev(ind)), 1);
+    model.ub = [model.ub; Inf * ones(size(model.lb(ind), 1), 1)];
     model.rxns = [model.rxns; rxns];
-    model.c = [model.c; zeros(length(model.rev(ind)),1)];
+    model.c = [model.c; zeros(length(model.rev(ind)), 1)];
     model.rxnNames = [model.rxnNames; rxns];
-    
 end
 
-%% converting a cobra model to CNA model 
-cnap      = CNAcobra2cna(model);
+%% converting a cobra model to CNA model
+cnap = CNAcobra2cna(model);
 cnap.path = directory;
-cnap      = CNAsaveNetwork(cnap);
+cnap = CNAsaveNetwork(cnap);
 
+%% computing convex basis or elementary modes
+[output.efm, rev, idx, ray] = CNAcomputeEFM(cnap, constraints, mexVersion, ...
+                                            irrevFlag, convBasisFlag, ...
+                                            isoFlag, cMacro, display, ...
+                                            efmToolOptions);
+%% export CNA model to COBRA model
+cbmodel = CNAcna2cobra(cnap);
 
-%% computing convex basis or elementary modes 
-[output.efm,rev,idx,ray] = CNAcomputeEFM(cnap,constraints,mexVersion,...
-                                         irrevFlag,convBasisFlag,...
-                                         isoFlag,cMacro,display,...
-                                         efmToolOptions);
-%% export CNA model to COBRA model              
-cbmodel           = CNAcna2cobra(cnap);                
-
-%% OUTPUT 
-output.rev   = rev;
-output.idx   = idx;
-output.ray   = ray;
+%% OUTPUT
+output.rev = rev;
+output.idx = idx;
+output.ray = ray;
 output.model = cbmodel;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
