b67d316e3fde5aebe02f41c496ec7498e1bb905e
German Preciat <gapreciat@gmail.com>
2017-06-27 17:33:49 +0200

    tutorial atomicallyResolveMetabolicReconstruction

--- tutorials/moieties/Data/construct_DAS_elemental_matrix.m
+++
@@ -1,7 +0,0 @@
-% Construct the elemental matrix and electron vector for DAS
-
-load DAS.mat
-
-[E,elements] = constructElementalMatrix(model.metFormulas,model.metCharges);
-
-save elementalMatrix.mat E elements
