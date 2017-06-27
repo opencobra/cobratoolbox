9d2ed78bba58b3272c08db3b2b644da516b13b25
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-06-27 14:51:45 +0200

    adaptation for the CI

--- test/verifiedTests/testSubspaces/testMinSpan.m
+++ test/verifiedTests/testSubspaces/testMinSpan.m
@@ -1,29 +1,36 @@
-function x = testMinSpan()
+% The COBRAToolbox: testSubspaces.m
+%
+% Purpose:
+%     - test the ability to determine the MinSpan vectors of the
+%       E.coli core model
+%
+% Authors:
+%     - Original test file: Aarash Bordbar
+%     - CI integration: Sylvain Arreckx, June 2017
 
-% This function tests the ability to determine the MinSpan vectors of the
-% E.coli core model by:
-% 1) Loading the E.coli core model (e_coli_core.mat)
-% 2) Calculating the MinSpan for the E.coli core model
+global CBTDIR
 
-initCobraToolbox();
+% save the current path
+currentDir = pwd;
 
-% Load E.coli core model
-load e_coli_core
-if ~exist('e_coli_core')
-    error('Unable to load E. coli core model');
-end
+% initialize the test
+fileDir = fileparts(which('testMinSpan'));
+cd(fileDir);
+
+% load the model
+load([CBTDIR, filesep, 'test' filesep 'models' filesep 'ecoli_core_model.mat'], 'model');
 
 % Remove biomass equation for MinSpan calculation
-bmName = {'BIOMASS_Ecoli_core_w_GAM'};
-e_coli_core = removeRxns(e_coli_core, bmName);
+bmName = {'Biomass_Ecoli_core_w_GAM'};
+model = removeRxns(model, bmName);
 
-[m, n] = size(e_coli_core.S);
+[m, n] = size(model.S);
 assert(m == 72 & n == 94, ...
-    'Unable to setup input for MinSpan determination');
+      'Unable to setup input for MinSpan determination');
 
 % Setup parameters and run detMinSpan
 params.saveIntV = 0; % Do not save intermediate output
-minSpanVectors = detMinSpan(e_coli_core, params);
+minSpanVectors = detMinSpan(model, params);
 
 % Check size of vectors and number of entries
 [r, c] = size(minSpanVectors);
@@ -31,5 +38,3 @@ numEntries = nnz(minSpanVectors);
 
 assert(r == 94 & c == 23, 'MinSpan vector matrix wrong size');
 assert(numEntries > 479, 'MinSpan vector matrix is not minimal');
-
-x = true;
