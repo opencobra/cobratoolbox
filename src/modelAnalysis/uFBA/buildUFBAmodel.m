2fabd0940761b201e0b68052845134632d2fd133
Thomas Pfau <thomas.pfau@uni.lu>
2017-06-30 12:56:49 +0200

    :bug: updated uFBA model construction to correctly update all metabolite fields.

--- src/modelAnalysis/uFBA/buildUFBAmodel.m
+++ src/modelAnalysis/uFBA/buildUFBAmodel.m
@@ -189,16 +189,23 @@ model = removeRxns(model, model.rxns(exRxns));
 if ~isempty(neededSinks)
     model = addSinkReactions(model, neededSinks, -1000 * ones(length(neededSinks), 1), 1000 * ones(length(neededSinks), 1));
 end
-model.csense = '';
-model.csense(1:length(model.mets)) = 'E';
+if ~isfield(model, 'csense')
+    model.csense = repmat('E', length(model.mets),1);
+end
 
 % build UFBAmodel
 uFBAmodel = model;
-uFBAmodel.S = [model.S; model.S];
-uFBAmodel.b = [model.b; model.b];
+
+[metFields,dimension] = getModelFieldsForType(model,'mets');
+metFields = setdiff(metFields,'mets'); % mets is special.
 uFBAmodel.mets = [strcat(model.mets, '_G'); strcat(model.mets, '_L')];
-uFBAmodel.metNames = [model.metNames; model.metNames];
-uFBAmodel.csense = [model.csense, model.csense];
+for field = 1:numel(metFields)
+    if dimension(field) == 1
+        model.(metFields{field}) = [model.metFields{field};model.metFields{field}];
+    elseif dimension(field) == 2
+        model.(metFields{field}) = [model.metFields{field},model.metFields{field}];
+    end
+end
 
 % Filter out non-quantified metabolites
 toRemove = find(changeIntervals == 0);
