9b2fe707451ed882e05c29ed7cabb158a90f2920
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-07-04 14:07:59 +0200

    minor updates

--- src/analysis/KLdistance/KLDis.m
+++ src/analysis/KLdistance/KLDis.m
@@ -1,11 +1,11 @@
-function dist=KLDis(P,Q);
-% This function calculates the Kullback-Leibler Distance of two discrete
+function dist = KLDis(P, Q);
+% Calculates the Kullback-Leibler Distance of two discrete
 % probability distributions. P and Q  are automatically normalised to have
 % the sum of one on rows have the length of one at each.
 %
 % USAGE:
 %
-%    dist = KLDis(P,Q)
+%    dist = KLDis(P, Q)
 %
 % INPUTS:
 %    P =  n x nbins
@@ -14,8 +14,10 @@ function dist=KLDis(P,Q);
 % OUTPUTS:
 %    dist = n x 1
 %
-% .. Authors:
-%       - Siham Hachi 29/06/2017
+% .. Author:
+%       - Nima Razavi
+% NOTE:
+%    adapted from https://nl.mathworks.com/matlabcentral/fileexchange/20688-kullback-leibler-divergence
 
 if size(P,2)~=size(Q,2)
     error('the number of columns in P and Q should be the same');
@@ -30,15 +32,14 @@ if size(Q,1)==1
     Q = Q ./sum(Q);
     P = P ./repmat(sum(P,2),[1 size(P,2)]);
     temp =  (P - Q).*log10(P./repmat(Q,[size(P,1) 1]));
-    temp(isnan(temp))=0;% resolving the case when P(i)==0
+    temp(isnan(temp))=0;  % resolving the case when P(i)==0
     dist = sum(temp,2);
-    
-    
+
 elseif size(Q,1)==size(P,1)
-    
+
     Q = Q ./repmat(sum(Q,2),[1 size(Q,2)]);
     P = P ./repmat(sum(P,2),[1 size(P,2)]);
     temp =  (P - Q).*log10(P./Q);
-    temp(isnan(temp))=0; % resolving the case when P(i)==0
+    temp(isnan(temp))=0;  % resolving the case when P(i)==0
     dist = sum(temp,2);
 end
