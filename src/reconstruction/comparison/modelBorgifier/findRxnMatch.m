c737a3109a6e3c0e9456b24d23ee0f7b303456f1
jtsauls <saulsopolis@gmail.com>
2017-07-07 15:35:25 -0700

    add modelBorgifier source code

---
+++ src/reconstruction/comparison/modelBorgifier/findRxnMatch.m
@@ -0,0 +1,236 @@
+% This file is published under Creative Commons BY-NC-SA.
+%
+% Please cite:
+% Sauls, J. T., & Buescher, J. M. (2014). Assimilating genome-scale 
+% metabolic reconstructions with modelBorgifier. Bioinformatics 
+% (Oxford, England), 30(7), 1036?8. http://doi.org/10.1093/bioinformatics/btt747
+%
+% Correspondance:
+% johntsauls@gmail.com
+%
+% Developed at:
+% BRAIN Aktiengesellschaft
+% Microbial Production Technologies Unit
+% Quantitative Biology and Sequencing Platform
+% Darmstaeter Str. 34-36
+% 64673 Zwingenberg, Germany
+% www.brain-biotech.de
+%
+function Data = findRxnMatch(cRxn,nMatch,scoreTotal)
+% findRxnMatch Finds the highest scoring matches for a given reaction. 
+%
+% USAGE:
+%    Data = findRxnMatch(cRxn,nMatch) 
+% 
+% INPUTS:
+%    cRxn:          Number of the compared rxn.
+%    nMatch:        Number of matches to return.
+%    scoreTotal:    summed SCORE of reactions vs reactions. 
+%
+% GLOBAL INPUTS:
+%    CMODEL
+%    TMODEL
+% 
+% OUTPUTS:
+%    Data:          Structure containing comparison information.
+%
+% CALLS:
+%
+% CALLED BY:
+%    reactionCompareGUI
+
+%% Declare variables.
+global CMODEL TMODEL
+
+% Structure comtaining comparison information
+Data = ([]);
+
+%% cRxn Reaction Info
+Data.cRxnTable = cell(8,1) ;
+Data.cRxnTable(:) = {''} ;
+Data.cRxnTable{1} = num2str(cRxn) ;
+Data.cRxnTable{2} = CMODEL.rxns{cRxn} ; 
+Data.cRxnTable{3} = CMODEL.rxnNames{cRxn} ;
+Data.cRxnTable{4} = CMODEL.rxnEquations{cRxn} ;
+Data.cRxnTable{5} = CMODEL.rxnECNumbers{cRxn} ;
+Data.cRxnTable{6} = CMODEL.rxnKEGGID{cRxn} ;
+Data.cRxnTable{7} = CMODEL.rxnSEEDID{cRxn} ;
+Data.cRxnTable{8} = CMODEL.subSystems{cRxn} ;
+
+%% cRxn Metabolite Info
+Data.cMetTable = cell(14, 1);
+Data.cMetTable(:) = {''};
+
+% Reactants from model
+Data.cMetTable{1} = num2str(CMODEL.metNums(cRxn, 3)); % # of reactants
+
+metPos = find(CMODEL.S(:, cRxn) < 0) ; % find reactants
+if ~isempty(metPos)
+    metlist = CMODEL.mets{metPos(1)} ; % add in first metabolite Id
+    names = CMODEL.metNames{metPos(1)} ;
+    stoics = num2str(abs(CMODEL.S(metPos(1), cRxn))) ; % add in first stoic
+    forms = CMODEL.metFormulas{metPos(1)} ; % first formula
+    charges = num2str(CMODEL.metCharge(metPos(1))) ; % first charge
+    keggs = CMODEL.metKEGGID{metPos(1)} ; % first charge
+
+    % If there are more than one mets, add additional info.
+    if length(metPos) > 1 
+        for j = 2:length(metPos)
+            metlist = strcat(metlist, '; ', CMODEL.mets{metPos(j)});
+            names = strcat(names, '; ', CMODEL.metNames{metPos(j)});
+            stoics = strcat(stoics, '; ', ...
+                            num2str(abs(CMODEL.S(metPos(j), cRxn))));
+            forms = strcat(forms, '; ', CMODEL.metFormulas{metPos(j)});
+            charges = strcat(charges, '; ', ...
+                             num2str(CMODEL.metCharge(metPos(j))));
+            keggs = strcat(keggs, '; ', CMODEL.metKEGGID{metPos(j)});
+        end
+    end
+
+    % Assign to table.
+    Data.cMetTable{2} = metlist ;
+    Data.cMetTable{3} = names ;
+    Data.cMetTable{4} = stoics ;
+    Data.cMetTable{5} = forms ;
+    Data.cMetTable{6} = charges ;
+    Data.cMetTable{7} = keggs ; 
+end
+
+% Products from crxn.
+% Number of products.
+Data.cMetTable{8} = num2str(CMODEL.metNums(cRxn, 5));
+
+% Find product position.
+metPos = find(CMODEL.S(:, cRxn) > 0) ;
+
+if ~isempty(metPos)
+    % First product.
+    metlist = CMODEL.mets{metPos(1)};
+    names = CMODEL.metNames{metPos(1)} ;
+    stoics = num2str(abs(CMODEL.S(metPos(1), cRxn)));
+    forms = CMODEL.metFormulas{metPos(1)};
+    charges = num2str(CMODEL.metCharge(metPos(1)));
+    keggs = CMODEL.metKEGGID{metPos(1)};
+    
+    % Additional products.
+    if length(metPos) > 1
+        for j = 2:length(metPos)
+            metlist = strcat(metlist, '; ', CMODEL.mets{metPos(j)});
+            names = strcat(names, '; ', CMODEL.metNames{metPos(j)});
+            stoics = strcat(stoics, '; ', ...
+                            num2str(abs(CMODEL.S(metPos(j), cRxn))));
+            forms = strcat(forms, '; ', CMODEL.metFormulas{metPos(j)});
+            charges = strcat(charges, '; ', ...
+                             num2str(CMODEL.metCharge(metPos(j))));
+            keggs = strcat(keggs, '; ', CMODEL.metKEGGID{metPos(j)});
+        end
+    end
+    
+    % Assign data.
+    Data.cMetTable{9} = metlist ;
+    Data.cMetTable{10} = names ;
+    Data.cMetTable{11} = stoics ;
+    Data.cMetTable{12} = forms ;
+    Data.cMetTable{13} = charges ;
+    Data.cMetTable{14} = keggs ;
+    clear metlist; clear stoics; clear forms; clear charges; clear keggs
+end
+
+%% Match Reaction(s) Info
+% Allocate match reaction table.
+Data.tRxnTable = cell(8, nMatch);
+Data.tRxnTable(:) = {''};
+
+% Sort scores, I is the original index.
+[y, I] = sort(scoreTotal(cRxn, :), 'descend'); 
+
+for i = 1:nMatch
+    Data.tRxnTable{1, i} = strcat(num2str(y(i)), '; ', num2str(I(i)));
+    Data.tRxnTable{2, i} = TMODEL.rxns{I(i)} ;
+    Data.tRxnTable{3, i} = TMODEL.rxnNames{I(i)} ;
+    Data.tRxnTable{4, i} = TMODEL.rxnEquations{I(i)} ;
+    Data.tRxnTable{5, i} = TMODEL.rxnECNumbers{I(i)} ;
+    Data.tRxnTable{6, i} = TMODEL.rxnKEGGID{I(i)} ;
+    Data.tRxnTable{7, i} = TMODEL.rxnSEEDID{I(i)} ;
+    Data.tRxnTable{8, i} = TMODEL.subSystems{I(i)} ;
+end
+
+%% Match Metabolite Info
+Data.tMetTable = cell(14, nMatch);
+Data.tMetTable(:) = {''};
+
+for i = 1:nMatch
+    % Reactants from model.
+    Data.tMetTable{1,i} = num2str(TMODEL.metNums(I(i),3));  
+    
+    % First reactant.
+    metPos = find(TMODEL.S(:,I(i)) < 0) ;
+    
+    if ~isempty(metPos)
+        metlist = TMODEL.mets{metPos(1)};
+        names = TMODEL.metNames{metPos(1)} ;
+        stoics = num2str(abs(TMODEL.S(metPos(1),I(i))));
+        forms = TMODEL.metFormulas{metPos(1)};
+        charges = num2str(TMODEL.metCharge(metPos(1)));
+        keggs = TMODEL.metKEGGID{metPos(1)};
+        
+        % Additioanl Reactants.
+        if length(metPos) > 1
+            for j = 2:length(metPos)
+                metlist = strcat(metlist, '; ', TMODEL.mets{metPos(j)});
+                names = strcat(names, '; ', TMODEL.metNames{metPos(j)});
+                stoics = strcat(stoics, '; ', ...
+                                num2str(abs(TMODEL.S(metPos(j), I(i)))));
+                forms = strcat(forms, '; ', TMODEL.metFormulas{metPos(j)});
+                charges = strcat(charges, '; ', ...
+                                 num2str(TMODEL.metCharge(metPos(j))));
+                keggs = strcat(keggs, '; ', TMODEL.metKEGGID{metPos(j)});
+            end
+        end
+        
+        Data.tMetTable{2, i} = metlist ;
+        Data.tMetTable{3, i} = names ;
+        Data.tMetTable{4, i} = stoics ;
+        Data.tMetTable{5, i} = forms ;
+        Data.tMetTable{6, i} = charges ;
+        Data.tMetTable{7, i} = keggs ;
+    end
+    
+    % Products from model.
+    if TMODEL.metNums(I(i), 5) ~= 0
+        Data.tMetTable{8, i} = num2str(TMODEL.metNums(I(i), 5));
+        
+        % Find product indexes.
+        metPos = find(TMODEL.S(:, I(i)) > 0) ; 
+        
+        % First product    
+        metlist = TMODEL.mets{metPos(1)}; 
+        names = TMODEL.metNames{metPos(1)} ;
+        stoics = num2str(abs(TMODEL.S(metPos(1), I(i))));
+        forms = TMODEL.metFormulas{metPos(1)};
+        charges = num2str(TMODEL.metCharge(metPos(1))); 
+        keggs = TMODEL.metKEGGID{metPos(1)}; 
+        
+        % Additional products.
+        if length(metPos) > 1 
+            for j = 2:length(metPos)
+                metlist = strcat(metlist, '; ', TMODEL.mets{metPos(j)});
+                names = strcat(names, '; ', TMODEL.metNames{metPos(j)});
+                stoics = strcat(stoics, '; ', ...
+                                num2str(abs(TMODEL.S(metPos(j), I(i)))));
+                forms = strcat(forms, '; ', TMODEL.metFormulas{metPos(j)});
+                charges = strcat(charges, '; ', ...
+                                 num2str(TMODEL.metCharge(metPos(j))));
+                keggs = strcat(keggs, '; ', TMODEL.metKEGGID{metPos(j)});
+            end
+        end
+        
+        % Assign to table.
+        Data.tMetTable{9, i} = metlist ;
+        Data.tMetTable{10, i} = names ;
+        Data.tMetTable{11, i} = stoics ;
+        Data.tMetTable{12, i} = forms ;
+        Data.tMetTable{13, i} = charges ;
+        Data.tMetTable{14, i} = keggs ;
+    end
+end
---
+++ src/reconstruction/comparison/modelBorgifier/organizeModelCool.m
@@ -0,0 +1,171 @@
+% This file is published under Creative Commons BY-NC-SA.
+%
+% Please cite:
+% Sauls, J. T., & Buescher, J. M. (2014). Assimilating genome-scale 
+% metabolic reconstructions with modelBorgifier. Bioinformatics 
+% (Oxford, England), 30(7), 1036?8. http://doi.org/10.1093/bioinformatics/btt747
+%
+% Correspondance:
+% johntsauls@gmail.com
+%
+% Developed at:
+% BRAIN Aktiengesellschaft
+% Microbial Production Technologies Unit
+% Quantitative Biology and Sequencing Platform
+% Darmstaeter Str. 34-36
+% 64673 Zwingenberg, Germany
+% www.brain-biotech.de
+%
+function Model = organizeModelCool(Model)
+% organizeModelCool reorders a COBRA model such that the most promiscuous
+% mets are listed first, with sister mets grouped together. Reactions are
+% then organized so that the S matrix appoximates a rank order matrix, with
+% exchange reactions at the end. 
+% 
+% USAGE:
+%    Model = organizeModelCool(Model)
+%
+% INPUTS:
+%    Model:     Model in COBRA format. Also accept a Tmodel.
+%
+% OUTPUTS:
+%    Model:     Organized model.
+% 
+% CALLS:
+%    TmodelFields
+%
+% CALLED BY:
+%    mergeModels
+%    readCbTmodel
+%    verifyModel
+%    readCbTmodel
+%
+
+%% Declare variables for met.
+nMets = length(Model.mets) ;
+metIndex = zeros(nMets, 1) ; 
+metPos = 1 ; 
+
+% Unique metabolites sans compartment, used to find sisters.
+metsNoComp = cell(nMets, 1) ;
+for iMet = 1:nMets
+    metsNoComp{iMet} = Model.mets{iMet}(1:end - 3) ;
+end
+
+%% Order metabolites
+% Sort by most promiscuous metabolites, grouping sisters.
+rxnCount = full(sum((Model.S ~= 0), 2)) ;
+
+while metPos <= nMets
+    % Most promiscuous met.
+    [~, maxIndex] = max(rxnCount) ;
+
+    % If the is a tie, choose the first alphabetically.
+    maxIndex = maxIndex(1) ;
+
+    % Find sister metabolites.
+    nowMetNameNoComp = Model.mets{maxIndex}(1:end - 3) ;
+    noCompIndex = find(strcmp(nowMetNameNoComp, metsNoComp)) ;
+
+    % Add mets to future index.
+    for iMet = 1:length(noCompIndex)
+        metIndex(metPos) = noCompIndex(iMet) ; 
+        metPos = metPos + 1 ;
+        % Take those mets out of the running. 
+        rxnCount(noCompIndex(iMet)) = -1 ; 
+    end
+end
+
+%% Order reactions. 
+nRxns = length(Model.rxns) ;
+rxnIndex = zeros(nRxns, 1) ; 
+% Index and number of exchange reactions.
+exIndexes = false(nRxns, 1) ;
+exIndexes(sum(logical(abs(full(Model.S))), 1) == 1) = true ;
+nExRxns = sum(exIndexes) ;
+exRxnPos = nRxns ; 
+rxnPos = nRxns - nExRxns ;
+% S matix used to find involved reactions.
+SearchS = Model.S ;
+
+% Starting from last metabolite in new order, find involved reactions.
+for iMet = nMets:-1:1
+    involvedRxns = find(SearchS(metIndex(iMet), :)) ;
+    for iRxn = 1:length(involvedRxns)
+        % It it is an exchange reaction, put it at the end.
+        if exIndexes(involvedRxns(iRxn))
+            rxnIndex(exRxnPos) = involvedRxns(iRxn) ;
+            exRxnPos = exRxnPos - 1 ;
+        else % Otherwise put it here. 
+            rxnIndex(rxnPos) = involvedRxns(iRxn) ;
+            rxnPos = rxnPos - 1 ;
+        end
+    end
+    % Remove reaction from running.
+    SearchS(:,involvedRxns) = 0 ;
+end
+% put remaining reactions on top
+rxnIndex(rxnIndex == 0) = setdiff(1:nRxns, rxnIndex) ;
+
+%% Reorder everything.
+% Grab fields names.
+fields = TmodelFields ; 
+
+% Reaction related cell field names.
+rxnFields = fields{1} ; 
+% Reaction related double Fields (which are kept model specific).
+rNumFields = fields{2} ;
+% Metabolite related cell field names. 
+metFields = fields{3} ; 
+% Metabolite related double field names. 
+mNumFields = fields{4} ; 
+
+% Reorder reaction related lists.
+for iField = 1:length(rxnFields)
+    if isfield(Model, rxnFields{iField})
+        Model.(rxnFields{iField}) = Model.(rxnFields{iField})(rxnIndex) ;
+    end
+end
+
+% Reorder metabolite related lists.
+for iField = 1:length(metFields)
+    if isfield(Model, metFields{iField})
+        Model.(metFields{iField}) = Model.(metFields{iField})(metIndex) ;
+    end
+end
+for iField = 1:length(mNumFields)
+    if isfield(Model, mNumFields{iField})
+        Model.(mNumFields{iField}) = ...
+            Model.(mNumFields{iField})(metIndex) ;
+    end
+end
+
+% If this is a Tmodel.
+if isfield(Model,'Models')
+    % Model names.
+    moNames = fieldnames(Model.Models) ;
+
+    % Reorder identity arrays.
+    for iMo = 1:length(moNames)
+        Model.Models.(moNames{iMo}).rxns = ... 
+            Model.Models.(moNames{iMo}).rxns(rxnIndex) ;
+        Model.Models.(moNames{iMo}).mets = ...
+            Model.Models.(moNames{iMo}).mets(metIndex) ;
+    end
+    
+    for iField = 1:length(rNumFields)
+        for iMo = 1:length(moNames)
+            Model.(rNumFields{iField}).(moNames{iMo}) = ...
+                Model.(rNumFields{iField}).(moNames{iMo})(rxnIndex) ;
+        end
+    end
+else
+    for iField = 1:length(rNumFields)
+        Model.(rNumFields{iField}) = ...
+            Model.(rNumFields{iField})(rxnIndex) ;
+    end    
+end
+
+% Reorder S matrix.
+Model.S = Model.S(:, rxnIndex) ;
+Model.S = Model.S(metIndex, :) ;
