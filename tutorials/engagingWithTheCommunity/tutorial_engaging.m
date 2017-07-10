da28fed816119a4fb07369a267d50171d6145275
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-07-10 21:57:30 +0200

    new tutorial

---
+++ tutorials/engagingWithTheCommunity/tutorial_engaging.m
@@ -0,0 +1,38 @@
+%% Engaging with the COBRA Toolbox community
+% *Authors: Sylvain Arreckx, Luxembourg Centre for Systems Biomedicine*
+% 
+% *Reviewers: *
+%% Search for existing solutions to problems 
+% * The Frequently Asked Questions (FAQ) section of the documentation (<https://opencobra.github.io/cobratoolbox/docs/FAQ.html 
+% https://opencobra.github.io/cobratoolbox/docs/FAQ.html>) is a good starting 
+% point to find answers to questions you may have or issues you may face.
+% * The public forum associated with The COBRA toolbox, available at <https://groups.google.com/forum/#!forum/cobra- 
+% toolbox https://groups.google.com/forum/#!forum/cobra- toolbox>, is a great 
+% way to search for solutions to previously recognised problems that are similar 
+% to problems novel to the user. This is especially so with respect to recent 
+% installation and configuration issues that have arisen due to asynchronous development 
+% of the many software packages integrated with The COBRA Toolbox. 
+%% Register online
+% Before posting a question to a problem, an application for membership at <https://groups.google.com/forum/#!forum/cobra-toolbox 
+% https://groups.google.com/forum/#!forum/cobra-toolbox> is required to eliminate 
+% spam.
+%% Facing an issue?
+% Post your question online to the COBRA Toolbox forum at <https://groups.google.com/forum/#!forum/cobra- 
+% toolbox https://groups.google.com/forum/#!forum/cobra- toolbox>. Questions posted 
+% to the forum are welcome provided that some simple guidelines are followed to 
+% encourage a rapid and effective response from the community. 
+% 
+% # Make the question as detailed as possible to increase the probability of 
+% a rapid and helpful reply.
+% # Append your message with the result of running |generateSystemConfigReport| 
+% so that we know what system we are helping you to debug. That is often the first 
+% question that comes to mind when considering to respond.
+
+generateSystemConfigReport
+%% ANTICIPATED RESULTS
+% Generally, responses to questions can be expected within 1-2 days of posting, 
+% provided that posting guidelines are followed. 
+%% Suggest new solutions to problems 
+% Reply to a question online COBRA toolbox forum. Community contributions are 
+% welcomed to help users overcome any issues they face and are noticed by existing 
+% COBRA community members.
