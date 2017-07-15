eb8fe2eee10f4fb17f17da12a3d1d0aeab2a67a7
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-07-15 15:45:50 +0200

    WIP on optForce/findMustU

--- src/design/optForce/findMustL.m
+++ src/design/optForce/findMustL.m
@@ -1,7 +1,7 @@
 function [mustLSet, posMustL] = findMustL(model, minFluxesW, maxFluxesW, varargin)
-% This function runs the second step of optForce, that is to solve a
+% This function runs the second step of `optForce`, that is to solve a
 % bilevel mixed integer linear programming  problem to find a first order
-% MustL set.
+% `MustL` set.
 %
 % USAGE:
 %
@@ -14,21 +14,21 @@ function [mustLSet, posMustL] = findMustL(model, minFluxesW, maxFluxesW, varargi
 %                                 * .rxns - Reaction IDs in the model
 %                                 * .mets - Metabolite IDs in the model
 %                                 * .S -    Stoichiometric matrix (sparse)
-%                                 * .b -    RHS of Sv = b (usually zeros)
+%                                 * .b -    RHS of `Sv = b` (usually zeros)
 %                                 * .c -    Objective coefficients
 %                                 * .lb -   Lower bounds for fluxes
 %                                 * .ub -   Upper bounds for fluxes
-%    minFluxesW:                (double array) of size n_rxns x1
+%    minFluxesW:                (double array) of `size n_rxns x 1`.
 %                               Minimum fluxes for each
 %                               reaction in the model for wild-type strain.
 %                               This can be obtained by running the
-%                               function FVAOptForce.
-%                               E.g.: minFluxesW = [-90; -56];
-%    maxFluxesW:                (double array) of size n_rxns x1
+%                               function `FVAOptForce`.
+%                               E.g.: `minFluxesW = [-90; -56];`
+%    maxFluxesW:                (double array) of size `n_rxns x 1`.
 %                               Maximum fluxes for each
 %                               reaction in the model for wild-type strain.
 %                               This can be obtained by running the
-%                               function FVAOptForce.
+%                               function `FVAOptForce`.
 %
 % OPTIONAL INPUTS:
 %    constrOpt:                 (structure) structure containing
@@ -45,85 +45,89 @@ function [mustLSet, posMustL] = findMustL(model, minFluxesW, maxFluxesW, varargi
 %                                 * .rxnList - Reaction list (cell array)
 %                                 * .values -  Values for constrained
 %                                   reactions (double array)
-%                                   E.g.: struct('rxnList', ...
+%                                   E.g.: `struct('rxnList', ...
 %                                   {{'EX_gluc', 'R75', 'EX_suc'}}, ...
-%                                   'values', [-100, 0, 155.5]');
-%    runID:                     (string) ID for identifying this run
+%                                   'values', [-100, 0, 155.5]');`
+%    runID:                     (string) ID for identifying this run.
 %                               Default: ['run' date hour].
 %    outputFolder:              (string) name for folder in which results
-%                               will be stored
+%                               will be stored.
 %                               Default: 'OutputsFindMustL'.
 %    outputFileName:            (string) name for files in which results
-%                               will be stored
+%                               will be stored.
 %                               Default: 'MustLSet'.
 %    printExcel:                (double) boolean to describe wheter data
-%                               must be printed in an excel file or not
+%                               must be printed in an excel file or not.
 %                               Default: 1
 %    printText:                 (double) boolean to describe wheter data
-%                               must be printed in an plaint text file or not
+%                               must be printed in an plaint text file or not.
 %                               Default: 1
 %    printReport:               (double) 1 to generate a report in a plain
 %                               text file. 0 otherwise.
 %                               Default: 1
 %    keepInputs:                (double) 1 to save inputs to run
-%                               findMustL.m 0 otherwise.
+%                               `findMustL.m` 0 otherwise.
 %                               Default: 1
 %    verbose:                   (double) 1 to print results in console.
 %                               0 otherwise.
 %                               Default: 0
 %
 % OUTPUTS:
-%    mustLSet:                  (cell array) Size: number of reactions found X 1
+%    mustLSet:                  (cell array) Size: number of reactions found X 1.
 %                               Cell array containing the
-%                               reactions ID which belong to the Must_U Set
+%                               reactions ID which belong to the `Must_U` Set
 %    posMustL:                  (double array)
 %                               Size: number of reactions found X 1
 %                               double array containing the
 %                               positions of reactions in the model.
-%    outputFileName.xls          File containing one column array
-%                               with identifiers for reactions in MustL. This
+%    outputFileName.xls         File containing one column array
+%                               with identifiers for reactions in `MustL`. This
 %                               file will only be generated if the user entered
-%                               printExcel = 1. Note that the user can choose
+%                               `printExcel = 1`. Note that the user can choose
 %                               the name of this file entering the input
-%                               outputFileName = 'PutYourOwnFileNameHere';
-%    outputFileName.txt          File containing one column array
-%                               with identifiers for reactions in MustL. This
+%                               `outputFileName = 'PutYourOwnFileNameHere';`
+%    outputFileName.txt         File containing one column array
+%                               with identifiers for reactions in `MustL`. This
 %                               file will only be generated if the user entered
-%                               printText = 1. Note that the user can choose
+%                               `printText = 1`. Note that the user can choose
 %                               the name of this file entering the input
-%                               outputFileName = 'PutYourOwnFileNameHere';
-%    outputFileName_Info.xls     File containing five column arrays.
-%                               C1: identifiers for reactions in MustL
-%                               C2: min fluxes for reactions according to FVA
-%                               C3: max fluxes for reactions according to FVA
-%                               C4: min fluxes achieved for reactions,
-%                               according to findMustL
-%                               C5: max fluxes achieved for reactions,
-%                               according to findMustL
+%                               `outputFileName = 'PutYourOwnFileNameHere';`
+%    outputFileName_Info.xls    File containing five column arrays.
+%
+%                                 * C1: identifiers for reactions in `MustL`
+%                                 * C2: min fluxes for reactions according to FVA
+%                                 * C3: max fluxes for reactions according to FVA
+%                                 * C4: min fluxes achieved for reactions,
+%                                   according to `findMustL`
+%                                 * C5: max fluxes achieved for reactions,
+%                                   according to `findMustL`
 %                               This file will only be generated if the user
-%                               entered printExcel = 1. Note that the user can
+%                               entered `printExcel = 1`. Note that the user can
 %                               choose the name of this file entering the input
-%                               outputFileName = 'PutYourOwnFileNameHere';
-%    outputFileName_Info.txt     File containing five column arrays.
-%                               C1: identifiers for reactions in MustL.
-%                               C2: min fluxes for reactions according to FVA
-%                               C3: max fluxes for reactions according to FVA
-%                               C4: min fluxes achieved for reactions,
-%                               according to findMustL
-%                               C5: max fluxes achieved for reactions,
-%                               according to findMustL
+%                               `outputFileName = 'PutYourOwnFileNameHere';`
+%    outputFileName_Info.txt    File containing five column arrays.
+%
+%                                 * C1: identifiers for reactions in `MustL`.
+%                                 * C2: min fluxes for reactions according to FVA
+%                                 * C3: max fluxes for reactions according to FVA
+%                                 * C4: min fluxes achieved for reactions,
+%                                   according to `findMustL`
+%                                 * C5: max fluxes achieved for reactions,
+%                                   according to `findMustL`
 %                               This file will only be generated if the user
-%                               entered printText = 1. Note that the user can
+%                               entered `printText = 1`. Note that the user can
 %                               choose the name of this file entering the input
-%                               outputFileName = 'PutYourOwnFileNameHere';
+%                               `outputFileName = 'PutYourOwnFileNameHere';`
+%
 % NOTE:
+%
 %    This function is based in the GAMS files written by Sridhar
 %    Ranganathan which were provided by the research group of Costas D.
-%    Maranas. For a detailed description of the optForce procedure, please
-%    see: Ranganathan S, Suthers PF, Maranas CD (2010) OptForce: An
+%    Maranas. For a detailed description of the `optForce` procedure, please
+%    see: `Ranganathan S, Suthers PF, Maranas CD (2010) OptForce: An
 %    Optimization Procedure for Identifying All Genetic Manipulations
 %    Leading to Targeted Overproductions. PLOS Computational Biology 6(4):
-%    e1000744. https://doi.org/10.1371/journal.pcbi.1000744
+%    e1000744`. https://doi.org/10.1371/journal.pcbi.1000744
 %
 % .. Author: - Sebastian Mendoza, May 30th 2017, Center for Mathematical Modeling, University of Chile, snmendoz@uc.cl
 
