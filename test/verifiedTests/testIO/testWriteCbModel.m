badb9f855390f3ced9b5519753e8f8aa7feadb87
laurentheirendt <laurent.heirendt@uni.lu>
2017-07-18 14:58:01 +0200

    Test for writeCbModel

---
+++ test/verifiedTests/testIO/testWriteCbModel.m
@@ -0,0 +1,62 @@
+% The COBRAToolbox: testWriteCbModel.m
+%
+% Purpose:
+%     - test the writeCbModel function
+%
+% Authors:
+%     - Laurent Heirendt
+
+% save the current path
+currentDir = pwd;
+
+% initialize the test
+fileDir = fileparts(which('testWriteCbModel'));
+cd(fileDir);
+
+% define tolerance
+tol = 1e-6;
+
+% load the model
+load('ecoli_core_model.mat', 'model');
+
+%system('cp refXLSX.xlsx testData.xlsx');
+
+% write the model to an xls file
+writeCbModel(model, 'xlsx', 'testData.xlsx');
+
+% read in the xls model file
+modelIn = xls2model('testData.xlsx');
+
+% convert an old style model
+model = convertOldStyleModel(model);
+
+% test
+assert(isequal(model.lb, modelIn.lb))
+assert(isequal(model.b, modelIn.b))
+assert(isequal(model.ub, modelIn.ub))
+assert(isequal(sort(model.mets), sort(modelIn.mets)))
+assert(isequal(model.csense, modelIn.csense))
+assert(isequal(model.osense, modelIn.osense))
+assert(isequal(model.rxns, modelIn.rxns))
+assert(isequal(sort(model.genes), sort(modelIn.genes)))
+assert(isequal(model.c, modelIn.c))
+
+% NOTE: model.rules and model.S are different from modelIn.S and modelIn.rules
+%       as the metabolites are not ordered in the same way.
+
+solverOK = changeCobraSolver('glpk');
+
+if solverOK
+    % run an LP and compare the solutions
+    solModel = optimizeCbModel(model);
+    solModelIn = optimizeCbModel(modelIn);
+
+    assert(abs(solModel.f - solModelIn.f) < tol)
+    assert(solModel.stat == solModelIn.stat)
+end
+
+% remove the generated file
+delete('testData.xlsx');
+
+% change to old directory
+cd(currentDir);
