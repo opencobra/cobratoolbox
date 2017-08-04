7775a5edebfa0ba10907eccef8d094a562376188
akaviaLab <uri.david.akavia@mcgill.ca>
2017-08-04 08:44:08 -0400

    Added test script for update Genes.

--- src/reconstruction/refinement/updateGenes.m
+++ src/reconstruction/refinement/updateGenes.m
@@ -11,6 +11,10 @@ function [modelNew] = updateGenes(model)
 % OUTPUT:
 %    modelNew:           COBRA model structure with corrected genes
 %
+% Note - the gene order will be changed to sorted. You should probably use
+%        buildRxnGeneMat to make sure the order matches in all relevant
+%        fields.
+%
 % .. Authors:
 %       - written by Diana El Assal 30/06/2017 
 %       - fixed by Uri David Akavia 16/07/2017
