1640ae566b9bc8b1ffacee5af6e8fc8d1bd58c06
akaviaLab <uri.david.akavia@mcgill.ca>
2017-08-06 17:44:19 -0400

    Fix bug in generateRules and speedup

--- src/reconstruction/refinement/generateRules.m
+++ src/reconstruction/refinement/generateRules.m
@@ -13,60 +13,18 @@ function [model2] = generateRules(model)
 %    model2:    same model but with model.rules added
 %
 % .. Author: -  Aarash Bordar 11/17/2010
+%          :    Uri David Akavia 6-Aug-2017, bug fixes and speedup
 
 grRules = model.grRules;
 genes = model.genes;
-
-[m,n] = size(model.S);
-
-rules(1:n,1) = {''};
-
-for i = 1:n
-    if length(grRules{i}) > 0
-        tmp = grRules{i};
-        %The function assumes that there are no spaces between parenthesis
-        %and formulas, or it creates "empty" gene associations. so we
-        %replace all spaces trailing or preceding parenthesis.
-        tmp = regexprep(tmp,'\( *','('); %replace all spaces after opening parenthesis
-        tmp = regexprep(tmp,' *\)',')'); %replace all spaces before closing paranthesis.
-        tmp = regexprep(tmp,' * (and|AND|or|OR|And|Or)  *',' $1 '); %replace all surplus spaces around logical operators
-
-        tmp = splitString(tmp,' ');
-        tmp = strrep(tmp,' ','');
-
-        tmp2 = [];
-        for j = 1:length(tmp)
-            if strcmp(tmp{j},'or')
-                tmp2 = [tmp2,'| '];
-            elseif strcmp(tmp{j},'and')
-                tmp2 = [tmp2,'& '];
-            elseif strcmp(tmp{j}(1),'(') & strcmp(tmp{j}(end),')')
-                tmp{j} = strrep(tmp{j},'(','');
-                tmp{j} = strrep(tmp{j},')','');
-                loc = strmatch(tmp{j},genes,'exact');
-                tmp2 = [tmp2,'(x(',num2str(loc),')) '];
-            elseif strcmp(tmp{j}(1),'(')
-                tmp{j} = strrep(tmp{j},'(','');
-                tmp{j} = strrep(tmp{j},')','');
-                loc = strmatch(tmp{j},genes,'exact');
-                tmp2 = [tmp2,'(x(',num2str(loc),') '];
-            elseif strcmp(tmp{j}(end),')')
-                tmp{j} = strrep(tmp{j},'(','');
-                tmp{j} = strrep(tmp{j},')','');
-                loc = strmatch(tmp{j},genes,'exact');
-                tmp2 = [tmp2,'x(',num2str(loc),')) '];
-            else
-                loc = strmatch(tmp{j},genes,'exact');
-                tmp2 = [tmp2,'x(',num2str(loc),') '];
-            end
-        end
-
-        tmp2 = tmp2(1:end-1);
-        rules{i} = tmp2;
-
-    end
-end
-
+convertGenes = @(x) sprintf('%d', find(strcmp(x, genes)));
+tmp = regexprep(grRules,'\( *','('); %replace all spaces after opening parenthesis
+tmp = regexprep(tmp,' *\)',')'); %replace all spaces before closing paranthesis.
+tmp = regexprep(tmp, ' * (?i)(and) *', ' & ');
+tmp = regexprep(tmp, ' * (?i)(or) *', ' | ');
+rules = regexprep(tmp, '([^\(\)\|\&\ ]+)', 'x(${convertGenes($0)})');
 
 model2 = model;
 model2.rules = rules;
+
+end
---
+++ test/verifiedTests/reconstruction/testModelManipulation/testGenerateRules.m
@@ -0,0 +1,30 @@
+% The COBRAToolbox: testGenerateRules.m
+%
+% Purpose:
+%     - testGenerateRules tests generateRules
+%
+% Authors:
+%     - Initial Version: Uri David Akavia - August 2017
+
+global CBTDIR
+
+% save the current path
+currentDir = pwd;
+
+% initialize the test
+fileDir = fileparts(which('testGenerateRules'));
+cd(fileDir);
+
+modelsToTry = {'Acidaminococcus_intestini_RyC_MR95.mat', 'Acidaminococcus_sp_D21.mat', 'Recon1.0model.mat', 'ecoli_core_model.mat', 'modelReg.mat'};
+%fileList = dir([CBTDIR filesep 'test' filesep 'models' filesep '*.mat']);
+
+for i=1:length(modelsToTry)
+    model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep modelsToTry{i}]);
+    fprintf('Beginning model %s\n', fileList(i).name);
+    model2 = generateRules(model);
+    model.rules = strrep(model.rules, '  ', ' ');
+    model.rules = strrep(model.rules, ' )', ')');
+    model.rules = strrep(model.rules, '( ', '(');
+    assert(all(strcmp(model.rules, model2.rules2)));
+    fprintf('Succesfully completed model %s\n', fileList(i).name);
+end
