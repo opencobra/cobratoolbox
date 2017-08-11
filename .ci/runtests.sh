205e851b32d56c5b5582bbaebad708738361fb8d
laurentheirendt <laurent.heirendt@uni.lu>
2017-08-11 13:16:30 +0200

    Launching ci script

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -5,14 +5,12 @@ if [ "$ARCH" == "Linux" ]; then
 elif [ "$ARCH" == "macOS" ]; then
     /Applications/MATLAB_$MATLAB_VER.app/bin/matlab -nodesktop -nosplash < test/testAll.m
 elif [ "$ARCH" == "Windows" ]; then
-    echo "Je suis ici"
-    #cd /D D:
+    echo " -- Changing to the build directory --"
     cd "D:\jenkins\workspace\COBRAToolbox-windows\MATLAB_VER\R2016b\label\windows-biocore"
     # echo the current directory
-    cd
     whoami
-    #cd "D:"
-    "C:\Program Files\Matlab\R2016b\bin\matlab.exe" -logfile output.log -wait -r "initCobraToolbox;"
+    echo " -- Launching MATLAB --"
+    "C:\Program Files\Matlab\R2016b\bin\matlab.exe" -logfile output.log -wait -r "initCobraToolbox; exit;"
     #"run('D:\jenkins\workspace\COBRAToolbox-windows\MATLAB_VER\R2016b\label\windows-biocore\test\testAll.m')"
     cat output.log
 fi
