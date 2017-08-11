ae4239da2890dcdb36217b41f5a06bafcc5dbce9
laurentheirendt <laurent.heirendt@uni.lu>
2017-08-11 22:42:59 +0200

    change in the command

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -9,7 +9,7 @@ elif [ "$ARCH" == "Windows" ]; then
     cd "D:\jenkins\workspace\COBRAToolbox-windows\MATLAB_VER\R2016b\label\windows-biocore"
     whoami
     echo " -- Launching MATLAB --"
-    nohup "C:\Program Files\Matlab\R2016b\bin\matlab.exe" -useStartupFolderPref -logfile output.log -wait -r "cd test; testAll;" & # #fprintf('%s\n', getenv('USERPROFILE')); pwd; initCobraToolbox; exit;
+    nohup "C:\Program Files\Matlab\R2016b\bin\matlab.exe" -useStartupFolderPref -logfile output.log -wait -r "initCobraToolbox; cd test; testAll;" & # #fprintf('%s\n', getenv('USERPROFILE')); pwd; initCobraToolbox; exit;
     tail -n0 -F --pid=$! output.log 2>/dev/null
 fi
 
