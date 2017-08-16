70a66466dbe8042cc9a14a0a99b31dcaf08273f8
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-08-16 13:18:39 +0200

    Test for planariseModel

---
+++ test/verifiedTests/base/testIO/testUtilities/testPlanariseModel.m
@@ -0,0 +1,29 @@
+% The COBRAToolbox: testPlanariseModel.m
+%
+% Purpose:
+%     - test the planariseModel function
+%
+% Authors:
+%     - Jacek Wachowiak
+global CBTDIR
+% save the current path
+currentDir = pwd;
+
+% initialize the test
+fileDir = fileparts(which('testPlanariseModel'));
+cd(fileDir);
+
+% test variables
+model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
+
+% function outputs
+[modelPlane, replicateMetBool, metData, rxnData] = planariseModel(model);
+
+% tests
+assert(isequal(model.rxns, modelPlane.rxns));
+assert(isequal(size(model.mets), size(replicateMetBool)));
+assert(isequal(metData{1}, 'Salmon'));
+assert(isequal(rxnData{1}, 'PaleBlue'));
+
+% change to old directory
+cd(currentDir);
