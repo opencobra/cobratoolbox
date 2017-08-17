9f156416d3865ddf4c246fde44c9f0b7cab3bfc9
laurentheirendt <laurent.heirendt@uni.lu>
2017-08-17 09:33:26 +0200

    Update in gitignore after moving tutorials

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -7,9 +7,13 @@ elif [ "$ARCH" == "macOS" ]; then
 
 elif [ "$ARCH" == "Windows" ]; then
     # change to the build directory
-    echo " -- Changing to the build directory --"
+    echo " -- killing the sh process --"
+    nohup "C:\\Windows\\System32\\cmd.exe" /c taskkill /im ssh-agent.exe /f /fi "memusage gt 40" 2>NUL | findstr SUCCESS >NUL && if errorlevel 1 ( echo ssh-agent was not killed ) else ( echo ssh-agent was killed )
+    nohup "C:\\Windows\\System32\\cmd.exe" /c taskkill /im sh.exe /f /fi "memusage gt 40" 2>NUL | findstr SUCCESS >NUL && if errorlevel 1 ( echo sh was not killed ) else ( echo sh was killed )
+
+    echo " -- changing to the build directory --"
     cd "D:\\jenkins\\workspace\\COBRAToolbox-windows\\MATLAB_VER\\$MATLAB_VER\\label\\windows-biocore"
-    echo " -- Launching MATLAB --"
+    echo " -- launching MATLAB --"
 
     # launch the test suite as a background process
     nohup "C:\\Program Files\\Matlab\\$MATLAB_VER\\\bin\\matlab.exe" -nojvm -nodesktop -nosplash -useStartupFolderPref -logfile output.log -wait -r "cd test; testAll;" & PID=$!
