19c43cadb5c7ea901734e962b548f94bf2648093
laurentheirendt <laurent.heirendt@uni.lu>
2017-08-18 16:25:18 +0200

    Unset Path

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -31,9 +31,10 @@ elif [ "$ARCH" == "Windows" ]; then
     #nohup "C:\\Windows\\System32\\cmd.exe" /c "SET GIT_EXEC_PATH=C:\Program Files\Git"
 
     echo " -- launching MATLAB --"
+    unset Path
     # launch the test suite as a background process
     #matlab -logfile output.log -wait -r "system('which sh'); system('which git'); system('git --version'); system('whoami'); system('pwd'); system('git submodule');"
-    nohup "/cygdrive/c/Program Files/MATLAB/R2016b/bin/matlab" -nojvm -nodesktop -nosplash -useStartupFolderPref -logfile output.log -wait -r "system('which sh'); system('which git'); system('git --version'); system('whoami'); system('pwd'); system('git submodule');" & PID=$!
+    nohup "/cygdrive/c/Program Files/MATLAB/R2016b/bin/matlab" -nojvm -nodesktop -nosplash -useStartupFolderPref -logfile output.log -wait -r "system('which sh'); system('which git'); system('git --version'); system('whoami'); system('pwd'); system('git submodule'); exit;" & PID=$!
     #nohup "C:\\Program Files\\Matlab\\$MATLAB_VER\\\bin\\matlab.exe"  -useStartupFolderPref -logfile output.log -wait -r "system('which git'); system('git --version'); system('whoami'); system('pwd'); initCobraToolbox" & PID=$! #cd test; testAll;
     #"C:\\Program Files\\Matlab\\R2016b\\\bin\\matlab.exe" -nojvm -nodesktop -nosplash -useStartupFolderPref -logfile output.log -wait -r "initCobraToolbox;" -nojvm -nodesktop -nosplash
     # follow the log file
