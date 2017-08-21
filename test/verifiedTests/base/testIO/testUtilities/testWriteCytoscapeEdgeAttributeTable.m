27787b6cedfb45913f207176d3cb16d52449ae72
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-08-21 13:26:21 +0200

    Tests plus reference files

--- test/verifiedTests/base/testIO/testUtilities/testWriteCytoscapeEdgeAttributeTable.m
+++ test/verifiedTests/base/testIO/testUtilities/testWriteCytoscapeEdgeAttributeTable.m
@@ -16,16 +16,40 @@ cd(fileDir);
 % test variables
 model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
 [modelPlane, replicateMetBool, metData, rxnData] = planariseModel(model);
+C = cell(95, 1);
+C{1} = '1';
+B = ones(95);
+B_2 = zeros(95);
+N = zeros(95, 1);
+
 % function outputs
-%writeCytoscapeEdgeAttributeTable(model, C, B, N, replicateMetBool, filename);
-%writeCytoscapeEdgeAttributeTable(model, [], [], [], replicateMetBool);
-writeCytoscapeEdgeAttributeTable(model, {'1'}, [1], [1], replicateMetBool);
+writeCytoscapeEdgeAttributeTable(model, [], [], [], replicateMetBool);
+writeCytoscapeEdgeAttributeTable(model, C, B, N, replicateMetBool);
+model = rmfield(model, 'description');
+writeCytoscapeEdgeAttributeTable(model, C, B_2, N, replicateMetBool);
 
-% test
-assert(isequal(0, 0))
+% tests
+refFile = fopen('refData_edgeAttributes.txt', 'r');
+refVar = fscanf(refFile, '%c');
+fclose(refFile);
+testFile = fopen('edgeAttributes.txt', 'r');
+testVar = fscanf(testFile, '%c');
+fclose(testFile);
 
-% removal of the created filename
-delete 'ecoli_core_model.matedgeAttributes.txt'
+assert(isequal(refVar, testVar))
+
+refFile_2 = fopen('refData_ecoli_core_model.matedgeAttributes.txt', 'r');
+refVar_2 = fscanf(refFile_2, '%c');
+fclose(refFile_2);
+testFile_2 = fopen('ecoli_core_model.matedgeAttributes.txt', 'r');
+testVar_2 = fscanf(testFile_2, '%c');
+fclose(testFile_2);
+
+assert(isequal(refVar_2, testVar_2))
+
+% removal of the created files
+delete 'ecoli_core_model.matedgeAttributes.txt';
+delete 'edgeAttributes.txt';
 
 % change to old directory
 cd(currentDir);
