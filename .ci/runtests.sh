0cc9b2f59096d6d784f8923e21cdc35d52ca492c
laurentheirendt <laurent.heirendt@uni.lu>
2017-08-26 20:24:52 +0200

    Trigger macOS

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 if [ $ARCH = "Linux" ]; then
     /mnt/data/MATLAB/$MATLAB_VER/bin/./matlab -nodesktop -nosplash < test/testAll.m
+
 elif [ $ARCH = "macOS" ]; then
     /Applications/MATLAB_$MATLAB_VER.app/bin/matlab -nodesktop -nosplash < test/testAll.m
+
 elif [ $ARCH = "Windows" ]; then
     c:/Program\ Files/MATLAB/$MATLAB_VER/bin/matlab -nodesktop -nosplash < test/testAll.m
 fi
