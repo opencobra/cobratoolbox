e8edb81349a5da11a73f3591ce43dcfed9867b3e
laurentheirendt <laurent.heirendt@uni.lu>
2017-08-26 20:32:52 +0200

    change in options

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -3,7 +3,7 @@ if [ $ARCH = "Linux" ]; then
     /mnt/data/MATLAB/$MATLAB_VER/bin/./matlab -nodesktop -nosplash < test/testAll.m
 
 elif [ $ARCH = "macOS" ]; then
-    /Applications/MATLAB_$MATLAB_VER.app/bin/matlab -nodesktop -nosplash < test/testAll.m
+    /Applications/MATLAB_$MATLAB_VER.app/bin/matlab -nosplash < test/testAll.m
 
 elif [ $ARCH = "Windows" ]; then
     c:/Program\ Files/MATLAB/$MATLAB_VER/bin/matlab -nodesktop -nosplash < test/testAll.m
