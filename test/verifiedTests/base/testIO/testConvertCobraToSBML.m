f760d0062ab1252bf041a50dc00c070b24e0c3ce
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-08-28 12:08:55 +0200

    Test for convertCobraToSBML

---
+++ test/verifiedTests/base/testIO/testConvertCobraToSBML.m
@@ -0,0 +1,33 @@
+% The COBRAToolbox: testConvertCobraToSBML.m
+%
+% Purpose:
+%     - test the convertCobraToSBML function
+%
+% Authors:
+%     - Jacek Wachowiak
+global CBTDIR
+% save the current path
+currentDir = pwd;
+
+% initialize the test
+fileDir = fileparts(which('testConvertCobraToSBML'));
+cd(fileDir);
+
+% test variables
+sbmlLevel = 0;
+sbmlVersion = 0;
+compSymbolList = '';
+compNameList = '';
+model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
+
+% function outputs
+warning('off', 'all')
+  sbmlModel = convertCobraToSBML(model, sbmlLevel, sbmlVersion, compSymbolList, compNameList);
+  sbmlModel_2 = convertCobraToSBML(model, sbmlLevel, sbmlVersion);
+  assert(length(lastwarn()) > 0)
+warning('on', 'all')
+% test
+assert(isequal(sbmlModel, sbmlModel_2));
+
+% change to old directory
+cd(currentDir);
