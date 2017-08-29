aaffc4201dffc6ff848653a54db6bda313ea61b3
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-08-29 12:46:53 +0200

    Test for sbmlTestModelToMat

--- test/verifiedTests/base/testIO/testSbmlTestModelToMat.m
+++ test/verifiedTests/base/testIO/testSbmlTestModelToMat.m
@@ -20,7 +20,16 @@ CBTDIR = [];
 sbmlTestModelToMat();
 
 % test
-assert(isequal(0, 0));
+x = exist('m_model_collection');
+assert(isequal(x, 7));
+cd([CBTDIR, filesep, 'test', filesep, 'verifiedTests', filesep, 'base', filesep, 'testIO', filesep, 'm_model_collection']);
+x = exist('Abiotrophia_defectiva_ATCC_49176.xml');
+assert(isequal(x, 2));
+x = exist('Abiotrophia_defectiva_ATCC_49176.mat');
+assert(isequal(x, 2));
+x = exist('Sc_iND750_flux1.xml');
+assert(isequal(x, 2));
+cd([CBTDIR, filesep, 'test', filesep, 'verifiedTests', filesep, 'base', filesep, 'testIO']);
 
 % change to old directory
 cd(currentDir);
