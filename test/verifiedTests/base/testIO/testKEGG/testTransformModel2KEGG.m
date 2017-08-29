611a402b74b9cf064d450b83f171350dfbd213a9
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-08-29 13:58:10 +0200

    Test for transformModelToKEGG

---
+++ test/verifiedTests/base/testIO/testKEGG/testTransformModel2KEGG.m
@@ -0,0 +1,34 @@
+% The COBRAToolbox: testTransformModel2KEGG.m
+%
+% Purpose:
+%     - test the transformModel2KEGG function
+%
+% Authors:
+%     - Jacek Wachowiak
+global CBTDIR
+% save the current path
+currentDir = pwd;
+
+% initialize the test
+fileDir = fileparts(which('testTransformModel2KEGG'));
+cd(fileDir);
+
+% test variables
+model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
+Dictionary = {'0', '1'; '2', '3'};
+ref_KEGGID = cell(72, 1);
+
+% function outputs
+modelKEGG = transformModel2KEGG(model, Dictionary);
+try
+    modelKEGG_2 = transformModel2KEGG(model);
+catch ME
+    assert(length(ME.message) > 0);
+end
+
+% test
+assert(isequal(modelKEGG.metsAbr, model.mets));
+assert(isequal(modelKEGG.metKEGGID, ref_KEGGID));
+
+% change to old directory
+cd(currentDir);
