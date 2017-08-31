6a92aebd07e650c9bb2440e07d6abd2b1703c100
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-08-31 13:56:09 +0200

    Test for convertCobraLP2mps

--- test/verifiedTests/base/testIO/testConvertCobraLP2mps.m
+++ test/verifiedTests/base/testIO/testConvertCobraLP2mps.m
@@ -14,13 +14,17 @@ fileDir = fileparts(which('testConvertCobraLP2mps'));
 cd(fileDir);
 
 % test variables
-model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
+LPProblem = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
+LPProblem_2 = rmfield(LPProblem, 'csense');
+LPProblem_2 = rmfield(LPProblem_2, 'osense');
 
 % function outputs
-OK = convertCobraLP2mps(LPProblem)
+OK = convertCobraLP2mps(LPProblem);
+OK_2 = convertCobraLP2mps(LPProblem_2);
 
 % test
-assert(isequal(0, 0));
+assert(isequal(OK, 1));
+assert(isequal(OK_2, 1));
 
 % change to old directory
 cd(currentDir);
