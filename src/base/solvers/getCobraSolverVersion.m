3d8f4a977dd107f5b611a53eb00d0bdcf7c53b27
laurentheirendt <laurent.heirendt@uni.lu>
2017-09-08 11:18:27 +0200

    Change of getCobraSolverVersion in associated functions

--- src/base/solvers/getCobraSolverVersion.m
+++ src/base/solvers/getCobraSolverVersion.m
@@ -1,15 +1,16 @@
-function cplexVersion = getCPLEXversion(rootPathCPLEX, printLevel)
+function solverVersion = getCobraSolverVersion(solverName, rootPathSolver, printLevel)
 % detects the CPLEX version
 %
 % USAGE:
-%    cplexVersion = getCPLEXversion(rootPathCPLEX, printLevel)
+%    solverVersion = getCobraSolverVersion(solverName, rootPathSolver, printLevel)
 %
 % INPUT:
-%    rootPathCPLEX:     Path to the CPLEX installation
+%    solverName:        Name of the solver
+%    rootPathSolver:    Path to the CPLEX installation
 %    printLevel:        verbose level (default: 0)
 %
 % OUTPUT:
-%    cplexVersion:      string that contains the CPLEX version number
+%    solverVersion:     string that contains the CPLEX version number
 %
 % .. Author: - Laurent Heirendt, June 2017
 %
@@ -21,7 +22,7 @@ function cplexVersion = getCPLEXversion(rootPathCPLEX, printLevel)
     % save the userpath
     originalUserPath = path;
 
-    if nargin > 1
+    if nargin > 2
         restoredefaultpath;
     end
 
@@ -34,18 +35,18 @@ function cplexVersion = getCPLEXversion(rootPathCPLEX, printLevel)
 
     % Set the CPLEX file path
     index = strfind(ILOG_CPLEX_PATH, 'cplex') + 4;
-    rootPathCPLEX = ILOG_CPLEX_PATH(1:index);
+    rootPathSolver = ILOG_CPLEX_PATH(1:index);
 
-    if nargin < 2
+    if nargin < 3
         printLevel = 1;
     end
 
     % try to set the ILOG cplex solver
     %cplexInstalled = changeCobraSolver('ibm_cplex', 'LP', printLevel);
-    rootPathCPLEX = strrep(rootPathCPLEX, '~', getenv('HOME'));
-    addpath(genpath(rootPathCPLEX));
+    rootPathSolver = strrep(rootPathSolver, '~', getenv('HOME'));
+    addpath(genpath(rootPathSolver));
 
-    if exist(rootPathCPLEX, 'dir') == 7
+    if exist(rootPathSolver, 'dir') == 7
         cplexInstalled = true;
     else
         cplexInstalled = false;
@@ -56,27 +57,27 @@ function cplexVersion = getCPLEXversion(rootPathCPLEX, printLevel)
         possibleVersions = {'1262', '1263', '1270', '1271'};
 
         % check the version based on the presence of a precompiled MEX file
-        cplexVersion = 'undetermined';
+        solverVersion = 'undetermined';
         for i = 1:length(possibleVersions)
             if isunix == 1 && ismac ~= 1
-                versionLink = [rootPathCPLEX filesep 'matlab/x86-64_linux/cplexlink' possibleVersions{i} '.mexa64'];
+                versionLink = [rootPathSolver filesep 'matlab/x86-64_linux/cplexlink' possibleVersions{i} '.mexa64'];
             elseif ismac == 1
-                versionLink = [rootPathCPLEX filesep 'matlab/x86-64_osx/cplexlink' possibleVersions{i} '.mexmaci64'];
+                versionLink = [rootPathSolver filesep 'matlab/x86-64_osx/cplexlink' possibleVersions{i} '.mexmaci64'];
             else
-                versionLink = [rootPathCPLEX filesep 'matlab\x64_win64\cplexlink' possibleVersions{i} '.mexw64'];
+                versionLink = [rootPathSolver filesep 'matlab\x64_win64\cplexlink' possibleVersions{i} '.mexw64'];
             end
 
             % if the file exists, set the version
             if exist(versionLink) == 3
-                cplexVersion = possibleVersions{i};
+                solverVersion = possibleVersions{i};
             end
         end
 
-        if ~strcmpi(cplexVersion, 'undetermined')
-            fprintf([' > The CPLEX version has been determined as ' cplexVersion '.\n']);
+        if ~strcmpi(solverVersion, 'undetermined')
+            fprintf([' > The CPLEX version has been determined as ' solverVersion '.\n']);
         else
-            fprintf([' > CPLEX installation path: ', rootPathCPLEX, '\n']);
-            fprintf([' > The CPLEX version is ' cplexVersion '\n. Your currently installed version of CPLEX is unsupported or you have multiple versions of CPLEX in the path.']);
+            fprintf([' > CPLEX installation path: ', rootPathSolver, '\n']);
+            fprintf([' > The CPLEX version is ' solverVersion '\n. Your currently installed version of CPLEX is unsupported or you have multiple versions of CPLEX in the path.']);
         end
     else
         error(['CPLEX is not installed. Please follow the installation instructions here: ', ...
