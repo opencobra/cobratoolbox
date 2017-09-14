18105bb04dfdb1383ee42d904de01af4c8ff701d
Thomas Pfau <thomas.pfau@uni.lu>
2017-09-14 09:43:29 +0200

    :racehorse: Speedup and cleanup

--- src/reconstruction/modelGeneration/fastLeakTest.m
+++ src/reconstruction/modelGeneration/fastLeakTest.m
@@ -27,18 +27,15 @@ end
 tol = 1e-06;
 modelClosed = model;
 % find all reactions that have only one entry in S
-clear count ExR
-for i = 1 :size(modelClosed.S,2)
-    if length(find(modelClosed.S(:,i))<0)==1
-        count(i,1)=1;
-        modelClosed.lb(i)=0;
-               % modelClosed.ub(i)=0;
-    elseif length(find(modelClosed.S(:,i))>0)==1
-        count(i,1)=1;
-     %   modelClosed.ub(i)=0;
-        modelClosed.lb(i)=0;
-    end
-end
+exp = full((sum(model.S ~= 0) == 1) & (sum(model.S < 0) == 1))';
+upt = full((sum(model.S ~= 0) == 1) & (sum(model.S > 0) == 1))';
+count = exp | upt;
+%Exporters should not be able to have a flux lower than zero
+%Importers should not be able to have a flux larger than zero
+
+modelClosed.lb(exp) = 0;
+modelClosed.ub(upt) = 0;
+
 ExR = modelClosed.rxns(find(count));
 
 modelexchangesAbbr = unique([testRxns;ExR]);
@@ -53,18 +50,24 @@ else
     rxnNames = '';
 end
 modelexchangesAbbr = unique([modelexchangesAbbr;rxnNames']);
-TestRxnNum = length(modelexchangesAbbr)
+TestRxnNum = length(modelexchangesAbbr);
 FluxExV =[];
 while cnt == 1
     modelClosed = changeObjective(modelClosed,modelexchangesAbbr);
     FF2=optimizeCbModel(modelClosed,'max');
-   ObjValue = FF2.f
+    if FF2.stat == 0
+        %This should not happen, but can due to constraints making the
+        %problem infeasible. However this should not happen in a leak test
+        %so we throw an error.
+        error(['Trivial solution is not a solution of the model.\n',...
+               'Check that you are not enforcing flux as Leak testing does not work with forced fluxes.']);
+    end
+   ObjValue = FF2.f;
     if FF2.f >= tol
         FluxR = modelClosed.rxns(find(abs(FF2.x)>tol));
         FluxEx = [FluxEx;intersect(modelexchangesAbbr,FluxR)];
         FluxExV = [FluxExV;FF2.x(find(ismember( modelClosed.rxns,intersect(modelexchangesAbbr,FluxR))))];
         modelexchangesAbbr = setdiff(modelexchangesAbbr, FluxEx);
-        length(unique(FluxEx))
     else
         cnt = 2;
     end
