20ca136d59ce91dd139eae4f68ed4dd3479922b6
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-09-18 14:41:42 +0200

    test for singleProductionEnvelope WIP

---
+++ test/verifiedTests/design/testSingleProductionEnvelope.m
@@ -0,0 +1,31 @@
+% The COBRAToolbox: testSingleProductionEnvelope.m
+%
+% Purpose:
+%     - test the singleProductionEnvelope function
+%
+% Authors:
+%     - Jacek Wachowiak
+global CBTDIR
+% save the current path
+currentDir = pwd;
+
+% initialize the test
+fileDir = fileparts(which('testSingleProductionEnvelope'));
+cd(fileDir);
+
+% test variables
+model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
+deletions = model.rxns(2);
+product = char(model.rxns(3));
+biomassRxn = char(model.rxns(1));
+
+% function outputs
+% requires Global Optimization Toolbox
+singleProductionEnvelope(model, deletions, product, biomassRxn, 'savePlot', 1);
+singleProductionEnvelope(model, deletions, product, biomassRxn, 'geneDelFlag', 1);
+
+% test
+assert(isequal(0, 0));
+
+% change to old directory
+cd(currentDir);
