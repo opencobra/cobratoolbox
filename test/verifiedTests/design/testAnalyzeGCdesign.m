5ec4214b3bf68758accbf6666d53e58903eefd78
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-09-18 13:41:18 +0200

    Test for analyzeGCDesign WIP

---
+++ test/verifiedTests/design/testAnalyzeGCdesign.m
@@ -0,0 +1,31 @@
+% The COBRAToolbox: testAnalyzeGCdesign.m
+%
+% Purpose:
+%     - test the analyzeGCdesign function
+%
+% Authors:
+%     - Jacek Wachowiak
+global CBTDIR
+% save the current path
+currentDir = pwd;
+
+% initialize the test
+fileDir = fileparts(which('testAnalyzeGCdesign'));
+cd(fileDir);
+
+% test variables
+model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
+selectedRxns = model.rxns(2);
+target = model.rxns(3);
+deletions = model.rxns(1);
+modelRed = reduceModel(model)
+
+% function outputs
+% requires Global Optimization Toolbox
+[improvedRxns, intermediateSlns] = analyzeGCdesign(modelRed, selectedRxns, target, deletions)
+
+% test
+assert(isequal(0, 0));
+
+% change to old directory
+cd(currentDir);
