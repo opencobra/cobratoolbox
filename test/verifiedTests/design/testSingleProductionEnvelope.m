3ab0824b71c3f38968c8d3b13cff04d9cdd127f5
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-09-19 12:54:08 +0200

    Test for singleProductionEnvelope

--- test/verifiedTests/design/testSingleProductionEnvelope.m
+++ test/verifiedTests/design/testSingleProductionEnvelope.m
@@ -16,16 +16,31 @@ cd(fileDir);
 % test variables
 model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
 deletions = model.rxns(2);
+deletions2 = model.genes(2);
 product = char(model.rxns(3));
 biomassRxn = char(model.rxns(1));
 
 % function outputs
 % requires Global Optimization Toolbox
+% this function's output is a plot therefore it can be tested only by checking if the plots were created and then deleted successfully
 singleProductionEnvelope(model, deletions, product, biomassRxn, 'savePlot', 1);
-%singleProductionEnvelope(model, deletions, product, biomassRxn, 'geneDelFlag', 1);
+singleProductionEnvelope(model, deletions2, product, biomassRxn, 'geneDelFlag', 1);
 
-% test
-assert(isequal(0, 0));
+% remove the created plots and the folder - loop for windows because apparently 'rmdir' fails sometimes
+ isdeleted = false;
+ while ~isdeleted
+   try
+     [status, message] = rmdir([CBTDIR filesep 'tutorials' filesep 'additionalTutorials' filesep 'optKnock' filesep 'Results'], 's');
+     isdeleted = true;
+   catch
+     pause(1); %wait a second before retry
+     rehash;
+   end
+ end
+
+% test for the successful removal of the files
+assert(isequal(status, 1));
+assert(isequal(message, ''));
 
 % change to old directory
 cd(currentDir);
