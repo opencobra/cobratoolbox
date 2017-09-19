7a70fddfd73aafa7e40ec7aad4f825caca5880e1
Thomas Pfau <thomas.pfau@uni.lu>
2017-09-19 10:43:05 +0200

    Moved Gene Field recreation to removeUnusedGenes function, as it is called in updateGenes anyways and this would otherwise lead to a back and force

--- src/reconstruction/refinement/removeUnusedGenes.m
+++ src/reconstruction/refinement/removeUnusedGenes.m
@@ -17,23 +17,33 @@ function modelNew = removeUnusedGenes(model)
 %           - Sjoerd Opdam - 6/24/2014
 %           - Thomas Pfau - June 2016 - updated to catch all fields.
 
+modelNew=model;
 if ~isfield(model,'genes')
-    %This should not happen, but well, we can generate the genes field from
-    %the grRules field, if that exists.
-    model = updateGenes(model);
+    %This is VERY odd and should not happen, but lets see    
+    grRules = model.grRules;
+    grRules = grRules(~cellfun(@isempty,grRules));
+    genes = regexprep(grRules, {'and', 'AND', 'or', 'OR', '(', ')'}, '');
+    genes = splitString(genes, ' ');
+    genes = [genes{:}]';
+    genes = unique(genes(cellfun('isclass', genes, 'char')));    
+    %Now, we created the genes field, so lets build the rules field as
+    %well.
+    modelNew.genes = genes(~cellfun('isempty', genes));  % Not sure this is necessary anymore, but it won't hurt.
+    modelNew = generateRules(modelNew);   
 end
 
+
 if ~isfield(model,'rules')
-    model = generateRules(model);
+    modelNew = generateRules(modelNew);
 end
 
 if ~isfield(model,'rxnGeneMat')
     %If it doesn't exist, we generate the rxnGeneMat field.
-    model = buildRxnGeneMat(model);
+    modelNew = buildRxnGeneMat(modelNew);
 end
 
-genesToRemove = sum(model.rxnGeneMat) == 0;
-model = removeFieldEntriesForType(model,genesToRemove,'genes',numel(model.genes));
-modelNew=model;
+genesToRemove = sum(modelNew.rxnGeneMat) == 0;
+modelNew = removeFieldEntriesForType(modelNew,genesToRemove,'genes',numel(modelNew.genes));
+
 
 end
