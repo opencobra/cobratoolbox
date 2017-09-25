1c808206f41ccc5f25bacf9517dbf18c554bb72d
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-09-25 11:32:01 +0200

    Added tests

--- test/verifiedTests/design/testSimpleOptKnock.m
+++ test/verifiedTests/design/testSimpleOptKnock.m
@@ -19,9 +19,18 @@ targetRxn = model.rxns(1);
 
 % function outputs
 [wtRes, delRes] = simpleOptKnock(model, targetRxn);
-[wtRes, delRes] = simpleOptKnock(model, targetRxn, model.genes, 1, 0.05, 1);
-% test for the successful removal of the files
-assert(isequal(0, 0));
+[wtRes2, delRes2] = simpleOptKnock(model, targetRxn, model.genes, 1, 0.05, 1);
+
+% tests
+assert(isequal(wtRes.minProd, wtRes2.minProd));
+assert(isequal(wtRes.maxProd, wtRes2.maxProd));
+assert(isequal(wtRes.growth, wtRes2.growth));
+assert(isequal(size(delRes.minProd), size(zeros(95,1))));
+assert(isequal(size(delRes.maxProd), size(zeros(95,1))));
+assert(isequal(size(delRes.growth), size(zeros(95,1))));
+assert(isequal(size(delRes2.minProd), size(zeros(137))));
+assert(isequal(size(delRes2.maxProd), size(zeros(137))));
+assert(isequal(size(delRes2.growth), size(zeros(137))));
 
 % change to old directory
 cd(currentDir);
