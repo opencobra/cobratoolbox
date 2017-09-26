e55f923570bef01554d849aaaa8d4581d0313bd5
Thomas Pfau <thomas.pfau@uni.lu>
2017-09-26 13:25:30 +0200

    Updated test to really test outputs

--- test/verifiedTests/base/testIO/testUtilities/testGetDatabaseMappings.m
+++ test/verifiedTests/base/testIO/testUtilities/testGetDatabaseMappings.m
@@ -14,15 +14,32 @@ fileDir = fileparts(which('testGetDatabaseMappings'));
 cd(fileDir);
 
 % test variables
-field = 'mets';
+field = 'met';
 qualifiers = {};
 refData_returnedmappings = cell(0, 5);
 
 % function outputs
 returnedmappings = getDatabaseMappings(field, qualifiers);
 
-% test
+%We request "no" qualifiers, so we expect nothing to be returned (i.e. an empty 0,5 cell array.
 assert(isequal(refData_returnedmappings, returnedmappings));
+qualifiers = 'is';
+returnedmappings = getDatabaseMappings(field, qualifiers);
+%We request is, so we should get only 'is'
+assert(size(returnedmappings,1) > 0);
+assert(all(cellfun(@(x) isequal('is',x), returnedmappings(:,2))));
+
+field = 'rxn';
+%Lets request everything for rxns
+returnedmappings = getDatabaseMappings(field);
+%We know that e.g. kegg.reaction is part of the mapped fields.
+keggpos = ismember(returnedmappings(:,1),'kegg');
+isqualifiers = cellfun(@(x) strcmp('is',x), returnedmappings(:,2));
+assert(any(keggpos));
+
+%Now, we can savely assume, that rxnKEGGID should be the target of a 'is' qualifier for kegg. 
+assert(isequal(returnedmappings{keggpos & isqualifiers,3},'rxnKEGGID'));
+
 
 % change to old directory
 cd(currentDir);
