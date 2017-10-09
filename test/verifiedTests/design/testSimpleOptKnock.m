a0084a4b7b0bea2a28161d0da67109375bde04b2
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-10-09 14:02:48 +0200

    WIP

--- test/verifiedTests/design/testSimpleOptKnock.m
+++ test/verifiedTests/design/testSimpleOptKnock.m
@@ -14,24 +14,27 @@ fileDir = fileparts(which('testSimpleOptKnock'));
 cd(fileDir);
 
 % test variables
-model = createModel();
-model = addReaction(model,'R1','reactionFormula','A <=>','lowerBound',-20);
-model = addReaction(model,'R2','A -> B');
-model = addReaction(model,'R3','B -> C + D');
-model = addReaction(model,'R4','reactionFormula','D -> E','geneRule','(G1 and G2) and (G3 or G4)');
-model = addReaction(model,'R4a','reactionFormula','D -> 0.5 C + 0.5 E','geneRule','(G1 and G2) and (G3 or G4)');
-model = addReaction(model,'R5','C -> E');
-model = addReaction(model,'R6','reactionFormula','E <=> ', 'objectiveCoef',1);
-model = addReaction(model,'R7','reactionFormula','D <=> ','lowerBound',0);
+model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
+targetRxn = model.rxns(1);
 
 % function outputs
-[wtRes, delRes] = simpleOptKnock(model, 'R7', {'R4'})
-[wtRes2, delRes2] = simpleOptKnock(model, 'R7', {'R4', 'R4a'})
-[wtRes3, delRes3] = simpleOptKnock(model, 'R7', {'G1'}, 1)
-[wtRes4, delRes4] = simpleOptKnock(model, 'R7', {'G3', 'G4'}, 1, 0.05, 1)
+[wtRes, delRes] = simpleOptKnock(model, targetRxn);
+%[wtRes2, delRes2] = simpleOptKnock(model, targetRxn, model.genes, 1, 0.05, 1);
+
+%[wtRes, delRes] = simpleOptKnock(model, 'R7', {'R4'})
+%[wtRes2, delRes2] = simpleOptKnock(model, 'R7', {'R4', 'R4a'})
+%[wtRes3, delRes3] = simpleOptKnock(model, 'R7', {'G1'}, 1)
+%[wtRes4, delRes4] = simpleOptKnock(model, 'R7', {'G3', 'G4'}, 1, 0.05, 1)
 
 % tests
-assert(isequal(0, 0));
+assert(isequal(size(delRes.minProd), size(zeros(95,1))));
+assert(isequal(size(delRes.maxProd), size(zeros(95,1))));
+assert(isequal(size(delRes.growth), size(zeros(95,1))));
+assert(isequal(size(delRes2.minProd), size(zeros(137))));
+assert(isequal(size(delRes2.maxProd), size(zeros(137))));
+assert(isequal(size(delRes2.growth), size(zeros(137))));
+
+%assert(isequal(0, 0));
 
 
 % change to old directory
