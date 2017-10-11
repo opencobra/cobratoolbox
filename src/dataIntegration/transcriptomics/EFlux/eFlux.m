cd471939cf1db04159c4ab053f7235150d38f9c7
Thomas Pfau <thomas.pfau@uni.lu>
2017-10-11 12:26:57 +0200

    Updated

--- src/dataIntegration/transcriptomics/EFlux/eFlux.m
+++ src/dataIntegration/transcriptomics/EFlux/eFlux.m
@@ -1,14 +1,38 @@
-function [ constraintModel ] = eFlux( model, expression, expressionFunction, excludedReactions)
-% EFLUX implementation of the EFlux algorthm byy Coljin et al.
+function [ constraintModel ] = eFlux( model, expression, minSum)
+% Implementation of the EFlux algorthm by Coljin et al.
 %
 % USAGE:
 %
 %    constraintModel = eFlux(model,expression)
 %
+% INPUTS:
+%    model:         The model to Constrain.
+%    expression:    struct with two fields required and one optional field:
+%                   * .target       - the names of the target (rxns or genes)
+%                   * .value        - the value for the target. Positive values
+%                                     for all constraint reactions, negative
+%                                     values for unconstraint reactions.
+%                   * .preprocessed - Indicator whether the provided
+%                                     targets are genes (false), or reactions (true)
+%                                     Default: false
+%
+% OPTIONAL INPUTS:
+%
+%    minSum:       Switch for the processing of Genetic data. If false
+%                  (default), ORs in the GPR will be treated as min. If
+%                  true, ORs will be treated as addition.
 %
 
-if ~exist('expressionFunction','var')
-    expressionFunction = @(x) x;
+if ~isfield(expression,'preprocessed')
+    expression.preprocessed = 0;
+end
+
+if ~expression.preprocessed
+    reactionExpression = mapExpressionToReactions(model,struct('gene',expression.target,'value',expression.value));
+else
+    reactionExpression = -ones(size(model.rxns));
+    [pres,pos] = ismember(model.rxns,expression.target);
+    reactionExpression(pres) = expression.target(pos(pres));    
 end
 
 unconstraintReactions = expression == -1;
