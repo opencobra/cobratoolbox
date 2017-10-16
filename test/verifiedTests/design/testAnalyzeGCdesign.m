9a4a302945955c7858f53983c670f4f8a11f2a0d
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-10-16 16:11:57 +0200

    Fixed, tests passing

--- test/verifiedTests/design/testAnalyzeGCdesign.m
+++ test/verifiedTests/design/testAnalyzeGCdesign.m
@@ -16,19 +16,29 @@ cd(fileDir);
 % test variables
 model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
 modelRed = reduceModel(model);
-selectedRxns = modelRed.rxns(20);
-target = modelRed.rxns(20);
-deletions = modelRed.rxns(60:87);
+selectedRxns = modelRed.rxns(22:25);
+target = modelRed.rxns(20); % 'EX_ac(e)'
+deletions = modelRed.rxns(21);
+improvedRxnsM = {};
+intermediateSlnsM = {};
 
 % function outputs
-% function operates on a not full rank matrix and therefore cannot end without an error
+% solver change due to instability of QP
 changeCobraSolver('gurobi', 'QP');
+[improvedRxns, intermediateSlns] = analyzeGCdesign(modelRed, selectedRxns, target, deletions);
 
-[improvedRxns, intermediateSlns] = analyzeGCdesign(modelRed, selectedRxns, target, deletions)
-
-for i=2:9
-      [improvedRxns, intermediateSlns] = analyzeGCdesign(modelRed, selectedRxns, target, deletions, i, i)
+for i=2:8
+  i
+      [improvedRxns2, intermediateSlns2] = analyzeGCdesign(modelRed, selectedRxns, target, deletions, i, i);
+      improvedRxnsM{end+1} = improvedRxns2;
+      intermediateSlnsM{end+1} = intermediateSlns2;
 end
+
+% tests
+assert(isequal({'EX_akg(e)', 'EX_co2(e)', 'EX_etoh(e)'}, improvedRxns));
+assert(isequal({'EX_acald(e)'}, improvedRxnsM{7}));
+assert(isequal({'EX_acald(e)'}, intermediateSlnsM{7}{1}));
+
 
 % change to old directory
 cd(currentDir);
