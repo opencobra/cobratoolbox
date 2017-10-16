bb139eaf2044a585eb139d1400486fc736a3892f
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-10-16 17:15:02 +0200

    Test with ecoli model

--- test/verifiedTests/design/testSimpleOptKnock.m
+++ test/verifiedTests/design/testSimpleOptKnock.m
@@ -15,27 +15,22 @@ cd(fileDir);
 
 % test variables
 model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
-targetRxn = model.rxns(1);
+targetRxn = model.rxns(20); % 'EX_ac(e)'
 
 % function outputs
 [wtRes, delRes] = simpleOptKnock(model, targetRxn);
-%[wtRes2, delRes2] = simpleOptKnock(model, targetRxn, model.genes, 1, 0.05, 1);
-
-%[wtRes, delRes] = simpleOptKnock(model, 'R7', {'R4'})
-%[wtRes2, delRes2] = simpleOptKnock(model, 'R7', {'R4', 'R4a'})
-%[wtRes3, delRes3] = simpleOptKnock(model, 'R7', {'G1'}, 1)
-%[wtRes4, delRes4] = simpleOptKnock(model, 'R7', {'G3', 'G4'}, 1, 0.05, 1)
+[wtRes2, delRes2] = simpleOptKnock(model, targetRxn, model.genes(1), 1, 0.5, 1);
 
 % tests
-assert(isequal(size(delRes.minProd), size(zeros(95,1))));
-assert(isequal(size(delRes.maxProd), size(zeros(95,1))));
-assert(isequal(size(delRes.growth), size(zeros(95,1))));
-assert(isequal(size(delRes2.minProd), size(zeros(137))));
-assert(isequal(size(delRes2.maxProd), size(zeros(137))));
-assert(isequal(size(delRes2.growth), size(zeros(137))));
-
-%assert(isequal(0, 0));
-
+assert(abs(12.1585 - delRes.maxProd(23)) < 1e-4); % 'EX_co2(e)'
+assert(abs(14.3123 - delRes.maxProd(12)) < 1e-4);% 'ATPS4r'
+assert(abs(12.1584 - delRes.minProd(14)) < 1e-4);% 'CO2t'
+assert(abs(8.5036 - delRes.minProd(16)) < 1e-4);% 'CYTBD'
+assert(abs(0.8739 - wtRes.growth) < 1e-4);% always the same
+%for gene deletions
+assert(abs(0.8739 - wtRes2.growth) < 1e-4);% always the same
+assert(abs(3e-07 - delRes2.maxProd) < 1e-4);
+assert(abs(delRes2.minProd) < 1e-4);
 
 % change to old directory
 cd(currentDir);
