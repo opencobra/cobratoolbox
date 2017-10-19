f7dc8f1aaa8687925739dd88daba48864f207d45
Thomas Pfau <thomas.pfau@uni.lu>
2017-10-19 14:19:36 +0200

    Replace brackets first for easier regexp rules

--- src/base/parseGPR.m
+++ src/base/parseGPR.m
@@ -27,13 +27,12 @@ function [ruleString, totalGeneList,newGeneList] = parseGPR( grRuleString, curre
 %
 % .. Author: -  Thomas Pfau Okt 2017 
 
-
-tmp = regexprep(grRuleString,'([\(\{\[])\s*','$1'); %replace all spaces after opening parenthesis
-tmp = regexprep(tmp,'\s*([\]\}\)])','$1'); %replace all spaces before closing paranthesis.
-tmp = regexprep(tmp, '([\]\}\)]\s?|\s)\s*(?i)(and)\s*?(\s?[\[\{\(]|\s)\s*', '$1&$3'); %Replace all ands
-tmp = regexprep(tmp, '([\]\}\)]\s?|\s)\s*(?i)(or)\s*?(\s?[\[\{\(]|\s)\s*', '$1|$3'); %replace all ors
 tmp = regexprep(tmp, '[\]\}]',')'); %replace other brackets by parenthesis.
 tmp = regexprep(tmp, '[\[\{]','('); %replace other brackets by parenthesis.
+tmp = regexprep(grRuleString,'([\(])\s*','$1'); %replace all spaces after opening parenthesis
+tmp = regexprep(tmp,'\s*([\)])','$1'); %replace all spaces before closing paranthesis.
+tmp = regexprep(tmp, '([\)]\s?|\s)\s*(?i)(and)\s*?(\s?[\(]|\s)\s*', '$1&$3'); %Replace all ands
+tmp = regexprep(tmp, '([\)]\s?|\s)\s*(?i)(or)\s*?(\s?[\(]|\s)\s*', '$1|$3'); %replace all ors
 
 %Now, genes are items which do not have brackets, operators or whitespace
 %characters
