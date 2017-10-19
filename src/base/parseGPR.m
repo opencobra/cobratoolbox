1f7163e969c1fc663ebe9df8718960829b833b22
Thomas Pfau <thomas.pfau@uni.lu>
2017-10-19 14:53:29 +0200

    :bug: using the wrong input for the first regexprep

--- src/base/parseGPR.m
+++ src/base/parseGPR.m
@@ -27,9 +27,9 @@ function [ruleString, totalGeneList,newGeneList] = parseGPR( grRuleString, curre
 %
 % .. Author: -  Thomas Pfau Okt 2017 
 
-tmp = regexprep(tmp, '[\]\}]',')'); %replace other brackets by parenthesis.
+tmp = regexprep(grRuleString, '[\]\}]',')'); %replace other brackets by parenthesis.
 tmp = regexprep(tmp, '[\[\{]','('); %replace other brackets by parenthesis.
-tmp = regexprep(grRuleString,'([\(])\s*','$1'); %replace all spaces after opening parenthesis
+tmp = regexprep(tmp,'([\(])\s*','$1'); %replace all spaces after opening parenthesis
 tmp = regexprep(tmp,'\s*([\)])','$1'); %replace all spaces before closing paranthesis.
 tmp = regexprep(tmp, '([\)]\s?|\s)\s*(?i)(and)\s*?(\s?[\(]|\s)\s*', '$1&$3'); %Replace all ands
 tmp = regexprep(tmp, '([\)]\s?|\s)\s*(?i)(or)\s*?(\s?[\(]|\s)\s*', '$1|$3'); %replace all ors
