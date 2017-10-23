96c3fd6b17132a8b9e26d05eb0bb018770f6d7a9
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-10-23 12:55:10 +0200

    Minor fixes

--- test/verifiedTests/design/testTheoretMaxProd.m
+++ test/verifiedTests/design/testTheoretMaxProd.m
@@ -16,13 +16,14 @@ cd(fileDir);
 % test variables
 model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
 
-%[ExRxns, MaxTheoOut] = theoretMaxProd(model, inputrxn, criterion, normalize, rxns)
+% function calls
 [ExRxns, MaxTheoOut] = theoretMaxProd(model, model.rxns(20)); % 'EX_ac(e)'
 [ExRxns1, MaxTheoOut1] = theoretMaxProd(model, model.rxns(20), '', true, findExcRxns(model,0,0)); % not default options
 [ExRxns2, MaxTheoOut2] = theoretMaxProd(model, model.rxns(20), 'pr_mw');
 [ExRxns3, MaxTheoOut3] = theoretMaxProd(model, model.rxns(20), 'pr_other_mol');
 [ExRxns4, MaxTheoOut4] = theoretMaxProd(model, model.rxns(20), 'pr_other_mw');
 [ExRxns5, MaxTheoOut5] = theoretMaxProd(model, model.rxns(20), 'x'); % bad criterion
+
 % tests
 assert(isequal(MaxTheoOut(1), 20)); % 'EX_ac(e)'
 assert(isequal(MaxTheoOut1(1), 1)); % the same but scaled to 0-1
