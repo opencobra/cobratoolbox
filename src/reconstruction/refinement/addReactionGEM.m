842d057fd7307f413ca36be785d47bf42831293a
Thomas Pfau <thomas.pfau@uni.lu>
2017-10-26 14:39:15 +0200

    Updated creation of gene field (correct empty size)

--- src/reconstruction/refinement/addReactionGEM.m
+++ src/reconstruction/refinement/addReactionGEM.m
@@ -76,7 +76,7 @@ if ~exist('rules', 'var')|| isempty(rules)
 end
 
 if ~exist('genes', 'var')|| isempty(genes)
-    genes = {''};
+    genes = cell(0,1);
 end
 
 if ~exist('nRxn', 'var') || isempty(nRxn)
