590e10efd9c5f4b8d7f972c8aca4d988209eb401
Jacek Wachowiak <jacekwachowiak94@gmail.com>
2017-10-30 11:17:48 +0100

    Changed order of the arguments and tests

--- src/design/theoretMaxProd.m
+++ src/design/theoretMaxProd.m
@@ -1,12 +1,15 @@
-function [ExRxns, MaxTheoOut] = theoretMaxProd(model, criterion, inputrxn, normalize, rxns)
+function [ExRxns, MaxTheoOut] = theoretMaxProd(model, inputrxn, criterion, normalize, rxns)
 % Determines the max theoretical output for each exchange reaction
 %
 % USAGE:
 %
-%    [ExRxns, MaxTheoOut]= theoreticalMaxProduction(model, criterion, inputrxn, normalize, rxns)
+%    [ExRxns, MaxTheoOut]= theoreticalMaxProduction(model, inputrxn, criterion, normalize, rxns)
 %
 % INPUT:
 %    model:         model structure
+%    inputrxn:      the input reaction ('EX_glu(e)', etc.)
+%
+% OPTIONAL INPUT:
 %    criterion:     One of
 %
 %                     * 'pr_mol' (default)
@@ -14,9 +17,6 @@ function [ExRxns, MaxTheoOut] = theoretMaxProd(model, criterion, inputrxn, norma
 %                     * 'pr_other_mol' (other carbon compounds secretion rate)
 %                     * 'pr_other_mw'  (same thing in molecular weight)
 %                       weight yield)
-%    inputrxn:      the input reaction ('EX_glu(e)', etc.)
-%
-% OPTIONAL INPUT:
 %    normalize:     normalize by input flux.  Either the flux rate in mol or
 %                   in molecular weight (Default = false)
 %    rxns:          Selection Vector (1 for selected, 0 otherwise)
@@ -27,7 +27,7 @@ function [ExRxns, MaxTheoOut] = theoretMaxProd(model, criterion, inputrxn, norma
 %
 % .. Author: - Jan Schellenberger 11/7/08
 
-if isempty(criterion)
+if nargin < 3 || isempty(criterion)
     criterion = 'pr_mol';
 end
 if nargin < 4
--- test/verifiedTests/design/testTheoretMaxProd.m
+++ test/verifiedTests/design/testTheoretMaxProd.m
@@ -14,22 +14,22 @@ fileDir = fileparts(which('testTheoretMaxProd'));
 cd(fileDir);
 
 % test variables
-model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'ecoli_core_model.mat']);
+model = readCbModel([CBTDIR filesep 'test' filesep 'models' filesep 'mat' filesep 'ecoli_core_model.mat']);
 
 % function calls
-[ExRxns, MaxTheoOut] = theoretMaxProd(model, '', model.rxns(20)); % 'EX_ac(e)'
-[ExRxns1, MaxTheoOut1] = theoretMaxProd(model, '', model.rxns(20), true, findExcRxns(model,0,0)); % not default options
-[ExRxns2, MaxTheoOut2] = theoretMaxProd(model, 'pr_mw', model.rxns(20));
-[ExRxns3, MaxTheoOut3] = theoretMaxProd(model, 'pr_other_mol', model.rxns(20));
-[ExRxns4, MaxTheoOut4] = theoretMaxProd(model, 'pr_other_mw', model.rxns(20));
-[ExRxns5, MaxTheoOut5] = theoretMaxProd(model, 'x', model.rxns(20)); % bad criterion
+[ExRxns, MaxTheoOut] = theoretMaxProd(model, model.rxns(20)); % 'EX_ac(e)'
+[ExRxns1, MaxTheoOut1] = theoretMaxProd(model, model.rxns(20), '', true, findExcRxns(model,0,0)); % not default options
+[ExRxns2, MaxTheoOut2] = theoretMaxProd(model, model.rxns(20), 'pr_mw');
+[ExRxns3, MaxTheoOut3] = theoretMaxProd(model, model.rxns(20), 'pr_other_mol');
+[ExRxns4, MaxTheoOut4] = theoretMaxProd(model, model.rxns(20), 'pr_other_mw');
+[ExRxns5, MaxTheoOut5] = theoretMaxProd(model, model.rxns(20), 'x'); % bad criterion
 
 % tests
-assert(isequal(MaxTheoOut(1), 20)); % 'EX_ac(e)'
-assert(isequal(MaxTheoOut1(1), 1)); % the same but scaled to 0-1
-assert(isequal(MaxTheoOut2(1), 1180)); % 'EX_ac(e)' in weight
-assert(isequal(MaxTheoOut3(1), 20)); % 'EX_ac(e)'
-assert(isequal(MaxTheoOut4(1), 900)); % 'EX_ac(e)' in weight yield
+assert(abs(MaxTheoOut(1) - 20) < 1e-4); % 'EX_ac(e)'
+assert(abs(MaxTheoOut1(1) - 1) < 1e-4); % the same but scaled to 0-1
+assert(abs(MaxTheoOut2(1) - 1180) < 1e-4); % 'EX_ac(e)' in weight
+assert(abs(MaxTheoOut3(1) - 20) < 1e-4); % 'EX_ac(e)'
+assert(abs(MaxTheoOut4(1) - 900) < 1e-4); % 'EX_ac(e)' in weight yield
 assert(isequal(MaxTheoOut5, zeros(20, 1))); % bad criterion returns only 0s
 
 % change to old directory
