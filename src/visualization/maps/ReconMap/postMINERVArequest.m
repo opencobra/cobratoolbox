7e4115da79fd41aeeff4b96598dff94308237094
Alberto NORONHA <alberto.noronha@bmp00166.local>
2017-11-07 18:12:03 +0100

    Changes to ReconMap remote call

--- src/visualization/maps/ReconMap/postMINERVArequest.m
+++ src/visualization/maps/ReconMap/postMINERVArequest.m
@@ -1,4 +1,4 @@
-function [ response ] = postMINERVArequest(minerva_servlet, login, password, map, identifier, content)
+function [ response ] = postMINERVArequest(login, password, map, identifier, content)
 % Sends a new layout to a MINERVA instance
 %
 % USAGE:
@@ -19,22 +19,27 @@ function [ response ] = postMINERVArequest(minerva_servlet, login, password, map
 %                               index 2 - success or error message
 %
 % .. Author: - Alberto Noronha Jan/2016
+    
+   
 
-   content = {'identifier', identifier, 'login', login, 'password', password, 'model', map, 'expression_value', content};
-   xmlresponse = urlread(minerva_servlet, 'POST', content);
-
-   success = strfind(xmlresponse, '<span id="default_form:status">OK</span>');
-   response = cell(1,2);
-   if isempty(success)
-       split = strsplit(xmlresponse, '<span id="default_form:status">');
-       split = split{2};
-       split = strsplit(split, '</span>');
-       split = split{1};
-       split = strrep(split, '&quot;', '"');
-       response{1,1} = 0;
-       response{1,2} = split;
+%    content = {'identifier', identifier, 'login', login, 'password', password, 'model', map, 'expression_value', content};
+%    xmlresponse = urlread(minerva_servlet, 'POST', content);
+   
+   loginURL = strcat({'curl -X POST -c - --data "login='}, login, {'&password='}, password, {'" https://vmh.uni.lu/MapViewer/api/doLogin/'});
+   [x , command_out] = system(char(loginURL));
+   
+   if ~contains(command_out, 'Invalid credentials')
+       [startIndex,endIndex] = regexp(command_out,'MINERVA_AUTH_TOKEN\s+(.*)$');
+       split = strsplit(command_out(startIndex:endIndex), '\t');
+       minerva_auth_token = split{2};
+       minerva_server = strcat('https://vmh.uni.lu/MapViewer/api/projects/', map, '/overlays/');
+       filename = strcat(identifier, '.txt');
+       curl_str = strcat('curl -X POST --data "content=', content, '&description=', identifier ,'&filename=', filename, '&name=', identifier, {'" --cookie "MINERVA_AUTH_TOKEN='}, minerva_auth_token, {'" '}, minerva_server);
+       [x , response] = system(char(curl_str));
+       if contains(response, '"status":"OK"')
+           response = 'Overlay generated successfully!'
+       end
    else
-       response{1,1} = 1;
-       response{1,2} = 'Overlay was sucessfully sent to ReconMap!';
+       response = 'Invalid credentials. Please make sure your login and password are correct.';
    end
 end
