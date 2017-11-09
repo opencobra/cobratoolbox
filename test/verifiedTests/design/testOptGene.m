a5da8468bc448d49216d817eec6093adcbb96319
Thomas Pfau <thomas.pfau@uni.lu>
2017-11-09 11:05:35 +0100

    :bug: due to remaining local variables it worked, but several necessary code lines were removed

--- test/verifiedTests/design/testOptGene.m
+++ test/verifiedTests/design/testOptGene.m
@@ -27,9 +27,8 @@ fructose_substrateRxn = model.rxns{26}; %Fructose, even though this has no inclu
 generxnList = model.rxns(setdiff([1:95],[11,13,26,39])); %Everything besides the ATP Maintenance, The biomass reaction and the substrate and target reactions.
 
 solverPkgs = {'ibm_cplex','gurobi'};
-basicsolution = optimizeCbModel(model);
-
 tested = false;
+basicsolution = optimizeCbModel(model);
 for k = 1:length(solverPkgs)
 
     % set the solver
@@ -43,18 +42,21 @@ for k = 1:length(solverPkgs)
         fprintf('Testing optGene using %s ...\n',solverPkgs{k});
         % function outputs
         % requires Global Optimization Toolbox
-        %Set the rng, for reproducability (even though this seems not to
-        %help for gurobi.
+        %Set the rng, for reproducability
         rng(0);
-        [x, population, scores, optGeneSol] = optGene(model, targetRxn, fructose_substrateRxn,generxnList, 'StallTimeLimit',5,'TimeLimit',15);        
+        [x, population, scores, optGeneSol] = optGene(model, targetRxn, fructose_substrateRxn,generxnList, 'StallTimeLimit',5,'TimeLimit',15);
+        %Check, that we get the expected solution from a previous run.
+        optSols = population((optGeneSol.scores == min(optGeneSol.scores)),:); %Get the set of optimal Solutions.
+        optReacs = sum(optSols,1) == max(sum(optSols,1));
         model2 = model;
         model2.lb(ismember(model2.rxns,generxnList(optReacs))) = 0;
         model2.ub(ismember(model2.rxns,generxnList(optReacs))) = 0;
         sol = optimizeCbModel(model2);
         %Lets only assert, that we have some improvement.
-        assert(sol.full(39) - basicsolution.full(39) > 0);    
+        assert(sol.full(39) -basicsolution.full(39) > 0);            
     end
 end
+
 assert(tested,sprintf('This method is only tested with gurobi and ibm_cplex but should work with other solvers as well.\n To test it please add your prefered solver to the list in this test and rerun the test'));
 % close the open windows
 close all
