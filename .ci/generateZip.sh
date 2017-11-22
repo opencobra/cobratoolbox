9a2311b0ac25ec18004d3276c0a187feb85cbefc
laurentheirendt <laurent.heirendt@uni.lu>
2017-11-22 15:55:56 +0100

    Adding output to generate zip script

--- .ci/generateZip.sh
+++ .ci/generateZip.sh
@@ -12,8 +12,15 @@ if [ "$ARCH" = "Linux" ] && [ "$MATLAB_VER" = "R2016b" ]; then
     # make a local temporary release directory
     mkdir /tmp/releaseCT
 
-    # copy to /tmp directory and remove .git directory
-    cp -r /mnt/prince-data/jenkins/workspace/COBRAToolbox-branches-auto-linux/MATLAB_VER/$MATLAB_VER/label/linux/* /tmp/releaseCT/.
+    echo " > Temporary folder created: /tmp/releaseCT"
+
+    # copy all files and folders to the /tmp directory
+    cp -rf /mnt/prince-data/jenkins/workspace/COBRAToolbox-branches-auto-linux/MATLAB_VER/$MATLAB_VER/label/linux/* /tmp/releaseCT
+
+    # copy all hiden files and folders to the /tmp directory
+    cp -rf /mnt/prince-data/jenkins/workspace/COBRAToolbox-branches-auto-linux/MATLAB_VER/$MATLAB_VER/label/linux/.[^.]* /tmp/releaseCT
+
+    echo " > Workspace copied"
 
     # change to the local temporary release directory
     cd /tmp/releaseCT
@@ -24,25 +31,40 @@ if [ "$ARCH" = "Linux" ] && [ "$MATLAB_VER" = "R2016b" ]; then
     # clean all files that are not tracked
     git clean -fd
 
+    echo " > git repo cleaned"
+
     # retrieve the SHA1 of the detached head
     lastCommit=$(git rev-parse --short HEAD)
 
+    echo " > Last commit determined as $lastCommit"
+
     # remove the .git directory inside of the local temporary release directory
     rm -rf /tmp/releaseCT/.git
+
+    echo " > .git folder removed"
 
     # change to the local temporary release directory
     cd /tmp/releaseCT
 
     # zip the entire directory
-    zip -r /mnt/prince-data/releases/the_COBRA_Toolbox-$lastCommit.zip .
+    zip -qr /mnt/prince-data/releases/the_COBRA_Toolbox-$lastCommit.zip .
+
+    echo " > Workspace zipped"
 
     # move the zip file to /mnt/isilon-dat/releases
     scp -P 8022 /mnt/prince-data/releases/the_COBRA_Toolbox-$lastCommit.zip jenkins@prince-server.lcsb.uni.lux:/mnt/isilon-dat/releases
 
+    echo " > .zip file sent to prince-server"
+
     # remove temporary directory
     rm -rf /tmp/releaseCT
 
+    echo " > Temporary folder removed"
+
     # remove the local zip file
     rm /mnt/prince-data/releases/the_COBRA_Toolbox-$lastCommit.zip
+
+    echo " > Local .zip file removed"
+    echo " > Done."
 
 fi
