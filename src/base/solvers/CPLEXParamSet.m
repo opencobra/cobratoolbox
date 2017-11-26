f37e8377d732a40ef401ee451fffaa355c9315f8
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-11-26 08:52:28 +0100

    removed non ASCII caracters from CPLEXParamSet file.

--- src/base/solvers/CPLEXParamSet.m
+++ src/base/solvers/CPLEXParamSet.m
@@ -63,7 +63,7 @@ if strcmp(interface,'tomlab_cplex')
 
     % Barrier iteration limit.
     % Sets the number of Barrier iterations before termination. When set to 0, no Barrier iterations occur, but problem
-    % �setup� occurs and information about the setup is displayed (such as Cholesky factorization information).
+    % setup occurs and information about the setup is displayed (such as Cholesky factorization information).
     % 0 No Barrier iterations
     % or, any positive integer
     % Default: Large (varies by computer)
@@ -136,7 +136,7 @@ if strcmp(interface,'tomlab_cplex')
     cpxControl.EPRELAX = 1e-6;
 
     % Feasibility tolerance.
-    % The feasibility tolerance specifies the degree to which a problem�s basic variables may violate their bounds.
+    % The feasibility tolerance specifies the degree to which a problem's basic variables may violate their bounds.
     % FEASIBILITY influences the selection of an optimal basis and can be reset to a higher value when a problem
     % is having difficulty maintaining feasibility during optimization. You may also wish to lower this tolerance after
     % finding an optimal solution if there is any doubt that the solution is truly optimal. If the feasibility tolerance is
@@ -236,7 +236,7 @@ if strcmp(interface,'tomlab_cplex')
     % of the CLOCKTYPE parameter. The time limit applies to primal simplex, dual simplex, barrier, and mixed
     % integer optimizations, as well as infeasibility finder computations. (Network simplex and barrier crossover
     % operations are exceptions; these processes do not terminate if the time limit is exceeded.) The time limit
-    % includes preprocessing time. For �hybrid� optimizations (such as network optimization followed by dual or
+    % includes preprocessing time. For hybrid optimizations (such as network optimization followed by dual or
     % primal simplex, barrier optimization followed by crossover), the
     % cumulative time applies.
     cpxControl.TILIM = 600;%sec
