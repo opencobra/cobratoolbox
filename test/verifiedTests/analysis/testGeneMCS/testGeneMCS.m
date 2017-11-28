a1b4b881893470dd6108cf5ab8709af7ca89729e
Luis V. Valcarcel <lvalcarcelg@alumni.tecnun.es>
2017-11-28 09:26:43 +0100

    USe toy example model for test

--- test/verifiedTests/analysis/testGeneMCS/testGeneMCS.m
+++ test/verifiedTests/analysis/testGeneMCS/testGeneMCS.m
@@ -18,50 +18,31 @@ cd(fileparts(which('testGeneMCS')));
 % define the solver packages to be used to run this test
 solverPkgs = {'ibm_cplex','gurobi'}; 
 
-% Load Ecoli core
-load([CBTDIR filesep 'test' filesep 'models' filesep 'mat' filesep 'ecoli_core_model.mat'], 'model');
-% make all boundaries +1000, 0 or -1000
-model.ub(:) = +1000;
-model.lb(logical(model.rev)) = -1000;
-model.lb(~logical(model.rev)) = 0;
+% Load Toy Example
+load gMCStoyExample.mat
   
 % expected solution
-true_gmcs = cell(16,1);
-true_gmcs{1} = {'b2415' 'b4025'}';
-true_gmcs{2} = {'b2415' 'b3919'}';
-true_gmcs{3} = {'b2415' 'b2779'}';
-true_gmcs{4} = {'b2416' 'b2779'}';
-true_gmcs{5} = {'b1779' 'b2416'}';
-true_gmcs{6} = {'b2987' 'b3493'}';
-true_gmcs{7} = {'b2416' 'b3919'}';
-true_gmcs{8} = {'b1779' 'b2415'}';
-true_gmcs{9} = {'b2779' 'b2926'}';
-true_gmcs{10} = {'b2416' 'b4025'}';
-true_gmcs{11} = {'b2415' 'b2926'}';
-true_gmcs{12} = {'b3236' 'b3956'}';
-true_gmcs{13} = {'b2914' 'b4090'}';
-true_gmcs{14} = {'b2465' 'b2935'}';
-true_gmcs{15} = {'b1779' 'b2779'}';
-true_gmcs{16} = {'b2416' 'b2926'}';
-
+true_gmcs = cell(3,1);
+true_gmcs{1} = {'g5'}';
+true_gmcs{2} = {'g1' 'g4'}';
+true_gmcs{3} = {'g2' 'g3' 'g4'}';
 
 for k = 1:length(solverPkgs)
-    fprintf(' -- Running <testFile> using the solver interface: %s ... ', solverPkgs{k});
+    fprintf(' -- Running testGeneMCS using the solver interface: %s ... ', solverPkgs{k});
 
     solverLPOK = changeCobraSolver(solverPkgs{k}, 'MILP', 0);
 
     if solverLPOK
         % Eliminate G-matrix if it exist
-        if exist('G_ecoli_core_test.mat', 'file')
-            delete G_ecoli_core_test.mat
+        if exist('G_toy_example_gMCS.mat', 'file')
+            delete G_toy_example_gMCS.mat
         end
         
         % Calculate GMCS
-        [gmcs, gmcs_time] = calculateGeneMCS('ecoli_core_test', model, 20);
+        [gmcs, gmcs_time] = calculateGeneMCS('toy_example_gMCS', model, 20);
                
         % Eliminante Nan -> no more gMCS
         gmcs = gmcs(cellfun('isclass', gmcs,'cell'));
-        gmcs = gmcs(cellfun(@length, gmcs)<=2);
         
         % Check if the solution obtained is the same as the expected
         % solution
