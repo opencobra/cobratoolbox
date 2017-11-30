efcd46f2420ba58199dbbe8ac8e36afe41d9cf1f
Thomas Pfau <thomas.pfau@uni.lu>
2017-11-30 07:40:59 +0100

    Added test cases for errors (i.e. invalid input) and Linear Constraints

--- test/verifiedTests/analysis/testOptimizeCbModel/testOptimizeCbModel.m
+++ test/verifiedTests/analysis/testOptimizeCbModel/testOptimizeCbModel.m
@@ -21,7 +21,7 @@ tol = 1e-6;
 solverPkgs = {'tomlab_cplex', 'glpk'};
 
 % load the model
-model = getDistributedModel('ecoli_core_model.mat');
+origmodel = getDistributedModel('ecoli_core_model.mat');
 
 osenseStr = 'max';
 allowLoops = true;
@@ -30,7 +30,7 @@ for k = 1:length(solverPkgs)
 
     % change the COBRA solver (LP)
     solverOK = changeCobraSolver(solverPkgs{k}, 'LP', 0);
-
+    model = origmodel;
     if solverOK == 1
         fprintf('   Testing optimizeCbModel using solver %s ... ', solverPkgs{k})
 
@@ -65,7 +65,44 @@ for k = 1:length(solverPkgs)
             assert(norm(model.S * L2solution.x - model.b, 2) < tol);
             assert(abs(FBAsolution.f - L2solution.x'* model.c) < 0.01);
         end
-
+        
+        %% Test some error cases. 
+        %wrong size of csense
+        model = origmodel;
+        model.csense(4) = [];        
+        assert(verifyCobraFunctionError(@() optimizeCbModel(model)));
+        %Test S of different Size
+        model = origmodel;
+        model.S(:,3) = [];
+        assert(verifyCobraFunctionError(@() optimizeCbModel(model)));
+        
+        %% Test additional constraints
+        %get two random reactions which carried flux. in the basic
+        %FBASolution
+        PositiveFluxes = find(FBAsolution.full > 1); %Take something with clearly positive flux
+        selectedFluxes = PositiveFluxes(randperm(numel(PositiveFluxes),2));
+        totalFlux = sum(FBAsolution.full(selectedFluxes)) / 2;
+        %Add A Constraint that makes this lower than half the flux before.
+        model = addCOBRAConstraint(origmodel,origmodel.rxns(selectedFluxes),totalFlux,'c',[1 1]);
+        ConstraintSolution = optimizeCbModel(model);
+        assert(sum(ConstraintSolution.full(selectedFluxes)) - totalFlux <= tol);
+        
+        % And test some inconsistency cases
+        %Wrong size of C
+        incModel = model;
+        incModel.C(:,6) = [];
+        assert(verifyCobraFunctionError(@() optimizeCbModel(incModel)));
+        %wrong size of d
+        incModel = model;
+        incModel.d(end+1) = 2;
+        assert(verifyCobraFunctionError(@() optimizeCbModel(incModel)));        
+        %wrong size of dsense
+        incModel = model;
+        incModel.dsense(end+1) = 'G';
+        assert(verifyCobraFunctionError(@() optimizeCbModel(incModel)));        
+        % missing field
+        incModel = rmfield(model,'dsense');
+        assert(verifyCobraFunctionError(@() optimizeCbModel(incModel)));               
         % output a success message
         fprintf('Done.\n');
     end
