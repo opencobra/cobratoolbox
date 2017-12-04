1bdc6829e53eaae648df1f13820b56c2ff3a1f2e
jennifer modamio <jennifer.modamio@uni.lu>
2017-12-04 18:56:26 +0100

    moving unifyMetabolicMapCD.m

---
+++ src/unifyMetabolicMapCD.m
@@ -0,0 +1,30 @@
+function map2 = unifyMetabolicMapCD(map)
+
+    % Unify colours in a metabolic map as a standard. Reaction will be Gray
+    % and Metabolites will be White.
+    %
+    %   map2 = unifyMetabolicMapCD(map);
+    %
+    % INPUTS:
+    %
+    %   map:    MATLAB structure of CD map
+    %
+    % OUTPUT:
+    %
+    %   map2:   Map with grey reactions colour, width 1 and white nodes colour.
+    %
+    % J.Modamio LCSB, Belval, Luxembourg. 19.08.2017
+    % LCSB. Belval. Luxembourg
+    
+    map2 = map;
+    
+    for i = 1:length(map2.rxnName)   
+        map2.rxnColor{i,1} = 'FFDCDCDC';
+        map2.rxnWidth{i,1} = 1; 
+    end
+    
+    for j = 1:length(map2.molColor)
+        map2.molColor{j,1} = 'FFFFFFFF';
+    end
+    
+end
