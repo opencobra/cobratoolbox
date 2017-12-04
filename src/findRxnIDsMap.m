75d44371d81523687cfc41f644baf32da7c5215f
jennifer modamio <jennifer.modamio@uni.lu>
2017-12-04 19:12:50 +0100

    moving findRxnIDsMap.m

---
+++ src/findRxnIDsMap.m
@@ -0,0 +1,49 @@
+function [rxnID,rxnIDref] = findRxnIDsMap(map, rxnList)
+
+    % Finds reaction indexes in a CD map from a list of names
+    %
+    % USAGE:
+    %
+    %    rxnID = findRxnIDs(map, rxnList)
+    %
+    % INPUTS:
+    %
+    %    map:           Map from CD parsed to matlab format
+    %
+    %    rxnList:       List of reaction names
+    %
+    % OUTPUT:
+    %
+    %    rxnIDref:      ID reference for reactions
+    %
+    %    rxnID:         List of reactions indexes
+    %
+    % .. Authors:
+    % Mouss Rouquaya LSCB, Belval, Luxembourg. Date: - 24.07.2017
+    % N.Sompairac - Institut Curie, Paris, 11/10/2017 (Code checking)
+
+    if (iscell(rxnList))
+        
+        [~,rxnID] = ismember(rxnList,map.rxnName);
+        rxnIDref = map.rxnID(rxnID);
+        
+    else
+        
+        rxnID = find(strcmp(map.rxnName,rxnList));
+        
+        if (isempty(rxnID))
+            
+            rxnID = 0;
+            rxnIDref = {};
+            
+        end
+        
+        if (length(rxnID) > 1)
+            
+            rxnID = rxnID(1);
+            rxnIDref = map.rxnID(rxnID);
+            
+        end
+
+    end
+end
