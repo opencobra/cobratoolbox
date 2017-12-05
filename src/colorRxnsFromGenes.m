6f8d3d909e8c627d702b68dce22a555754ebb49e
NicolasSompairac <nsompairac@gmail.com>
2017-12-05 17:08:03 +0100

    Clean code

--- src/colorRxnsFromGenes.m
+++ src/colorRxnsFromGenes.m
@@ -1,57 +1,54 @@
-function newmap = ColorRxnsFromGenes(map,model,EntrezList,color,width)
+function newmap = colorRxnsFromGenes(map, model, EntrezList, color, width)
 
-    % Color and change the width of reactions based on the imlicated gene
-    % given from a list of EntezIDs.
-    %
-    % USAGE:
-    %
-    %   ColorRxnsFromGenes(map,model,EntrezList,color,width);
-    %
-    % INPUTS:
-    %
-    %   map:            map from CD parsed to matlab format
-    %
-    %   model:          COBRA model
-    %
-    %   EntrezList:     List of genes, given as EntrezIDs. 
-    %
-    % OPTIONAL INPUTS:
-    %
-    %   color:          Preferred color, as written in function
-    %                   'Create_colors_map'. (default: 'RED')
-    %
-    %   width:          Preferred width of lines. (default: 8)
-    %
-    % OUTPUT:
-    %
-    %   newmap          Matlab structure of new map with default look
-    %
-    % A.Danielsdottir 01/08/2017 
-    % MOUSS Rouquaya and J.Modamio 21/08/2017 LCSB. Belval. Luxembourg
-    % N.Sompairac - Institut Curie, Paris, 11/10/2017 (Code Checking)
-    
+% Color and change the width of reactions based on the imlicated gene
+% given from a list of EntezIDs.
+%
+% USAGE:
+%
+%   ColorRxnsFromGenes(map, model, EntrezList, color, width);
+%
+% INPUTS:
+%
+%   map:            map from CD parsed to matlab format
+%
+%   model:          COBRA model
+%
+%   EntrezList:     List of genes, given as EntrezIDs. 
+%
+% OPTIONAL INPUTS:
+%
+%   color:          Preferred color, as written in function
+%                   'createColorsMap'. (default: 'RED')
+%
+%   width:          Preferred width of lines. (default: 8)
+%
+% OUTPUT:
+%
+%   newmap          Matlab structure of new map with default look
+%
+% A.Danielsdottir 01/08/2017 
+% MOUSS Rouquaya and J.Modamio 21/08/2017 LCSB. Belval. Luxembourg
+% N.Sompairac - Institut Curie, Paris, 11/10/2017 (Code Checking)
+
     if nargin<5
-
         width = 8;
-
     end
     if nargin<4
-
        color = 'RED';
-
     end
 
     newmodel = model;
     newmap = map;
-    %remove '.x' and '.xx' from Entrez ID in model list
+    
+    % Remove '.x' and '.xx' from Entrez ID in model list
     for i = 1:length(newmodel.genes)
         newmodel.genes{i} = regexprep((newmodel.genes{i}),'\.[\w]+',''); 
     end
     
-    %find index of Entrez IDs provided in input list
+    % Find index of Entrez IDs provided in input list
     geneIndex = find(ismember(newmodel.genes,EntrezList));
     
-    %Find the rxns that correspond to these genes
+    % Find the rxns that correspond to these genes
     summary = num2cell(geneIndex);
     globalList = [];
     
@@ -65,41 +62,30 @@ function newmap = ColorRxnsFromGenes(map,model,EntrezList,color,width)
     
     clear rxnName
     rxnName = unique(globalList);
-    %find the same rxns on the map and change the color
-    Colors = Create_colors_map;
+    
+    % Find the same rxns on the map and change the color
+    Colors = createColorsMap;
     index = find(ismember(newmap.rxnName,rxnName));
     
     for j = index'
-
         newmap.rxnColor{j,1} = Colors(color);
-
         newmap.rxnWidth{j,1} = width;
-
     end
-    %use the existence of reactant lines to check if the map has the
-    %complete structure, and if so change also secondary lines.
     
+    % Use the existence of reactant lines to check if the map has the
+    % complete structure, and if so change also secondary lines.
     if any(strcmp('rxnReactantLineColor',fieldnames(map))) == 1
-        
         for j = index'
-            
             if ~isempty(newmap.rxnReactantLineColor{j})
-                
                 for k = 1:length(map.rxnReactantLineColor{j})
-                    
                     newmap.rxnReactantLineColor{j,1}{k,1} = Colors(color);
-                    newmap.rxnReactantLineWidth{j,1}{k,1} = width;
-                    
+                    newmap.rxnReactantLineWidth{j,1}{k,1} = width;     
                 end
             end
-            
             if ~isempty(newmap.rxnProductLineColor{j})
-                
                 for m = 1:1:length(newmap.rxnProductLineColor{j})
-                    
                     newmap.rxnProductLineColor{j,1}{m,1} = Colors(color);
-                    newmap.rxnProductLineWidth{j,1}{m,1} = width;
-                    
+                    newmap.rxnProductLineWidth{j,1}{m,1} = width;   
                 end
             end
         end
