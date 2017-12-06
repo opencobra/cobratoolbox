3f80b03fba085b980cf00bb7a72746474cf8a848
NicolasSompairac <nsompairac@gmail.com>
2017-12-06 18:40:06 +0100

    Renaming Functions

--- src/findMetIDs.m
+++ src/findMetIDs.m
@@ -1,24 +1,20 @@
-function metID = findMetIDsMap(map, metList)
-
+function [metID] = findMetIDsMap(map, metList)
 % Finds metabolites Indexes in a map for a given list of names
 %
 % USAGE:
 %
-%    metID = findMetIdsMap(map, metList)
+%    [metID] = findMetIdsMap(map, metList)
 %
 % INPUTS:
-%
-%    map:      Map from CD parsed to matlab format
-%
+%    map:      Map from CellDesigner parsed to matlab format
 %    metList:  List of metabolites names
 %
 % OUTPUT:
-%
 %    metID:    List of metabolite indexes corresponding to `metList`
 %
 % .. Authors:
-% .. Mouss Rouquaya 24/07/2017
-% .. N.Sompairac - Institut Curie, Paris, 11/10/2017 (Code checking)
+%       - Mouss Rouquaya 24/07/2017
+%       - N.Sompairac - Institut Curie, Paris, 11/10/2017 (Code checking)
 
     metID = find(ismember(map.specName, metList));
 
--- src/findRxnIDs.m
+++ src/findRxnIDs.m
@@ -1,5 +1,4 @@
 function [rxnID, rxnIDref] = findRxnIDsMap(map, rxnList)
-
 % Finds reaction indexes in a CD map from a list of names
 %
 % USAGE:
@@ -7,20 +6,16 @@ function [rxnID, rxnIDref] = findRxnIDsMap(map, rxnList)
 %    [rxnID, rxnIDref] = findRxnIDs(map, rxnList)
 %
 % INPUTS:
-%
 %    map:           Map from CD parsed to matlab format
-%
 %    rxnList:       List of reaction names
 %
-% OUTPUT:
-%
+% OUTPUTS:
 %    rxnIDref:      ID reference for reactions
-%
 %    rxnID:         List of reactions indexes
 %
 % .. Authors:
-% .. Mouss Rouquaya LSCB, Belval, Luxembourg. Date: - 24.07.2017
-% .. N.Sompairac - Institut Curie, Paris, 11/10/2017
+%       - Mouss Rouquaya LSCB, Belval, Luxembourg. Date: - 24.07.2017
+%       - N.Sompairac - Institut Curie, Paris, 11/10/2017
 
     if (iscell(rxnList))
         [~,rxnID] = ismember(rxnList,map.rxnName);
