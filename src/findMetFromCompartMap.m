f13241e5568a035ffd1adc2ed63c315b7b5fa159
NicolasSompairac <nsompairac@gmail.com>
2017-12-06 19:15:22 +0100

    Correct description format

--- src/findMetFromCompartMap.m
+++ src/findMetFromCompartMap.m
@@ -1,5 +1,4 @@
 function [mets, ID] = findMetFromCompartMap(map, compartment)
-
 % Finds all the metabolites and their names in the map structure for
 % a compartment of interest.
 %
@@ -8,20 +7,17 @@ function [mets, ID] = findMetFromCompartMap(map, compartment)
 %    [mets, ID] = findMetFromCompartMap(map, Compartment)
 %
 % INPUTS:
-%
-%    map:               Map from CD parsed to matlab format
-%
+%    map:               Map from CellDesigner parsed to matlab format
 %    compartment:       Compartment of interest (e.g.: '[m]','[n]','[e]',etc.)
 %
-% OUTPUT:
-%
+% OUTPUTS:
 %    mets:              List of metabolites names
-%
 %    ID:                Metabolites indexes
 %
-% .. Authors: MOUSS Rouquaya 24/07/2017
-% .. Modifications added J.Modamio BLCSB, Belval, Luxembourg.
-% .. Improvements N.Sompairac - Institut Curie, Paris, 11/10/2017
+% .. Authors:
+%       - MOUSS Rouquaya 24/07/2017
+%       - J.Modamio BLCSB, Belval, Luxembourg.
+%       - N.Sompairac - Institut Curie, Paris, 11/10/2017
 
     index = strfind(map.specName, compartment);
     ID = find(~cellfun(@isempty, index));
