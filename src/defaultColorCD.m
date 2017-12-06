72cebfe7b01afd29f96bbc14fc4eb4bb03ee86b9
NicolasSompairac <nsompairac@gmail.com>
2017-12-06 19:15:08 +0100

    Correct description format

--- src/defaultColorCD.m
+++ src/defaultColorCD.m
@@ -1,31 +1,28 @@
-function newmap = defaultColorCD(map)
-
-% Change all reaction lines to black and default width
+function [newmap] = defaultColorCD(map)
+% Change all reaction lines to black and default areaWidth
 %
 % USAGE:
 %
-%   newmap = defaultColorCD(map);
+%   [newmap] = defaultColorCD(map);
 %
-% INPUTS:
-%
-%   map:        file from CD parsed to matlab format
+% INPUT:
+%   map:        file from CellDesigner parsed to matlab format
 %
 % OUTPUT:
-%
-%   newmap:     Matlab structure of map with all rxn lines as default
-%               color and width
+%   newmap:     MATLAB structure of map with all rxn lines as default
+%               color and areaWidth
 %
 % .. Authors:
-% .. A.Danielsdottir 17/07/2017 LCSB. Belval. Luxembourg
-% .. N.Sompairac - Institut Curie, Paris, 17/07/2017.
+%       - A.Danielsdottir 17/07/2017 LCSB. Belval. Luxembourg
+%       - N.Sompairac - Institut Curie, Paris, 17/07/2017.
 
     newmap = map;
     color = 'ff000000';
-    width = 1.0;
+    areaWidth = 1.0;
 
     for j = 1:length(newmap.rxnName)
         newmap.rxnColor{j,1} = color;
-        newmap.rxnWidth{j,1} = width;
+        newmap.rxnWidth{j,1} = areaWidth;
     end
     % Use the existence of reactant lines to check if the map has the
     % complete structure, and if so change also secondary lines.
@@ -34,13 +31,13 @@ function newmap = defaultColorCD(map)
             if ~isempty(newmap.rxnReactantLineColor{j})
                 for k = 1:length(newmap.rxnReactantLineColor{j})
                     newmap.rxnReactantLineColor{j,1}{k,1} = Colors(color);
-                    newmap.rxnReactantLineWidth{j,1}{k,1} = width;
+                    newmap.rxnReactantLineWidth{j,1}{k,1} = areaWidth;
                 end
             end
             if ~isempty(newmap.rxnProductLineColor{j})
                 for m = 1:1:length(newmap.rxnProductLineColor{j})
                     newmap.rxnProductLineColor{j,1}{m,1} = Colors(color);
-                    newmap.rxnProductLineWidth{j,1}{m,1} = width;   
+                    newmap.rxnProductLineWidth{j,1}{m,1} = areaWidth;   
                 end
             end
         end
