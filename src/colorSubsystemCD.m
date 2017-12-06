46d4898f5b029854e8361717e7665736078c39bb
NicolasSompairac <nsompairac@gmail.com>
2017-12-06 19:17:58 +0100

    Correct description format

--- src/colorSubsystemCD.m
+++ src/colorSubsystemCD.m
@@ -1,64 +1,57 @@
-function newmap = colorSubsystemCD(map, model, subsystem, color, width)
-
-% Color and increase width of every reaction in a specific subsystem
+function [newMap] = colorSubsystemCD(map, model, subsystem, color, areaWidth)
+% Color and increase areaWidth of every reaction in a specific subsystem
 %
 % USAGE:
 %
-%   newmap = colorSubsystemCD(map, model, subsystem, color, width);
+%   [newMap] = colorSubsystemCD(map, model, subsystem, color, areaWidth);
 %
 % INPUTS:
-%
 %   map:            File from CD parsed to matlab format
-%
 %   model:          COBRA model structure
-%
 %   subsystem:      Name of a subsystem as a String
 %
 % OPTIONAL INPUTS:
-%
 %   color:          Color desired for reactions in CAPITALS
-%
-%   width:          Width desired for reactions
+%   areaWidth:          Width desired for reactions
 %
 % OUTPUT:
-%
-%   newmap          Matlab structure of map with reaction modifications
+%   newMap          Matlab structure of map with reaction modifications
 %
 % .. Authors:
-% .. A.Danielsdottir 17/07/2017 LCSB. Belval. Luxembourg
-% .. N.Sompairac - Institut Curie, Paris, 11/10/2017.
+%       - A.Danielsdottir 17/07/2017 LCSB. Belval. Luxembourg
+%       - N.Sompairac - Institut Curie, Paris, 11/10/2017.
 
     if nargin<5
-        width = 8; 
+        areaWidth = 8; 
     end
     if nargin<4
        color = 'RED';
     end
 
-    newmap = map;
+    newMap = map;
     rxnList = model.rxns(ismember(model.subSystems,subsystem));
     Colors = createColorsMap;
 
-    index = find(ismember(newmap.rxnName,rxnList));
+    index = find(ismember(newMap.rxnName,rxnList));
     for j = index'
-        newmap.rxnColor{j,1} = Colors(color);
-        newmap.rxnWidth{j,1} = width;
+        newMap.rxnColor{j,1} = Colors(color);
+        newMap.rxnWidth{j,1} = areaWidth;
     end
     
     % Use the existence of reactant lines to check if the map has the
     % complete structure, and if so change also secondary lines.
-    if any(strcmp('rxnReactantLineColor',fieldnames(newmap))) == 1
+    if any(strcmp('rxnReactantLineColor',fieldnames(newMap))) == 1
         for j = index'
-            if ~isempty(newmap.rxnReactantLineColor{j})
-                for k = 1:length(newmap.rxnReactantLineColor{j})
-                    newmap.rxnReactantLineColor{j,1}{k,1} = Colors(color);
-                    newmap.rxnReactantLineWidth{j,1}{k,1} = width;
+            if ~isempty(newMap.rxnReactantLineColor{j})
+                for k = 1:length(newMap.rxnReactantLineColor{j})
+                    newMap.rxnReactantLineColor{j,1}{k,1} = Colors(color);
+                    newMap.rxnReactantLineWidth{j,1}{k,1} = areaWidth;
                 end
             end
-            if ~isempty(newmap.rxnProductLineColor{j})
-                for m = 1:1:length(newmap.rxnProductLineColor{j})
-                    newmap.rxnProductLineColor{j,1}{m,1} = Colors(color);
-                    newmap.rxnProductLineWidth{j,1}{m,1} = width;
+            if ~isempty(newMap.rxnProductLineColor{j})
+                for m = 1:1:length(newMap.rxnProductLineColor{j})
+                    newMap.rxnProductLineColor{j,1}{m,1} = Colors(color);
+                    newMap.rxnProductLineWidth{j,1}{m,1} = areaWidth;
                 end
             end
         end
