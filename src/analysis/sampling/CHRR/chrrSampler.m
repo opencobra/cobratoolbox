90fd5c231c7e723e4433b8ba4566ef7afc5b347f
laurentheirendt <laurent.heirendt@uni.lu>
2017-12-06 10:31:20 +0100

    Documentation formatting

--- src/analysis/sampling/CHRR/chrrSampler.m
+++ src/analysis/sampling/CHRR/chrrSampler.m
@@ -1,38 +1,38 @@
-function [samples,roundedPolytope] = chrrSampler(model,numSkip,numSamples,toRound,roundedPolytope,useFastFVA)
-% CHRRSAMPLER Generate uniform random flux samples with CHRR
-%   Coordinate Hit-and-Run with Rounding
+function [samples, roundedPolytope] = chrrSampler(model, numSkip, numSamples, toRound, roundedPolytope, useFastFVA)
+% Generate uniform random flux samples with CHRR Coordinate Hit-and-Run with Rounding
+% chrrSampler will generate numSamples samples from model, taking
+% numSkip steps of a random walk between each sample
 %
-% [samples,roundedPolytope,minFlux,maxFlux] = chrrSampler(model,numSkip,numSamples,toRound,roundedPolytope,minFlux,maxFlux);
+% Rounding the polytope is a potentially expensive step. If you generate multiple rounds
+% of samples from a single model, you can save roundedPolytope from the first round and
+% input it for subsequent rounds.
 %
-%   chrrSampler will generate numSamples samples from model, taking
-%   numSkip steps of a random walk between each sample
+% USAGE:
 %
-%   Rounding the polytope is a potentially expensive step. If you generate multiple rounds
-%   of samples from a single model, you can save roundedPolytope from the first round and
-%   input it for subsequent rounds.
+%    [samples, roundedPolytope] = chrrSampler(model, numSkip, numSamples, toRound, roundedPolytope, useFastFVA)
 %
 % INPUTS:
-% model ... COBRA model structure with fields:
-% .S ... The m x n stoichiometric matrix
-% .lb ... n x 1 lower bounds on fluxes
-% .ub ... n x 1 upper bounds on fluxes
-% .c ... n x 1 linear objective
-% numSkip ... Number of steps of coordinate hit-and-run between samples
-% numSamples ... Number of samples
+%    model             COBRA model structure with fields:
+%                        * .S - The m x n stoichiometric matrix
+%                        * .lb - n x 1 lower bounds on fluxes
+%                        * .ub - n x 1 upper bounds on fluxes
+%                        * .c - n x 1 linear objective
+%    numSkip           Number of steps of coordinate hit-and-run between samples
+%    numSamples        Number of samples
 %
 % OPTIONAL INPUTS:
-% toRound ... {0,(1)} Option to round the polytope before sampling.
-% roundedPolytope ... The rounded polytope from a previous round of
-%                     sampling the same model.
+%    toRound           {0,(1)} Option to round the polytope before sampling.
+%    roundedPolytope   The rounded polytope from a previous round of
+%                        sampling the same model.
 %
 % OUTPUTS:
-% samples ... n x numSamples matrix of random flux samples
-% roundedPolytope ... The rounded polytope. Save for use in subsequent
-%                     rounds of sampling.
+%    samples           n x numSamples matrix of random flux samples
+%    roundedPolytope   The rounded polytope. Save for use in subsequent
+%                      rounds of sampling.
 %
-% .. Authors: 
-%       -Ben Cousins and Hulda S. Haraldsdóttir, 03/2017, Original code
-%       -Ben Cousins, 12/2017, Updated to use sampling submodule
+% .. Authors:
+%       - Ben Cousins and Hulda S. Haraldsdóttir, 03/2017, Original code
+%       - Ben Cousins, 12/2017, Updated to use sampling submodule
 
 % Define defaults
 if nargin>=5 && isempty(numSkip)
@@ -55,42 +55,42 @@ end
 
 if nargin < 6 || isempty(useFastFVA)
     useFastFVA = false;
-end 
+end
 
 % Preprocess model
 if toPreprocess
-    
+
     % parse the model to get the meat
     P = chrrParseModel(model);
-    
+
     %check for width 0 facets to make sure we are full dimensional
     %also check for feasibility
     fprintf('Checking for width 0 facets...\n');
-    
+
     %check to make sure P.A and P.b are defined, and appropriately sized
     if (isfield(P,'A')==0 || isfield(P,'b')==0) || (isempty(P.A) || isempty(P.b))
         %either P.A or P.b do not exist
         error('You need to define both P.A and P.b for a polytope {x | P.A*x <= P.b}.');
     end
-    
+
     [num_constraints,dim] = size(P.A);
-    
+
     if exist('numSkip')~=1 || isempty(numSkip)
         numSkip=8*dim^2;
     end
-    
+
     fprintf('Currently (P.A, P.b) are in %d dimensions\n', dim);
-    
+
     if size(P.b,2)~= 1 || num_constraints ~= size(P.b,1)
         error('Dimensions of P.b do not align with P.A.\nP.b should be a %d x 1 vector.',num_constraints);
     end
-    
+
     if (isfield(P,'A_eq')==0 || isempty(P.A_eq)) && ...
             (isfield(P,'b_eq')==0 || isempty(P.b_eq))
         P.A_eq = [];
         P.b_eq = [];
     end
-    
+
     %preprocess the polytope of feasible solutions
     %restict to null space
     %round via maximum volume ellipsoid
