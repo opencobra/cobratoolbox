ce7ed4899019ed881a79e0794d0fbef327af12bc
NicolasSompairac <nsompairac@gmail.com>
2017-12-06 19:13:42 +0100

    Correct description format

--- src/changeMetColor.m
+++ src/changeMetColor.m
@@ -1,40 +1,38 @@
-function map = changeMetColor(map, metList, color)
-
+function [newMap] = changeMetColor(map, metList, color)
 % Change color of every metabolite from a list of Names
 %
 % USAGE:
 %
-%   new_map = changeMetColor(map, metList, color);
+%   [newMap] = changeMetColor(map, metList, color);
 %
 % INPUTS:
-%
 %   map:            file from CD parsed to matlab format
 %   metList:        List of metabolites names
 % 
 % OPTIONAL INPUT:
-%
 %   color:          New color of metabolites from list(default: RED)
 %
 % OUTPUT:
-%
-%   new_map:        Matlab structure of map with reaction modifications
+%   newMap:         Matlab structure of map with reaction modifications
 %
 % .. Authors:
-% .. A.Danielsdottir 17/07/2017 LCSB. Belval. Luxembourg
-% .. N.Sompairac - Institut Curie, Paris, 17/07/2017.
+%       - A.Danielsdottir 17/07/2017 LCSB. Belval. Luxembourg
+%       - N.Sompairac - Institut Curie, Paris, 17/07/2017.
 
     if nargin<3
        color = 'RED';
     end
 
-    Colors = createColorsMap;
+    newMap = map;
+    
+    Colors = createColorsnew_map;
     % Index for specName is the same as for corresponding specID
-    spec_ID = map.specID(ismember(map.specName,metList));
-    index = find(ismember(map.molID,spec_ID));
+    spec_ID = newMap.specID(ismember(newMap.specName,metList));
+    index = find(ismember(newMap.molID,spec_ID));
 
     % Change color
     for i = index'
-       map.molColor{i} = Colors(color); 
+       newMap.molColor{i} = Colors(color); 
     end
 
 end 
