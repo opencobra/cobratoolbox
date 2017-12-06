0f437b850e83b867916608828f3ee1bd92d91ba0
NicolasSompairac <nsompairac@gmail.com>
2017-12-06 19:17:25 +0100

    Correct description format

--- src/transformToReversibleMap.m
+++ src/transformToReversibleMap.m
@@ -1,27 +1,24 @@
-function map = transformToReversibleMap(map, rxnlist)
-
+function [mapRev] = transformToReversibleMap(map, rxnlist)
 % Converts a map structure from irreversible format to
 % reversible format for a list of reaction names
 %
 % USAGE:
 %
-%   mapRev = transformToReversibleMap(map, rxnlist)
+%   [mapRev] = transformToReversibleMap(map, rxnlist)
 %
-% INPUT:
-%
+% INPUTS:
 %   map:   Map from CD parsed to matlab format
-%
 %   rxnlist: List of reaction names to transform
 %
 % OUTPUT:
-%
 %   mapRev:   Map in reversible format
 %
 % .. Authors:
-% .. MOUSS Rouquaya 24/07/2017
-% .. N.Sompairac - Institut Curie, Paris 25/07/2017
+%       - MOUSS Rouquaya 24/07/2017
+%       - N.Sompairac - Institut Curie, Paris 25/07/2017
 
-    index = find(ismember(map.rxnName,rxnlist));
-    map.rxnReversibility(index,1) = {'true'};
+    mapRev = map;
+    index = find(ismember(mapRev.rxnName,rxnlist));
+    mapRev.rxnReversibility(index,1) = {'true'};
 
 end
