0a1ce262a5584da399b49620cc09c38763b1fff0
NicolasSompairac <nsompairac@gmail.com>
2017-12-07 12:26:42 +0100

    Change variable to camelToe format

--- src/findMetFromCompartMap.m
+++ src/findMetFromCompartMap.m
@@ -1,10 +1,10 @@
-function [mets, ID] = findMetFromCompartMap(map, compartment)
+function [mets, id] = findMetFromCompartMap(map, compartment)
 % Finds all the metabolites and their names in the map structure for
 % a compartment of interest.
 %
 % USAGE:
 %
-%    [mets, ID] = findMetFromCompartMap(map, Compartment)
+%    [mets, id] = findMetFromCompartMap(map, Compartment)
 %
 % INPUTS:
 %    map:               Map from CellDesigner parsed to matlab format
@@ -12,7 +12,7 @@ function [mets, ID] = findMetFromCompartMap(map, compartment)
 %
 % OUTPUTS:
 %    mets:              List of metabolites names
-%    ID:                Metabolites indexes
+%    id:                Metabolites indexes
 %
 % .. Authors:
 %       - MOUSS Rouquaya 24/07/2017
@@ -20,7 +20,7 @@ function [mets, ID] = findMetFromCompartMap(map, compartment)
 %       - N.Sompairac - Institut Curie, Paris, 11/10/2017
 
     index = strfind(map.specName, compartment);
-    ID = find(~cellfun(@isempty, index));
-    mets = map.specName(ID);
+    id = find(~cellfun(@isempty, index));
+    mets = map.specName(id);
     
 end
