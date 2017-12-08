9db22699280aa51214389c1e6515873b592a7959
Thomas Pfau <ThomasPfau@gmx.de>
2017-12-08 13:11:17 +0100

    :bug: Fixed grRules generation (missed replacing | and &)

--- src/base/io/utilities/creategrRulesField.m
+++ src/base/io/utilities/creategrRulesField.m
@@ -1,24 +1,47 @@
-function model = creategrRulesField(model)
+function model = creategrRulesField(model, positions)
 % Generates the grRules optional model field from the required rules and gene fields.
 %
 % USAGE:
 %
-%    modelWithField = creategrRulesField(model)
-%
+%    modelWithField = creategrRulesField(model, positions)
+%    
 % INPUT:
 %    model:             The COBRA Model structure to generate the grRules Field for
 %                       an existing grRules field will be overwritten
+%
+% OPTIONAL INPUT:
+%
+%    positions:         The positions to update. Can be supplied either as 
+%                       logical array, double indices, or reaction names (Default: model.rxns)
 %
 % OUTPUT:
 %    model:    The Output model with a grRules field
 %
 % .. Authors: - Thomas Pfau May 2017
 
+if ~exist('positions','var')
+    pos = true(size(model.rxns));
+else
+    if islogical(positions)
+        pos = positions;
+    else
+        if isnumeric(positions)
+            pos = false(size(model.rxns));
+            pos(positions) = true;
+        elseif iscell(positions)
+            pos = ismember(model.rxns,positions);
+            if ~sum(pos) == numel(unique(positions))
+                error('The following reactions are not part of this model:\n%s\n',positions(~ismember(postions, model.rxns)));
+            end
+        end
+        
+    end
+end
 
-currentrules = model.rules;
+currentrules(pos) = model.rules(pos);
 currentrules = strrep(currentrules,'&','and');
 currentrules = strrep(currentrules,'|','or');
 for i = 1:numel(model.genes)
     currentrules = strrep(currentrules,['x(' num2str(i) ')'],['(' model.genes{i} ')']);
 end
-model.grRules = currentrules;
+model.grRules(pos) = currentrules;
