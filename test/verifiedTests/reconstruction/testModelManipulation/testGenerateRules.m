d1d0231efa740865971f6f1694dd312890d33e30
Thomas Pfau <thomas.pfau@uni.lu>
2017-12-11 07:35:58 +0100

    Testing rules on boolean equality not string equality

--- test/verifiedTests/reconstruction/testModelManipulation/testGenerateRules.m
+++ test/verifiedTests/reconstruction/testModelManipulation/testGenerateRules.m
@@ -15,7 +15,7 @@ currentDir = pwd;
 fileDir = fileparts(which('testGenerateRules'));
 cd(fileDir);
 
-modelsToTry = {'Acidaminococcus_intestini_RyC_MR95.mat', 'Acidaminococcus_sp_D21.mat', 'Recon1.0model.mat', 'Recon2.v04.mat', 'ecoli_core_model.mat', 'modelReg.mat'};
+modelsToTry = {'Recon1.0model.mat', 'ecoli_core_model.mat'};
 
 for i=1:length(modelsToTry)
     model = getDistributedModel(modelsToTry{i});
@@ -23,20 +23,17 @@ for i=1:length(modelsToTry)
 
     model2 = generateRules(model);
     model.rules = strrep(model.rules, '  ', ' ');
-    model.rules = strrep(model.rules, ' )', ')');
-    model.rules = strrep(model.rules, '( ', '(');
-
-    % fix for Recon2
-    if strcmp(modelsToTry{i}, 'Recon2.v04.mat')
-        model.rules(2240) = {'(x(2)) | (x(4)) | (x(3))'}; % '(26.1) or (314.1) or (314.2)'
-        model.rules(2543) = {'(x(2)) | (x(4)) | (x(1)) | (x(3))'}; % '(26.1) or (314.1) or (8639.1) or (314.2)'
-        model.rules(2750) = {'(x(2)) | (x(4)) | (x(1)) | (x(3))'}; % '(26.1) or (314.1) or (8639.1) or (314.2)'
-        model.rules(2940) = {'(x(4)) | (x(60)) | (x(2)) | (x(61)) | (x(3))'}; % '(314.1) or (4128.1) or (26.1) or (4129.1) or (314.2)'
-        model.rules(3133) = {'(x(2)) | (x(4)) | (x(1)) | (x(3))'}; % '(26.1) or (314.1) or (314.2)'
+    fp = FormulaParser();
+    for rule = 1:numel(model.rules)
+        if isempty(model.rules{rule})
+            %assert that both formulas are empty (and thus equal).
+            assert(isequal(model.rules{rule},model2.rules{rule}))
+            continue;
+        end
+        head1 = fp.parseFormula(model.rules{rule});
+        head2 = fp.parseFormula(model2.rules{rule});
+        %Assert that the formulas are equal.
+        assert(head1.isequal(head2));
     end
-
-    % assert if the generated names are the same
-    assert(all(strcmp(model.rules, model2.rules)));
-
     fprintf('Succesfully completed model %s\n', modelsToTry{i});
 end
