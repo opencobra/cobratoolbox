ccbf875400c21c4fce4998811d0773b57417aea7
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-12-11 10:36:38 +0100

    linting

--- src/visualization/metabolicCartography/unifyMetabolicMapCD.m
+++ src/visualization/metabolicCartography/unifyMetabolicMapCD.m
@@ -13,15 +13,15 @@ function [map2] = unifyMetabolicMapCD(map)
 %   map2:   Map with grey reactions colour, width 1 and white nodes colour.
 %
 % .. Authors: - J.Modamio LCSB, Belval, Luxembourg. 19.08.2017
-    
+
     map2 = map;
-    
-    for i = 1:length(map2.rxnName)   
-        map2.rxnColor{i,1} = 'FFDCDCDC';
-        map2.rxnWidth{i,1} = 1; 
+
+    for i = 1:length(map2.rxnName)
+        map2.rxnColor{i, 1} = 'FFDCDCDC';
+        map2.rxnWidth{i, 1} = 1;
     end
     for j = 1:length(map2.molColor)
-        map2.molColor{j,1} = 'FFFFFFFF';
+        map2.molColor{j, 1} = 'FFFFFFFF';
     end
-    
+
 end
--- src/visualization/metabolicCartography/unifyMetabolicPPImapCD.m
+++ src/visualization/metabolicCartography/unifyMetabolicPPImapCD.m
@@ -11,7 +11,7 @@ function [map2] = unifyMetabolicPPImapCD(map)
 %
 % OUTPUT:
 %   map2:   Map with grey reactions colour, width 1 and white nodes colour.
-%           Change colour complex to white 
+%           Change colour complex to white
 %
 % .. Authors:
 %       - J.Modamio LCSB, Belval, Luxembourg. 19.08.2017 LCSB. Belval. Luxembourg
@@ -20,49 +20,49 @@ function [map2] = unifyMetabolicPPImapCD(map)
     map2 = map;
 
     % Change reaction colour to ligth grey and and width to 1.0
-    for i = 1:length(map2.rxnName)   
-        map2.rxnColor{i,1} = 'FFDCDCDC';
-        map2.rxnWidth{i,1} = 1; 
+    for i = 1:length(map2.rxnName)
+        map2.rxnColor{i, 1} = 'FFDCDCDC';
+        map2.rxnWidth{i, 1} = 1;
     end
-    % Change molecules color to white 
+    % Change molecules color to white
     for j = 1:length(map2.molColor)
-        map2.molColor{j,1} = 'FFFFFFFF';
-    end 
-    % Change complex colour to white 
+        map2.molColor{j, 1} = 'FFFFFFFF';
+    end
+    % Change complex colour to white
     for c = 1:length(map2.complexColor)
-        map2.complexColor{c,1} = 'FFFFFFFF';
-    end 
+        map2.complexColor{c, 1} = 'FFFFFFFF';
+    end
 
     % Change reactions secondary links to light grey (products)
     % and width to 1
-    index=find(~cellfun(@isempty, map2.rxnProductLineColor));
-    for t = 1:length(index)         
-        if length(map2.rxnProductLineColor{t,1}) > 1            
-            for d = 1:length(map2.rxnProductLineColor{t,1})
-                map2.rxnProductLineColor{t,1}{d,1} = 'FFDCDCDC';
-                map2.rxnProductLineWidth{t,1}{d,1} = 1;
-            end                
-        elseif length(map2.rxnProductLineColor{t,1}) == 1            
-             map2.rxnProductLineColor{t,1}{1,1} = 'FFDCDCDC';
-             map2.rxnProductLineWidth{t,1}{1,1} = 1;
-        end 
-    end    
-    clear id index
-    
-    % Change reactions secondary links to light grey (reactants)
-    % and width to 1
-    index=find(~cellfun(@isempty, map2.rxnReactantLineColor));
-    for r = 1:length(index)        
-        if length(map2.rxnReactantLineColor{r,1}) > 1            
-            for p = 1:length(map2.rxnReactantLineColor{r,1})
-                map2.rxnReactantLineColor{r,1}{p,1} = 'FFDCDCDC';
-                map2.rxnReactantLineWidth{r,1}{p,1} = 1;
+    index = find(~cellfun(@isempty, map2.rxnProductLineColor));
+    for t = 1:length(index)
+        if length(map2.rxnProductLineColor{t, 1}) > 1
+            for d = 1:length(map2.rxnProductLineColor{t, 1})
+                map2.rxnProductLineColor{t, 1}{d, 1} = 'FFDCDCDC';
+                map2.rxnProductLineWidth{t, 1}{d, 1} = 1;
             end
-        elseif length(map2.rxnReactantLineColor{r,1}) == 1
-            map2.rxnReactantLineColor{r,1}{1,1} = 'FFDCDCDC';
-            map2.rxnReactantLineWidth{r,1}{1,1} = 1;
+        elseif length(map2.rxnProductLineColor{t, 1}) == 1
+            map2.rxnProductLineColor{t, 1}{1, 1} = 'FFDCDCDC';
+            map2.rxnProductLineWidth{t, 1}{1, 1} = 1;
         end
     end
     clear id index
-    
-end  
+
+    % Change reactions secondary links to light grey (reactants)
+    % and width to 1
+    index = find(~cellfun(@isempty, map2.rxnReactantLineColor));
+    for r = 1:length(index)
+        if length(map2.rxnReactantLineColor{r, 1}) > 1
+            for p = 1:length(map2.rxnReactantLineColor{r, 1})
+                map2.rxnReactantLineColor{r, 1}{p, 1} = 'FFDCDCDC';
+                map2.rxnReactantLineWidth{r, 1}{p, 1} = 1;
+            end
+        elseif length(map2.rxnReactantLineColor{r, 1}) == 1
+            map2.rxnReactantLineColor{r, 1}{1, 1} = 'FFDCDCDC';
+            map2.rxnReactantLineWidth{r, 1}{1, 1} = 1;
+        end
+    end
+    clear id index
+
+end
