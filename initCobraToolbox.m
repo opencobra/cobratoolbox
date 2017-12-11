0354b1e2245ae6f101831c45b0fba459221c0791
Thomas Pfau <thomas.pfau@uni.lu>
2017-12-11 10:17:34 +0100

    Separated adaptMacPath functionality

--- initCobraToolbox.m
+++ initCobraToolbox.m
@@ -531,46 +531,6 @@ function initCobraToolbox()
     
 end
 
-
-function adaptMacPath()
-% Adapts the path for binaries on a mac with Sierra/High Sierra or higher
-% and Anything below.
-%
-% USAGE:
-%     adaptMacPath();
-%
-global CBTDIR;
-oldMac = false;
-
-[status,macVer] = system('sw_vers')
-if status ~= 0
-    oldMac = true;
-else
-    try
-        macVer = strsplit(macVer,'\n');
-        macVer = macVer{2};
-        macVer = strsplit(macVer,':');
-        macVer = str2double(macVer{2});
-        if macVer < 10.12 %Lower than Sierra
-            oldMac = true;
-        end
-    catch
-        oldMac = true;
-    end
-end
-
-macBinaryPath = [CBTDIR filesep 'binary' filesep 'maci64' filesep 'bin'];
-
-%Set the current and remove the other path. 
-if oldMac    
-    addpath([macBinaryPath filesep 'preSierra']);
-    rmpath([macBinaryPath filesep 'postSierra']);
-else
-    addpath([macBinaryPath filesep 'postSierra']);
-    rmpath([macBinaryPath filesep 'preSierra']);
-end
-end
-
 function checkGit()
 % Checks if git is installed on the system and throws an error if not
 %
