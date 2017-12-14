4f7a4eb315c4c9be63b56eeb6dd03e22f3d99760
laurentheirendt <laurent.heirendt@uni.lu>
2017-12-14 11:21:19 +0100

    Removing global variables

--- test/verifiedTests/analysis/testtuneParam/testtuneParam.m
+++ test/verifiedTests/analysis/testtuneParam/testtuneParam.m
@@ -1,18 +1,12 @@
 % The COBRAToolbox: testtuneParam.m
 %
 % Purpose:
-%     - testtuneParam tests the CPLEX parameter tuning tool of
-%       IBM CPLEX
+%     - testtuneParam tests the CPLEX parameter tuning tool of IBM CPLEX
 %
 % Author:
 %     - Marouen BEN GUEBILA 04/12/2017
 
-global CBTDIR
-global ILOG_CPLEX_PATH
-
-addpath(genpath(ILOG_CPLEX_PATH));
-
-%Load the model
+% load the model
 model = getDistributedModel('ecoli_core_model.mat');
 
 % save the current path
@@ -20,28 +14,30 @@ currentDir = pwd;
 
 % initialize the test
 fileDir = fileparts(which('testtuneParam'));
-cd(fileDir);    
+cd(fileDir);
 
-%Change the solver to IBM Cplex
+% change the solver to IBM CPLEX
 solverOK = changeCobraSolver('ibm_cplex');
 
 if solverOK
     %retrieve all IBM Cplex paramters
     cpxControl = CPLEXParamSet('ILOGcomplex');
+
     %set barrier as solver algorithm and set numerical emphasis to 1
-    cpxControl.lpmethod=4;
-    cpxControl.emphasis.numerical=1;
+    cpxControl.lpmethod = 4;
+    cpxControl.emphasis.numerical = 1;
+
     %Optimize parameter values
-    timeLimit=5;%seconds
-    nRuns=5;%runs
-    printLevel=0;
-    optimParam = tuneParam(model,cpxControl,timeLimit,nRuns,printLevel);
+    timeLimit = 5;%seconds
+    nRuns = 5;%runs
+    printLevel = 0;
+    optimParam = tuneParam(model, cpxControl, timeLimit, nRuns, printLevel);
 
     %test results
     %check if automatic is the optimal solver algorithm for Ecoli and
     %numerical emphasis desactivated
-    assert(isequal(optimParam.lpmethod,0))
-    assert(isequal(optimParam.emphasis.numerical,0))
+    assert(isequal(optimParam.lpmethod, 0))
+    assert(isequal(optimParam.emphasis.numerical, 0))
 end
 
 % change the directory
