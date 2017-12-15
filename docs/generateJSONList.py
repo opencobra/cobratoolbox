638b94339d6341dcd748a651884ef35a6385ed31
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-12-15 11:15:32 +0100

    create folders

--- docs/generateJSONList.py
+++ docs/generateJSONList.py
@@ -1,19 +1,32 @@
 import os
 import json
 
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
 def path_to_list(path):
     d = []
+    path_length = len(path)
     for root, directories, filenames in os.walk(path):
         for filename in filenames:
             if filename[-2:] == '.m':
-                website_url = os.path.join("modules", root[9:], "index.html")
+                website_url = os.path.join("modules", root[path_length+1:], "index.html")
                 website_url += "?highlight=" + filename[:-2]
-                website_url += "#" + '.'.join(['src'] + root[9:].split(os.path.sep) + [filename[:-2]])
+                website_url += "#" + '.'.join(['src'] + root[path_length+1:].split(os.path.sep) + [filename[:-2]])
                 d.append({'name': filename[:-2], 
                           'website_url': website_url})
                 print root, filename
     return d
 
-with open('source/_static/json/functions.json', 'w') as f:
+destination_dir = os.path.join('source', '_static', 'json')
+mkdir_p(destination_dir)
+with open(os.path.join(destination_dir, 'functions.json'), 'w') as f:
     d = path_to_list('../src/.')
     json.dump(d, f)
