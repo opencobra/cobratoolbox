7b773a910d26c93f894592ed65cf78a859dde316
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2017-12-15 11:15:32 +0100

    create folders

--- docs/generateJSONList.py
+++ docs/generateJSONList.py
@@ -11,6 +11,16 @@ def mkdir_p(path):
         else:
             raise
 
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
 def path_to_list(path):
     d = []
     path_length = len(path)
