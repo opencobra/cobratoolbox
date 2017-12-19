7ef4bef81f783dc47a5836163e2a9efeba586bf8
Jacek <jacekwachowiak94@gmail.com>
2017-12-19 14:16:18 +0100

    Change to the model

--- test/verifiedTests/analysis/testThermo/testVonBertalanffy/testConfigureSetupThermoModelInputs.m
+++ test/verifiedTests/analysis/testThermo/testVonBertalanffy/testConfigureSetupThermoModelInputs.m
@@ -5,7 +5,7 @@
 %
 % Authors:
 %     - Jacek Wachowiak
-global CBTDIR
+
 % save the current path
 currentDir = pwd;
 
@@ -14,7 +14,7 @@ fileDir = fileparts(which('testConfigureSetupThermoModelInputs'));
 cd(fileDir);
 
 % test variables
-model = getDistributedModel('Recon3D_Dec2017.mat');
+model = getDistributedModel('Recon3DModel_Dec2017.mat');
 model.csense(1:size(model.S,1),1)='E';
 model.metFormulas{strcmp(model.mets,'h[i]')}='H';
 model.metFormulas(cellfun('isempty',model.metFormulas)) = {'R'};
