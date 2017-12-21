d7035a9b0186883a30d86681fa5a874e6fd1b2db
laurentheirendt <laurent.heirendt@uni.lu>
2017-12-21 14:08:53 +0100

    Adding check installation function for CNA

---
+++ src/base/install/checkCNAinstallation.m
@@ -0,0 +1,41 @@
+function status = checkCNAinstallation(printLevel)
+% checks the installation of CellNetAnalyzer
+%
+% USAGE:
+%    status = checkCNAinstallation(printLevel)
+%
+% INPUT:
+%
+%    printLevel:    verbose level (default: 1)
+%
+% OUTPUT:
+%
+%   status:         Boolean if CellNetAnalyzer is installed properly
+%
+
+    global CBTDIR
+
+    % set default printLevel
+    if nargin < 1
+        printLevel = 1;
+    end
+
+    % make sure that the interface functions from the submodule are used
+    fullPath = which('CNAcobra2cna');
+    if isempty(strfind(fullPath, 'external'))
+        addpath(genpath([CBTDIR filesep 'external' filesep 'CnaCobraInterface']));
+    end
+
+    % check if CNA is available and convert the model
+    if ~isempty(which('startcna'))
+        if printLevel > 0
+            fprintf(' > CellNetAnalyzer is installed properly.\n');
+        end
+        status = true;
+    else
+        if printLevel > 0
+            fprintf(' > CellNetAnalyzer is not properly installed or not added to the MATLAB path.\n');
+        end
+        status = false;
+    end
+end
