64a4cc96f52479d34bd514559756a14b4843dc30
laurentheirendt <laurent.heirendt@uni.lu>
2017-12-21 14:09:24 +0100

    Adding test for convert functions

---
+++ test/verifiedTests/base/testIO/testConvertCNAModelToCbModel.m
@@ -0,0 +1,45 @@
+% The COBRAToolbox: testConvertCNAModelToCbModel.m
+%
+% Purpose:
+%     - convert a CNA model to a COBRA model
+%
+
+% save the current path
+currentDir = pwd;
+
+% initialize the test
+fileDir = fileparts(which('testWriteSBML'));
+cd(fileDir);
+
+% check if CNA is properly installed
+statusCNA = checkCNAinstallation();
+
+if statusCNA
+    model = getDistributedModel('ecoli_core_model.mat');
+
+    % load reference data
+    load('refData_cnaModel.mat')
+
+    % convert a COBRA model to a CNA model
+    cnaModel_new = convertCbModelToCNAModel(model);
+
+    % assert if the cnaModel_new is equal to the reference CNA model
+    assert(isequaln(cnaModel_new, cnaModel));
+
+    % convert the reference CNA model to the COBRA model
+    model_new = convertCNAModelToCbModel(cnaModel);
+
+    assert(isequal(model_new.rxns, model.rxns));
+    assert(isequal(model_new.mets, model.mets));
+    assert(isequal(model_new.S, model.S));
+    assert(isequal(model_new.lb, model.lb));
+    assert(isequal(model_new.ub, model.ub));
+    assert(isequal(model_new.c, model.c));
+    assert(isequal(model_new.metNames, model.metNames));
+    assert(isequal(model_new.b, model.b));
+
+    % rxnNames and rev are different
+end
+
+% change the directory
+cd(currentDir)
