7fb6b33e2210b61cc66e9764a4b95c78b9697f9a
Jacek <jacekwachowiak94@gmail.com>
2017-12-21 10:16:31 +0100

    Editing and reopening

--- test/verifiedTests/analysis/testThermo/testVonBertalanffy/testSetupComponentContribution.m
+++ test/verifiedTests/analysis/testThermo/testVonBertalanffy/testSetupComponentContribution.m
@@ -13,6 +13,7 @@ currentDir = pwd;
 fileDir = fileparts(which('testSetupComponentContribution'));
 cd(fileDir);
 
+% this test is using babel library - if it is not installed, no computation will be made
 [status,result] = system('babel'); %if run without initVonBertalanffy it is needed to fix the path
 if status ~= 0
     setenv('LD_LIBRARY_PATH',['/usr/lib/x86_64-linux-gnu:' getenv('LD_LIBRARY_PATH')]);
@@ -23,6 +24,7 @@ if status ~= 0
   disp('No babel installed, test cannot proceed - for babel installation see docs/source/installation/vonBertalanffy.md')
   return
 end
+
 % test variables
 model = getDistributedModel('Recon3DModel_Dec2017.mat');
 model.csense(1:size(model.S,1),1)='E';
