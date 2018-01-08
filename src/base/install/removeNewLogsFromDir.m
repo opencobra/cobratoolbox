1e7e1ac6899a3550d80ebafc8a7d3ac37d4cfd47
Thomas Pfau <thomas.pfau@uni.lu>
2018-01-08 10:12:33 +0100

    Adding cleanup Method for log files

---
+++ src/base/install/removeNewLogsFromDir.m
@@ -0,0 +1,25 @@
+function removeNewLogsFromDir(folder, oldcontent)
+% Removes all log files from the cobra base folder that were not present
+% in the content
+%
+% USAGE:
+%     removeNewLogsFromDir(folder, content)
+%
+% INPUT:
+%     content:   a directory structure as obtained by dir
+%
+
+
+newContent = dir(folder);%Get the Content of the CBTDIR
+
+%Get all .log files that were present only after initCobraToolbox was
+%called.
+diffContent = setdiff({newContent.name},{oldcontent.name});
+matching = cellfun(@(x) ~isempty(regexp(x,'\.log$','ONCE')),diffContent);
+LogFiles = diffContent(matching);
+%Attach the CBTDirectory to delete the right files.
+LogFiles = strcat(folder, filesep, LogFiles);
+if ~isempty(LogFiles)
+    delete(LogFiles{:});
+end
+end
