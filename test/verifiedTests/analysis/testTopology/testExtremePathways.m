f3dec7a1386d861d2501c45d352dd65216b4d8d9
Thomas Pfau <thomas.pfau@uni.lu>
2018-01-11 09:01:11 +0100

    Adding Support on windows machines

--- test/verifiedTests/analysis/testTopology/testExtremePathways.m
+++ test/verifiedTests/analysis/testTopology/testExtremePathways.m
@@ -10,73 +10,69 @@
 %     Extreme Pathway Lengths and Reaction Participation in Genome-Scale Metabolic Networks
 %     Jason A. Papin, Nathan D. Price and Bernhard Ø. Palsson
 
-if isunix
-    [status, result] = system('which lrs');
-    if ~isempty(strfind(result, '/lrs'))
-
-        % save the current path
-        currentDir = pwd;
-
-        % initialize the test
-        fileDir = fileparts(which('testExtremePathways'));
-        cd(fileDir);
-        model = createExtremePathwayModel();
-        S = [-1,  0,  0,  0,  0,  0, 1,  0,  0;
-              1, -2, -2,  0,  0,  0, 0,  0,  0;
-              0,  1,  0,  0, -1, -1, 0,  0,  0;
-              0,  0,  1, -1,  1,  0, 0,  0,  0;
-              0,  0,  0,  1,  0,  1, 0, -1,  0;
-              0,  1,  1,  0,  0,  0, 0,  0, -1;
-              0,  0, -1,  1, -1,  0, 0,  0,  0];
-        
-        minimalModel = struct();
-        minimalModel.S = model.S;
-
-        % calculates the matrix of extreme pathways, P
-        [P, V] = extremePathways(minimalModel);
-
-        refP = [2, 2, 2;
-                1, 0, 1;
-                0, 1, 0;
-                0, 1, 1;
-                0, 0, 1;
-                1, 0, 0;
-                2, 2, 2;
-                1, 1, 1;
-                1, 1, 1];
-
-        assert(all(all(refP(:, [2, 1, 3]) == P)))
-
-        positivity = 0;
-        inequality = 1;
-
-        [P, V] = extremePathways(model, positivity, inequality)
-
-        refP = [ 0,  0, 2;
-                 1,  1, 0;
-                -1, -1, 1;
-                 0, -1, 1;
-                 1,  0, 0;
-                 0,  1, 0;
-                 0,  0, 2;
-                 0,  0, 1;
-                 0,  0, 1];
-
-        assert(all(all(refP == P)))
-
-        % Change the model to have one non integer entry.
-        model.S(1, 1) = 0.5;
-        assert(verifyCobraFunctionError(@() extremePathways(model)));        
-
-        % delete generated files
-        delete('*.ine');
-        delete('*.ext');
-
-        % change the directory
-        cd(currentDir)
-    else
-        fprintf('lrs not installed or not in path\n');
-    end
+[status, result] = system('which lrs');
+if ~isempty(strfind(result, '/lrs'))
+    
+    % save the current path
+    currentDir = pwd;
+    
+    % initialize the test
+    fileDir = fileparts(which('testExtremePathways'));
+    cd(fileDir);
+    model = createExtremePathwayModel();
+    S = [-1,  0,  0,  0,  0,  0, 1,  0,  0;
+        1, -2, -2,  0,  0,  0, 0,  0,  0;
+        0,  1,  0,  0, -1, -1, 0,  0,  0;
+        0,  0,  1, -1,  1,  0, 0,  0,  0;
+        0,  0,  0,  1,  0,  1, 0, -1,  0;
+        0,  1,  1,  0,  0,  0, 0,  0, -1;
+        0,  0, -1,  1, -1,  0, 0,  0,  0];
+    
+    minimalModel = struct();
+    minimalModel.S = model.S;
+    
+    % calculates the matrix of extreme pathways, P
+    [P, V] = extremePathways(minimalModel);
+    
+    refP = [2, 2, 2;
+        1, 0, 1;
+        0, 1, 0;
+        0, 1, 1;
+        0, 0, 1;
+        1, 0, 0;
+        2, 2, 2;
+        1, 1, 1;
+        1, 1, 1];
+    
+    assert(all(all(refP(:, [2, 1, 3]) == P)))
+    
+    positivity = 0;
+    inequality = 1;
+    
+    [P, V] = extremePathways(model, positivity, inequality)
+    
+    refP = [ 0,  0, 2;
+        1,  1, 0;
+        -1, -1, 1;
+        0, -1, 1;
+        1,  0, 0;
+        0,  1, 0;
+        0,  0, 2;
+        0,  0, 1;
+        0,  0, 1];
+    
+    assert(all(all(refP == P)))
+    
+    % Change the model to have one non integer entry.
+    model.S(1, 1) = 0.5;
+    assert(verifyCobraFunctionError(@() extremePathways(model)));
+    
+    % delete generated files
+    delete('*.ine');
+    delete('*.ext');
+    
+    % change the directory
+    cd(currentDir)
 else
-    fprintf('non unix machines not yet supported\n');
+    fprintf('lrs not installed or not in path\n');
 end
