cb51974aa573137777a85d0e5259693847dfb96f
Thomas Pfau <ThomasPfau@gmx.de>
2018-01-15 10:47:34 +0100

    Updating Documentation and variable Names

--- src/base/install/removeGitIgnoredNewFiles.m
+++ src/base/install/removeGitIgnoredNewFiles.m
@@ -1,16 +1,20 @@
-function removeGitIgnoredNewFiles(folder, oldcontent)
-% Removes all files that are part of the specified folder but not part of
+function removeGitIgnoredNewFiles(directory, oldcontent)
+% Removes all files that are in the specified directory but not part of
 % the oldcontent if they match any file indicated by the gitignored files
 % of the COBRA Toolbox .gitignore file.
 %
 % USAGE:
-%     removeGitIgnoredNewFiles(folder, content)
+%     removeGitIgnoredNewFiles(directory, content)
 %
 % INPUT:
-%     content:   a directory structure as obtained by dir
+%     directory: The directory which should be checked for changing files.
+%     content:   The directory structure obtained by rdir called in the
+%                directory indicated (i.e. cd(directory); content =
+%                rdir(['**' filesep '*'])
 %
 
-orig = cd(folder);
+
+currentDir = cd(directory);
 
 newContent = rdir(['**' filesep '*']);%Get the new Content of the folder.
 
@@ -34,5 +38,5 @@ if ~isempty(LogFiles)
     delete(LogFiles{:});
 end
 
-cd(orig);
+cd(currentDir);
 end
