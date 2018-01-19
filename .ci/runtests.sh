84f602a4062d36ecb6b31c6a057ad34f096b6fd3
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2018-01-19 15:00:49 +0100

    trying not to run the tests if only documentation files have been updated

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -1,18 +1,52 @@
 #!/bin/sh
 
-lastCommitMsg=$(git log -1 --pretty=%B 2>&1)
-echo $lastCommitMsg
+# Finding the right way to make bold characters depending on OS
+bold=$(tput bold)
+normal=$(tput sgr0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
 
+echo "Guessing if the tests should be run..."
+if [[ ! -z $GIT_PREVIOUS_SUCCESSFUL_COMMIT ]]; then
+   commitHashs=($(git cherry $GIT_PREVIOUS_SUCCESSFUL_COMMIT HEAD 2>&1))
 
+   # check if all commit messages contains only [documentation]
+   allDocumentationLabel=true
+   for s in "${commitHashs[@]}"
+   do
+       if  [[ $s != "+" ]]; then
+           commitHash=$s
+           commitMsg=$(git show $commitHash -q --pretty=%B 2>&1)
 
-# Exit if commit message contains the string: [documentation]
-if [[ $commitMsg == *"[documentation]"* ]]; then
-    echo "Test suite should not be ran."
-    exit;
+           # Exit if commit message contains the string: [documentation]
+           if [[ ! $commitMsg == *"[documentation]"* ]]; then
+               allDocumentationLabel=false;
+               echo "-- at least one commit message ($commitHash) does not contain the label [documentation]."
+               break;
+           fi
+       fi
+   done
+
+   modifiedFiles=($(git diff --name-only $GIT_PREVIOUS_SUCCESSFUL_COMMIT HEAD 2>&1))
+   onlyDocFiles=true
+   for f in "${modifiedFiles[@]}"
+   do
+       if  [[ ! $f == *"docs/"* ]]; then
+           onlyDocFiles=false
+           echo "-- at least one modified file ($f) is not stored under the docs/ folder."
+           break;
+       fi
+   done
+
+   if [ "$allDocumentationLabel" = true ] || [ "$onlyDocFiles" = true ]; then
+       echo "> Tests will be ${green}skipped${normal} as modified files (since last previous successful commit) are ${green}only${normal} documentation files."
+       exit;
+   else
+       echo "> Tests will be ${green}ran${normal} as modified files (since last previous successful commit) are ${green}not only${normal} documentation files."
+   fi
+else
+    echo "> Tests will be ${green}ran${normal} as variable $GIT_PREVIOUS_SUCCESSFUL_COMMIT is not set."
 fi
-
-
-
 
 if [ "$ARCH" == "Linux" ]; then
     /mnt/prince-data/MATLAB/$MATLAB_VER/bin/./matlab -nodesktop -nosplash < test/testAll.m
