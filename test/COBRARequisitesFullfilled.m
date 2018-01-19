a2446df1c3f90628467d0fcae757d5173cb23d3c
Thomas Pfau <thomas.pfau@uni.lu>
2018-01-19 12:02:45 +0100

    Added OS requirements

--- test/COBRARequisitesFullfilled.m
+++ test/COBRARequisitesFullfilled.m
@@ -26,23 +26,31 @@ function [solversToUse] = COBRARequisitesFullfilled(varargin)
 %                                       (Default = false);
 %                   * 'NeedsNLP'       - Whether a NLP solver is required.
 %                                       (Default = false);
-
+%                   * 'NeedsUnix'      - Whether the test only works on a
+%                                        Unix system (mac or linux)
+%                                        (Default = false);
+%                   * 'NeedsWindows'   - Whether the test only works on a Windows system 
+%                                        (Default = false);
+%                   * 'NeedsMac'       - Whether the test only works on a Mac system 
+%                                        (Default = false);
+%                   * 'NeedsLinux'     - Whether the test only works on a Linux system 
+%                                        (Default = false);
 
 %Do some precomputation.
 global CBT_MISSING_REQUIREMENTS;
-
+global OPT_PROB_TYPES
 persistent availableSolvers
-
 
 %Some Matlab Toolboxes currently in use in the COBRA Toolbox. 
 %This might have to be extended in the future. 
-toolboxInfo = struct('statistics_toolbox',{'Statistics and Machine Learning Toolbox','Statistics Toolbox'},...
-                     'bioinformatics_toolbox',{'Bioinformatics Toolbox'},...
-                     'distrib_computing_toolbox',{'Parallel Computing Toolbox'},...
-                     'optimization_toolbox',{'Optimization Toolbox'},...
-                     'global_optimization_toolbox',{'Global Optimization Toolbox'},...
-                     'image_toolbox','Image Processing Toolbox');
-
+toolboxInfo = struct('statistics_toolbox',{{'Statistics and Machine Learning Toolbox','Statistics Toolbox'}},...
+                     'bioinformatics_toolbox',{{'Bioinformatics Toolbox'}},...
+                     'distrib_computing_toolbox',{{'Parallel Computing Toolbox'}},...
+                     'optimization_toolbox',{{'Optimization Toolbox'}},...
+                     'global_optimization_toolbox',{{'Global Optimization Toolbox'}},...
+                     'image_toolbox',{{'Image Processing Toolbox'}},...
+                     'gads_toolbox',{{'Global Optimization Toolbox'}});
+                
 if isempty(availableSolvers)
     availableSolvers = getAvailableSolversByType();
     fieldsWithSolvers = fieldnames(availableSolvers);
@@ -53,15 +61,20 @@ if isempty(availableSolvers)
 end
 
 
-parser = inputParser()
+parser = inputParser();
 parser.addParamValue('Toolboxes',{},@iscell);
 parser.addParamValue('ReqSolvers',{},@iscell);
 parser.addParamValue('UseIfAvailable',{},@iscell);
-parser.addParamValue('NeedsLP',false,@(x) islogic(x) || x == 1 || x == 0 );
-parser.addParamValue('NeedsMILP',false,@(x) islogic(x) || x == 1 || x == 0 );
-parser.addParamValue('NeedsNLP',false,@(x) islogic(x) || x == 1 || x == 0 );
-parser.addParamValue('NeedsQP',false,@(x) islogic(x) || x == 1 || x == 0 );
-parser.addParamValue('NeedsMIQP',false,@(x) islogic(x) || x == 1 || x == 0 );
+parser.addParamValue('NeedsLP',false,@(x) islogical(x) || x == 1 || x == 0 );
+parser.addParamValue('NeedsMILP',false,@(x) islogical(x) || x == 1 || x == 0 );
+parser.addParamValue('NeedsNLP',false,@(x) islogical(x) || x == 1 || x == 0 );
+parser.addParamValue('NeedsQP',false,@(x) islogical(x) || x == 1 || x == 0 );
+parser.addParamValue('NeedsMIQP',false,@(x) islogical(x) || x == 1 || x == 0 );
+parser.addParamValue('NeedsUnix',false,@(x) islogical(x) || x == 1 || x == 0 );
+parser.addParamValue('NeedsLinux',false,@(x) islogical(x) || x == 1 || x == 0 );
+parser.addParamValue('NeedsWindows',false,@(x) islogical(x) || x == 1 || x == 0 );
+parser.addParamValue('NeedsMac',false,@(x) islogical(x) || x == 1 || x == 0 );
+
 
 parser.parse(varargin{:});
 
@@ -71,13 +84,52 @@ UseMIQP = parser.Results.NeedsMIQP;
 UseNLP = parser.Results.NeedsNLP;
 UseMILP = parser.Results.NeedsMILP;
 
+
+macOnly = parser.Results.NeedsMac;
+windowsOnly = parser.Results.NeedsWindows;
+unixOnly = parser.Results.NeedsUnix;
+linuxOnly = parser.Results.NeedsLinux;
+
+
+
 Toolboxes = parser.Results.Toolboxes;
 RequiredSolvers = parser.Results.ReqSolvers;
 PreferredSolvers = parser.Results.UseIfAvailable;
 
 errorMessage = {};
 
-%First, check the required Solvers
+%First, check whether the OS is applicable
+if macOnly
+    if ~ismac
+        errorMessage{end+1} = 'This test only works on macOS';
+    end
+end
+
+if windowsOnly
+    if ~ispc
+        errorMessage{end+1} = 'This test only works on Windows';
+    end
+end
+
+
+if linuxOnly
+    if ~isunix
+        errorMessage{end+1} = 'This test only works on Linux Systems';
+    else
+        if ~strcmp(computer('arch'),'glnx64')
+            errorMessage{end+1} = 'This test only works on Linux Systems';
+        end
+    end
+end
+
+if unixOnly
+    if ~isunix
+        errorMessage{end+1} = 'This test only works on Unix Systems (Mac and Linux)';
+    end
+end
+
+
+%Then, check the required Solvers
 if ~isempty(RequiredSolvers) && ~all(ismember(RequiredSolvers,availableSolvers.ALL))
     %We have required solvers and some are missing
     missing = ~ismember(RequiredSolvers,availableSolvers.ALL);
@@ -86,12 +138,12 @@ end
 
 %Now, Check the Toolboxes
 res = ver;
-missingTBs = struct('License',{},'Installation',{});
+missingTBs = struct('License',{{}},'Installation',{{}});
 for i = 1:numel(Toolboxes)
     tbstring = lower(Toolboxes{i});
     licpres = license('test',tbstring);
-    if any(tbstring,fieldnames(toolboxInfo))
-        tbpres = any(ismember(toolboxInfo.(Toolboxes{i}),{ver.name}));
+    if any(ismember(tbstring,fieldnames(toolboxInfo)))
+        tbpres = any(ismember(toolboxInfo.(lower(Toolboxes{i})),{res.Name}));
     else
         %We will rely on the license....
         tbpres = licpres;
@@ -103,17 +155,19 @@ for i = 1:numel(Toolboxes)
         missingTBs.License{end+1} = tbstring;       
     end
 end
+
+
 %Append the error message.
-if ~isempty(missingTbs.License)
-    errorMessage{end+1} = sprintf('The test requires licenses for the following Toolboxes: %s', strjoin(missingTbs.License,' and '));     
+if ~isempty(missingTBs.License)
+    errorMessage{end+1} = sprintf('The test requires licenses for the following Toolboxes: %s', strjoin(missingTBs.License,' and '));     
 end
-if ~isempty(missingTbs.Installation)
-    errorMessage{end+1} = sprintf('The test the following Toolboxes to be installed: %s', strjoin(missingTbs.Installation,' and '));     
+if ~isempty(missingTBs.Installation)
+    errorMessage{end+1} = sprintf('The test the following Toolboxes to be installed: %s', strjoin(missingTBs.Installation,' and '));     
 end
 
-%Set up default solvers.
+%Set up default solvers. And test whether the test is useable.
 if isempty(availableSolvers.LP)    
-    if needLP
+    if UseLP
         errorMessage{end+1} = 'The test requires at least one LP solver but no solver is installed';
     end    
 else
@@ -121,7 +175,7 @@ else
 end
 
 if isempty(availableSolvers.QP)    
-    if needQP
+    if UseQP
         errorMessage{end+1} = 'The test requires at least one QP solver but no solver is installed';
     end    
 else
@@ -129,7 +183,7 @@ else
 end
 
 if isempty(availableSolvers.MILP)    
-    if needMILP
+    if UseMILP
         errorMessage{end+1} = 'The test requires at least one MILP solver but no solver is installed';
     end    
 else
@@ -137,7 +191,7 @@ else
 end
 
 if isempty(availableSolvers.MIQP)    
-    if needMIQP
+    if UseMIQP
         errorMessage{end+1} = 'The test requires at least one MIQP solver but no solver is installed';
     end    
 else
@@ -145,7 +199,7 @@ else
 end
 
 if isempty(availableSolvers.NLP)    
-    if needNLP
+    if UseNLP
         errorMessage{end+1} = 'The test requires at least one NLP solver but no solver is installed';
     end    
 else
@@ -160,10 +214,10 @@ end
 
 %Ok, we are successfull. so lets collect the Used Solvers.
 solversToUse = struct();
-problemTypes = fieldnames(availableSolvers);
-for i = 1:problemTypes
+problemTypes = OPT_PROB_TYPES;
+for i = 1:numel(problemTypes)
     solversToUse.(problemTypes{i}) = intersect(PreferredSolvers,availableSolvers.(problemTypes{i}));
     if isempty(solversToUse.(problemTypes{i})) && ~isempty(availableSolvers.(problemTypes{i}))
-        eval(['solversToUse.' problemTypes{i} ' = default' problemTypes{i} 'Solver']);
+        eval(['solversToUse.' problemTypes{i} ' = default' problemTypes{i} 'Solver;']);
     end
 end
