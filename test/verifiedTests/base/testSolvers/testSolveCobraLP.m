9852294870984d84c7070f6821ef89ba62ea322d
Thomas Pfau <thomas.pfau@uni.lu>
2018-01-22 12:27:12 +0100

    Changed test to use whichever solver is available

--- test/verifiedTests/base/testSolvers/testSolveCobraLP.m
+++ test/verifiedTests/base/testSolvers/testSolveCobraLP.m
@@ -11,6 +11,11 @@
 
 global CBTDIR
 
+%Test the requirements
+useSolversIfAvailable = {'cplex_direct', 'glpk', 'gurobi', 'ibm_cplex', 'matlab', 'mosek', ...
+            'pdco', 'quadMinos', 'tomlab_cplex', 'mosek_linprog', 'dqqMinos'}; % 'lp_solve': legacy
+solvers = COBRARequisitesFullfilled('needsLP',true,'useIfAvailable',useSolversIfAvailable);
+
 % save the current path
 currentDir = pwd;
 
@@ -32,60 +37,56 @@ LPproblem.csense = ['L'; 'L'];
 tol = 1e-4;
 
 % test solver packages
-solverPkgs = {'cplex_direct', 'tomlab_cplex', 'gurobi6', 'glpk'};
+solverPkgs = solvers.LP;
 
 % list of tests
 testSuite = {'dummyModel', 'ecoli'};
 
 for k = 1:length(solverPkgs)
-
-    if ~verLessThan('matlab', '8') && strcmp(solverPkgs{k}, 'cplex_direct')  % 2015
-        fprintf(['\n IBM ILOG CPLEX - ', solverPkgs{k}, ' - is incompatible with this version of MATLAB, please downgrade or change solver\n'])
-    else
-        % change the COBRA solver (LP)
-        solverOK = changeCobraSolver(solverPkgs{k}, 'LP', 0);
-
-        if solverOK == 1
-            for p = 1:length(testSuite)
-                fprintf('   Running %s with solveCobraLP using %s ... ', testSuite{p}, solverPkgs{k});
-
-                if p == 1
-                    % solve LP problem printing summary information
-                    for printLevel = 0:3
-                        LPsolution = solveCobraLP(LPproblem, 'printLevel', printLevel);
-                    end
-
-                    for i = 1:length(LPsolution.full)
-                        assert((abs(LPsolution.full(i) - 1) < tol))
-                    end
-                    assert(abs(LPsolution.obj) - 600 < tol)
-
-                elseif p == 2
-                    % solve th ecoli_core_model (csense vector is missing)
-                    %This is explicitly a load, to test missing csense
-                    %vector compensation
-                    load([getDistributedModelFolder('ecoli_core_model.mat') filesep 'ecoli_core_model.mat'], 'model');
-
-                    % solveCobraLP
-                    solution_solveCobraLP = solveCobraLP(model);
-
-                    % optimizeCbModel
-                    solution_optimizeCbModel = optimizeCbModel(model);
-
-                    % compare both solution objects
-                    assert(solution_solveCobraLP.obj == solution_optimizeCbModel.f)
-                    assert(isequal(solution_solveCobraLP.full, solution_optimizeCbModel.x))
-                    assert(isequal(solution_solveCobraLP.rcost, solution_optimizeCbModel.w))
-                    assert(isequal(solution_solveCobraLP.dual, solution_optimizeCbModel.y))
-                    assert(solution_solveCobraLP.stat == solution_optimizeCbModel.stat)
-                end
-
-                % output a success message
-                fprintf('Done.\n');
+    
+    
+    % change the COBRA solver (LP)
+    solverOK = changeCobraSolver(solverPkgs{k}, 'LP', 0);
+    
+    for p = 1:length(testSuite)
+        fprintf('   Running %s with solveCobraLP using %s ... ', testSuite{p}, solverPkgs{k});
+        
+        if p == 1
+            % solve LP problem printing summary information
+            for printLevel = 0:3
+                LPsolution = solveCobraLP(LPproblem, 'printLevel', printLevel);
             end
+            
+            for i = 1:length(LPsolution.full)
+                assert((abs(LPsolution.full(i) - 1) < tol))
+            end
+            assert(abs(LPsolution.obj) - 600 < tol)
+            
+        elseif p == 2
+            % solve th ecoli_core_model (csense vector is missing)
+            %This is explicitly a load, to test missing csense
+            %vector compensation
+            load([getDistributedModelFolder('ecoli_core_model.mat') filesep 'ecoli_core_model.mat'], 'model');
+            
+            % solveCobraLP
+            solution_solveCobraLP = solveCobraLP(model);
+            
+            % optimizeCbModel
+            solution_optimizeCbModel = optimizeCbModel(model);
+            
+            % compare both solution objects
+            assert(abs(solution_solveCobraLP.obj-solution_optimizeCbModel.f) < 1e-6);
+            assert(isequal(solution_solveCobraLP.full, solution_optimizeCbModel.x))
+            assert(isequal(solution_solveCobraLP.rcost, solution_optimizeCbModel.w))
+            assert(isequal(solution_solveCobraLP.dual, solution_optimizeCbModel.y))
+            assert(solution_solveCobraLP.stat == solution_optimizeCbModel.stat)
         end
+        
+        % output a success message
+        fprintf('Done.\n');
     end
 end
+
 
 % define solver packages
 solverPkgs={'cplex_direct', 'glpk', 'gurobi', 'ibm_cplex', 'matlab', 'mosek', ...
