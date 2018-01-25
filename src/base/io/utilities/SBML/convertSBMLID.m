d960fbd72297adc35a8fd6941785373145ffb6f2
Thomas Pfau <thomas.pfau@uni.lu>
2018-01-25 11:26:28 +0100

    Readded old style conversions

--- src/base/io/utilities/SBML/convertSBMLID.m
+++ src/base/io/utilities/SBML/convertSBMLID.m
@@ -1,4 +1,4 @@
-function [ convertedstr ] = convertSBMLID( str, toSBML )
+function [ convertedstr ] = convertSBMLID( convertedstr, toSBML )
 %CONVERTSBMLID converts the given str to a valid SBML ID 
 % USAGE:
 %
@@ -22,11 +22,22 @@ function [ convertedstr ] = convertSBMLID( str, toSBML )
 if ~exist('toSBML','var')
     toSBML = true;
 end
-if toSBML
-    
+if toSBML    
     convertedstr = regexprep(str,'([^0-9_a-zA-Z])','__${num2str($1+0)}__');
 else   
     convertedstr = regexprep(str,'__([0-9]+)__','${char(str2num($1))}');
+    %Biomodels specific conversions for Input of Biomodels 
+    convertedstr = strrep(convertedstr,'-DASH-','-');
+    convertedstr = strrep(convertedstr,'_DASH_','-');
+    convertedstr = strrep(convertedstr,'_FSLASH_','/');
+    convertedstr = strrep(convertedstr,'_BSLASH_','\');
+    convertedstr = strrep(convertedstr,'_LPAREN_','(');
+    convertedstr = strrep(convertedstr,'_LSQBKT_','[');
+    convertedstr = strrep(convertedstr,'_RSQBKT_',']');
+    convertedstr = strrep(convertedstr,'_RPAREN_',')');
+    convertedstr = strrep(convertedstr,'_COMMA_',',');
+    convertedstr = strrep(convertedstr,'_PERIOD_','.');
+    convertedstr = strrep(convertedstr,'_APOS_','''');        
 end
 
 end
