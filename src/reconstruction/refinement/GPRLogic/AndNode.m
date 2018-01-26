e36507b7666e67e1c7c733bf57396ddc3f4fc846
Thomas Pfau <thomas.pfau@uni.lu>
2018-01-26 13:27:42 +0100

    Removing commented code

--- src/reconstruction/refinement/GPRLogic/AndNode.m
+++ src/reconstruction/refinement/GPRLogic/AndNode.m
@@ -160,46 +160,6 @@ classdef (HandleCompatible) AndNode < Node
             for i = 1:numel(self.children)
                 self.children(i).parent = self;
             end
-            
-%             child = 1;
-%             delchilds = [];
-%             while child <= numel(self.children)
-%                 cchild = self.children(child);
-%                 %Merge Nodes from the same class.
-%                 if strcmp(class(self.children(child)),class(self))
-%                     %reduce the child, merging and removing "singular
-%                     %nodes"
-%                     cchild.reduce();
-%                     for cc = 1:numel(cchild.children)
-%                         cchildchild = cchild.children(cc);
-%                         self.children(end+1) = cchildchild;
-%                         cchildchild.parent = self;
-%                     end
-%                     delchilds(end+1) = child;
-%                     %If a child is not a literal but has only one child, move
-%                     %that child up.
-%                 else
-%                     while ( numel(cchild.children) <= 1 && ~(isa(cchild,'LiteralNode')) )
-%                         cchildchild = cchild.children(1);
-%                         self.children(child) = cchildchild;
-%                         cchildchild.parent = self;
-%                         %we can't continue yet, as this child could now be
-%                         %an AND node.
-%                         cchild = cchildchild;
-%                     end
-%                 end
-%                 child = child + 1;
-%             end
-%             %Remove Merged childs
-%             self.children(delchilds)  = [];
-%             %And reduce all non literal and non same class children,
-%             %everything else should already be reduced.
-%             for child = 1:numel(self.children)
-%                 if (strcmp(class(self.children(child)),class(self)) && ~(isa(cchild,'LiteralNode')) )
-%                     cchild = self.children(child);
-%                     cchild.reduce();
-%                 end
-%             end
         end
     end
     
