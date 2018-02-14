e40fc77b1d3f0614a6b260db624737a40f0fab9f
Fede-edef <federico.baldini@stud.unifi.it>
2018-02-14 19:27:26 +0100

    adding autorun function and removing deprecated variables

--- StartMgPipe.m
+++ StartMgPipe.m
@@ -18,11 +18,12 @@ autofix=1 %autofix for names mismatch
 figform = '-depsc' %the output is vectorised picture, change to '-dpng' for .png
 objre={'EX_biomass(e)'}; %name of objective function of organism
 rdiet=0 %to enable rich simulations also 
-sdiet='EUAverageDiet'
-cobrajl=0
+sdiet='EUAverageDiet' %diet type
+cobrajl=0 
 newFVA = 0;
 %END OF REQUIRED INPUT VARIABLES
 
+%%
 %Start warning section
 if compmod == 1
     warning('compatibility mode activated. Output will also be saved in .csv / .sbml format. Time of computations will be affected.')    
@@ -38,35 +39,18 @@ if patstat==0
 end
 %end of warning section
 
-fprintf('Well done! Pipeline successfully activated and ready to run!')
-%%
 %PIPELINE LAUNCHER -> Please don't modify this section !
-parpool(nwok)
-MgPipe
+autorun = 1
+if autorun==1
+    parpool(nwok)
+    disp('Well done! Pipeline successfully activated and running!')
+    MgPipe
+else
+    warning('autorun function was disabled. You are now running in manual / debug mode. If this is not what you wanted change back to ‘autorun’=1. Please note that the usage in manual mode is strongly discouraged and should be used only for debugging pourposes.')
+    edit('MgPipe.m')
+end
+
 %%
-%Don't change inputs 
-newFVA = 0;
-
-%Decide what to do with these inputs 
-dietpath='Y:\Federico\PD\'; %path from where to read the diet 
-hostnam='Recon3'; %name that the host cell will have
-dietnam = 'Diet4pipe'; %name of the diet (csv file name without extension)
-dietcomp = '[d]'; %name of the diet compartment
-fwok = 1; %number of cores dedicated of paralellisation of fastFVA 
-comobj={'EX_microbeBiomass[fe]'}; % comunity objective function
-reid = '[fe]'; % identifier for set of reactions of which to run FVA (default fecal Exchanges)
-modbuild = 1; %if part 2.1 (pan model construction)needs to be excecuted (=1) or not (=0)  (default = 0) 
-storvec = 0 % if to save files separately (0) or in one big vector (1)
-setspec=1
-changeCobraSolver('tomlab_cplex','LP');
-setuptype = 2 % 1 = comp + host ; 2 = comp no host ; 3 = no comp harvey compatible
-
-pdiet=0 %if personalised diet is available (disabled for default)
-cobrajl=0
-dietT='Rich'
-fileNameDiets='Rugby-fluxes.xlsx'
-
-fprintf('Well done! Pipeline successfully activated and ready to run!')
 
 
 
