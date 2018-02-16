774a86ff832f70e0fd071b5e598c981c878d32be
Fede-edef <federico.baldini@stud.unifi.it>
2018-02-16 19:17:10 +0100

    cleaning a bit the script

--- MgResCollect.m
+++ MgResCollect.m
@@ -1,23 +1,26 @@
-%%Analysisng results from simulations
+%This script is called from the MgPipe pipeline. Its purpose is to compute 
+%NMPCs from simulations with different diet on multiple microbiota models. 
+%Results are outputted as .csv and a PCoA on NMPCs to group microbiota 
+%models of individuals for similar metabolic profile is also 
+%computed and outputted. 
+
+% Federico Baldini, 2017-2018
+
+%%Analyzing results from simulations
 if cobrajl==1
-for k=2:patnumb+1
-pp=cell2mat(sampname((k-1),1))
-load(strcat(resPath,dietT,'\','microbiota_model_richD_',pp,'.mat'))
-load(strcat(resPath,dietT,'\','results\','summary_microbiota_model_richD_',pp,'.mat'))
-%loop for all the entries 
-%strmatch(DietID(i),microbiota_model.rxns)
-%for f =1:patnumb 
-FVAct{1,(k-1)}=ID
-for i=1:length(ID)
-find=strmatch(microbiota_model.rxns(i),ID(i))
-if find ==1
-    FVAct{1,(k-1)}{i,2}=sumResults(i,2)
-    FVAct{1,(k-1)}{i,3}=sumResults(i,3)
-    %temp_mn(i)=sumResults(i,2)
-    %temp_mx(i)=sumResults(i,3)
-end
-end
-end   
+% for k=2:patnumb+1
+%     pp=cell2mat(sampname((k-1),1))
+%     load(strcat(resPath,dietT,'\','microbiota_model_richD_',pp,'.mat'))
+%     load(strcat(resPath,dietT,'\','results\','summary_microbiota_model_richD_',pp,'.mat'))
+%     FVAct{1,(k-1)}=ID
+%     for i=1:length(ID)
+%         find=strmatch(microbiota_model.rxns(i),ID(i))
+%     if find ==1
+%         FVAct{1,(k-1)}{i,2}=sumResults(i,2)
+%         FVAct{1,(k-1)}{i,3}=sumResults(i,3)
+%     end
+%     end
+% end   
 end
 
 %Exporting set of simulated reactions 
@@ -36,7 +39,7 @@ end
 %and saved in .csv format and a PCoA which aims to group individuals for 
 %similarity in their metabolic profile is also computed.
 
-%In this section NMPCs are automatically computed for all tipes of diets. 
+%In this section NMPCs are automatically computed for all types of diets. 
 %Number of different diets are automatically computed from the dimensions of 
 %the simulation object.
 
@@ -52,13 +55,13 @@ else
     fl=3
 end
 
-names={'rich','standard','personalised'}
+names={'rich','standard','personalized'}
 for j=init:fl
 
 for k=2:patnumb+1
 if isempty(FVAct{fl,(k-1)})==1 
-    disp('Jumping not feasable model')
-    warning('NAN rows in flux span matrix, no PCoA will be plotted')
+    disp('Jumping not feasible model')
+    warning('NAN rows in fluxes matrix, no PCoA will be plotted')
     sp=NaN(length(ID),1);
     Fsp(:,k-1)=sp;
     noPcoa=1
@@ -77,13 +80,13 @@ end
 
 csvwrite(strcat(resPath,names{1,j},'.csv'),Fsp) 
 if noPcoa==1
-    disp('Jumping plotting')
+    disp('Jump plotting')
 else
     JD = pdist(Fsp','euclidean');
     [Y,eigvals] = cmdscale(JD);
     P = [eigvals eigvals/max(abs(eigvals))]
     plot(Y(:,1),Y(:,2),'bx') 
-    print(strcat(resPath,'PCoA patients fluxes_',names{1,j}),figform)
+    print(strcat(resPath,'PCoA_individuals_fluxes_',names{1,j}),figform)
 end
 end
 
