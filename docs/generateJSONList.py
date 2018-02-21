0072983fe33c9a75cf8fc163311692abe21f30c1
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2018-02-21 11:25:40 +0100

    fix order of function names

--- docs/generateJSONList.py
+++ docs/generateJSONList.py
@@ -1,6 +1,8 @@
 import os
 import json
 import errno
+import pdb
+
 
 def mkdir_p(path):
     try:
@@ -31,34 +33,33 @@ def path_to_list_easycomplete(path):
 def path_to_list(path, folder):
     d = []
     path_length = len(path)
-    counter = 1
     for root, directories, filenames in os.walk(path):
         for filename in filenames:
-            print os.path.join('source', 'modules', folder, root[path_length+1:], 'index.rst')
             if filename[-2:] == '.m' and os.path.isfile(os.path.join('source', 'modules', folder, 'index.rst')):
-                website_url = os.path.join("https://opencobra.github.io/cobratoolbox/stable/modules", folder, "index.html")
+                website_url = os.path.join("https://opencobra.github.io/cobratoolbox/stable/modules", folder, root[path_length+1:], "index.html")
                 website_url += "?highlight=" + filename[:-2]
                 website_url += "#" + '.'.join(['src', folder] + root[path_length+1:].split(os.path.sep) + [filename[:-2]])
                 d.append({'name': filename[:-2],
-                          'id': "%d" % counter,
                            'parent_id': "-1",
-                          'url': website_url})
-                counter += 1
+                           'url': website_url})
                 print root, filename
+    d = sorted(d, key=lambda k: k.get('name', 0).lower(), reverse=False)
+    for counter, item in enumerate(d):
+        item.update( {"id": "%s" % (counter+1)})
     return d
 
 
-# destination_dir = os.path.join('source', '_static', 'json')
-# mkdir_p(destination_dir)
-# with open(os.path.join(destination_dir, 'functions.json'), 'w') as f:
-#     d = path_to_list_easycomplete('../src/.')
-#     json.dump(d, f)
+destination_dir = os.path.join('source', '_static', 'json')
+mkdir_p(destination_dir)
+with open(os.path.join(destination_dir, 'functions.json'), 'w') as f:
+    d = path_to_list_easycomplete('../src/.')
+    json.dump(d, f)
 
-for section in ["analysis"]:  # , "base", "dataIntegration", "design", "reconstruction", "visualization"]:
+for section in ["analysis", "base", "dataIntegration", "design", "reconstruction", "visualization"]:
     destination_dir = os.path.join('source', '_static', 'json')
     mkdir_p(destination_dir)
     with open(os.path.join(destination_dir, section + 'functions.json'), 'w') as f:
         d = path_to_list('../src/' + section + '/.', section)
-        json.dump(d, f)
+        json.dump(d, f)  #, indent=4)
 
 
