02bb36d7ee8763e648d29db6ed795feeb9ba367a
Fede-edef <federico.baldini@stud.unifi.it>
2018-02-22 15:54:16 +0100

    changing input

--- examples/StartMgPipeHMP.m
+++ examples/StartMgPipeHMP.m
@@ -8,25 +8,25 @@
 
 %REQUIRED INPUT VARIABLES
 modPath='Y:\Federico\Eldermet\191017\models\'; %path to microbiota models
-infoPath='Y:\Federico\HMP\Run_Almut_17_03_31_with10_new_strains\'; %path to the information files
-resPath='Y:\Federico\testingMgPipe\'; %path to results directory 
+infoPath='P:\Documenti\GitLab\Microbiome_Toolbox\MgPipe\examples\'; %path to the information files
+resPath='Y:\Federico\testingMgPipe\fede\'; %path to results directory 
 objre={'EX_biomass(e)'}; %name of objective function of organisms
-sdiet='EUAverageDiet' %standard diet type
-figform = '-depsc' %the output is vectorized picture, change to '-dpng' for .png
-nwok = 3; %number of cores dedicated for parallelization 
-autofix = 1 %autofix for names mismatch
-compmod = 0; % if outputs in open formats should be produced for each section (1=T)
-patstat = 0; %if documentations on patient health status is provided (0 not 1 yes)
-rdiet = 0 %to enable also rich diet simulations 
-cobrajl = 0 
-FVAtype = 0;
+sDiet='EUAverageDiet' %standard diet type
+figForm = '-depsc' %the output is vectorized picture, change to '-dpng' for .png
+nWok = 3; %number of cores dedicated for parallelization 
+autoFix = 1 %autofix for names mismatch
+compMod = 0; % if outputs in open formats should be produced for each section (1=T)
+patStat = 0; %if documentations on patient health status is provided (0 not 1 yes)
+rDiet = 0 %to enable also rich diet simulations 
+extSolve = 0 
+fvaType = 0;
 %END OF REQUIRED INPUT VARIABLES
 
 %%
 %PIPELINE LAUNCHER -> Please don't modify this section !
 autorun = 1
 if autorun==1
-    parpool(nwok)
+    parpool(nWok)
     disp('Well done! Pipeline successfully activated and running!')
     MgPipe
 else
