3653eb1568ba1cd7f59fa1b98f7898b0483787c1
Thomas Pfau <thomas.pfau@uni.lu>
2018-04-13 09:52:14 +0200

    Now returns an ALL field listing all installed solvers

--- src/base/solvers/getAvailableSolversByType.m
+++ src/base/solvers/getAvailableSolversByType.m
@@ -7,6 +7,7 @@ function solvers = getAvailableSolversByType()
 % OUTPUT:
 %    solvers:   struct containing one field per Problem type listing all
 %               solvers installed on the system for that problem type.
+%               Also contains an ALL field indicating all available solvers
 %
 
 global OPT_PROB_TYPES
@@ -24,11 +25,16 @@ for i = 1:numel(OPT_PROB_TYPES)
     solvers.(OPT_PROB_TYPES{i}) = {};
 end
 
+allSolvers = {};
+
 for i = 1:numel(solverNames)
     if SOLVERS.(solverNames{i}).working
+        allSolvers{end+1} = solverNames{i};
         availableTypes = SOLVERS.(solverNames{i}).type;
         for j = 1:numel(availableTypes)
             solvers.(availableTypes{j}) = [solvers.(availableTypes{j}), solverNames{i}];
         end
     end
 end
+
+solvers.ALL = allSolvers;
