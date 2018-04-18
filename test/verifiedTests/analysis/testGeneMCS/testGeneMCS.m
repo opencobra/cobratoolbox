6a32862b65e37f8a4eddd9adbc83db380cc91cb1
lvalcarcel <lvalcarcel@tecnun.es>
2018-04-18 15:17:11 +0200

    Use Cobra functions to check the existance of Cplex for the test

--- test/verifiedTests/analysis/testGeneMCS/testGeneMCS.m
+++ test/verifiedTests/analysis/testGeneMCS/testGeneMCS.m
@@ -8,10 +8,8 @@
 %
 
 global CBTDIR
-global SOLVERS;
-
-% Error message if there is no Cplex
-errorMessage = 'This version calculateMCS only works with IBM CPLEX. Newer versions will include more solvers included in COBRA Toolbox';
+requiredSolvers = {'ibm_cplex'};
+prepareTest('requiredSolvers',requiredSolvers);
 
 % save the current path
 currentDir = pwd;
@@ -31,9 +29,6 @@ true_gmcs = cell(3,1);
 true_gmcs{1} = {'g5'}';
 true_gmcs{2} = {'g1' 'g4'}';
 true_gmcs{3} = {'g2' 'g3' 'g4'}';
-
-% Check Existence of Cplex in this computer
-assert(SOLVERS.ibm_cplex.installed && SOLVERS.ibm_cplex.working, 'ibm_cplex is a required solver for the test and not available on your system.')
 
 for k = 1:length(solverPkgs)
     fprintf(' -- Running testGeneMCS using the solver interface: %s ... ', solverPkgs{k});
