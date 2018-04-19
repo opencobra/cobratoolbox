62339979ca054f262db5c6298767d0052f9d8046
Marouen BEN GUEBILA <marouen.benguebila@uni.lu>
2018-04-19 12:02:35 +0200

    took out find

--- src/analysis/exploration/findBlockedReaction.m
+++ src/analysis/exploration/findBlockedReaction.m
@@ -33,7 +33,7 @@ if (nargin < 2 || isequal(method, 'FVA'))
     end
 else
     solution = solveCobraLPCPLEX(model, 0, 0, 0, [], 1e-6);
-    blockedReactions = model.rxns(find(abs(solution.full) < 1e-11))';
+    blockedReactions = model.rxns(abs(solution.full) < 1e-11)';
 end
 
 end
