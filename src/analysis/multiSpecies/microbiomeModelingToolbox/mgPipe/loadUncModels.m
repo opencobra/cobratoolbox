5b476c310c11627acdadf139bf3878f79d1088c1
laurentheirendt <laurent.heirendt@uni.lu>
2018-05-07 18:22:55 +0200

    Implementation of verbose level

--- src/analysis/multiSpecies/microbiomeModelingToolbox/mgPipe/loadUncModels.m
+++ src/analysis/multiSpecies/microbiomeModelingToolbox/mgPipe/loadUncModels.m
@@ -1,4 +1,4 @@
-function models = loadUncModels(modPath, organisms, objre)
+function models = loadUncModels(modPath, organisms, objre, printLevel)
 % This function loads and unconstrains metabolic models from a specific folder
 %
 % USAGE:
@@ -9,18 +9,33 @@ function models = loadUncModels(modPath, organisms, objre)
 %   organisms:           nx1 cell array cell array with names of organisms in the study
 %   modPath:             char with path of directory where models are stored
 %   objre:               char with reaction name of objective function of organisms
+%   printLevel:          Verbose level (default: printLevel = 1)
 %
 % OUTPUT:
 %   models:              double indicating if object was found in the result folder
 %
 % .. Author: Federico Baldini 2017-2018
+if ~exist('printLevel', 'var')
+    printLevel = 0;
+end
 
-models = {[]};  % empty cell array to be filled with models
+    % add an extra file separator if missing
+    if ~strcmpi(modPath(end), filesep)
+        modPath = [modPath filesep];
+    end
+
+    environment = getEnvironment();
+
+    models = {[]};  % empty cell array to be filled with models
     parfor i = 1:length(organisms)
-    % reading the models
+        % reading the models
+
+        restoreEnvironment(environment);
+
         pn = strcat(modPath, organisms(i, 1), {'.mat'});  % complete path from which to read the models
         cpn = char(pn);  % conversion of the path in character
         ldm = readCbModel(cpn)
+
         % removing possible constraints of the bacs
         [selExc, selUpt] = findExcRxns(ldm);
         Reactions2 = ldm.rxns(find(selExc));
@@ -29,9 +44,11 @@ models = {[]};  % empty cell array to be filled with models
         finrex = setdiff(allex, biomass);
         ldm = changeRxnBounds(ldm, finrex, -1000, 'l');
         ldm = changeRxnBounds(ldm, finrex, 1000, 'u');
+
         % removing blocked reactions from the bacs
-        [BlockedRxns] = identifyFastBlockedRxns(ldm,ldm.rxns);
+        [BlockedRxns] = identifyFastBlockedRxns(ldm,ldm.rxns, printLevel);
         ldm= removeRxns(ldm,BlockedRxns);
+
         % creating array with models as required as input from the following functions
         models(i, 1) = {ldm};
     end
