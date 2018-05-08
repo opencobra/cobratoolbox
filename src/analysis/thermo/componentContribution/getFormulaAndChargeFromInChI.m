65e0707c622e1416b05f7bfe8fe4a8b82db818e2
Sylvain Arreckx <sylvain.arreckx@gmail.com>
2018-05-08 17:21:52 +0200

    [documentation] fix for doc

--- src/analysis/thermo/componentContribution/getFormulaAndChargeFromInChI.m
+++ src/analysis/thermo/componentContribution/getFormulaAndChargeFromInChI.m
@@ -10,12 +10,13 @@ function [formula, nH, charge] = getFormulaAndChargeFromInChI(inchi)
 % OUTPUTS:
 %    formula:    The chemical formula for the input pseudoisomer
 %    nH:         The number of total Hydrogen in the actual protonation form
-%    charge:    The charge on the input pseudoisomer (excluding the protonation state)
+%    charge:     The charge on the input pseudoisomer (excluding the protonation state)
+%
 
-%Get the Formula and the number of protons
-[formula,nH]=getFormulaFromInChI(inchi);
+% Get the Formula and the number of protons
+[formula, nH] = getFormulaFromInChI(inchi);
 
-%Get the charge, of the unprotonated compound.
-[~,charge] = getChargeFromInChI(inchi);
+% Get the charge of the unprotonated compound.
+[~, charge] = getChargeFromInChI(inchi);
 
 end
