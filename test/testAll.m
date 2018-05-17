bc016631d0de20600e05eb583825391bd1919418
laurentheirendt <laurent.heirendt@uni.lu>
2018-05-17 17:26:34 +0200

    Explicit request for launching test suite

--- test/testAll.m
+++ test/testAll.m
@@ -4,30 +4,38 @@ global GUROBI_PATH
 global ILOG_CPLEX_PATH
 global TOMLAB_PATH
 
-fprintf('The COBRAToolbox testing suite\n')
-fprintf('------------------------------\n')
+c = clock;
+fprintf('\n\n      _____   _____   _____   _____     _____     |\n');
+fprintf('     /  ___| /  _  \\ |  _  \\ |  _  \\   / ___ \\    |   COnstraint-Based Reconstruction and Analysis\n');
+fprintf(['     | |     | | | | | |_| | | |_| |  | |___| |   |   The COBRA Toolbox - ' num2str(c(1)) '\n']);
+fprintf('     | |     | | | | |  _  { |  _  /  |  ___  |   |\n');
+fprintf('     | |___  | |_| | | |_| | | | \\ \\  | |   | |   |   Test Suite\n');
+fprintf(['     \\_____| \\_____/ |_____/ |_|  \\_\\ |_|   |_|   |\n']);
+fprintf('                                                  | \n\n');
 
-if ~isempty(getenv('MOCOV_PATH')) && ~isempty(getenv('JSONLAB_PATH'))
-    addpath(genpath(getenv('MOCOV_PATH')))
-    addpath(genpath(getenv('JSONLAB_PATH')))
-    COVERAGE = true;
-    fprintf('MoCov and JsonLab are on path, coverage will be computed.\n')
-else
-    COVERAGE = false;
+% request explicitly from the user to launch test suite locally
+if isempty(strfind(getenv('HOME'), 'jenkins'))
+    reply = '';
+    while isempty(reply)
+        reply = input([' -> Do you want to launch the test suite locally? Time estimate: more than 60 minutes Y/N: '], 's');
+    end
+
+    if strcmpi(reply, 'y') || strcmpi(reply, 'yes')
+        launchTestSuite = true;
+    else
+        launchTestSuite = false;
+    end
 end
 
-% Save the folder we were in.
+% save the current folder
 origDir = pwd;
 
 % if the location of initCobraToolbox is not yet known
-if length(which('initCobraToolbox.m')) == 0
-    % define the path to The COBRA Toolbox
-    pth = fileparts(which('testAll.m'));
-    % Now, we are in the test folder
-    cd(pth)
-    % Switch to the base folder
-    cd ..
-    % And assign the CBTDIR variable
+if isempty(which('initCobraToolbox.m'))
+    % move back to the root of the repository
+    cd([fileparts(which('testAll.m')) filesep '..'])
+
+    % assign the path
     CBTDIR = pwd;
 else
     CBTDIR = fileparts(which('initCobraToolbox.m'));
@@ -41,200 +49,212 @@ addpath(genpath([pwd filesep 'test']));
 cd(CBTDIR);
 
 % run the official initialisation script
-initCobraToolbox;
+if launchTestSuite
 
-% Init the cleanup:
-currentDir = cd('test');
-testDirContent = getFilesInDir('type', 'all');  % Get all currently present files in the folder.
-testDirPath = pwd;
-cd(currentDir);
+    if ~isempty(getenv('MOCOV_PATH')) && ~isempty(getenv('JSONLAB_PATH'))
+        addpath(genpath(getenv('MOCOV_PATH')))
+        addpath(genpath(getenv('JSONLAB_PATH')))
+        COVERAGE = true;
+        fprintf('MoCov and JsonLab are on path, coverage will be computed.\n')
+    else
+        COVERAGE = false;
+    end
 
+    initCobraToolbox;
 
-if ~isempty(strfind(getenv('HOME'), 'jenkins')) || ~isempty(strfind(getenv('USERPROFILE'), 'jenkins'))
-    WAITBAR_TYPE = 0;
+    % initialize the cleanup
+    currentDir = cd('test');
+    testDirContent = getFilesInDir('type', 'all');  % Get all currently present files in the folder.
+    testDirPath = pwd;
+    cd(currentDir);
 
-    % add CellNetAnalyzer for testing purposes
-    addpath(genpath(getenv('CNA_PATH')));
+    if ~isempty(strfind(getenv('HOME'), 'jenkins')) || ~isempty(strfind(getenv('USERPROFILE'), 'jenkins'))
+        WAITBAR_TYPE = 0;
 
-    % check the CNA installation
-    checkCNAinstallation(0);
-else
-    WAITBAR_TYPE = 1;
-end
+        % add CellNetAnalyzer for testing purposes
+        addpath(genpath(getenv('CNA_PATH')));
 
-if verLessThan('matlab', '8.2')
-    error('The testsuite of The COBRA Toolbox can only be run with MATLAB R2014b+.')
-end
+        % check the CNA installation
+        checkCNAinstallation(0);
+    else
+        WAITBAR_TYPE = 1;
+    end
 
-% define a success exit code
-exit_code = 0;
+    if verLessThan('matlab', '8.2')
+        error('The testsuite of The COBRA Toolbox can only be run with MATLAB R2014b+.')
+    end
 
-% enable profiler
-profile on;
+    % define a success exit code
+    exit_code = 0;
 
-if COVERAGE
-    % Get the ignored Files from gitIgnore
-    % only retain the lines that end with .txt and .m and
-    % are not comments and point to files in the /src folder
-    ignoredPatterns = {'^.{0,3}$', ...  % Is smaller than four.
-                       ['^[^s][^r][^c][^' regexptranslate('escape', filesep) ']']};  % does not start with src/
-    filterPatterns = {'\.txt$', '\.m$'};  % Is either a .m file or a .txt file.
-    ignoreFiles = getIgnoredFiles(ignoredPatterns, filterPatterns);
+    % enable profiler
+    profile on;
 
-    % check the code quality
-    listFiles = getFilesInDir('type', 'tracked', 'restrictToPattern', '^.*\.m$', 'checkSubFolders', true);
+    if COVERAGE
+        % Get the ignored Files from gitIgnore
+        % only retain the lines that end with .txt and .m and
+        % are not comments and point to files in the /src folder
+        ignoredPatterns = {'^.{0,3}$', ...  % Is smaller than four.
+                        ['^[^s][^r][^c][^' regexptranslate('escape', filesep) ']']};  % does not start with src/
+        filterPatterns = {'\.txt$', '\.m$'};  % Is either a .m file or a .txt file.
+        ignoreFiles = getIgnoredFiles(ignoredPatterns, filterPatterns);
 
-    % count the number of failed code quality checks per file
-    nMsgs = 0;
-    nCodeLines = 0;
-    nEmptyLines = 0;
-    nCommentLines = 0;
+        % check the code quality
+        listFiles = getFilesInDir('type', 'tracked', 'restrictToPattern', '^.*\.m$', 'checkSubFolders', true);
 
-    for i = 1:length(listFiles)
-        nMsgs = nMsgs + length(checkcode(listFiles{i}));
-        fid = fopen(listFiles{i});
+        % count the number of failed code quality checks per file
+        nMsgs = 0;
+        nCodeLines = 0;
+        nEmptyLines = 0;
+        nCommentLines = 0;
 
-        while ~feof(fid)
-            lineOfFile = strtrim(char(fgetl(fid)));
-            if length(lineOfFile) > 0 && length(strfind(lineOfFile(1), '%')) ~= 1  ...
-                && length(strfind(lineOfFile, 'end')) ~= 1 && length(strfind(lineOfFile, 'otherwise')) ~= 1 ...
-                && length(strfind(lineOfFile, 'switch')) ~= 1 && length(strfind(lineOfFile, 'else')) ~= 1  ...
-                && length(strfind(lineOfFile, 'case')) ~= 1 && length(strfind(lineOfFile, 'function')) ~= 1
-                    nCodeLines = nCodeLines + 1;
+        for i = 1:length(listFiles)
+            nMsgs = nMsgs + length(checkcode(listFiles{i}));
+            fid = fopen(listFiles{i});
 
-            elseif length(lineOfFile) == 0
-                nEmptyLines = nEmptyLines + 1;
+            while ~feof(fid)
+                lineOfFile = strtrim(char(fgetl(fid)));
+                if length(lineOfFile) > 0 && length(strfind(lineOfFile(1), '%')) ~= 1  ...
+                    && length(strfind(lineOfFile, 'end')) ~= 1 && length(strfind(lineOfFile, 'otherwise')) ~= 1 ...
+                    && length(strfind(lineOfFile, 'switch')) ~= 1 && length(strfind(lineOfFile, 'else')) ~= 1  ...
+                    && length(strfind(lineOfFile, 'case')) ~= 1 && length(strfind(lineOfFile, 'function')) ~= 1
+                        nCodeLines = nCodeLines + 1;
 
-            elseif length(strfind(lineOfFile(1), '%')) == 1
-                nCommentLines = nCommentLines + 1;
+                elseif length(lineOfFile) == 0
+                    nEmptyLines = nEmptyLines + 1;
+
+                elseif length(strfind(lineOfFile(1), '%')) == 1
+                    nCommentLines = nCommentLines + 1;
+                end
+            end
+            fclose(fid);
+        end
+
+        % average number of messages per codeLines
+        avMsgsPerc = floor(nMsgs / nCodeLines * 100);
+
+        grades = {'A', 'B', 'C', 'D', 'E', 'F'};
+        intervals = [0, 3;
+                    3, 6;
+                    6, 9;
+                    9, 12;
+                    12, 15;
+                    15, 100];
+
+        grade = 'F';
+        for i = 1:length(intervals)
+            if avMsgsPerc >= intervals(i, 1) && avMsgsPerc < intervals(i, 2)
+                grade = grades{i};
             end
         end
-        fclose(fid);
-    end
 
-    % average number of messages per codeLines
-    avMsgsPerc = floor(nMsgs / nCodeLines * 100);
+        fprintf('\n\n -> The code grade is %s (%1.2f%%).\n\n', grade, avMsgsPerc);
 
-    grades = {'A', 'B', 'C', 'D', 'E', 'F'};
-    intervals = [0, 3;
-                 3, 6;
-                 6, 9;
-                 9, 12;
-                 12, 15;
-                 15, 100];
+        if ~isempty(strfind(getenv('HOME'), 'jenkins'))
+            % set the new badge
+            system(['cp /mnt/prince-data/jenkins/userContent/codegrade-', grade, '.svg /mnt/prince-data/jenkins/userContent/codegrade.svg']);
 
-    grade = 'F';
-    for i = 1:length(intervals)
-        if avMsgsPerc >= intervals(i, 1) && avMsgsPerc < intervals(i, 2)
-            grade = grades{i};
+            % secure copy the badge from the slave
+            system('scp -P 8022 /mnt/prince-data/jenkins/userContent/codegrade.svg jenkins@prince-server.lcsb.uni.lux:/var/lib/jenkins/userContent');
         end
-    end
-
-    fprintf('\n\n -> The code grade is %s (%1.2f%%).\n\n', grade, avMsgsPerc);
-
-    if ~isempty(strfind(getenv('HOME'), 'jenkins'))
-        % set the new badge
-        system(['cp /mnt/prince-data/jenkins/userContent/codegrade-', grade, '.svg /mnt/prince-data/jenkins/userContent/codegrade.svg']);
-
-        % secure copy the badge from the slave
-        system('scp -P 8022 /mnt/prince-data/jenkins/userContent/codegrade.svg jenkins@prince-server.lcsb.uni.lux:/var/lib/jenkins/userContent');
     end
 end
 
 try
+    if launchTestSuite
+        % save the userpath
+        originalUserPath = path;
 
-    % save the userpath
-    originalUserPath = path;
+        % run the tests in the subfolder verifiedTests/ recursively
+        [result, resultTable] = runTestSuite();
 
-    % run the tests in the subfolder verifiedTests/ recursively
-    [result, resultTable] = runTestSuite();
+        sumSkipped = sum(resultTable.Skipped);
+        sumFailed = sum(resultTable.Failed) - sumSkipped;
 
-    sumSkipped = sum(resultTable.Skipped);
-    sumFailed = sum(resultTable.Failed) - sumSkipped;
+        fprintf(['\n > ', num2str(sumFailed), ' tests failed. ', num2str(sumSkipped), ' tests were skipped due to missing requirements.\n\n']);
 
-    fprintf(['\n > ', num2str(sumFailed), ' tests failed. ', num2str(sumSkipped), ' tests were skipped due to missing requirements.\n\n']);
+        % count the number of covered lines of code
+        if COVERAGE
+            % write coverage based on profile('info')
+            fprintf('Running MoCov ... \n')
+            mocov('-cover', 'src', ...
+                '-profile_info', ...
+                '-cover_json_file', 'coverage.json', ...
+                '-cover_html_dir', 'coverage_html', ...
+                '-cover_method', 'profile', ...
+                '-verbose');
 
-    % count the number of covered lines of code
-    if COVERAGE
-        % write coverage based on profile('info')
-        fprintf('Running MoCov ... \n')
-        mocov('-cover', 'src', ...
-              '-profile_info', ...
-              '-cover_json_file', 'coverage.json', ...
-              '-cover_html_dir', 'coverage_html', ...
-              '-cover_method', 'profile', ...
-              '-verbose');
+            % load the coverage file
+            data = loadjson('coverage.json', 'SimplifyCell', 1);
 
-        % load the coverage file
-        data = loadjson('coverage.json', 'SimplifyCell', 1);
+            sf = data.source_files;
+            clFiles = zeros(length(sf), 1);
+            tlFiles = zeros(length(sf), 1);
 
-        sf = data.source_files;
-        clFiles = zeros(length(sf), 1);
-        tlFiles = zeros(length(sf), 1);
+            for i = 1:length(sf)
+                clFiles(i) = nnz(sf(i).coverage);
+                tlFiles(i) = length(sf(i).coverage);
+            end
 
-        for i = 1:length(sf)
-            clFiles(i) = nnz(sf(i).coverage);
-            tlFiles(i) = length(sf(i).coverage);
+            % average the values for each file
+            cl = sum(clFiles);
+            tl = sum(tlFiles);
+
+            % print out the coverage
+            fprintf('Covered Lines: %i, Total Lines: %i, Coverage: %f%%.\n', cl, tl, cl / tl * 100);
         end
 
-        % average the values for each file
-        cl = sum(clFiles);
-        tl = sum(tlFiles);
+        % print out a summary table
+        resultTable
 
-        % print out the coverage
-        fprintf('Covered Lines: %i, Total Lines: %i, Coverage: %f%%.\n', cl, tl, cl / tl * 100);
-    end
-
-    % print out a summary table
-    resultTable
-
-    % Print some information on failed and skipped tests.
-    skippedTests = find(resultTable.Skipped);
-    if sum(skippedTests > 0)
-        fprintf('The following tests were skipped:\n%s\n\n', strjoin(resultTable.TestName(skippedTests), '\n'));
-        fprintf('The reasons were as follows:\n')
-        for i = 1:numel(skippedTests)
-            fprintf('------------------------------------------------\n')
-            fprintf('%s:\n', resultTable.TestName{skippedTests(i)});
-            fprintf('%s\n', resultTable.Details{skippedTests(i)});
-            fprintf('------------------------------------------------\n')
+        % Print some information on failed and skipped tests.
+        skippedTests = find(resultTable.Skipped);
+        if sum(skippedTests > 0)
+            fprintf('The following tests were skipped:\n%s\n\n', strjoin(resultTable.TestName(skippedTests), '\n'));
+            fprintf('The reasons were as follows:\n')
+            for i = 1:numel(skippedTests)
+                fprintf('------------------------------------------------\n')
+                fprintf('%s:\n', resultTable.TestName{skippedTests(i)});
+                fprintf('%s\n', resultTable.Details{skippedTests(i)});
+                fprintf('------------------------------------------------\n')
+            end
+            fprintf('\n\n')
         end
-        fprintf('\n\n')
-    end
 
-    failedTests = find(resultTable.Failed & ~resultTable.Skipped);
-    if sum(failedTests > 0)
-        fprintf('The following tests failed:\n%s\n\n', strjoin(resultTable.TestName(failedTests), '\n'));
-        fprintf('The reasons were as follows:\n')
-        for i = 1:numel(failedTests)
-            fprintf('------------------------------------------------\n')
-            fprintf('%s:\n', resultTable.TestName{failedTests(i)});
-            trace = result(failedTests(i)).Error.getReport();
-            tracePerLine = strsplit(trace, '\n');
-            testSuitePosition = find(cellfun(@(x) ~isempty(strfind(x, 'runTestSuite')), tracePerLine));
-            trace = sprintf(strjoin(tracePerLine(1:(testSuitePosition - 7)), '\n'));  % Remove the testSuiteTrace.
-            fprintf('%s\n', trace);
-            fprintf('------------------------------------------------\n')
+        failedTests = find(resultTable.Failed & ~resultTable.Skipped);
+        if sum(failedTests > 0)
+            fprintf('The following tests failed:\n%s\n\n', strjoin(resultTable.TestName(failedTests), '\n'));
+            fprintf('The reasons were as follows:\n')
+            for i = 1:numel(failedTests)
+                fprintf('------------------------------------------------\n')
+                fprintf('%s:\n', resultTable.TestName{failedTests(i)});
+                trace = result(failedTests(i)).Error.getReport();
+                tracePerLine = strsplit(trace, '\n');
+                testSuitePosition = find(cellfun(@(x) ~isempty(strfind(x, 'runTestSuite')), tracePerLine));
+                trace = sprintf(strjoin(tracePerLine(1:(testSuitePosition - 7)), '\n'));  % Remove the testSuiteTrace.
+                fprintf('%s\n', trace);
+                fprintf('------------------------------------------------\n')
+            end
+            fprintf('\n\n')
         end
-        fprintf('\n\n')
-    end
 
-    % restore the original path
-    restoredefaultpath;
-    addpath(originalUserPath);
+        % restore the original path
+        restoredefaultpath;
+        addpath(originalUserPath);
 
-    if sumFailed > 0
-        exit_code = 1;
-    end
+        if sumFailed > 0
+            exit_code = 1;
+        end
 
-    fprintf(['\n > The exit code is ', num2str(exit_code), '.\n\n']);
+        fprintf(['\n > The exit code is ', num2str(exit_code), '.\n\n']);
 
-    % clean up temporary files.
-    removeTempFiles(testDirPath, testDirContent);
+        % clean up temporary files.
+        removeTempFiles(testDirPath, testDirContent);
 
-    % ensure that we ALWAYS call exit
-    if ~isempty(strfind(getenv('HOME'), 'jenkins')) || ~isempty(strfind(getenv('USERPROFILE'), 'jenkins'))
-        exit(exit_code);
+        % ensure that we ALWAYS call exit
+        if ~isempty(strfind(getenv('HOME'), 'jenkins')) || ~isempty(strfind(getenv('USERPROFILE'), 'jenkins'))
+            exit(exit_code);
+        end
     end
 catch ME
     % Also clean up temporary files in case of an error.
