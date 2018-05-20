df927d26142a36be252e2b545da5e3386fc1b198
Fede-edef <federico.baldini@stud.unifi.it>
2018-05-20 20:54:29 +0200

    adding tests for the all pipeline

--- test/verifiedTests/analysis/testMultiSpeciesModelling/testMgPipe.m
+++ test/verifiedTests/analysis/testMultiSpeciesModelling/testMgPipe.m
@@ -28,8 +28,8 @@ modPath = [CBTDIR filesep 'test' filesep 'models' filesep 'mat'];
 resPath= [CBTDIR filesep '.tmp'] ;
 
 % path to and name of the file with dietary information.
-dietFilePath = [CBTDIR filesep 'papers' filesep '2017_AGORA' filesep 'resourceForMicrobiomeModelingToolbox' filesep 'AverageEuropeanDiet'];
-
+dietFilePath=[CBTDIR filesep 'papers' filesep '2018_microbiomeModelingToolbox' filesep 'resources' filesep 'AverageEuropeanDiet'];
+    
 % path to and name of the file with abundance information.
 abunFilePath=[CBTDIR filesep 'papers' filesep '2018_microbiomeModelingToolbox' filesep 'examples' filesep 'normCoverage.csv'];
 
@@ -87,6 +87,11 @@ assert(verifyCobraFunctionError('initMgPipe','inputs',{'/tmp/abcdef'}))
 % turn all warnings off
 warning('off', 'all')
 
+% adding a filesep at the end of the path
+if ~strcmpi(resPath(end), filesep)
+    resPath = [resPath filesep];
+end
+
 % test if the resPath is set to default value
 abunFilePath = [CBTDIR filesep 'papers' filesep '2018_microbiomeModelingToolbox' filesep 'examples' filesep 'normCoverage.csv'];
 init = initMgPipe(modPath, CBTDIR, '', '', abunFilePath);
@@ -134,3 +139,78 @@ warning('on', 'all');
 
 % change the directory
 cd(currentDir)
+
+% test getMappingInfo
+[reac,micRea,binOrg,patOrg,reacPat,reacNumb,reacSet,reacTab,reacAbun,reacNumber]=getMappingInfo(models,abunFilePath,indNumb);
+
+assert(exist('reac', 'var') == 1)
+assert(exist('micRea', 'var') == 1)
+assert(exist('binOrg', 'var') == 1)
+assert(exist('patOrg', 'var') == 1)
+assert(exist('reacPat', 'var') == 1)
+assert(exist('reacNumb', 'var') == 1)
+assert(exist('reacSet', 'var') == 1)
+assert(exist('reacTab', 'var') == 1)
+assert(exist('reacAbun', 'var') == 1)
+assert(exist('reacNumber', 'var') == 1)
+
+% test checkNomenConsist
+[autoStat,fixVec,organisms]=checkNomenConsist(organisms,autoFix);
+
+assert(exist('autoStat', 'var') == 1)
+assert(exist('fixVec', 'var') == 1)
+assert(length(organisms) == 5)
+
+% test fastSetupCreator
+
+setup=fastSetupCreator(models, organisms, {},objre);
+ 
+assert(exist('setup', 'var') == 1)
+assert(strcmp(class(setup),'struct') == 1)
+assert((length(setup.S) == length(setup.rxns)) == 1)
+assert((length(setup.lb) == length(setup.rxns)) == 1)  
+assert((length(setup.c) == length(setup.rxns)) == 1) 
+assert((length(setup.mets) == length(setup.metNames)) == 1)  
+ 
+assert(length(setup.rxns(strmatch('EX',setup.rxns))) /2 == length(setup.rxns(strmatch('DUt',setup.rxns))) == 1) 
+assert(length(setup.rxns(strmatch('EX',setup.rxns))) /2 == length(setup.rxns(strmatch('UFEt',setup.rxns))) == 1) 
+assert(length(setup.rxns(strmatch('DUt',setup.rxns)))== length(setup.rxns(strmatch('UFEt',setup.rxns))) == 1)
+
+assert((length(strmatch(organisms(1),setup.rxns)) > 0) == 1)
+assert((length(strmatch(organisms(2),setup.rxns)) > 0) == 1)
+assert((length(strmatch(organisms(3),setup.rxns)) > 0) == 1)
+assert((length(strmatch(organisms(4),setup.rxns)) > 0) == 1)
+assert((length(strmatch(organisms(5),setup.rxns)) > 0) == 1)
+
+% test createdModels
+[createdModels]=createPersonalizedModel(abunFilePath,resPath,setup,sampName,organisms,indNumb);
+assert(length(createdModels)== 5)
+assert(strcmp(createdModels(2,1),'Test1') == 1)
+assert(strcmp(createdModels(5,1),'Test4') == 1)
+
+microbiota_model=load(strcat(resPath,'microbiota_model_samp_Test1')); 
+microbiota_model=microbiota_model.microbiota_model;
+assert(length(microbiota_model.A)> length(microbiota_model.S))
+assert(length(microbiota_model.csense) == length(microbiota_model.A))
+assert((length(strmatch('communityBiomass',microbiota_model.rxns)) > 0) == 1)
+assert((length(strmatch('UFEt_microbeBiomass',microbiota_model.rxns)) > 0) == 1)
+assert((length(strmatch('EX_microbeBiomass[fe]',microbiota_model.rxns)) > 0) == 1)
+assert(length(microbiota_model.rxns(strmatch('DUt',microbiota_model.rxns)))== length(microbiota_model.rxns(strmatch('UFEt',microbiota_model.rxns)))-1 == 1)
+assert(microbiota_model.A(1,1) == -0.3000)
+assert(microbiota_model.A(2,1) == -0.2000)
+assert(microbiota_model.A(3,1) == -0.1000)
+assert(microbiota_model.A(4,1) == -0.2000)
+assert(microbiota_model.A(5,1) == -0.2000)
+assert(microbiota_model.A(6,1) == 1)
+
+% test createdModels
+[ID,fvaCt,nsCt,presol,inFesMat]=microbiotaModelSimulator(resPath,setup,sampName,dietFilePath,rDiet,0,extSolve,indNumb,fvaType);
+
+assert(exist('ID', 'var') == 1)
+assert(exist('fvaCt', 'var') == 1)
+assert(exist('nsCt', 'var') == 1)
+assert(exist('inFesMat', 'var') == 1)
+assert((length(presol)==5) == 1)
+assert(sum(cell2mat(presol(:,1)))==4)
+% cd resPath
+% delete [resPath 'simRes.mat'] 
