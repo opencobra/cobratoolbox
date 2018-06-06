fc976052ad7f5d0cc9e6a87e80fa16556dc5ba6c
Thomas Pfau <thomas.pfau@uni.lu>
2018-06-06 08:40:55 +0200

    :racehorse: Massive speedup of the generate Rules test.

--- test/verifiedTests/reconstruction/testModelManipulation/testGenerateRules.m
+++ test/verifiedTests/reconstruction/testModelManipulation/testGenerateRules.m
@@ -23,6 +23,7 @@ for i=1:length(modelsToTry)
     
     model2 = generateRules(model);
     model.rules = strrep(model.rules, '  ', ' ');
+    
     fp = FormulaParser();
     % fix for Recon2
     if strcmp(modelsToTry{i}, 'Recon2.v04.mat')
@@ -32,12 +33,21 @@ for i=1:length(modelsToTry)
         model.rules(2940) = {'(x(4)) | (x(60)) | (x(2)) | (x(61)) | (x(3))'}; % '(314.1) or (4128.1) or (26.1) or (4129.1) or (314.2)'
         model.rules(3133) = {'(x(2)) | (x(4)) | (x(1)) | (x(3))'}; % '(26.1) or (314.1) or (314.2)'
     end
-    for rule = 1:numel(model.rules)
+    %also, introduce spaces around the individual genes:    
+    model.rules = regexprep(model.rules,'([^\s])(x\([0-9]+\))','$1 $2');
+    model.rules = regexprep(model.rules,'(x\([0-9]+\))([^\s])','$1 $2');
+    for rule = 1:numel(model.rules)        
         if isempty(model.rules{rule})
             %assert that both formulas are empty (and thus equal).
             assert(isequal(model.rules{rule},model2.rules{rule}))
             continue;
+        elseif strcmp(model.rules{rule},model2.rules{rule})
+            %If the strings are identical than we don't need to check
+            %equivalence.
+            assert(isequal(model.rules{rule},model2.rules{rule}))
+            continue
         end
+        
         head1 = fp.parseFormula(model.rules{rule});
         head2 = fp.parseFormula(model2.rules{rule});
         %Assert that the formulas are equal.
