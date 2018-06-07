714fb040dc552624450c2bf80128dbd0782eeadb
laurentheirendt <laurent.heirendt@uni.lu>
2018-06-07 18:18:45 +0200

    Linting of external function

--- lnbin/lnbin.m
+++ lnbin/lnbin.m
@@ -6,43 +6,43 @@
 % option is not required, just call the function without including the third out
 % put; i.e.: [midpts Freq]=lnbin(x,BinNum).
 
-function [midpts Freq eFreq]=lnbin(x,BinNum)
-x=sort(x);
-i=1;
-while x(i)<=0;
-   i=i+1;
+function [midpts Freq eFreq] = lnbin(x, BinNum)
+x = sort(x);
+i = 1;
+while x(i) <= 0;
+    i = i + 1;
 end
-str = num2str((length(x)-i)/length(x)*100);
-stuff='Percentage of input vec binned ';
+str = num2str((length(x) - i) / length(x) * 100);
+stuff = 'Percentage of input vec binned ';
 disp([stuff str])
 
-FPT=x(i:length(x));
-LFPT=log10(FPT);
-max1=log10( ceil(max(FPT)) );
-min1=log10(floor(min(FPT)));
+FPT = x(i:length(x));
+LFPT = log10(FPT);
+max1 = log10(ceil(max(FPT)));
+min1 = log10(floor(min(FPT)));
 % min1=1;
 
-LFreq=zeros(BinNum,1);
-LTime=zeros(BinNum,1);
-Lends=zeros(BinNum,2);
+LFreq = zeros(BinNum, 1);
+LTime = zeros(BinNum, 1);
+Lends = zeros(BinNum, 2);
 
-step=(max1-min1)/BinNum;
+step = (max1 - min1) / BinNum;
 
 
 % ------------ LOG Binning Data ------------------------
-for i=1:length(FPT)
-    for k=1:BinNum
-       if( (k-1)*step+min1 <= LFPT(i) && LFPT(i) < k*step+min1)
-           LFreq(k)=LFreq(k)+1;
-       end
-        LTime(k)=k*step-(0.5*step)+min1;
-        Lends(k,1)=(k-1)*step+min1;
-        Lends(k,2)=(k)*step+min1;
+for i = 1:length(FPT)
+    for k = 1:BinNum
+        if((k - 1) * step + min1 <= LFPT(i) && LFPT(i) < k * step + min1)
+            LFreq(k) = LFreq(k) + 1;
+        end
+            LTime(k) = k * step - (0.5 * step) + min1;
+            Lends(k, 1) = (k - 1) * step + min1;
+            Lends(k, 2) = (k) * step + min1;
     end
 end
-ends=exp(Lends);
+ends = exp(Lends);
 
-widths=ends(1:length(ends),2)-ends(1:length(ends),1);
-Freq=LFreq./widths./length(x);
-eFreq=1./sqrt(LFreq).*Freq;
+widths = ends(1:length(ends), 2) - ends(1:length(ends), 1);
+Freq = LFreq./widths./length(x);
+eFreq = 1. / sqrt(LFreq).*Freq;
 midpts = exp(LTime);
