fe04ba2ce40608501935a8909d584d6793d704e6
laurentheirendt <laurent.heirendt@uni.lu>
2018-06-07 17:09:38 +0200

    Change of log value

--- lnbin/lnbin.m
+++ lnbin/lnbin.m
@@ -1,25 +1,25 @@
 % This function take the input of a data vector x, which is to be binned;
 % it also takes in the amount bins one would like the data binned into. The
-% output is two vectors, one containing the normalised frequency of each bin 
+% output is two vectors, one containing the normalised frequency of each bin
 % (Freq), the other, the midpoint of each bin (midpts).
 % Added and error to the binned frequency: eFreq (As of June 30 2010). If this
 % option is not required, just call the function without including the third out
-% put; i.e.: [midpts Freq]=lnbin(x,BinNum). 
+% put; i.e.: [midpts Freq]=lnbin(x,BinNum).
 
 function [midpts Freq eFreq]=lnbin(x,BinNum)
 x=sort(x);
 i=1;
 while x(i)<=0;
-   i=i+1; 
+   i=i+1;
 end
 str = num2str((length(x)-i)/length(x)*100);
 stuff='Percentage of input vec binned ';
 disp([stuff str])
 
 FPT=x(i:length(x));
-LFPT=log(FPT);
-max1=log( ceil(max(FPT)) );
-min1=log(floor(min(FPT)));
+LFPT=log10(FPT);
+max1=log10( ceil(max(FPT)) );
+min1=log10(floor(min(FPT)));
 % min1=1;
 
 LFreq=zeros(BinNum,1);
@@ -38,8 +38,8 @@ for i=1:length(FPT)
         LTime(k)=k*step-(0.5*step)+min1;
         Lends(k,1)=(k-1)*step+min1;
         Lends(k,2)=(k)*step+min1;
-    end  
-end 
+    end
+end
 ends=exp(Lends);
 
 widths=ends(1:length(ends),2)-ends(1:length(ends),1);
