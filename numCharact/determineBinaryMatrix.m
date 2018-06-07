2720dbd2c8981710dcb99bb4dc15d2b3b91a7d74
laurentheirendt <laurent.heirendt@uni.lu>
2018-06-07 17:24:22 +0200

    Vectorization

--- numCharact/determineBinaryMatrix.m
+++ numCharact/determineBinaryMatrix.m
@@ -1,4 +1,4 @@
-function [Shat, mconnect, mconnectin, mconnectout, nconnect] = determineBinaryMatrix(S)
+function [Shatabs, mconnect, mconnectin, mconnectout, nconnect] = determineBinaryMatrix(S)
 % Determine the binary form of S
 %
 % INPUT:
@@ -13,51 +13,21 @@ function [Shat, mconnect, mconnectin, mconnectout, nconnect] = determineBinaryMa
 %
 % .. Author: Laurent Heirendt, June 2018
 
-    % determine the size of S
-    [m,n] = size(S);
-
-    % initialize the connectivity vectors
-    mconnect = zeros(m,1);
-    mconnectin = zeros(m,1);
-    mconnectout = zeros(m,1);
-    nconnect = zeros(n,1);
-
     % initialize the binary matrix
     Shat = S;
     Shat(S > 0) = 1;
     Shat(S < 0) = -1;
 
     % determine the row connectivity
+    Shatabs = abs(Shat);
     if nargout > 1
-        for i = 1:m
-            mconnect(i) = sum(abs(Shat(i, :)));
-            for j = 1:n
-                if Shat(i, j) == 1
-                    mconnectin(i) = mconnectin(i) + 1;
-                elseif Shat(i,j) == -1
-                    mconnectout(i) = mconnectout(i) + 1;
-                end
-            end
-        end
+        mconnect = sort(sum(Shatabs, 2), 'descend');
+        mconnectin = sum(Shat == 1, 2);
+        mconnectout = sum(Shat == -1, 2);
     end
 
     % determine the column connectivity
     if nargout > 5
-        for j = 1:n
-            nconnect(j) = sum(Shat(:, j));
-        end
+        nconnect = sort(sum(Shat, 1), 'descend');
     end
-
-    % sort the connectivity vectors
-    if nargout > 5
-        nconnect = sort(nconnect, 'descend');
-    end
-
-    if nargout > 1
-        mconnect = sort(mconnect, 'descend');
-    end
-
-    % return a true binary matrix
-    Shat = abs(Shat);
-
 end
