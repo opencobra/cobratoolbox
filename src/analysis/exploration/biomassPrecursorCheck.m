b8775aeb3853fc44a2185ce54e5f1642a0e34871
Thomas Pfau <thomas.pfau@uni.lu>
2018-06-12 07:26:13 +0200

    Added option to check for Coupled Metabolites

--- src/analysis/exploration/biomassPrecursorCheck.m
+++ src/analysis/exploration/biomassPrecursorCheck.m
@@ -1,10 +1,15 @@
-function [missingMets, presentMets] = biomassPrecursorCheck(model)
+function [missingMets, presentMets,coupledMets] = biomassPrecursorCheck(model,checkCoupling)
 % Checks if biomass precursors are able to be synthesized.
 %
 % [missingMets, presentMets] = biomassPrecursorCheck(model)
 %
 % INPUT:
-%    model:          COBRA model structure
+%    model:             COBRA model structure
+%
+% OPTIONAL INPUT:
+%    checkCoupling:     Test, whether some compounds can only be produced
+%                       if there is a sink for other biomass precursors
+%                       (Default: false)
 %
 % OUTPUTS:
 %    missingMets:    List of biomass precursors that are not able to be synthesized
@@ -13,6 +18,13 @@ function [missingMets, presentMets] = biomassPrecursorCheck(model)
 % .. Authors: - Pep Charusanti & Richard Que (July 2010)
 % May identify metabolites that are typically recycled within the
 % network such as ATP, NAD, NADPH, ACCOA.
+if nargin < 2
+    checkCoupling = 0;
+end
+
+if ~checkCoupling && nargout == 3
+    error('coupledMets are not being calculated if checkCoupling is not set to true!');
+end
 
 colS_biomass = model.c ~= 0;
 % FIND COLUMN IN S-MATRIX THAT CORRESPONDS TO BIOMASS EQUATION
@@ -24,20 +36,44 @@ biomassMetabs = model.mets(any(model.S(:, colS_biomass) < 0, 2));
 % AND OPTIMIZE.  NOTE: A CRITICAL ASSUMPTION IS THAT THE ADDED DEMAND
 % REACTION IS APPENDED TO THE FAR RIGHT OF THE S-MATRIX.  THE CODE NEEDS TO
 % BE REVISED IF THIS IS NOT THE CASE.
-k = 1;
 m = 1;
+p = 1;
+c = 1;
 % ADD DEMAND REACTIONS
 [model_newDemand, addedRxns] = addDemandReaction(model, biomassMetabs);
+
+if checkCoupling
+    %Close the precursors
+    model_newDemand = changeRxnBounds(model_newDemand,addedRxns,zeros(numel(addedRxns,1)),repmat('b',numel(addedRxns,1)));
+    coupledMets = {};
+end
+
 [missingMets, presentMets] = deal({});
 for i = 1:length(biomassMetabs)
+    if checkCoupling
+        model_newDemand = changeRxnBounds(model_newDemand, addedRxns{i}, 1000, 'u');
+    end
     model_newDemand = changeObjective(model_newDemand, addedRxns{i});
     solution = optimizeCbModel(model_newDemand);                                % OPTIMIZE
     if solution.f == 0                                                          % MAKE LIST OF WHICH BIOMASS PRECURSORS ARE ...
-        missingMets(k) = biomassMetabs(i);                                      %  SYNTHESIZED AND WHICH ARE NOT
-        k = k + 1;
+        if checkCoupling
+            model_newDemand = changeRxnBounds(model_newDemand, addedRxns, 1000, 'u');
+            solution = optimizeCbModel(model_newDemand);
+            if solution.f > 0
+                coupledMets(c) = biomassMetabs(i);                              % NEED ANOTHER SINK
+                c = c + 1;
+            else
+                missingMets(m) = biomassMetabs(i);                              %  SYNTHESIZED AND WHICH ARE NOT
+                m = m + 1;
+            end
+            model_newDemand = changeRxnBounds(model_newDemand, addedRxns, 0, 'u');
+        else
+            missingMets(m) = biomassMetabs(i);                                  %  SYNTHESIZED AND WHICH ARE NOT
+            m = m + 1;
+        end
     else
-        presentMets(m) = biomassMetabs(i);
-        m = m + 1;
+        presentMets(p) = biomassMetabs(i);
+        p = p + 1;
     end
 end
 
