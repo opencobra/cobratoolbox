0db888188d4c61b1bd4f660976f1c0bc837997fa
Siu Hung Joshua Chan <joshua.chan@connect.polyu.hk>
2018-06-21 02:06:59 -0400

    Include test for finding cofactor pairs that can/cannot be synthesized

--- test/verifiedTests/analysis/testBiomassPrecursorCheck/testBiomassPrecursorCheck.m
+++ test/verifiedTests/analysis/testBiomassPrecursorCheck/testBiomassPrecursorCheck.m
@@ -6,16 +6,16 @@
 % Authors:
 %    - Siu Hung Joshua Chan June 2018
 
-% save the current path
+% Save the current path
 currentDir = pwd;
 
-% initialize the test
+% Initialize the test
 fileDir = fileparts(which('testBiomassPrecursorCheck'));
 cd(fileDir);
 
 model = readCbModel('iJO1366.mat');
 
-% test where there is no missing metabolite
+% Test where there is no missing metabolite
 [missingMets, presentMets] = biomassPrecursorCheck(model);
 assert(isempty(missingMets))
 presentMetsResult = {'10fthf_c';'2fe2s_c';'2ohph_c';'4fe4s_c';'ala__L_c';...
@@ -30,29 +30,88 @@ presentMetsResult = {'10fthf_c';'2fe2s_c';'2ohph_c';'4fe4s_c';'ala__L_c';...
     'tyr__L_c';'udcpdp_c';'utp_c';'val__L_c';'zn2_c'};
 assert(isequal(sort(presentMets), presentMetsResult))
 
-% test where there is a missing metabolite
+% Test where there is a missing metabolite
 model = addReaction(model,'testBiomass','reactionFormula','NotExist + amet_c -> ');
 model = changeObjective(model,'testBiomass');
 [missingMets, presentMets] = biomassPrecursorCheck(model);
 assert(isempty(setxor(missingMets,{'NotExist'}))); % NotExist, and only NotExist is not producible.
 assert(isempty(setxor(presentMets,{'amet_c'}))); % amet_c and only amet_c is producible
 
-%Test coupling
+% Test coupling
 model = createToyModelForBiomassPrecursorCheck();
-%This model cannot carry flux through the objective, but each precursor can
-%be produced if exchangers exist for all precursors.
+% This model cannot carry flux through the objective, but each precursor can
+% be produced if exchangers exist for all precursors.
 [missingMets, presentMets] = biomassPrecursorCheck(model);
 assert(isempty(setxor(presentMets,{'E','F'})));
 
-%Check for coupled Mets
+% Check for coupled Mets
 [missingMets, presentMets, coupledMets] = biomassPrecursorCheck(model,true);
 
 assert(isempty(missingMets))
 assert(isempty(setxor(presentMets,{'F'}))); %F can be produced, as surplus E can be removed by the biomass function.
 assert(isempty(setxor(coupledMets,{'E'}))); % All E has to go to the biomass as long as there is no sink for F.
 
-%Test error
-assert(verifyCobraFunctionError('biomassPrecursorCheck','input',{model},'outputArgCount',3,'testMessage','coupledMets are not being calculated if checkCoupling is not set to true!'));
+% Test error
+assert(verifyCobraFunctionError('biomassPrecursorCheck','input',{model},'outputArgCount',3,'testMessage','coupledMets, missingCofs and presentCofs are not being calculated if checkCoupling is not set to true!'));
 
-% change the directory
+% Test cofactor pairs
+model = readCbModel('ecoli_core_model.mat');
+% Add a hypothetical non-producible cofactor pair
+model = addReaction(model, 'COF', 'reactionFormula', 'metTest1[c] -> metTest2[c]');
+model = addReaction(model, 'BIOMASS2', 'reactionFormula', 'metTest1[c] + atp[c] -> metTest2[c] + adp[c]');
+% Test the function's capability to handle >1 objective reactions 
+model = changeObjective(model, {'Biomass_Ecoli_core_N(w/GAM)-Nmet2'; 'BIOMASS2'}, 1);
+[missingMets0, presentMets0] = biomassPrecursorCheck(model);
+if exist([pwd filesep 'testBiomassPrecursorCheck.txt'], 'file')
+    delete([pwd filesep 'testBiomassPrecursorCheck.txt'])
+end
+diary('testBiomassPrecursorCheck.txt');
+[missingMets, presentMets,coupledMets, missingCofs, presentCofs] = biomassPrecursorCheck(model,true);
+diary off
+
+% Producible cofactors identified as missingMets when checkCoupling not turned on
+assert(isempty(setxor(missingMets0, {'accoa[c]';'atp[c]';'nad[c]';'nadph[c]';'metTest1[c]'})))
+assert(isempty(setxor(presentMets0, {'3pg[c]';'e4p[c]';'f6p[c]';'g3p[c]';'g6p[c]';...
+    'gln-L[c]';'glu-L[c]';'h2o[c]';'oaa[c]';'pep[c]';'pyr[c]';'r5p[c]'})))
+
+% No missingMets when checkCoupling turned on
+assert(isempty(missingMets))
+% Same set of presentMets
+assert(isempty(setxor(presentMets, presentMets0)))
+
+% missingCofs and presentCofs are identified
+missingCofsStr = cellfun(@(x) strjoin(x, '|'), missingCofs, 'UniformOutput', false);
+assert(isempty(setxor(missingCofsStr, {'metTest1[c]|metTest2[c]'})))
+presentCofsStr = cellfun(@(x) strjoin(x, '|'), presentCofs, 'UniformOutput', false);
+assert(isempty(setxor(presentCofsStr, {'nadp[c]|nadph[c]'; 'adp[c]|atp[c]'; ...
+    'accoa[c]|coa[c]'; 'nad[c]|nadh[c]'})))
+
+% Check the correct printing of the results
+f = fopen('testBiomassPrecursorCheck.txt', 'r');
+l = fgetl(f);
+text = {};
+while ~isequal(l, -1)
+    text{end + 1} = l;
+    l = fgetl(f);
+end
+fclose(f);
+
+lineCan = find(~cellfun(@isempty, strfind(text, 'Cofactors in the biomass reaction that CAN be produced:')));
+assert(isscalar(lineCan))
+lineCannot = find(~cellfun(@isempty, strfind(text, 'Cofactors in the biomass reaction that CANNOT be produced:')));
+assert(isscalar(lineCannot))
+cofactorCan = {'nadph[c]  -> nadp[c]', 'atp[c]  -> adp[c]', 'accoa[c]  -> coa[c]', 'nad[c]  -> nadh[c]'};
+cofactorCannot = {'metTest1[c]  -> metTest2[c]'};
+% Correct order of printing
+for j = 1:numel(cofactorCan)
+    lineJ = find(~cellfun(@isempty, strfind(text, cofactorCan{j})));
+    assert(~isempty(lineJ) && lineJ > lineCan && lineJ < lineCannot)
+end
+for j = 1:numel(cofactorCannot)
+    lineJ = find(~cellfun(@isempty, strfind(text, cofactorCannot{j})));
+    assert(~isempty(lineJ) && lineJ > lineCannot)
+end
+delete([pwd filesep 'testBiomassPrecursorCheck.txt'])
+
+% Change the directory
 cd(currentDir)
