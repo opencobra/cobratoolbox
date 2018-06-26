f39a5e3e343f6e70308ecb314ea7f300c398d888
Thomas Pfau <thomas.pfau@uni.lu>
2018-06-26 10:53:47 +0200

    Revert "Reducing Testsuite for CI testing"
    This reverts commit 163ed70c4c0241e60c0d21b051b0f9e838899a99.

--- test/runTestSuite.m
+++ test/runTestSuite.m
@@ -36,7 +36,7 @@ testDir = [CBTDIR filesep 'test'];
 currentDir = cd(testDir);
 
 % get all names of test files
-testFiles = rdir(['verifiedTests' filesep '**' filesep 'testSolveCobra*.m']);
+testFiles = rdir(['verifiedTests' filesep '**' filesep 'test*.m']);
 testFileNames = {testFiles.name};
 testFileNames = testFileNames(~cellfun(@(x) isempty(regexp(x,testNames,'ONCE')),testFileNames));
 
