93b511d9cbd79da66deec7b3de3edc336232d625
Thomas Pfau <thomas.pfau@uni.lu>
2018-06-26 14:33:35 +0200

    Adding Requirements for URLs to the tests

--- test/verifiedTests/visualization/testReconMap/testReconMap.m
+++ test/verifiedTests/visualization/testReconMap/testReconMap.m
@@ -9,8 +9,6 @@
 %     - Original file: Alberto Noronha 21/03/2017
 %
 
-global CBTDIR
-
 % save the current path
 currentDir = pwd;
 
@@ -24,40 +22,36 @@ model = getDistributedModel('ecoli_core_model.mat');
 % Get the minerva structure
 load('minerva.mat');
 
-[status_curl, result_curl] = system(['curl -s -k --head https://vmh.uni.lu/MapViewer/']);
+prepareTest('needsWebAddress','https://vmh.uni.lu/MapViewer/')
 
 % check if the URL exists
-if status_curl == 0 && ~isempty(strfind(result_curl, '200 OK'))
-    % Set the user to testing user
-    oldLogin = minerva.login;
-    oldPassword = minerva.password;
-    
-    minerva.login = 'cobratoolbox-test';
-    minerva.password = 'test';
-    minerva.googleLicenseConsent = 'true';
+% Set the user to testing user
+oldLogin = minerva.login;
+oldPassword = minerva.password;
 
-    % FBA solution for flux distribution
-    changeCobraSolver('glpk', 'LP');
-    FBAsolution = optimizeCbModel(model, 'max');
+minerva.login = 'cobratoolbox-test';
+minerva.password = 'test';
+minerva.googleLicenseConsent = 'true';
 
-    % Send the flux distribution to MINERVA
-    response = buildFluxDistLayout(minerva, model, FBAsolution, 'Test - Flux distribution 1');
+% FBA solution for flux distribution
+changeCobraSolver('glpk', 'LP');
+FBAsolution = optimizeCbModel(model, 'max');
 
-    % 2 Correct responses, either successful or layout exists already 
-    assert(~isempty(regexp(response, 'Overlay generated successfully!')) | ~isempty(regexp(response, 'ERROR. Layout with given identifier ("Pyruvate metabolism") already exists.')));
+% Send the flux distribution to MINERVA
+response = buildFluxDistLayout(minerva, model, FBAsolution, 'Test - Flux distribution 1');
 
-    % Send the subsystem layout to MINERVA
-    response = generateSubsytemsLayout(minerva, model, 'Pyruvate metabolism', '#6617B5');
+% 2 Correct responses, either successful or layout exists already
+assert(~isempty(regexp(response, 'Overlay generated successfully!')) | ~isempty(regexp(response, 'ERROR. Layout with given identifier ("Pyruvate metabolism") already exists.')));
 
-    % Same as before - two possible correct responses
-    assert(~isempty(regexp(response, 'Overlay generated successfully!')) | ~isempty(regexp(response, 'ERROR. Layout with given identifier ("Pyruvate metabolism") already exists.')));
-    
-    
-    minerva.login = oldLogin;
-    minerva.password = oldPassword;
-else
-    error('The remote repository cannot be reached. Please check your internet connection.');
-end
+% Send the subsystem layout to MINERVA
+response = generateSubsytemsLayout(minerva, model, 'Pyruvate metabolism', '#6617B5');
+
+% Same as before - two possible correct responses
+assert(~isempty(regexp(response, 'Overlay generated successfully!')) | ~isempty(regexp(response, 'ERROR. Layout with given identifier ("Pyruvate metabolism") already exists.')));
+
+
+minerva.login = oldLogin;
+minerva.password = oldPassword;
 
 % change the directory
 cd(currentDir)
