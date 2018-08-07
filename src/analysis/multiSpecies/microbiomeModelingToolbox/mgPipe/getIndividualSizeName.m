7369240459392f81383cfa1d60dc7909d4cdb31f
Fede-edef <federico.baldini@stud.unifi.it>
2018-08-07 17:20:17 +0200

    adding option for automatic detection and addition in case of need of index

--- src/analysis/multiSpecies/microbiomeModelingToolbox/mgPipe/getIndividualSizeName.m
+++ src/analysis/multiSpecies/microbiomeModelingToolbox/mgPipe/getIndividualSizeName.m
@@ -17,6 +17,30 @@ function [indNumb, sampName, organisms] = getIndividualSizeName(abunFilePath)
 % .. Author: Federico Baldini 2017-2018
 
 [sampName] = readtable(abunFilePath, 'ReadVariableNames', false);
+% Creating array to compare with first column 
+fcol=table2cell(sampName(2:height(sampName),1));
+%fcol=table2array(sampName(2:height(sampName),1));
+% if  ~isa(fcol(2,1),'char')
+%     fcol=cellstr(num2str(fcol));
+% end
+pIndex=cellstr(num2str((1:(height(sampName)-1))'));
+spaceInd=strmatch(' ',pIndex);
+pIndexN=pIndex;
+if length(spaceInd)>0
+    pIndexN(spaceInd)=strrep(pIndex(spaceInd),' ','');
+end
+% Adding index column if needed
+if  ~isa(fcol{2,1},'char')
+     fcol=cellstr(num2str(cell2mat(fcol))); 
+end
+if isequal(fcol,pIndexN)
+    disp('Index fashion input file detected');
+else
+   disp('Plain csv input format: adding index for internal purposes');
+   addIndex=['Index';pIndex];
+   sampName=horzcat((cell2table(addIndex)),sampName);
+end
+oldSampName=sampName;
 s = size(sampName);
 s = s(1, 2);
 sampName = sampName(1, 3:s);
@@ -25,8 +49,6 @@ sampName = sampName';
 indNumb = length(sampName);  % number of individuals
 
 % getting info on present strains
-% Reading models names
-[strains] = readtable(abunFilePath);
-strains = strains(:, 2);
-organisms = table2cell(strains);  % extracted names of models
+organisms = oldSampName(2:height(oldSampName), 2);  
+organisms = table2cell(organisms);  % extracted names of models
 end
