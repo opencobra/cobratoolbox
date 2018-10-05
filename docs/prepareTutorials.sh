df5cd8c11e0b87ce6b4cb9578515d3b82576d922
laurentheirendt <laurent.heirendt@uni.lu>
2018-10-05 22:11:03 +0200

    [documentation] introduction of sips

--- docs/prepareTutorials.sh
+++ docs/prepareTutorials.sh
@@ -302,9 +302,10 @@ if [ $buildPNG = true ] || [ $buildMD = true ] || [ $buildRST = true ]; then
             fi
 	    echo $pdfPath/tutorials/$tutorialFolder/$tutorialName.pdf
 	    export PATH=/usr/local/bin:$PATH;
-            /usr/local/bin/convert -density 900 "$pdfPath/tutorials/$tutorialFolder/$tutorialName.pdf" ${tutorialName}_%04d.png
-            /usr/local/bin/convert -shave 4%x5% -append ${tutorialName}*.png ${tutorialName}2.png && rm ${tutorialName}_*.png
-            /usr/local/bin/pngquant ${tutorialName}2.png --ext -2.png && mv ${tutorialName}2-2.png $pngPath/${tutorialName}.png && rm ${tutorialName}2.png
+            /usr/bin/sips -s format png "$pdfPath/tutorials/$tutorialFolder/$tutorialName.pdf" --out $pngPath/${tutorialName}.png
+            #/usr/local/bin/convert -density 900 "$pdfPath/tutorials/$tutorialFolder/$tutorialName.pdf" ${tutorialName}_%04d.png
+            #/usr/local/bin/convert -shave 4%x5% -append ${tutorialName}*.png ${tutorialName}2.png && rm ${tutorialName}_*.png
+            #/usr/local/bin/pngquant ${tutorialName}2.png --ext -2.png && mv ${tutorialName}2-2.png $pngPath/${tutorialName}.png && rm ${tutorialName}2.png
         fi
 
         # create markdown README
