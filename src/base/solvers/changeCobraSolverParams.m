737f0a8cf9823f9936b6777f2abd2e127e630340
Thomas Pfau <thomas.pfau@uni.lu>
2018-10-10 06:24:57 +0200

    updated CobraSolverParams Setting

--- src/base/solvers/changeCobraSolverParams.m
+++ src/base/solvers/changeCobraSolverParams.m
@@ -13,19 +13,6 @@ function changeOK = changeCobraSolverParams(solverType, paramName, paramValue)
 % OUTPUT:
 %    changeOK:          Logical inicator that supplied parameter is allowed (= 1)
 %
-% Allowed MILP parameter names:
-%
-%    * timeLimit:       Global time limit
-%    * intTol:          Integer tolerance
-%    * relMipGapTol:    Relative MIP gap tolerance
-%    * logFile:         Internal log file for solver
-%    * printLevel:      Print level for solver
-%
-% Allowed LP parameter names:
-%
-%    * optTol:          Optimal objective accuracy tolerance
-%    * teasTol:         Constraint feasibilty tolerance
-%    * minNorm:         {(0), scalar , `n` x 1 vector}, where `[m,n]=size(S)`;
 %
 % Explanation on parameters:
 %
@@ -41,6 +28,8 @@ function changeOK = changeCobraSolverParams(solverType, paramName, paramValue)
 %
 % NOTE:
 %
+%    The available solver Parameters can be obtained by calling
+%    getSolverParamsOptionsForType().
 %    If input argument `minNorm` is not zero, then minimise the Euclidean length
 %    of the solution to the LP problem. `minNorm ~1e-6` should be
 %    high enough for regularisation yet maintain the same value for
@@ -53,6 +42,13 @@ function changeOK = changeCobraSolverParams(solverType, paramName, paramValue)
 %       - Jan Schellenberger, 09/28/09
 %       - Ronan Fleming, 12/07/09 commenting of input/output
 
+
+global CBT_LP_PARAMS;
+global CBT_MILP_PARAMS;
+global CBT_QP_PARAMS;
+global CBT_MIQP_PARAMS;
+global CBT_NLP_PARAMS;
+% get the parameter structs
 changeOK = false;
 
 if strcmp(paramName,'objTol')
@@ -60,36 +56,11 @@ if strcmp(paramName,'objTol')
     paramName='optTol';
 end
 
-allowedLPparams = {'optTol', 'primalOnly', 'minNorm', 'printLevel','feasTol'};
-allowedQPparams = {'minNorm', 'printLevel'};
-allowedMILPparams = {'intTol','relMipGapTol','timeLimit','logFile','printLevel'};
-
-% Only LP, QP and MILP are currently included
-switch solverType
-    case 'LP'
-        if (ismember(paramName,allowedLPparams))
-            global CBT_LP_PARAMS;
-            CBT_LP_PARAMS.(paramName) = paramValue;
-            changeOK = true;
-        else
-            error(['Parameter name ' paramName ' not allowed for LP solvers']);
-        end
-    case 'QP'
-         if (ismember(paramName,allowedQPparams))
-            global CBT_QP_PARAMS;
-            CBT_QP_PARAMS.(paramName) = paramValue;
-            changeOK = true;
-        else
-            error(['Parameter name ' paramName ' not allowed for QP solvers']);
-        end
-    case 'MILP'
-        if (ismember(paramName,allowedMILPparams))
-            global CBT_MILP_PARAMS;
-            CBT_MILP_PARAMS.(paramName) = paramValue;
-            changeOK = true;
-        else
-            error(['Parameter name ' paramName ' not allowed for MILP solvers']);
-        end
-    otherwise
-        error(['solver type ' solverType ' not valid']);
+allowedParameters = getCobraSolverParamsOptionsForType(solverType);
+if (ismember(paramName,allowedParameters))   
+    eval(['CBT_' solverType '_PARAMS.(paramName) = paramValue;']);
+    changeOK = true;
+else
+    error(['Parameter name ' paramName ' not allowed for LP solvers']);
+end
 end
