b1b866015e633b8912ca43edc8e4f1b96132df02
Loic Marx <loic.marx@gmail.com>
2018-11-06 11:49:24 +0100

    WIP: testing conc2rate function

--- test/verifiedTests/dataIntegration/testMetaboTools/testConc2Rate.m
+++ test/verifiedTests/dataIntegration/testMetaboTools/testConc2Rate.m
@@ -17,7 +17,8 @@ fileDir = fileparts(which('testconc2Rate'));
 cd(fileDir);
 
 %calculate inputs
-metConc = [1 2]
+metConc = [1 2]% metConc = 1 
+
 cellConc = 2
 t = 3
 cellWeight = 8
@@ -25,7 +26,8 @@ cellWeight = 8
 
 % calculate reference value
 
-v_ref = [2.0833e-05, 4.1667e-05];
+% v_ref = [2.0833e-05, 4.1667e-05];
+v_ref = 2.0833e-05
 
 % v_ref = [1.0e-04 * 0.2083 , 1.0e-04 * 0.4167];
 % Comparison to ref value
