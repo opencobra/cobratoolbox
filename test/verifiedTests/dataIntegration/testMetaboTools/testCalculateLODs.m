1041d2d8a306e680aa65b25dd94517585c43986a
Loic Marx <loic.marx@gmail.com>
2018-11-06 14:21:59 +0100

    final testing function

--- test/verifiedTests/dataIntegration/testMetaboTools/testCalculateLODs.m
+++ test/verifiedTests/dataIntegration/testMetaboTools/testCalculateLODs.m
@@ -18,23 +18,25 @@ cd(fileDir);
 
 % calculate reference value
 theo_mass = [1, 2];
-v_ref = [1000, 500];
+v_ref = 1e-3 * [1; 0.5];
 
 %calculate inputs
 lod_ngmL = 1;
 
-calculateLOD = calculateLODs(theo_mass,lod_ngmL)
+calculateLOD = calculateLODs(theo_mass,lod_ngmL);
 % Comparison to ref value
 
 assert(isequal(calculateLOD, v_ref))
-% throwing an error if the dimension are not equal
-lod_ngmL_unequal = [1, 2, 3]
-assert(verifyCobraFunctionError('calculateLODs', 'theo_mass,lod_ngmL_unequal ', {theo_mass,lod_ngmL_unequal'}, 'testMessage', 'both inputs do not have the same size'));
+% throwing an error if the dimensions are not equal
+lod_ngmL_unequal = [1, 2, 3];
+
+assert(verifyCobraFunctionError('calculateLODs', 'inputs', {theo_mass,lod_ngmL_unequal'}, 'testMessage', 'both inputs do not have the same size'));
 % Testing the case where lod_ngmL is a vector of the same size as theo_mass
-lod_ngmL_vector = [1, 2]
-v_ref_vector = [1000, 1000]
+
+lod_ngmL_vector = [1, 2];
+v_ref_vector = 1e-3 * [1; 1];
 % Comparison to the ref value
-calculateLOD_vector = calculateLODs(theo_mass,lod_ngmL)
-assert(isequal(calculateLOD_vector, v_ref_vector))
+calculateLOD_vector = calculateLODs(theo_mass,lod_ngmL_vector);
+assert(isequal(calculateLOD_vector, v_ref_vector));
 % change the directory
 cd(currentDir)
