2dd1a2bb8a76241fb08e7e8e71ee278485ecf854
Loic Marx <loic.marx@gmail.com>
2018-11-08 15:52:52 +0100

    Reformating the style

--- src/dataIntegration/metabotools/calculateLODs.m
+++ src/dataIntegration/metabotools/calculateLODs.m
@@ -14,28 +14,22 @@ function [lod_mM] = calculateLODs(theo_mass,lod_ngmL)
 %   lod_mM:            Detection limits in mM
 %
 % .. Author: - Maike K. Aurich 27/05/15
-%    Modified by Loic Marx, November 2018
+%            - Modified by Loic Marx, November 2018
 
 % input checking
-if length(lod_ngmL) == 1
-    
+if size(lod_ngmL) == 1
     lod_ngmL = repmat(lod_ngmL, 1, length(theo_mass)); 
-    
-end    
+    end    
 
-if numel(lod_ngmL) ~= numel(theo_mass)
-    
+if size(lod_ngmL) ~= size(theo_mass)
     error('The number of elements in the input vectors do not match. They have to be either the same size, or lod_ngmL has to be a single value which is used for all elements');
 end
 
 lod_gL = lod_ngmL * 1e-6; 
-
 lod_mM = zeros(length(lod_gL), 1);
 
-for i=1 : length(lod_gL)
-
+for i = 1 : length(lod_gL)
     lod_mM(i) = lod_gL(i)/theo_mass(i)*1000;
-
 end
 
 end
--- test/verifiedTests/dataIntegration/testMetaboTools/testCalculateLODs.m
+++ test/verifiedTests/dataIntegration/testMetaboTools/testCalculateLODs.m
@@ -3,50 +3,40 @@
 % Purpose:
 %     - tests the calculateLODs function
 %
-% Authors:
-%     Loic Marx, November 2018
+% Author: - Loic Marx, November 2018
 %    
-   
-
-% Save the current path
+  
+% save the current path
 currentDir = pwd;
 
-% Initialize the test
+% initialize the test
 fileDir = fileparts(which('testcalculateLODs'));
-
 cd(fileDir);
 
-% Define the inputs
+% define the inputs
 lod_ngmL = 1;
-
 theo_mass = [1, 2];
 
-% Calculate the reference value
-
+% calculate the reference value
 v_ref = 1e-3 * [1; 0.5];
 
-% Calculate the actual value by using the function
-
+% calculate the actual value by using the function
 calculateLOD = calculateLODs(theo_mass,lod_ngmL);
 
-% Compare between the calculated value and the reference value
-
+% compare the calculated value and the reference value
 assert(isequal(calculateLOD, v_ref))
 
-% Throw an error if the dimensions of both inputs are not equal
+% throw an error if the dimensions of both inputs are not equal
 lod_ngmL_unequal = [1, 2, 3];
-
 assert(verifyCobraFunctionError('calculateLODs', 'inputs', {theo_mass,lod_ngmL_unequal'}, 'testMessage', 'The number of elements in the input vectors do not match. They have to be either the same size, or lod_ngmL has to be a single value which is used for all elements'));
 
 % lod_ngmL and theo_mass are two vectors of the same size
 lod_ngmL_vector = [1, 2];
-
 v_ref_vector = 1e-3 * [1; 1];
 
-% Compare the values calculated when the inputs have the same size and the reference value
+% compare the values calculated when the inputs have the same size and the reference value
 calculateLOD_vector = calculateLODs(theo_mass,lod_ngmL_vector);
-
 assert(isequal(calculateLOD_vector, v_ref_vector));
 
-% Change the directory
+% change the directory
 cd(currentDir)
