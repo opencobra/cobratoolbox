471e15092799c90fa8e58de52824fccf7a64bb0c
Loic Marx <loic.marx@gmail.com>
2018-11-08 15:38:40 +0100

    testing the function conc2rate

--- test/verifiedTests/dataIntegration/testMetaboTools/testConc2Rate.m
+++ test/verifiedTests/dataIntegration/testMetaboTools/testConc2Rate.m
@@ -4,7 +4,7 @@
 %     - test conc2Rate function
 %
 % Authors:
-%     Loic Marx, November 2018
+%     - Loic Marx, November 2018
 %     
 %
    
@@ -16,23 +16,34 @@ currentDir = pwd;
 fileDir = fileparts(which('testconc2Rate'));
 cd(fileDir);
 
-%calculate inputs
-metConc = [1 2]% metConc = 1 
-
+%define inputs
+% less than 4 inputs ,  CellWeight = 500 * 1e-12 g
+metConc = 1 
 cellConc = 2
 t = 3
-cellWeight = 8
-
+cellWeight = 500 * 1e-12
 
 % calculate reference value
+v_ref = 3.3333e+05;
 
-% v_ref = [2.0833e-05, 4.1667e-05];
-v_ref = 2.0833e-05
+% compare to the reference value
+calculate_conc2Rate = conc2Rate(1,2,3,500*1e-12);
+tol = 1e-4;
+assert(norm(calculate_conc2Rate - v_ref) < tol);
 
-% v_ref = [1.0e-04 * 0.2083 , 1.0e-04 * 0.4167];
-% Comparison to ref value
-calculate_conc2Rate = conc2Rate(metConc, cellConc, t, cellWeight)
-assert(isequal(calculate_conc2Rate, v_ref))
+% less than 3 inputs, CellWeight = 500 * 1e-12 g and the doubling time T = 24 and 
+metConc = 1 
+cellConc = 2
+cellWeight = 500 * 1e-12
+t = 24
 
+% calcule the reference value
+v_ref_2 = 4.1667e+04;
+
+% compare to the reference value
+
+calculate_conc2Rate_2 = conc2Rate(1,2,24,500 * 1e-12);
+tol = 1e-4;
+assert(norm(calculate_conc2Rate_2 - v_ref_2) < tol);
 % change the directory
 cd(currentDir)
