da9301c677c7f58e82dced78d9f6ddf30f16098b
Loic Marx <loic.marx@gmail.com>
2018-11-13 12:24:33 +0100

    WIP comparing two files

--- test/verifiedTests/design/testOptForce/testExportSetToGAMS.m
+++ test/verifiedTests/design/testOptForce/testExportSetToGAMS.m
@@ -20,8 +20,20 @@ model = load('ecoli_core_model.mat');
 set = model.model.rxns; 
 fileName = 'Reactions.txt';
 
- exportSetToGAMS(model.model.rxns, 'Reactions.txt');
+exportSetToGAMS(model.model.rxns, 'Reactions.txt');
+exportSetToGAMS(model.model.rxns, 'Reactions.m')
+
+
+% TextFolder = dlmread('Reactions.txt');  
+% save('Reactions.mat','TextFolder');
+
+v_ref = model.model.rxns
+ assert(isequal(value - v_ref))
  
+ % If nargin >2 
+ 
+assert(verifyCobraFunctionError('testexportSetToGAMS', 'inputs', {model.model.rxns,fileName'}, 'testMessage', 'The number of elements in the input vectors do not match. They have to be either the same size, or lod_ngmL has to be a single value which is used for all elements'));
+
 % Nargin <2
 % if nargin <2: 
 %      error('Not enough input arguments');
