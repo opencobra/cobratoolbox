92b58e4060d10c81e54f2412b4d155a1b6d27f1c
Thomas Pfau <thomas.pfau@uni.lu>
2018-11-16 12:56:23 +0100

    Adding Conversion for model.A

--- src/base/io/utilities/convertOldStyleModel.m
+++ src/base/io/utilities/convertOldStyleModel.m
@@ -100,6 +100,30 @@ newFields = {'rxnConfidenceScores', 'metCharges','rxnECNumbers',...
 mergefunction = {maxmerge, nanmerge,cellmerge,...
 		cellmerge,cellmerge,cellmerge,...
 		cellmerge,cellmerge,cellmerge};    
+
+if isfield(model,'A') && isfield(model,'S')
+    if printLevel >=1
+        warning('The inserted Model contains an old style coupling matrix (A). The MAtrix will be converted into a Coupling Matrix (C) and fields will be adapted.')
+    end
+    nMets = size(model.S,1);
+    % get the Constraint data
+    C = model.A(nMets+1:end,:);     
+    ctrs = columnVector(model.mets(nMets+1:end));    
+    dsense =  columnVector(model.csense(nMets+1:end));
+    d = columnVector(model.b(nMets+1:end));
+    % set the constraint data
+    model.C = C;
+    model.ctrs = ctrs;
+    model.dsense = dsense;
+    model.d = d;
+    % now, we assume, that those are the only modified fields, if not,
+    % something is seriously broken.
+    model.mets = columnVector(model.mets(1:nMets));
+    model.b = columnVector(model.b(1:nMets));
+    model.csense = columnVector(model.csense(1:nMets));
+    model = rmfield(model,'A');
+end
+    
     
 for i = 1:numel(oldFields)
     if (isfield(model,oldFields{i}))
