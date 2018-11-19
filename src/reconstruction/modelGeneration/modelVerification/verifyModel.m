876252a91e73ba238a4773944db110505d30e143
Thomas Pfau <thomas.pfau@uni.lu>
2018-11-19 07:56:11 +0100

    Correct output

--- src/reconstruction/modelGeneration/modelVerification/verifyModel.m
+++ src/reconstruction/modelGeneration/modelVerification/verifyModel.m
@@ -384,14 +384,16 @@ for i = 1:numel(presentFields)
             else
                 x_pres = numel(model.(xFieldMatch));
             end
+            errorMessage = sprintf('%s: Size of %s does not match elements in %s', xFieldMatch,testedField,xFieldMatch);
         elseif isnumeric(xFieldMatch)
+            errorMessage = sprintf('X Size of %s was %i. Expected %i',testedField, x_size,x_pres);
             x_pres = xFieldMatch;
         end
         if x_pres ~= x_size
             if ~isfield(results.Errors,'inconsistentFields')
                 results.Errors.inconsistentFields = struct();
             end
-            results.Errors.inconsistentFields.(testedField) = sprintf('%s: Size of %s does not match elements in %s', xFieldMatch,testedField,xFieldMatch);
+            results.Errors.inconsistentFields.(testedField) = errorMessage;
         end
     end
     if checkY
@@ -405,14 +407,16 @@ for i = 1:numel(presentFields)
             else
                 y_pres = numel(model.(yFieldMatch));
             end
+            errorMessage = sprintf('%s: Size of %s does not match elements in %s', yFieldMatch,testedField,yFieldMatch);
         elseif isnumeric(yFieldMatch)
             y_pres = yFieldMatch;
+            errorMessage = sprintf('Y Size of %s was %i. Expected %i',testedField, y_size,y_pres);
         end
         if y_pres ~= y_size
             if ~isfield(results.Errors,'inconsistentFields')
                 results.Errors.inconsistentFields = struct();
             end
-            results.Errors.inconsistentFields.(testedField) = sprintf('%s: Size of %s does not match elements in %s', yFieldMatch,testedField,yFieldMatch);
+            results.Errors.inconsistentFields.(testedField) = errorMessage;
         end
     end
     %Test the field content properties
