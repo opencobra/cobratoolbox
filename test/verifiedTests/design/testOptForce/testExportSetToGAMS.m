2023234f901b1e1569296fa227722a62b12b0585
laurentheirendt <laurent.heirendt@uni.lu>
2018-11-27 17:37:11 +0100

    essential changes to remove additional files

--- test/verifiedTests/design/testOptForce/testExportSetToGAMS.m
+++ test/verifiedTests/design/testOptForce/testExportSetToGAMS.m
@@ -13,7 +13,7 @@ currentDir = pwd;
 fileDir = fileparts(which('testexportSetToGAMS'));
 cd(fileDir);
 
-% if nargin = 2 : 
+% if nargin < 2
 model = getDistributedModel('ecoli_core_model.mat');
 fileName = 'refData_reactions.txt';
 
@@ -26,10 +26,10 @@ assert(isequal(rxnsRef{end}, '/'))
 
 % test if all the other cells correspond to the reaction names
 for k = 2:length(model.rxns)-1
-assert(isequal(cellstr(rxnsRef{k}(2:end-1)), model.rxns(k-1)));
+    assert(isequal(cellstr(rxnsRef{k}(2:end-1)), model.rxns(k-1)));
 end
 
-% run the function 
+% run the function
 exportSetToGAMS(model.rxns, 'Reactions.txt');
 
 % compare to the ref data
@@ -39,4 +39,8 @@ assert(isequal(refData, rxnsRef))
 % test for 1 input: exportSetToGAMS(model.rxns)
 assert(verifyCobraFunctionError('exportSetToGAMS', 'inputs', {model.rxns}, 'testMessage', 'All inputs for the function exportSetToGAMS must be specified'))
 
+% remove the test file
+delete('Reactions.txt');
 
+% change back to the current directory
+cd(currentDir);
