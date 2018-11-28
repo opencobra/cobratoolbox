8ef33fb97bd5fbf78824717d1e602bcc23347678
laurentheirendt <laurent.heirendt@uni.lu>
2018-11-28 13:42:13 +0100

    minor formatting

--- src/analysis/subspaces/determineBinaryMatrix.m
+++ src/analysis/subspaces/determineBinaryMatrix.m
@@ -1,26 +1,28 @@
-function [Shatabs, mconnect, mconnectin, mconnectout, nconnect] = determineBinaryMatrix(S)
-% Determine the binary form of S
+function [Shat, Shatabs, mconnect, nconnect, mconnectin, mconnectout] = determineBinaryMatrix(S)
+% Determine the binary form of the stoichiometric matrix S
 %
 % INPUT:
 %
-%    S:         Stoichiometric matrix of size m x n (m rows, n columns)
+%    S:            Stoichiometric matrix of size m x n (m rows, n columns)
 %
-% OUTPUT:
+% OUTPUTS:
 %
-%    Shat:      Binary form of the stoichiometric matrix
-%    mconnect:  Compound connectivity (connectivity number; sum of the number of non-zero entries in a row)
-%    nconnect:  Reaction participation number (sum of the number of non-zero entries in a column)
+%    Shat:         Binary form of the stoichiometric matrix
+%    Shatabs:      Absolute value of Shat
+%    mconnect:     Compound connectivity (connectivity number; sum of the number of non-zero entries in a row)
+%    nconnect:     Reaction participation number (sum of the number of non-zero entries in a column)
+%    mconnectin:   Produced compound connectivity
+%    mconnectout:  Consumed compound connectivity
 %
-% .. Author: Laurent Heirendt, June 2018
+% .. Author: - Laurent Heirendt, June 2018
 
-    % initialize the binary matrix
-    Shat = S;
+    Shat = S; % initialize the binary matrix
     Shat(S > 0) = 1; % products, produced -> kin
-    Shat(S < 0) = -1; % educts, reactants, consumed -> kout
+    Shat(S < 0) = -1; % reactants, consumed -> kout
 
     % determine the row connectivity
     Shatabs = abs(Shat);
-    if nargout > 1
+    if nargout > 2
         mconnect = sort(sum(Shatabs, 2), 'descend');
         mconnectin = sum(Shat == 1, 2);
         mconnectout = sum(Shat == -1, 2);
