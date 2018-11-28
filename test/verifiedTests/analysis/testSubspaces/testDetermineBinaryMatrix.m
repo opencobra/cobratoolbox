3c9dc5ac840e88610f601f3e68b40a176c9d119a
laurentheirendt <laurent.heirendt@uni.lu>
2018-11-28 15:27:49 +0100

    test for determinebinarymatrix

--- test/verifiedTests/analysis/testSubspaces/testDetermineBinaryMatrix.m
+++ test/verifiedTests/analysis/testSubspaces/testDetermineBinaryMatrix.m
@@ -8,13 +8,37 @@
 %
 
 % save the current path and initialize the test
-currentDir = cd(fileparts(which(mfilename)));
+currentDir = fileparts(which(mfilename));
 
 % load the model
 model = createToyModelForLifting(false);
 
+% load reference test data
+load([currentDir filesep 'refData_determineBinaryMatrix.mat']);
 
+% run the function with full output
+[test_Shat, test_Shatabs, test_mconnect, test_nconnect, test_mconnectin, test_mconnectout] = determineBinaryMatrix(model.S);
 
+assert(isequal(test_Shat, Shat))
+assert(isequal(test_Shatabs, Shatabs))
+assert(isequal(test_mconnect, mconnect))
+assert(isequal(test_nconnect, nconnect))
+assert(isequal(test_mconnectin, mconnectin))
+assert(isequal(test_mconnectout, mconnectout))
+
+% run the function with partial output
+[test_Shat, test_Shatabs, test_test_mconnect, test_nconnect] = determineBinaryMatrix(model.S);
+
+assert(isequal(test_Shat, Shat))
+assert(isequal(test_Shatabs, Shatabs))
+assert(isequal(test_mconnect, mconnect))
+assert(isequal(test_nconnect, nconnect))
+
+% run the function with minimal output
+[test_Shat, test_Shatabs] = determineBinaryMatrix(model.S);
+
+assert(isequal(test_Shat, Shat))
+assert(isequal(test_Shatabs, Shatabs))
 
 % change the directory
 cd(currentDir)
