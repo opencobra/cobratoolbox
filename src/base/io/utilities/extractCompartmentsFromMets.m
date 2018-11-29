03d635adca3bd4cb597fb497abb293ab508be654
Thomas Pfau <thomas.pfau@uni.lu>
2018-11-29 13:32:18 +0100

    Fix for char inputs

--- src/base/io/utilities/extractCompartmentsFromMets.m
+++ src/base/io/utilities/extractCompartmentsFromMets.m
@@ -1,4 +1,4 @@
-function [metComps,metIDs] = extractCompartmentsFromMets(mets, defaultCompartment)
+function [metComps,metIDs] = extractCompartmentsFromMets(mets, defaultCompartment,compartmentRegexp)
 % function to extract the compartment IDs from a list of metabolite IDs
 % (assuming the compartments are stored in '\[[^\]+]\]$' expressions, while
 % giving a default expression to empty elements.
@@ -12,20 +12,28 @@ function [metComps,metIDs] = extractCompartmentsFromMets(mets, defaultCompartmen
 % OPTIONAL INPUT:
 %    defaultCompartment:    The default compartment to use for mets which
 %                           don't have an id. (Default: 'k' for unknown)
+%    compartmentRegExp:     Regular expression matching compartments. with
+%                           needs to define a compID and a metID group. 
+%                           (default: '(?<metID>.*)\[(?<compID>[^\]]+)\]')
 %
 % OUTPUT:
 %    metComps:              The compartment IDs extracted. (cell array of
 %                           strings)
+%    metID:                 The compartmentless metabolite ID.
+%  
 
 if ~exist('defaultCompartment', 'var')
     defaultCompartment = {'k'};
+end
+if ~exist('compartmentRegExp','var')
+    compartmentRegExp = '(?<metID>.*)\[(?<compID>[^\]]+)\]';
 end
 
 if ischar(defaultCompartment)
     defaultCompartment = {defaultCompartment};
 end
 
-metMatches = regexp(mets,'(?<metID>.*)\[(?<compID>[^\]]+)\]','names');
+metMatches = regexp(mets,compartmentRegExp,'names');
 if isempty(metMatches) 
     if ischar(mets)
         metIDs = mets;
@@ -34,12 +42,17 @@ if isempty(metMatches)
         metIDs = mets;
         metComps = {'k'};       
     end
-else        
-    metComps = cell(numel(mets),1);
-    metIDs = cell(numel(mets),1);
-    emptyComps = cellfun(@isempty, metMatches);    
-    metComps(~emptyComps) = cellfun(@(x) x.compID,metMatches(~emptyComps),'Uniform',0);
-    metComps(emptyComps) = {defaultCompartment};
-    metIDs(~emptyComps) = cellfun(@(x) x.metID,metMatches(~emptyComps),'Uniform',0);
-    metIDs(emptyComps) = mets(emptyComps);
+else       
+    if ischar(mets)
+        metIDs = metMatches.metID;
+        metComps = metMatches.compID;
+    else
+        metComps = cell(numel(mets),1);
+        metIDs = cell(numel(mets),1);
+        emptyComps = cellfun(@isempty, metMatches);
+        metComps(~emptyComps) = cellfun(@(x) x.compID,metMatches(~emptyComps),'Uniform',0);
+        metComps(emptyComps) = {defaultCompartment};
+        metIDs(~emptyComps) = cellfun(@(x) x.metID,metMatches(~emptyComps),'Uniform',0);
+        metIDs(emptyComps) = mets(emptyComps);
+    end
 end
