45360a677d6cbe33a9716d81145d60ca7d458bb2
Thomas Pfau <thomas.pfau@uni.lu>
2018-11-30 07:49:53 +0100

    Added examples, and moved to varargin

--- src/base/io/utilities/extractCompartmentsFromMets.m
+++ src/base/io/utilities/extractCompartmentsFromMets.m
@@ -1,37 +1,53 @@
-function [metComps,metIDs] = extractCompartmentsFromMets(mets, defaultCompartment,compartmentRegexp)
+function [metComps,metIDs] = extractCompartmentsFromMets(mets, varargin)
 % function to extract the compartment IDs from a list of metabolite IDs
 % (assuming the compartments are stored in '\[[^\]+]\]$' expressions, while
 % giving a default expression to empty elements.
 % USAGE: 
-%    metComps = extractCompartmentsFromMets(mets, defaultCompartment)
+%    metComps = extractCompartmentsFromMets(mets, varargin)
 % 
 % INPUT:
 %    mets:                  The list of metabolites to extract compartments
 %                           from (cell array of strings)
 %
 % OPTIONAL INPUT:
-%    defaultCompartment:    The default compartment to use for mets which
-%                           don't have an id. (Default: 'k' for unknown)
-%    compartmentRegExp:     Regular expression matching compartments. with
-%                           needs to define a compID and a metID group. 
-%                           (default: '(?<metID>.*)\[(?<compID>[^\]]+)\]')
+%    varargin:              additional parameters as 'paramerName',value
+%                           pairs or parameter struct with the following
+%                           parameters:
+%                            * `defaultCompartment` - The default compartment to use for mets which don't have an id. (Default: 'k' for unknown)
+%                            * `compartmentRegExp` - Regular expression matching compartments. with needs to define a compID and a metID group and assume that those groups exist in ALL metabolites If they don't the regexp should not match. (default: '(?<metID>.*)\[(?<compID>[^\]]+)\]')
 %
 % OUTPUT:
 %    metComps:              The compartment IDs extracted. (cell array of
 %                           strings)
 %    metID:                 The compartmentless metabolite ID.
 %  
+% AUTHOR:   Thomas Pfau Nov 2018
+%
+% NOTE:
+%    The function should only be used when no `metComps` field is available,
+%    or when it is required to obtain metabolite IDs without any
+%    compartment information (e.g. when checking for unique metabolites).
+%    If the actual compartment of a metabolite is requested, please always
+%    refer to the metComps field!
+%
+% EXAMPLE:
+%    % Extract unique metabolites from a model
+%    [~,metIDs] = extractCompartmentsFromMets(model.mets)
+%    uniqueMets = unique(metIDs)
+%    % extract compartments form a model, which has BiGG style compartment
+%    % IDs using the cytosol ('c') as default compartment id:
+%    [metComps,metIDs] = extractCompartmentsFromMets(model.mets, 'defaultCompartment', 'c', 'compartmentRegExp', '^(?<metID>.*?)_(?<compID>[a-z][a-z0-9]?)(_([A-Z][A-Z0-9]?))?$)
+%    
+%
 
-if ~exist('defaultCompartment', 'var')
-    defaultCompartment = {'k'};
-end
-if ~exist('compartmentRegExp','var')
-    compartmentRegExp = '(?<metID>.*)\[(?<compID>[^\]]+)\]';
-end
+parser = inputParser();
+parser.addParameter('defaultCompartment','k',@ischar);
+parser.addParameter('compartmentRegExp','(?<metID>.*)\[(?<compID>[^\]]+)\]',@ischar);
 
-if ischar(defaultCompartment)
-    defaultCompartment = {defaultCompartment};
-end
+parser.parse(varargin{:});
+
+defaultCompartment = parser.Results.defaultCompartment;
+compartmentRegExp = parser.Results.compartmentRegExp;
 
 metMatches = regexp(mets,compartmentRegExp,'names');
 if isempty(metMatches) 
