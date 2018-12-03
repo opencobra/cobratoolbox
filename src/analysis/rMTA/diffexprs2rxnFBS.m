01ce0849e35e89127175884569ab0939d7ee2526
lvalcarcel <lvalcarcel@tecnun.es>
2018-12-03 09:14:33 +0100

    first submission

--- src/analysis/rMTA/diffexprs2rxnFBS.m
+++ src/analysis/rMTA/diffexprs2rxnFBS.m
@@ -1,8 +1,5 @@
 function rxnFBS = diffexprs2rxnFBS(model, diff_exprs, Vref, separate_transcript, logFC, pval)
-% Returns Forward - Backward - Unchanged (+1;0;-1) information of whatever 
-% a reaction associated to this gene should increase in Forward direction
-% or decrease inBackward direction (1), decrease or increase in Backward 
-% direction (-1) or remain unchanged.
+% Returns Forward - Backward - Unchanged (+1;0;-1) for each reaction.
 % (+1)R_f    (-1)R_b     (0)unchanged
 %
 % USAGE:
@@ -13,9 +10,9 @@ function rxnFBS = diffexprs2rxnFBS(model, diff_exprs, Vref, separate_transcript,
 %    model:             The COBRA Model structure
 %    diff_exprs:        MATLB Table including the information of the
 %                       differentially expressed genes.
-%                       Required columns (with this names):
-%                           - gene ( ID of gene, same as in model)
-%                           - logFC (Study must be SOURCE VS TARGET)
+%                       Required columns (with theses names):
+%                           - gene ( ID of gene, same as in the meabolic model)
+%                           - logFC (SOURCE VS TARGET)
 %                           - pval (p-value or adjusted-p-value)     
 %    Vref:              Reference flux of the model
 %
@@ -26,38 +23,38 @@ function rxnFBS = diffexprs2rxnFBS(model, diff_exprs, Vref, separate_transcript,
 %    pval:              maximum p-value admited (default = 0.05).
 %
 % OUTPUT:
-%    rxnFBS             array containting the information of the changed 
-%                       reactions. It has Forward - Backward - Unchanged 
-%                       (+1;0;-1) values corresponding to each reaction;
+%    rxnFBS             array containting the information of altered  
+%                       reactions: Forward - Backward - Unchanged 
+%                       (+1;0;-1);
 % 
 % Note:
 %    It is highly recommended to use as diff_exprs the TopTable result from
-%    limma in R and change names if neccesary
+%    limma in R and change names if neccesary.
 %    In the tutorial there is further information to load data from GEO or
-%    using R to calculate the absolute gene expression and the differential
-%    expression.
+%    using R to conduct absolute and differential gene expression analysis.
 %
 % .. Authors:
 %       - Luis V. Valcarcel, 25/06/2015, University of Navarra, CIMA & TECNUN School of Engineering.
 % .. Revisions:
 %       - Luis V. Valcarcel, 26/10/2018, University of Navarra, CIMA & TECNUN School of Engineering.
+%       - Francisco J. Planes, 26/10/2018, University of Navarra, TECNUN School of Engineering.
 
-% if no separate_transcript requered, default = ''
+% if no separate_transcript required, default = ''
 if ~exist('separate_transcript','var')
     separate_transcript = '';
 end
 
-% if no logFC requered, default = 0
+% if no logFC required, default = 0
 if ~exist('logFC','var')
     logFC = 0;
 end
 
-% if no p-value requered, default = 0.05
+% if no p-value required, default = 0.05
 if ~exist('pval','var')
     pval = 0.05;
 end
 
-% Traduce onmic data from gene level to reaction level
+% Traslate omics data from gene level to reaction level
 if ~isempty(separate_transcript)
     aux_table = table(strtok(model.genes,separate_transcript),model.genes,'VariableNames',{'gene' 'transcript'});
     diff_exprs = innerjoin(aux_table, diff_exprs, 'Keys','gene');
@@ -75,7 +72,7 @@ diff_exprs = diff_exprs(idx,:);
 pos_up = diff_exprs.logFC > 0;     % source state bigger than target state
 pos_down = diff_exprs.logFC < 0;   % source state smaller than target state
 
-% As we cannot map the reaction to genes, we can discretize this values as
+% As we cannot map the reaction to genes, we can discretize thess values as
 % (+1, 0 -1), to create geneFBS
 geneFBS_aux = zeros(size(diff_exprs,1),1);
 geneFBS_aux(pos_up) = +1;
@@ -86,13 +83,10 @@ geneFBS = zeros(length(model.genes),1);
 [~,idx] = ismember(diff_exprs.gene,model.genes);
 geneFBS(idx) = geneFBS_aux;
 
-% If the source state has a gene whose expression is reduced compared with
-% the target state, the flux activity related with this gene should
-% increase to be transformated into the target state. 
-% We can appreciate that if a gene is differentially expressed up, gene
-% should be reduced, and viceverse: if the gene is differentially expressed
-% down, it should increase
-% For this reason we change the sign of geneFBS
+% If a gene is down-regulated in the source state,the flux activity should
+% be increased. Similarly, if a gene is up-regulated in the source state, 
+% the flux activity should be decreased.
+% For this reason we change the sign of geneFBS (target vs source here).
 geneFBS = -geneFBS;
 
 % Change to Cobra format
@@ -126,14 +120,14 @@ rxnFBS_minus = mapExpressionToReactions(model, geneFBS_minus);  % COBRA function
 rxnFBS_plus(rxnFBS_plus<=0) = 0;
 rxnFBS_minus(rxnFBS_minus<=0) = 0;
 rxnFBS = rxnFBS_plus - rxnFBS_minus;
-rxnFBS(strcmp(model.grRules,'')) = 0; % not mapped reactions have no change in activity
+rxnFBS(strcmp(model.grRules,'')) = 0; % non-mapped reactions have no change in activity
 
 
 % We have considered all reactions as Forward reactions. GeneFBS gives the
-% informatio whatever a reaction should increase activity in absolute
+% information whatever a reaction should increase activity in absolute
 % values. 
 % For Forward reactions, (+1) increase flux, (-1) decrease flux
-% For Forward reactions, (+1) decrease flux (increase in absolute values), 
+% For Backward reactions, (+1) decrease flux (increase in absolute values), 
 % (-1) increase flux, (decrease in absolute values)
 rxnFBS(Vref < 0) = - rxnFBS(Vref < 0);
 fprintf('\tReaction expression changes calculated\n');
