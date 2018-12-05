1ff2f99b9486c2105646a3de3c6cae38e5e05464
Thomas Pfau <thomas.pfau@uni.lu>
2018-12-05 13:41:56 +0100

    Added Model Size utility

---
+++ src/analysis/exploration/getModelSizes.m
@@ -0,0 +1,37 @@
+function [nRxns, nMets, nCtrs, nVars, nGenes, nComps] = getModelSizes(model)
+% Get the sizes of the basic fields of the model structure
+%
+% USAGE:
+% [nRxns, nMets, nCtrs, nVars, nGenes, nComps] = getModelSizes(model)
+%
+% INPUT:
+%    model:     A COBRA model structure
+%
+% OUTPUT:
+%    nRxns:     The number of reactions in the model
+%    nMets:     The number of metabolites in the model
+%    nCtrs:     The number of constraints in the model
+%    nVars:     The number of variables in the model
+%    nGenes:    The number of genes in the model
+%    nComps:    The number of compartments in the model
+
+[nRxns,nMets] = size(model.S);
+
+nCtrs = 0;
+nVars = 0;
+nComps = 0;
+nGenes = 0;
+if isfield(model,'C')
+    nCtrs = size(model.C,1);
+end
+if isfield(model,'E')
+    nVars = size(model.E,2);
+end
+
+if isfield(model,'comps')
+    nComps = size(model.comps,1);
+end
+
+if isfield(model,'genes')
+    nGenes = size(model.genes,1);
+end
