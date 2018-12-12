9bb6a125ffc4a9e743053d7a41013b4c48d5c27b
Loic Marx <loic.marx@gmail.com>
2018-12-12 13:43:53 +0100

    comparison between generated data and reference data

--- test/verifiedTests/dataIntegration/testMetaboTools/testPruneModel.m
+++ test/verifiedTests/dataIntegration/testMetaboTools/testPruneModel.m
@@ -3,7 +3,7 @@
 % Purpose:
 %     - test the pruneModel function
 %
-% Authors:
+% Author:
 %     - Loic Marx, December 2018
 
 % save the current path
@@ -13,3 +13,16 @@ currentDir = pwd;
 fileDir = fileparts(which(mfilename));
 cd(fileDir);
 
+% load reference data
+load('refData_pruneModel.mat')
+
+% define input
+model = getDistributedModel('ecoli_core_model.mat');
+biomassRxn = model.rxns(13);
+minGrowth = 1; 
+
+% generate data
+modelPruned = pruneModel(model, minGrowth, biomassRxn);
+
+% comparison between refData and generated data
+assert(isequal(modelPruned_Ref, modelPruned))
