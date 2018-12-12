7e18b706423bd0a6ec6397c7e07f41ec91677c76
Loic Marx <loic.marx@gmail.com>
2018-12-12 11:40:04 +0100

    adding header of the test

---
+++ test/verifiedTests/dataIntegration/testMetaboTools/testPruneModel.m
@@ -0,0 +1,15 @@
+% The COBRA Toolbox: testPruneModel.m
+%
+% Purpose:
+%     - test the pruneModel function
+%
+% Authors:
+%     - Loic Marx, December 2018
+
+% save the current path
+currentDir = pwd;
+
+% initialize the test
+fileDir = fileparts(which(mfilename));
+cd(fileDir);
+
