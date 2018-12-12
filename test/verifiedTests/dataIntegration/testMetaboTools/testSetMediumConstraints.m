07d38ec2f349eb8a5a282d3df63d7bbf6f1cd53c
Loic Marx <loic.marx@gmail.com>
2018-12-12 16:26:35 +0100

    comparison between ref data and generated data

--- test/verifiedTests/dataIntegration/testMetaboTools/testSetMediumConstraints.m
+++ test/verifiedTests/dataIntegration/testMetaboTools/testSetMediumConstraints.m
@@ -21,15 +21,20 @@ model = getDistributedModel('Recon2.v04.mat');
 set_inf = 1000;
 current_inf = 500; 
 medium_composition = {'EX_ala_L(e)';'EX_arg_L(e)'} % related to the RPMI composition
-met_Conc_mM = [1;2]; %Change in metabolite concentration (mM); this value is for testing only 
+met_Conc_mM = [1;2]; %Change in metabolite concentration (mM)
 cellConc = 1; 
 t = 1; % Time in hours
 cellWeight = 500 * 1e-12; 
-mediumCompounds = model.rxns; %metabolites that are uptake from the medium but not captured by the measured data
-mediumCompounds_lb = 1;
-customizedConstraints = {'EX_o2(e)'};
+mediumCompounds = {'EX_co2(e)';'EX_h(e)'}; %metabolites that are uptake from the medium but not captured by the measured data
+mediumCompounds_lb = -10;
+customizedConstraints = {'EX_o2(e)'}; %limit the model behaviour to the secretion of oxygen
 customizedConstraints_lb = 1;
 customizedConstraints_ub = 2;
-close_exchanges = 1 ; % 1 : close exchange , 0: open exchange 
+close_exchanges = 1; % 1 : close exchange , 0: open exchange 
 
+% generate data
+[modelMedium, basisMedium] = setMediumConstraints(model, set_inf, current_inf, medium_composition, met_Conc_mM, cellConc, t, cellWeight, mediumCompounds, mediumCompounds_lb, customizedConstraints, customizedConstraints_ub, customizedConstraints_lb, close_exchanges);
 
+% comparison between refData and generated data
+assert(isequal(modelMedium_ref, modelMedium))
+assert(isequal(basisMedium_ref, basisMedium))
