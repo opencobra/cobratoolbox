4b238dcbb26bd38d5ad3c91f8e26e2f90384c2cd
Loic Marx <loic.marx@gmail.com>
2018-12-12 15:12:56 +0100

    adding header to the test

---
+++ test/verifiedTests/dataIntegration/testMetaboTools/testSetMediumConstraints.m
@@ -0,0 +1,14 @@
+% The COBRA Toolbox: setMediumConstraints.m
+%
+% Purpose:
+%     - test the setMediumConstraints function
+%
+% Author:
+%     - Loic Marx, December 2018
+
+% save the current path
+currentDir = pwd;
+
+% initialize the test
+fileDir = fileparts(which(mfilename));
+cd(fileDir);
