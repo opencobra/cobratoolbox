c122e09137ac130debda5ddd64cb348fbdc087a6
laurentheirendt <laurent.heirendt@uni.lu>
2018-12-13 11:27:14 +0100

    [documentation] minor changes to header of calculateGeneKOMatrix

--- src/analysis/rMTA/calculateGeneKOMatrix.m
+++ src/analysis/rMTA/calculateGeneKOMatrix.m
@@ -9,10 +9,10 @@ function [geneKO] = calculateGeneKOMatrix(model, varargin)
 % INPUT:
 %    model:             The COBRA Model structure
 %
-% OPTIONAL INPUT (name-value pair):
-%    SeparateTranscript - Character used to separate
-%                         different transcripts of a gene. (default = '')
-%                         Example: SeparateTranscript = ''
+% OPTIONAL INPUTS:
+%    SeparateTranscript Character used to separate
+%                       different transcripts of a gene. (default = '')
+%                       Example: SeparateTranscript = ''
 %                                   gene 10005.1    ==>    gene 10005.1
 %                                   gene 10005.2    ==>    gene 10005.2
 %                                   gene 10005.3    ==>    gene 10005.3
@@ -20,22 +20,19 @@ function [geneKO] = calculateGeneKOMatrix(model, varargin)
 %                                   gene 10005.1
 %                                   gene 10005.2    ==>    gene 10005
 %                                   gene 10005.3
-%    printLevel        Integer. 1 if the process is wanted to be shown
+%    printLevel:       Integer. 1 if the process is wanted to be shown
 %                      on the screen, 0 otherwise. (default = 1)
-% 
+%
 % OUTPUT:
-%    geneKO:            Struct which contains matrix with blocked 
+%    geneKO:            Struct which contains matrix with blocked
 %                       reactions for each gene in the metabolic model,
 %                       name of reactions and name of genes.
 %
 % .. Authors:
 %       - Luis V. Valcarcel, Oct 2017, University of Navarra, CIMA & TECNUN School of Engineering.
-% .. Revisions:
 %       - Luis V. Valcarcel, 03/11/2018, University of Navarra, CIMA & TECNUN School of Engineering.
 
-
-%% Check the input information
-p = inputParser;
+p = inputParser; % check the input information
 addRequired(p, 'model');
 addOptional(p, 'SeparateTranscript', '', @ischar);
 addOptional(p, 'printLevel', 1, @(x)isnumeric(x)&&isscalar(x));
@@ -56,11 +53,11 @@ for gen = 1:ngenes
     if p.Results.printLevel > 0
         showprogress(gen/ngenes, 'Calculate Gene Knock-out matrix');
     end
-    
+
 %     transcripts = model.genes(startsWith(model.genes,[genes{gen} SeparateTranscript]));
     transcripts = model.genes(strcmp(strtok(model.genes,p.Results.SeparateTranscript),genes{gen})); % to support R2015b
     [~, hasEffect, constrRxnNames] = deleteModelGenes(model, transcripts);
-    
+
     if hasEffect
         % search index of bloced reactions
         [~,idx] = ismember(constrRxnNames,model.rxns);
