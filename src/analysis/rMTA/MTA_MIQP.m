f4a0e5b669e2fdd6aaf2e271b3c886001ab009cf
laurentheirendt <laurent.heirendt@uni.lu>
2018-12-13 11:20:27 +0100

    [documentation] minor changes to header of MTA_MIQP

--- src/analysis/rMTA/MTA_MIQP.m
+++ src/analysis/rMTA/MTA_MIQP.m
@@ -7,23 +7,21 @@ function [v_res, solution] = MTA_MIQP (OptimizationModel, KOrxn, varargin)
 %    [v_res, success, unsuccess] = MTA_MIQP (OptimizationModel, KOrxn, numWorkers, timeLimit, printLevel)
 %
 % INPUT:
-%    OptimizationModel:       Cplex Model struct
-%    KOrxn:            perturbation in the model (reactions)
-%    numWorkers:       number of threads used by Cplex.
-%    printLevel:       1 if the process is wanted to be shown on the
-%                      screen, 0 otherwise. Default: 1.
+%    OptimizationModel:    Cplex Model struct
+%    KOrxn:                perturbation in the model (reactions)
+%    numWorkers:           number of threads used by Cplex.
+%    printLevel:           1 if the process is wanted to be shown on the
+%                          screen, 0 otherwise. Default: 1.
 %
 % OUTPUTS:
-%    Vout:             Solution flux of MIQP formulation for each case
-%    solution:         Cplex solution struct
+%    Vout:                 Solution flux of MIQP formulation for each case
+%    solution:             Cplex solution struct
 %
 % .. Authors:
 %       - Luis V. Valcarcel, 03/06/2015, University of Navarra, CIMA & TECNUN School of Engineering.
-% .. Revisions:
 %       - Luis V. Valcarcel, 26/10/2018, University of Navarra, CIMA & TECNUN School of Engineering.
 
-% Check the input information
-p = inputParser;
+p = inputParser; % check the input information
 % check requiered arguments
 addRequired(p, 'OptimizationModel');
 addRequired(p, 'KOrxn');
@@ -54,7 +52,7 @@ if SOLVERS.ibm_cplex.installed && isempty(CBT_MIQP_SOLVER)
     % Generate CPLEX model
     cplex = Cplex('MIQP');
     CplexModel = OptimizationModel;
-    
+
     b_L(CplexModel.csense == 'E') = CplexModel.b(CplexModel.csense == 'E');
     b_U(CplexModel.csense == 'E') = CplexModel.b(CplexModel.csense == 'E');
     b_L(CplexModel.csense == 'G') = CplexModel.b(CplexModel.csense == 'G');
@@ -67,12 +65,12 @@ if SOLVERS.ibm_cplex.installed && isempty(CBT_MIQP_SOLVER)
     CplexModel.obj = CplexModel.c;
     CplexModel.ctype = CplexModel.vartype;
     CplexModel.sense = 'minimize';
-    
+
     cplex.Model = CplexModel;
     % include the knock-out reactions
     cplex.Model.lb(KOrxn) = 0;
     cplex.Model.ub(KOrxn) = 0;
-    
+
     % Cplex Parameter
     if numWorkers>0
         cplex.Param.threads.Cur = numWorkers;
@@ -90,7 +88,7 @@ if SOLVERS.ibm_cplex.installed && isempty(CBT_MIQP_SOLVER)
     cplex.Param.mip.tolerances.mipgap.Cur = 1e-5;
     % cplex.Param.mip.tolerances.absmipgap.Cur = 1e-8;
     % cplex.Param.threads.Cur = 16;
-    
+
     % SOLVE the CPLEX problem if not singular
     try
         cplex.solve();
@@ -98,7 +96,7 @@ if SOLVERS.ibm_cplex.installed && isempty(CBT_MIQP_SOLVER)
         v_res = zeros(length(v),1);
         return
     end
-    
+
     if cplex.Solution.status ~= 103
         v_res = cplex.Solution.x(v);
         solution = cplex.Solution;
@@ -106,7 +104,7 @@ if SOLVERS.ibm_cplex.installed && isempty(CBT_MIQP_SOLVER)
         v_res = zeros(length(v),1);
         solution = nan;
     end
-    
+
     % clear the cplex object
     delete(cplex)
     clear cplex
@@ -116,26 +114,26 @@ else
     % include the knock-out reactions
     MIQPproblem.lb(KOrxn) = 0;
     MIQPproblem.ub(KOrxn) = 0;
-    
+
     % Solver Parameter
     if printLevel <=1
         logFile = 0;
         if timeLimit > 1e75
             timeLimit = 1e75;
         end
-        
+
         % SOLVE the MIQP problem
         solution = solveCobraMIQP(MIQPproblem, ...
             'timeLimit',timeLimit, 'relMipGapTol',  1e-5, ...
             'printLevel', 1, 'logFile', logFile,...
             'threads',numWorkers);
-        
+
         if solution.stat ~= 0
             v_res = solution.full(v);
         else
             v_res = zeros(length(v),1);
         end
     end
-    
-    
+
+
 end
