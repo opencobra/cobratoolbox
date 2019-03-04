faa0115615b2a270c4979b57e9ce19c787814f5c
laurentheirendt <laurent.heirendt@uni.lu>
2019-03-04 17:48:27 +0100

    change of env vars

--- .artenolis/deployTutorials.sh
+++ .artenolis/deployTutorials.sh
@@ -6,18 +6,18 @@ GIT_LOCAL_BRANCH='"'$GIT_LOCAL_BRANCH'"'
 echo $GIT_LOCAL_BRANCH
 
 # update the cobra tutorials repository
-cd $ARTENOLIS_ROOT_PATH_MAC/COBRA.tutorials
+cd $ARTENOLIS_ROOT_PATH_MAC/repos/COBRA.tutorials
 git stash
 git checkout $GIT_LOCAL_BRANCH
 git pull origin $GIT_LOCAL_BRANCH
 
 # update the cobratoolbox repository
-cd ~/artenolis/cobratoolbox
+cd $ARTENOLIS_ROOT_PATH_MAC/repos/cobratoolbox
 git stash
 git checkout $GIT_LOCAL_BRANCH
 git pull origin $GIT_LOCAL_BRANCH
 git submodule update --init
 
 # generate and deploy the tutorials
-cd ~/artenolis/cobratoolbox
-~/artenolis/cobratoolbox/docs/prepareTutorials.sh -c=~/artenolis/cobratoolbox -t=~/artenolis/COBRA.tutorials -p=~/artenolis -m=html,pdf,png
+cd $ARTENOLIS_ROOT_PATH_MAC/repos/cobratoolbox
+$ARTENOLIS_ROOT_PATH_MAC/repos/cobratoolbox/docs/prepareTutorials.sh -c=$ARTENOLIS_ROOT_PATH_MAC/repos/cobratoolbox -t=$ARTENOLIS_ROOT_PATH_MAC/repos/COBRA.tutorials -p=$ARTENOLIS_ROOT_PATH_MAC/scratch -m=html,pdf,png
