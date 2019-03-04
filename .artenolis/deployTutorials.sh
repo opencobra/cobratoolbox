1d1f1eaeef81f3d274256483c29d436d15027d2e
laurentheirendt <laurent.heirendt@uni.lu>
2019-03-04 11:16:29 +0100

    deploy tutorials bash script

---
+++ .artenolis/deployTutorials.sh
@@ -0,0 +1,23 @@
+# DESCRIPTION:
+#   deploy tutorials of the COBRA Toolbox
+
+# set the local branch
+GIT_LOCAL_BRANCH='"'$GIT_LOCAL_BRANCH'"'
+echo $GIT_LOCAL_BRANCH
+
+# update the cobra tutorials repository
+cd ~/artenolis/COBRA.tutorials
+git stash
+git checkout $GIT_LOCAL_BRANCH
+git pull origin $GIT_LOCAL_BRANCH
+
+# update the cobratoolbox repository
+cd ~/artenolis/cobratoolbox
+git stash
+git checkout $GIT_LOCAL_BRANCH
+git pull origin $GIT_LOCAL_BRANCH
+git submodule update --init
+
+# generate and deploy the tutorials
+cd ~/artenolis/cobratoolbox
+~/artenolis/cobratoolbox/docs/prepareTutorials.sh -c=~/artenolis/cobratoolbox -t=~/artenolis/COBRA.tutorials -p=~/artenolis -m=html,pdf,png
