eb91c80fd46edf535ae856da012db7008a6e4c0f
laurentheirendt <laurent.heirendt@uni.lu>
2019-03-09 11:59:47 +0100

    change of env variable name for node label

--- .artenolis/runtests.sh
+++ .artenolis/runtests.sh
@@ -80,11 +80,11 @@ elif [ "$ARCH" == "macOS" ]; then
 elif [ "$ARCH" == "windows" ]; then
     # change to the build directory
     echo " -- changing to the build directory --"
-    cd "$ARTENOLIS_DATA_PATH\jenkins\\workspace\\$CI_PROJECT_NAME\\MATLAB_VER\\$MATLAB_VER\\label\\$ARCHVERSION"
+    cd "$ARTENOLIS_DATA_PATH\jenkins\\workspace\\$CI_PROJECT_NAME\\MATLAB_VER\\$MATLAB_VER\\label\\$NODE_LABELS"
 
     echo " -- launching MATLAB --"
     unset Path
-    nohup "$ARTENOLIS_SOFT_PATH\MATLAB\\$MATLAB_VER\\\bin\\matlab.exe" -nojvm -nodesktop -nosplash -useStartupFolderPref -logfile output.log -wait -r "restoredefaultpath; cd $ARTENOLIS_DATA_PATH\jenkins\\workspace\\$CI_PROJECT_NAME\\MATLAB_VER\\$MATLAB_VER\\label\\$ARCHVERSION; cd test; testAll;" & PID=$!
+    nohup "$ARTENOLIS_SOFT_PATH\MATLAB\\$MATLAB_VER\\\bin\\matlab.exe" -nojvm -nodesktop -nosplash -useStartupFolderPref -logfile output.log -wait -r "restoredefaultpath; cd $ARTENOLIS_DATA_PATH\jenkins\\workspace\\$CI_PROJECT_NAME\\MATLAB_VER\\$MATLAB_VER\\label\\$NODE_LABELS; cd test; testAll;" & PID=$!
 
     # follow the log file
     tail -n0 -F --pid=$! output.log 2>/dev/null
