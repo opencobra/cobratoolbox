a5356923fbf452d8b655adfd23d043757c1a7ddc
Almut <almut.heinken@gmail.com>
2019-04-05 17:07:24 +0100

    Made requested changes

--- test/verifiedTests/analysis/testMultiSpeciesModelling/testTranslateMetagenome2AGORA.m
+++ test/verifiedTests/analysis/testMultiSpeciesModelling/testTranslateMetagenome2AGORA.m
@@ -1,4 +1,4 @@
-% The COBRAToolbox: translateMetagenome2AGORAn.m
+% The COBRAToolbox: testTranslateMetagenome2AGORAn.m
 %
 % Purpose:
 %     - tests the basic functionality of translateMetagenome2AGORA
@@ -6,42 +6,36 @@
 % Authors:
 %     - Almut Heinken - March 2019
 %
-% global CBTDIR
-% 
-% % save the current path
-% currentDir = pwd;
-%
-% % initialize the test
-% fileDir = fileparts(which('testTranslateMetagenome2AGORA'));
-% cd(fileDir);
-
 % Ensure that the organisms in the input file were correctly translated.
 % This is the case if all organisms in the output file correspond to an
 % AGORA strain model or AGORA pan-model.
-% Note that this test cannot tell if the biological identity of the 
-% translated organisms is correct as this requires manual inspection. 
+% Note that this test cannot tell if the biological identity of the
+% translated organisms is correct as this requires manual inspection.
 % Moreover, input files with abundance data from other source may fail due
 % to differences in formatting and nomenclature.
 
 % Read in the info file with all AGORA strains and taxa
 [~, infoFile, ~] = xlsread('AGORA_infoFile.xlsx');
 
+% Download an example file with abundance data from EMBL-EBI
+system('curl -O https://www.ebi.ac.uk/metagenomics/api/v1/studies/MGYS00001248/pipelines/3.0/file/SRP065497_taxonomy_abundances_v3.0.tsv')
+
 % Define the taxon level of the input file
 orgList=unique(infoFile(2:end,find(strcmp('Species',infoFile(1,:)))));
-[translatedAbundances,normalizedAbundances,unmappedRows]=translateMetagenome2AGORA('SRP065497_taxonomy_abundances_v3.0.csv','s__');
+[translatedAbundances,normalizedAbundances,unmappedRows]=translateMetagenome2AGORA('SRP065497_taxonomy_abundances_v3.0.tsv','s__');
 % Verify that the output file is not empty
 assert(size(translatedAbundances,1)>1)
 
 if size(translatedAbundances,1)>1
-translatedOrgs=translatedAbundances(2:end,1);
-translatedOrgs=strrep(translatedOrgs,'pan','');
-translatedOrgs=strrep(translatedOrgs,'_',' ');
-
-% Find the overlap between translated organisms and AGORA organisms
-C = intersect(translatedOrgs,orgList);
-
-% Verify that all output organisms overlap with AGORA organisms
-assert(length(translatedOrgs) == length(C))
+    translatedOrgs=translatedAbundances(2:end,1);
+    translatedOrgs=strrep(translatedOrgs,'pan','');
+    translatedOrgs=strrep(translatedOrgs,'_',' ');
+    
+    % Find the overlap between translated organisms and AGORA organisms
+    C = intersect(translatedOrgs,orgList);
+    
+    % Verify that all output organisms overlap with AGORA organisms
+    assert(length(translatedOrgs) == length(C))
 end
 
 % Verify that the relative abundances for each sample sum up to 1
