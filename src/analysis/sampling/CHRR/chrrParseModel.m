0304513eef7f7a887d4eded464e18c444cf4858f
Ben Cousins <bcousins615@gmail.com>
2019-05-01 23:44:35 -0400

    Added support in sampler for C,d inequality constraints

--- src/analysis/sampling/CHRR/chrrParseModel.m
+++ src/analysis/sampling/CHRR/chrrParseModel.m
@@ -18,6 +18,11 @@ function [P,model] = chrrParseModel(model)
 %               * .lb - `n x 1` lower bounds on fluxes
 %               * .ub - `n x 1` upper bounds on fluxes
 %
+% OPTIONAL INPUTS:
+%               * .C - 'k x n' matrix of additional inequality constraints
+%               * .d - 'k x 1' rhs of the above constraints
+%               * .dsense - 'k x 1' the sense of the above constraints ('L' or 'G')
+%
 % OUTPUTS:
 %    P:        A structure with fields:
 %
@@ -29,11 +34,26 @@ function [P,model] = chrrParseModel(model)
 % .. Authors:
 %       - Ben Cousins and Hulda Haraldsdóttir, 10/2016
 %       - Ben Cousins, 12/2017, Moved objective function handling to preprocess function
+%       - Ben Cousins, 05/2019, Added support for C,d inequalities.
 
 dim = length(model.lb);
 
 P.A = [eye(dim); -eye(dim)];
-P.b = [model.ub; -model.lb];
+P.b = [model.ub; -model.lb]; 
+
+if isfield(model,'C') && isfield(model,'d')
+   for i=1:size(model.C,1)
+      if model.dsense(i)=='G'
+          % convert constraint to <=
+          model.C(i,:) = model.C(i,:)*-1;
+          model.d(i) = model.d(i)*-1;
+      elseif model.dsense{i}=='E'
+          error('Equality constraints not supported in C,d fields.');
+      end
+   end
+   P.A = [P.A; model.C];
+   P.b = [P.b; model.d];
+end
 
 P.A_eq = model.S;
 P.b_eq = model.b;
