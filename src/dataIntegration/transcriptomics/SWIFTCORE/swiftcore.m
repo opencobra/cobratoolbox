0ec1283ad91b550a630936af51976f07cdacd9e8
Mojtaba Tefagh <mtefagh@stanford.edu>
2019-05-12 00:34:52 -0700

    change output format

--- src/dataIntegration/transcriptomics/SWIFTCORE/swiftcore.m
+++ src/dataIntegration/transcriptomics/SWIFTCORE/swiftcore.m
@@ -1,15 +1,20 @@
-function [reconstruction, LP] = swiftcore(S, rev, coreInd, weights, reduction, varargin)
+function [reconstruction, reconInd, LP] = swiftcore(model, coreInd, weights, tol, reduction, varargin)
 % swifcore is an even faster version of fastcore
 %
 % USAGE:
 %
-%    [reconstruction, LP] = swiftcore(S, rev, coreInd, weights, reduction [, solver])
+%    [reconstruction, reconInd, LP] = swiftcore(model, coreInd, weights, tol, reduction [, solver])
 %
 % INPUTS:
-%    S:            the associated sparse stoichiometric matrix
-%    rev:          the 0-1 vector with 1's corresponding to the reversible reactions
+%    model:        the metabolic network with fields:
+%                    * .S - the associated sparse stoichiometric matrix
+%                    * .lb - lower bounds on reaction rates
+%                    * .ub - lower bounds on reaction rates
+%                    * .rxns - the cell array of reaction abbreviations
+%                    * .mets - the cell array of metabolite abbreviations
 %    coreInd:      the set of indices corresponding to the core reactions
 %    weights:      weight vector for the penalties associated with each reaction
+%    tol:          zero-tolerance, i.e., the smallest flux value considered nonzero
 %    reduction:    boolean enabling the metabolic network reduction preprocess 
 % 
 % OPTIONAL INPUT:
@@ -19,9 +24,10 @@ function [reconstruction, LP] = swiftcore(S, rev, coreInd, weights, reduction, v
 %               another supported solver is called.
 %
 % OUTPUTS:
-%    reconstruction:    the 0-1 indicator vector of the reactions constituting 
-%                       the consistent metabolic network reconstructed from the 
+%    reconstruction:    the consistent metabolic network reconstructed from the 
 %                       core reactions
+%    reconInd:          the 0-1 indicator vector of the reactions constituting
+%                       the reconstruction
 %    LP:                the number of solved LPs
 %
 % NOTE:
@@ -32,7 +38,15 @@ function [reconstruction, LP] = swiftcore(S, rev, coreInd, weights, reduction, v
 % .. Authors:
 %       - Mojtaba Tefagh, Stephen P. Boyd, 2019, Stanford University
 
+    S = model.S;
     [m, n] = size(S);
+    rev = ones(n, 1);
+    rev(model.lb == 0) = 0;
+    rev(model.ub == 0) = -1;
+    model.ub(rev == -1) = -model.lb(rev == -1);
+    model.lb(rev == -1) = 0;
+    model.ub = model.ub/norm(model.ub, Inf);
+    model.lb = model.lb/norm(model.lb, Inf);
     reacNum = (1:n).';
     fullCouplings = (1:n).';
     
@@ -51,17 +65,26 @@ function [reconstruction, LP] = swiftcore(S, rev, coreInd, weights, reduction, v
                 nzcols = find(S(i, :));
                 % check to see if the i-th row of S has only two nonzero elements
                 if length(nzcols) == 2
-                    % deleting the reaction from the rev vector
-                    if rev(nzcols(2)) ~= 1
-                        if S(i, nzcols(1))/S(i, nzcols(2)) < 0
+                    c = S(i, nzcols(1))/S(i, nzcols(2));
+                    % deleting the reaction from the rev, lb, and ub vectors
+                    if c < 0
+                        if rev(nzcols(2)) ~= 1
                             rev(nzcols(1)) = rev(nzcols(2));
-                        else
+                        end
+                        model.lb(nzcols(1)) = max([model.lb(nzcols(1)), -model.lb(nzcols(2))/c]);
+                        model.ub(nzcols(1)) = min([model.ub(nzcols(1)), -model.ub(nzcols(2))/c]);
+                    else
+                        if rev(nzcols(2)) ~= 1
                             rev(nzcols(1)) = -1 - rev(nzcols(2));
                         end
+                        model.lb(nzcols(1)) = max([model.lb(nzcols(1)), -model.ub(nzcols(2))/c]);
+                        model.ub(nzcols(1)) = min([model.ub(nzcols(1)), -model.lb(nzcols(2))/c]);
                     end
                     rev(nzcols(2)) = [];
+                    model.lb(nzcols(2)) = [];
+                    model.ub(nzcols(2)) = [];
                     % merging the fully coupled pair of reactions
-                    S(:, nzcols(1)) = S(:, nzcols(1)) - S(i, nzcols(1))/S(i, nzcols(2))*S(:, nzcols(2));
+                    S(:, nzcols(1)) = S(:, nzcols(1)) - c*S(:, nzcols(2));
                     S(:, nzcols(2)) = [];
                     % deleting the zero rows from the stoichiometric matrix
                     S = S(any(S, 2), :);
@@ -77,31 +100,32 @@ function [reconstruction, LP] = swiftcore(S, rev, coreInd, weights, reduction, v
     end
     S(:, rev == -1) = -S(:, rev == -1);
     rev(rev == -1) = 0;
+    model.S = S;
+    model.rev = rev;
     
     %% the main algorithm
     weights(ismember(reacNum, coreInd)) = 0;
-    % the zero-tolerance parameter is the smallest flux value that is considered nonzero
-    tol = norm(S, 'fro')*eps(class(S));
-    % phase one of unblocking the irreversible reactions
     n_ = length(weights);
+    % phase one of unblocking the irreversible reactions
     blocked = zeros(n_, 1);
     LP = 1;
-    flux = core(S, rev, blocked, weights, solver);
+    flux = core(model, blocked, weights, solver);
     weights(abs(flux) > tol) = 0;
-    % identifying the blocked reversible reactions
     if n == n_
         blocked = ismember(reacNum, coreInd);
+        blocked(abs(flux) > tol) = false;
     else
-        [Q, R, ~] = qr(transpose(S(:, weights == 0)));
-        Z = Q(:, sum(abs(diag(R)) > tol)+1:end);
-        blocked(weights == 0) = diag(Z*Z.') < tol^2;
+        % identifying the blocked reversible reactions
+        [~, D, V] = svds(S(:, weights == 0), 10, 'smallest');
+        blocked(weights == 0) = vecnorm(V(:, diag(D) < ...
+            norm(S(:, weights == 0), 'fro')*eps(class(S))), Inf, 2) < tol;
     end
     % phase two of unblocking the reversible reactions
     while any(blocked)
         % incrementing the core set until no reversible blocked reaction remains
         blockedSize = sum(blocked);
         LP = LP + 1;
-        flux = core(S, rev, blocked, weights, solver);
+        flux = core(model, blocked, weights, solver);
         weights(abs(flux) > tol) = 0;
         blocked(abs(flux) > tol) = 0;
         % adjust the weights if the number of the blocked reactions is no longer reduced by more than half
@@ -109,5 +133,7 @@ function [reconstruction, LP] = swiftcore(S, rev, coreInd, weights, reduction, v
             weights = weights/2;
         end
     end
-    reconstruction = ismember(fullCouplings, reacNum(weights == 0));
+    reconInd = ismember(fullCouplings, reacNum(weights == 0));
+    reconstruction = removeRxns(model, model.rxns(~reconInd));
+    reconstruction = removeUnusedGenes(reconstruction);
 end
--- test/verifiedTests/dataIntegration/testSWIFTCORE/testSWIFTCORE.m
+++ test/verifiedTests/dataIntegration/testSWIFTCORE/testSWIFTCORE.m
@@ -25,6 +25,9 @@ model.rev = double(model.lb < 0);
 A = swiftcc(model.S, model.rev);
 model.S = model.S(:, A);
 model.rev = model.rev(A);
+model.ub = model.ub(A);
+model.lb = model.lb(A);
+model.rxns = model.rxns(A);
 n = length(model.rev);
 core = randsample(n, round(n/2));
 
@@ -32,12 +35,12 @@ core = randsample(n, round(n/2));
 solverLPOK = changeCobraSolver(solvers.LP{1}, 'LP', 0);
 
 fprintf(' -- Running swiftcore w/o reduction and using the %s solver...\n', solvers.LP{1});
-coreInd = swiftcore(model.S, model.rev, core, ones(n, 1), false, solvers.LP{1});
+[~, coreInd, ~] = swiftcore(model, core, ones(n, 1), 1e-10, false, solvers.LP{1});
 assert(all(coreInd(core)));
 A = swiftcc(model.S(:, coreInd), model.rev(coreInd));
 assert(all(A.' == 1:length(A)));
 fprintf(' -- Running swiftcore w/ reduction and using the %s solver...\n', solvers.LP{1});
-coreInd = swiftcore(model.S, model.rev, core, ones(n, 1), true, solvers.LP{1});
+[~, coreInd, ~] = swiftcore(model, core, ones(n, 1), 1e-10, true, solvers.LP{1});
 assert(all(coreInd(core)));
 A = swiftcc(model.S(:, coreInd), model.rev(coreInd));
 assert(all(A.' == 1:length(A)));
