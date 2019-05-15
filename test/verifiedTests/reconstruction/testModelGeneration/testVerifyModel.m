37cc7be8c97afab92eec5bcdb0c7aa017cc68513
tpfau <github@thomaspfau.de>
2019-05-15 15:30:00 +0200

    Adding basic test for verifyModel, not testing all rules

---
+++ test/verifiedTests/reconstruction/testModelGeneration/testVerifyModel.m
@@ -0,0 +1,24 @@
+% The COBRAToolbox: testVerifyModel.m
+%
+% Purpose:
+%     - Tests the Model Verification function
+%
+
+model = getDistributedModel('ecoli_core_model.mat');
+
+% test whether the correct "invalid" subSystem is found
+modelSub = model;
+modelSub.subSystems(20) = {'blubb'};
+
+res = verifyModel(modelSub,'silentCheck',true);
+
+assert(~isempty(res))
+assert(isequal(res.Errors.propertiesNotMatched.subSystems, sprintf('Field does not match the required properties at the following positions: \n            20')));
+
+% test whether rules are checked crrectly
+modelRule = model;
+modelRule.rules(3) = {'x(17) )'};
+res = verifyModel(modelRule,'silentCheck',true);
+
+assert(~isempty(res))
+assert(isequal(res.Errors.propertiesNotMatched.rules, sprintf('Field does not match the required properties at the following positions: \n       3')));
