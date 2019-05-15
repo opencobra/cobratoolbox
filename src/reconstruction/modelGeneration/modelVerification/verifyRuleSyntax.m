29784b99b01682707fd0fc55c9a988edd53126b6
tpfau <github@thomaspfau.de>
2019-05-15 13:50:52 +0200

    Fixing check

--- src/reconstruction/modelGeneration/modelVerification/verifyRuleSyntax.m
+++ src/reconstruction/modelGeneration/modelVerification/verifyRuleSyntax.m
@@ -5,10 +5,10 @@ function tf = verifyRuleSyntax(ruleString)
 %
 % INPUT:
 %    ruleString:    The string in rule format (i.e. x(1) | x(2) & x(3) ...)
-%um
+%
 % OUTPUT:
-%    tf:            Whether the given string is evaluateable (i.e. the
-%                   syntax is correct
+%    tf:            Whether the given string is evaluateable (i.e. the syntax is correct) 
+%                   
 persistent x
 
 if isempty(x)
@@ -16,29 +16,24 @@ if isempty(x)
     x = true(10000,1); 
 end
 
-if isempty(ruleString)
-    tf = true;
-else
+tf = true;
+
+if ~isempty(ruleString)
     try
         temp = eval(ruleString);
         % if we reach this point it is fine
-        tf = true;
     catch ME
         % now, either our x is not large enough, or the formula is invalid.
         % lets grab the largest number from the formula
         try
-            res = regexp('x(1) | x(2) | x(1000)','x\(([0-9]+)\)','tokens');
+            res = regexp(ruleString,'tokens');
             vals = cellfun(@(x) str2num(x{1}),res);
             % update x to the given values 
             x = true(max(vals),1);
             temp = eval(ruleString);
             % if we reach this point it is fine
-            tf = true;
         catch ME
             tf = false;
         end
     end
 end
-
-            
-        
