6f591a8f9a0474bc0ddce17a5bc15747071933dd
laurentheirendt <laurent.heirendt@uni.lu>
2020-02-10 19:00:20 +0100

    exclude IBM_cplex from testSwiftCore

--- test/verifiedTests/dataIntegration/testSWIFTCORE/testSWIFTCORE.m
+++ test/verifiedTests/dataIntegration/testSWIFTCORE/testSWIFTCORE.m
@@ -10,7 +10,7 @@
 global CBTDIR
 
 % require the specified toolboxes and solvers
-solvers = prepareTest('needsLP', true, 'excludeSolvers', {'glpk'});
+solvers = prepareTest('needsLP', true, 'excludeSolvers', {'glpk', 'ibm_cplex'});
 
 % save the current path
 currentDir = pwd;
