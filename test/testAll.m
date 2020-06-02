a470815125ffe09694798e951b1e05466d272aab
Joe Desbonnet <jdesbonnet@gmail.com>
2020-06-02 23:36:35 +0100

    running tests under vmhadmin user

--- test/testAll.m
+++ test/testAll.m
@@ -14,7 +14,7 @@ fprintf(['     \\_____| \\_____/ |_____/ |_|  \\_\\ |_|   |_|   |\n']);
 fprintf('                                                  | \n\n');
 
 % request explicitly from the user to launch test suite locally
-if isempty(strfind(getenv('HOME'), 'jenkins'))
+if isempty(strfind(getenv('HOME'), 'vmhadmin'))
     reply = '';
     while isempty(reply)
         reply = input([' -> Do you want to launch the test suite locally? Time estimate: more than 60 minutes Y/N: '], 's');
