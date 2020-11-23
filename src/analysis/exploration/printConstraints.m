273927f768a69dd5350f529c6aac5e37efd0927c
Ronan Fleming <ronan.mt.fleming@gmail.com>
2020-11-23 00:59:01 +0000

    relaxed fba before aftere

--- src/analysis/exploration/printConstraints.m
+++ src/analysis/exploration/printConstraints.m
@@ -1,4 +1,4 @@
-function printConstraints(model, minInf, maxInf, rxnBool)
+function printConstraints(model, minInf, maxInf, rxnBool, modelAfter)
 % Print all network constraints that are not `-Inf (minInf)` or `+Inf (maxInf)`
 %
 % USAGE:
@@ -17,22 +17,42 @@ if ~exist('rxnBool','var')
     rxnBool=true(size(model.S,2),1);
 end
 
-minConstraints = model.lb > minInf & model.lb~=0 & rxnBool;
-
-if ~any(minConstraints)
-    fprintf('No non-default maximum constraints.\n');
+if ~exist('modelAfter','var')
+    minConstraints = model.lb > minInf & model.lb~=0 & rxnBool;
+    if ~any(minConstraints)
+        fprintf('%s\n','No non-default maximum constraints.');
+    else
+        fprintf('%s\n', 'Table of minimum constraints:');
+        T = table(model.rxns(minConstraints),model.rxnNames(minConstraints),model.lb(minConstraints),model.ub(minConstraints),'VariableNames',{'Reaction','Name','lb','ub'});
+        disp(T);
+    end
+    
+    maxConstraints = model.ub < maxInf & model.ub~=0 & rxnBool;
+    if ~any(maxConstraints)
+        fprintf('%s\n','No non-default maximum constraints.');
+    else
+        fprintf('%s\n','Table of maximum constraints:');
+        T = table(model.rxns(maxConstraints),model.rxnNames(maxConstraints),model.lb(maxConstraints),model.ub(maxConstraints),'VariableNames',{'Reaction','Name','lb','ub'});
+        disp(T);
+    end
+    fprintf('\n');
 else
-    fprintf('Table of minimum constraints:\n');
-    T = table(model.rxns(minConstraints),model.rxnNames(minConstraints),model.lb(minConstraints),model.ub(minConstraints),'VariableNames',{'Reaction','Name','lb','ub'});
-   disp(T);
+    minConstraints = model.lb > minInf & model.lb~=0 & rxnBool;
+    if ~any(minConstraints)
+        fprintf('%s\n','No non-default maximum constraints.');
+    else
+        fprintf('%s\n','Table of minimum constraints:');
+        T = table(model.rxns(minConstraints),model.rxnNames(minConstraints),model.lb(minConstraints),modelAfter.lb(minConstraints),model.ub(minConstraints),modelAfter.ub(minConstraints),'VariableNames',{'Reaction','Name','lb_before','lb_after','ub_before','ub_after'});
+        disp(T);
+    end
+    
+    maxConstraints = model.ub < maxInf & model.ub~=0 & rxnBool;
+    if ~any(maxConstraints)
+        fprintf('%s\n','No non-default maximum constraints.');
+    else
+        fprintf('%s\n','Table of maximum constraints:');
+        T = table(model.rxns(maxConstraints),model.rxnNames(maxConstraints),model.lb(maxConstraints),modelAfter.lb(maxConstraints),model.ub(maxConstraints),modelAfter.ub(maxConstraints),'VariableNames',{'Reaction','Name','lb_before','lb_after','ub_before','ub_after'});
+        disp(T);
+    end
+    fprintf('\n');
 end
-
-maxConstraints = model.ub < maxInf & model.ub~=0 & rxnBool;
-if ~any(maxConstraints)
-    fprintf('No non-default maximum constraints.\n');
-else
-    fprintf('Table of maximum constraints:\n');
-    T = table(model.rxns(maxConstraints),model.rxnNames(maxConstraints),model.lb(maxConstraints),model.ub(maxConstraints),'VariableNames',{'Reaction','Name','lb','ub'});
-    disp(T);
-end
-fprintf('\n');
