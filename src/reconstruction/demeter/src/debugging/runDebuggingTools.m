11242bd5448985d21791c1ce313122b9eaea096c
almut.heinken <almut.heinken@gmail.com>
2021-01-15 00:09:42 +0000

    started adding debugging tools

--- src/reconstruction/demeter/src/debugging/runDebuggingTools.m
+++ src/reconstruction/demeter/src/debugging/runDebuggingTools.m
@@ -1,59 +1,51 @@
-function [reactionsToGapfill,reactionsToDelete,revisedModel]=runDebuggingTools(model,microbeID,biomassReaction,testResultsFolder,reconVersion)
-% This function runs a suite of debugging functions on a refine
+function [debuggingReport, unsuccessfulModels]=runDebuggingTools(refinedFolder,testResultsFolder,reconVersion)
+% This function runs a suite of debugging functions on a refined
 % reconstruction produced by the DEMETER pipeline. Tests
 % are performed whether or not the models can produce biomass aerobically
 % and anaerobically, and whether or not unrealistically high ATP is
 % produced on the Western diet.
 
-% load all test result files
-dInfo = dir([testResultsFolder filesep reconVersion '_refined']);
-fileList={dInfo.name};
-fileList=fileList';
-fileList(~(contains(fileList(:,1),{'.txt'})),:)=[];
+% get all models that failed at least one test
+failedModels = {};
 
-
-% find reactions that are causing futile cycles
-futileCycleReactions=identifyFutileCycles(model);
-
-% find reactions that are preventing the model from growing
-[blockedPrecursors,enablingMets]=findBlockedPrecursorsInRxn(model,biomassReaction,'max');
-
-% identify blocked biomass precursors on defined medium for the organism
-[growsOnDefinedMedium,constrainedModel,~] = testGrowthOnDefinedMedia(model, microbeID, biomassReaction);
-
-if growsOnDefinedMedium == 0
-[blockedPrecursors,enablingMets]=findBlockedPrecursorsInRxn(constrainedModel,biomassReaction,'max');
+if isfile([testResultsFolder filesep reconVersion '_refined' filesep 'notGrowing.mat'])
+    load([testResultsFolder filesep reconVersion '_refined' filesep 'notGrowing.mat']);
+    failedModels = union(failedModels,notGrowing);
+end
+if isfile([testResultsFolder filesep reconVersion '_refined' filesep 'tooHighATP.mat'])
+    load([testResultsFolder filesep reconVersion '_refined' filesep 'tooHighATP.mat']);
+    failedModels = union(failedModels,tooHighATP);
+end
+if isfile([testResultsFolder filesep reconVersion '_refined' filesep 'growsOnDefinedMedium_' reconVersion '_refined.txt'])
+    FNlist = readtable([testResultsFolder filesep reconVersion '_refined' filesep 'growsOnDefinedMedium_' reconVersion '_refined.txt'], 'ReadVariableNames', false, 'Delimiter', 'tab');
+    FNlist = table2cell(FNlist);
+    failedModels=union(failedModels,FNlist(find(strcmp(FNlist(:,2),'0')),1));
 end
 
+currentDir(pwd);
+% go to correct folder and load test results
+cd([testResultsFolder filesep reconVersion '_refined'])
+
 % load all test result files for experimental data
-dInfo = dir([testResultsFolder filesep reconVersion '_refined']);
+dInfo = dir(pwd);
 fileList={dInfo.name};
 fileList=fileList';
 fileList(~(contains(fileList(:,1),{'.txt'})),:)=[];
 fileList(~(contains(fileList(:,1),{'FalseNegatives'})),:)=[];
 
-% if there are any false negative predictions
-if size(fileList,1)>0
-    for i=1:size(fileList,1)
-        % define if objective should be maximized or minimized
-        if any(contains(fileList{i,1},{'Carbon_sources','Metabolite_uptake'}))
-            osenseStr = 'min';
-        elseif any(contains(fileList{i,1},{'Fermentation_products','Secretion_products'}))
-            osenseStr = 'max';
-        end
-        FNlist = readtable([[testResultsFolder filesep reconVersion '_refined'] filesep fileList{i,1}], 'ReadVariableNames', false, 'Delimiter', 'tab');
-        FNlist = table2cell(FNlist);
-        FNs = FNlist(find(strcmp(FNlist(:,1),microbeID)),2:end);
-        FNs = FNs(~cellfun(@isempty, FNs));
-        if ~isempty(FNs)
-            for j=1:length(FNs)
-                metExch=['EX_' metaboliteDatabase{find(strcmp(metaboliteDatabase(:,2),FNs{j})),1} '(e)'];
-                % find what reactions should be added to enable agreement with experimental
-                % data
-                [blockedPrecursors,enablingMets]=findBlockedPrecursorsInRxn(model,metExch,osenseStr);
-            end
-        end
-    end
+for i=1:size(fileList,1)
+    FNlist = readtable([[testResultsFolder filesep reconVersion '_refined'] filesep fileList{i,1}], 'ReadVariableNames', false, 'Delimiter', 'tab');
+    FNlist = table2cell(FNlist);
+    % remove all rows with no cases
+    FNlist(cellfun(@isempty, FNlist(:,2)),:)=[];
+    failedModels=union(failedModels,FNlist(:,1));
 end
 
+% perform debugging tools for each model for which addiitonal curation is
+% needed
+for i=1:length(failedModels)
+    
+end
+
+cd(currentDir);
 end
