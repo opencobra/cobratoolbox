e5aa77cee5325884095ea0fc31174ec16bdea041
almut.heinken <almut.heinken@gmail.com>
2021-02-01 17:28:31 +0000

    Started preparation of refinement report WIP

--- src/reconstruction/demeter/suite/tests/runTestSuiteTools.m
+++ src/reconstruction/demeter/suite/tests/runTestSuiteTools.m
@@ -69,7 +69,7 @@ testAllReconstructionFunctions(refinedFolder,[testResultsFolder filesep reconVer
 plotTestSuiteResults([testResultsFolder filesep reconVersion '_refined'],reconVersion);
 
 %% prepare a report of the QA/QC status of the models
-curationReport = printRefinementReport(testResultsFolder,reconVersion);
+% curationReport = printRefinementReport(testResultsFolder,reconVersion);
 
 %% Give an individual report of each reconstruction if desired.
 % Note: this is time-consuming.
