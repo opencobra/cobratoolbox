f6a5e2725abd464e2865a79b76273808776bd00a
YinTat <YinTatLee@gmail.com>
2021-07-26 23:28:03 -0700

    Update testSampleCbModel.m

--- test/verifiedTests/analysis/testSampling/testSampleCbModel.m
+++ test/verifiedTests/analysis/testSampling/testSampleCbModel.m
@@ -17,6 +17,7 @@ fileDir = fileparts(which('testSampleCbModel'));
 cd(fileDir);
 % define the samplers
 samplers = {'CHRR','CHRR_EXP','ACHR','RHMC'}; %'MFE'
+samplers = {'RHMC'};
 
 % create a parallel pool (if possible)
 try
