2ed4238214da01230ef7a864ff7daae08c31885a
Almut Heinken <almut.heinken@gmail.com>
2022-01-12 23:50:11 +0100

    Updated the readme for mgPipe

--- src/analysis/multiSpecies/microbiomeModelingToolbox/mgPipe/parsave.m
+++ src/analysis/multiSpecies/microbiomeModelingToolbox/mgPipe/parsave.m
@@ -10,5 +10,11 @@ function parsave(fname, microbiota_model)
 %   microbiota_model:    name of variable
 %
 
+% need to use v7.3 switch for very large models
+if length(microbiota_model.rxns) < 300000
     save(fname, 'microbiota_model')
+else
+    save(fname, 'microbiota_model','-v7.3')
+end
+
 end
