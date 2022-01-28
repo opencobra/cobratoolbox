494f04053ed262b30b670ffe2f6a487d36b7d518
Almut Heinken <almut.heinken@gmail.com>
2022-01-28 12:26:02 +0100

    made analyseObjectiveShadowPrices more efficient

--- src/analysis/multiSpecies/microbiomeModelingToolbox/mgPipe/parsave.m
+++ src/analysis/multiSpecies/microbiomeModelingToolbox/mgPipe/parsave.m
@@ -1,20 +1,26 @@
-function parsave(fname, microbiota_model)
-% Saves a model from a parfor loop - might not work in R2105b
+function parsave(fname, data)
+% Saves a data variable (e.g., model) from a parfor loop - might not work in R2105b
 %
 % USAGE:
 %
-%    parsave(fname, microbiota_model)
+%    parsave(fname, data)
 %
 % INPUTS:
-%   fname:               name of file
-%   microbiota_model:    name of variable
+%   fname:   name of file
+%   data:    name of variable
 %
 
-% need to use v7.3 switch for very large models
-if length(microbiota_model.rxns) < 300000
-    save(fname, 'microbiota_model')
+% need to use v7.3 switch for very large variables
+if isstruct(data)
+    if isfield(data,'rxns')
+        if length(data.rxns) < 300000
+            save(fname, 'data')
+        else
+            save(fname, 'data','-v7.3')
+        end
+    end
 else
-    save(fname, 'microbiota_model','-v7.3')
+    save(fname, 'data')
 end
 
 end
