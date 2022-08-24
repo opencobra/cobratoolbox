c6cb818a18e1f025f042eee94e7849a934bd2e0e
BigDataInSilicoBiologyGroup <kristina.grausa@gmail.com>
2022-08-24 19:41:16 +0300

    Changes and improvements according to our previous email

--- src/analysis/GT1/findGenesAboveThresholdGT1.m
+++ src/analysis/GT1/findGenesAboveThresholdGT1.m
@@ -1,4 +1,26 @@
 function genes = findGenesAboveThresholdGT1(threshold, geneNames, expressionValues)
+% Filters transcriptomics dataset and returns highly expressed gene cell array 
+% (columns: geneId, expressionValue, expression classification - 'High') containing 
+% genes with expression value above the given global threshold value 
+%
+% USAGE:
+%
+%   genes = findGenesAboveThresholdGT1(threshold, geneNames, expressionValues)
+%
+% INPUTS:
+%   threshold:              double
+%   geneNames:              char cell array ix1 with all gene Ids
+%   expressionValues:       double cell array ix1 with gene expression
+%                           values
+%
+% OUTPUTS:
+%	genes:                  cell array ix3 where columns include geneId, 
+%                           expressionValue, expression classification - 'High'
+%
+% .. Authors:
+%       - Kristina Grausa 05/16/2022
+%       - Kristina Grausa 08/22/2022 - standard header and formatting
+
     cnt = 1;
     for i=1:1:length(geneNames)
         if threshold < expressionValues(i)
