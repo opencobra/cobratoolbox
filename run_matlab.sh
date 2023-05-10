475d37bb78a6d8b830840a7d7f61744218db6fb2
AaronBrennan1 <aaronbrennan.brennan@gmail.com>
2023-05-10 14:25:50 +0000

    Update run_matlab.sh

--- run_matlab.sh
+++ run_matlab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Current working directory: $(pwd)"
 echo "Location of convert_files.m: $(pwd)/convert_files.m"
-"/usr/local/MATLAB/R2020a/bin/matlab" -batch "addpath('.'); convert_files"
+"/home/aaron/Documents/Matlab/bin/matlab" -batch "addpath('.'); convert_files"
 
 
 
