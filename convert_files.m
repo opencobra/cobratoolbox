256bd47aa25d3d336bee11ede81feffa452bdc00
AaronBrennan1 <aaronbrennan.brennan@gmail.com>
2023-05-11 10:55:51 +0100

    test

--- convert_files.m
+++ convert_files.m
@@ -1,12 +1,11 @@
-function convert_files
-    mlx_files = dir('**/*.mlx');
+function convert_files(mlx_files)
     if isempty(mlx_files)
         disp('No mlx files found.');
         return;
     end
     
     for k = 1:length(mlx_files)
-        mlx_path = fullfile(mlx_files(k).folder, mlx_files(k).name);
+        mlx_path = mlx_files{k};
         html_path = strrep(mlx_path, '.mlx', '.html');
         pdf_path = strrep(mlx_path, '.mlx', '.pdf');
         fprintf('Converting: %s\n', mlx_path);
@@ -16,9 +15,7 @@ function convert_files
             fprintf('HTML conversion successful: %s\n', html_path);
         catch ME
             fprintf('Error during HTML conversion: %s\n', ME.message);
-        end
-        
-       
+        end       
     end
 end
 
