945cd25e6ca613c1507f0682e5c80d223d6bb75f
AaronBrennan1 <68754265+AaronBrennan1@users.noreply.github.com>
2023-05-11 16:29:45 +0100

    Update run_matlab.sh

--- run_matlab.sh
+++ run_matlab.sh
@@ -3,8 +3,11 @@
 echo "Current working directory: $(pwd)"
 echo "Location of convert_files.m: $(pwd)/convert_files.m"
 
-# Get command-line arguments
-echo "MLX files: $1"
+# Start Xvfb on display :99
+Xvfb :99 -screen 0 1024x768x24 &
+
+# Set the DISPLAY environment variable
+export DISPLAY=:99
 
 # Call MATLAB with the convert_files function and pass the mlx_files as input
 "/home/aaron/Documents/Matlab/bin/matlab" -batch "addpath('.'); convert_files(strsplit('$1', ' '))"
