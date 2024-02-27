4f6b87a7a81fbd77638f139bc9b474d60a4c6769
farid <farid.zare.edu@gmail.com>
2024-02-27 16:58:59 +0000

    Update testJoinModelsPairwiseFromList with latest changes in the main function

--- test/verifiedTests/analysis/testMultiSpeciesModelling/testJoinModelsPairwiseFromList.m
+++ test/verifiedTests/analysis/testMultiSpeciesModelling/testJoinModelsPairwiseFromList.m
@@ -1,7 +1,7 @@
 % The COBRAToolbox: testJoinModelsPairwiseFromList.m
 %
 % Purpose:
-%     - test that the pairwise models created by function 
+%     - test that the pairwise models created by function
 %       joinModelsPairwiseFromList have all reactions of the respective
 %       microbe reconstructions that were joined.
 %
@@ -11,6 +11,8 @@
 %     - renamed script from testBuildPairwiseModels to
 %       testJoinModelsPairwiseFromList and adapted to the new module
 %       Microbiome_Modeling_Toolbox - Almut Heinken - 02/2018
+%     - Farid Zare - Feb 2024 Updated test function for latest function
+%     changes
 
 currentDir = pwd;
 
@@ -18,6 +20,7 @@ currentDir = pwd;
 fileDir = fileparts(which('testJoinModelsPairwiseFromList'));
 cd(fileDir);
 
+fprintf('   Testing joinModelsPairwiseFromList.m ...\n');
 % if the pairedModelInfo file does not exist yet, build the models first
 if ~exist('pairedModelInfo', 'var')
     % run the script to build the pairwise models
@@ -30,13 +33,19 @@ if ~exist('pairedModelInfo', 'var')
         };
     for i=1:length(modelList)
         model = getDistributedModel([modelList{i} '.mat']);
+        % Save all the models into the modelFolder
+        save(fullfile(fileDir, modelList{i}), 'model');
         inputModels{i,1}=model;
     end
-    [pairedModels,pairedModelInfo] = joinModelsPairwiseFromList(modelList,inputModels);
+    joinModelsPairwiseFromList(modelList, fileDir);
+
+    % Load pairedModelInfo
+    load('pairedModelInfo.mat');
 end
 
-for i = 2:size(pairedModelInfo, 1)
-    pairedModel=pairedModels{i};
+for i = 1:size(pairedModelInfo, 1)
+    load(pairedModelInfo{i,1});
+    % pairedModel=pairedModels{i};
     for p = [2, 4]
         model = getDistributedModel([pairedModelInfo{i, p} '.mat']);
         tmpStr = [pairedModelInfo{i, p}, '_'];
@@ -49,16 +58,26 @@ end
 assert(verifyCobraFunctionError('useDiet', 'inputs',{pairedModel,[]}))
 
 %cleanup
-delete('pairedModel_Abiotrophia_defectiva_ATCC_49176_Acidaminococcus_fermentans_DSM_20731.mat')    
-delete('pairedModel_Abiotrophia_defectiva_ATCC_49176_Acidaminococcus_intestini_RyC_MR95.mat')      
-delete('pairedModel_Abiotrophia_defectiva_ATCC_49176_Acidaminococcus_sp_D21.mat')                  
-delete('pairedModel_Abiotrophia_defectiva_ATCC_49176_Acinetobacter_calcoaceticus_PHEA_2.mat')      
-delete('pairedModel_Acidaminococcus_fermentans_DSM_20731_Acidaminococcus_intestini_RyC_MR95.mat')  
-delete('pairedModel_Acidaminococcus_fermentans_DSM_20731_Acidaminococcus_sp_D21.mat')              
-delete('pairedModel_Acidaminococcus_fermentans_DSM_20731_Acinetobacter_calcoaceticus_PHEA_2.mat')  
-delete('pairedModel_Acidaminococcus_intestini_RyC_MR95_Acidaminococcus_sp_D21.mat')                
-delete('pairedModel_Acidaminococcus_intestini_RyC_MR95_Acinetobacter_calcoaceticus_PHEA_2.mat')    
-delete('pairedModel_Acidaminococcus_sp_D21_Acinetobacter_calcoaceticus_PHEA_2.mat')   
+clear('pairedModelInfo')
+delete('Abiotrophia_defectiva_ATCC_49176.mat')
+delete('Acidaminococcus_fermentans_DSM_20731.mat')
+delete('Acidaminococcus_intestini_RyC_MR95.mat')
+delete('Acidaminococcus_sp_D21.mat')
+delete('Acinetobacter_calcoaceticus_PHEA_2.mat')
+delete('pairedModelInfo.mat')
+delete('pairedModel_Abiotrophia_defectiva_ATCC_49176_Acidaminococcus_fermentans_DSM_20731.mat')
+delete('pairedModel_Abiotrophia_defectiva_ATCC_49176_Acidaminococcus_intestini_RyC_MR95.mat')
+delete('pairedModel_Abiotrophia_defectiva_ATCC_49176_Acidaminococcus_sp_D21.mat')
+delete('pairedModel_Abiotrophia_defectiva_ATCC_49176_Acinetobacter_calcoaceticus_PHEA_2.mat')
+delete('pairedModel_Acidaminococcus_fermentans_DSM_20731_Acidaminococcus_intestini_RyC_MR95.mat')
+delete('pairedModel_Acidaminococcus_fermentans_DSM_20731_Acidaminococcus_sp_D21.mat')
+delete('pairedModel_Acidaminococcus_fermentans_DSM_20731_Acinetobacter_calcoaceticus_PHEA_2.mat')
+delete('pairedModel_Acidaminococcus_intestini_RyC_MR95_Acidaminococcus_sp_D21.mat')
+delete('pairedModel_Acidaminococcus_intestini_RyC_MR95_Acinetobacter_calcoaceticus_PHEA_2.mat')
+delete('pairedModel_Acidaminococcus_sp_D21_Acinetobacter_calcoaceticus_PHEA_2.mat')
+
+% output a success message
+fprintf('Done.\n')
 
 % change to the current directory
 cd(currentDir)
