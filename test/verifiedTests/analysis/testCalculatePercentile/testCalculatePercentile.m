e1b9547465f34492bdfbc17f3f24c74e1def99f0
farid <farid.zare.edu@gmail.com>
2024-02-27 17:40:53 +0000

    Update testPercentile, changed the name to testCalculatePercentile

--- test/verifiedTests/analysis/testCalculatePercentile/testCalculatePercentile.m
+++ test/verifiedTests/analysis/testCalculatePercentile/testCalculatePercentile.m
@@ -1,4 +1,4 @@
-% The COBRAToolbox: calculatePercentile.m
+% The COBRAToolbox: This is a test function for calculatePercentile.m
 %
 % Purpose:
 %     - For testing the IgemRNA preprocessing function for calculating
@@ -7,21 +7,28 @@
 % Authors:
 %     - Kristina Grausa 05/16/2022 - created 
 %     - Kristina Grausa 08/22/2022 - standard header and formatting
+%     - Farid Zare 27 Feb 2024, Standard formatting and name
 %
+
+global CBTDIR
+
+% save the current path and initialize the test
+currentDir = cd(fileparts(which(mfilename)));
+
+% require the specified toolboxes and solvers, along with a UNIX OS
+solvers = prepareTest('needsLP', true);
 
 % load reference data
 trData = readtable('testData_transcriptomics.xlsx','Sheet','SRR8994357_WT');
 expressionValues = trData.Data;
-k = '5';
 
 for k = 1:length(solvers.LP)
-    fprintf(' -- Running testPercentile.m using the solver interface: %s ... ', solvers.LP{k});
+    fprintf(' -- Running testCalculatePercentile.m using the solver interface: %s ... ', solvers.LP{k});
     
     solverLPOK = changeCobraSolver(solvers.LP{k}, 'LP', 0);
 
     if solverLPOK
        value = calculatePercentile(expressionValues, k);
-      
        assert(value == 4)
        
        % output a success message
