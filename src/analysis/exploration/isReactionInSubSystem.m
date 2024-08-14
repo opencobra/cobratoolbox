8024e54e284274f3196e6cd0a3d6ab6611192a22
farid <farid.zare.edu@gmail.com>
2024-08-14 15:36:28 +0100

    update isReactionInSubSystem with COBRA V4

--- src/analysis/exploration/isReactionInSubSystem.m
+++ src/analysis/exploration/isReactionInSubSystem.m
@@ -1,13 +1,13 @@
-function [present]  = isReactionInSubSystem(model,reactions,subSystem)
+function [present] = isReactionInSubSystem(model,reactions,subSystem)
 % Determine whether a reaction is in a given subSystem.
 %
 % USAGE:
 %
-%    [subSystems]  = getModelSubSystems(model)
+%    [present]  = isReactionInSubSystem(model,reactions,subSystem)
 %
 % INPUT:
 %    model:                 A COBRA model struct with at least rxns and
-%                           subSystems fields
+%                           subSystems or rxn2subSystem(COBRA V4) fields
 %    reactions:             Either a string identifying a reaction, or a
 %                           cell array of strings identifying multiple
 %                           reactions, or a double vector identifying the
@@ -16,22 +16,38 @@ function [present]  = isReactionInSubSystem(model,reactions,subSystem)
 % OUTPUT:
 %    present:               a boolean vector for each provided reaction.
 %
-% USAGE:
-%    %Get all subSystems present in the model.
-%    [subSystems]  = getModelSubSystems(model)
 %
 % .. Author: - Thomas Pfau Nov 2017
+%            - Farid Zare 2024/08/14 support COBRA model V4
 
 if ischar(reactions)
     reactions = {reactions};
 end
 
-if iscell(reactions)
-    [~,reactions] = ismember(reactions,model.rxns);
+if ischar(subSystem)
+    subSystem = {subSystem};
+end
+if numel(subSystem) > 1
+    error('This function can support only one subSystem as input')
 end
 
-if isfield(model, 'subSystems')
-    present = cellfun(@(x) any(ismember(x,subSystem)),model.subSystems(reactions));    
-else
-    present = false(numel(reactions));
+% Set defualt value
+present = false(size(reactions));
+
+if iscell(reactions)
+    [rxnID] = ismember(model.rxns, reactions);
+    [rxnExists] = ismember(reactions, model.rxns);
 end
+
+% Check to see if model already has "rxn2subSystem" fields
+if ~isfield(model, 'rxn2subSystem')
+    warning('The "rxn2subSystem" field has been generated because it was not in the model.')
+    model = buildRxn2subSystem(model);
+end
+
+% find subSystem from reactions
+subSystemID = ismember(model.subSystemNames, subSystem);
+rxn2subSystemMat = model.rxn2subSystem(rxnID, subSystemID);
+
+% Find existing reaction IDs
+present(rxnExists) = logical(rxn2subSystemMat);
---
+++ test/verifiedTests/analysis/testIsReactionInSubSystem/testIsReactionInSubSystem.m
@@ -0,0 +1,49 @@
+% The COBRAToolbox: <testIsReactionInSubSystem>.m
+%
+% Purpose:
+%     - This test checks the correct functionality of isReactionInSubSystem
+%
+% Authors:
+%     - Farid Zare 2024/08/14
+%
+
+% save the current path and initialize the test
+currentDir = cd(fileparts(which(mfilename)));
+
+% determine the test path for references
+testPath = pwd;
+
+% Create a Toy model
+model = createToyModel(0, 0, 0);
+
+% Add subSystems field
+subSystemsCell = {''; 'S1'; {'S1'; 'S2'}; 'S3'; {'S3'; 'S1'}; ''};
+model.subSystems = subSystemsCell;
+
+% Load reference data
+presentref = [0 1 1];
+
+% Initiate the test
+fprintf(' -- Running testIsReactionInSubSystem ... \n');
+
+[present] = isReactionInSubSystem(model, {'Ex_A', 'R4', 'R5'}, {'S3'});
+assert(isequal(present, presentref))
+
+% Test vertical input
+[present] = isReactionInSubSystem(model, {'Ex_A'; 'R4'; 'R5'}, {'S3', 'S2'});
+presentref = [0; 1; 1];
+assert(isequal(present, presentref))
+
+% Test subSystem as a string input
+[present] = isReactionInSubSystem(model, {'Ex_A'; 'R4'; 'R5'}, 'S3');
+assert(isequal(present, presentref))
+
+% Test COBRA V4 model
+model = buildRxn2subSystem(model);
+[present] = isReactionInSubSystem(model, {'Ex_A'; 'R4'; 'R5'}, 'S3');
+assert(isequal(present, presentref))
+
+% output a success message
+fprintf('Done.\n');
+% change the directory
+cd(currentDir)
