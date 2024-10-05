1c706ec8b33dfa0afeee780c9c617d9ac49dc14e
rosieluo2021 <rosieluo2021@gmail.com>
2024-10-05 14:43:05 +0100

    simply debug_testLiftModel

--- test/verifiedTests/base/testSolvers/createToyModelForLifting.m
+++ test/verifiedTests/base/testSolvers/createToyModelForLifting.m
@@ -10,17 +10,11 @@ function model = createToyModelForLifting(Coupling)
 
 model = createModel();
 %Reactions in {Rxn Name, MetaboliteNames, Stoichiometric coefficient} format
-if ~Coupling
-    Reactions = {'R1',{'A','B'},[-1 1e10];...
-        'R2',{'B', 'D','C'},[-1e5 1e7 1];...
-        'R3',{'C','D','E'},[-1, -1e7, 1];...
-        'R4',{'F','G',},[-1, 1]};
-else
-    Reactions = {'R1',{'A','B'},[-1 1e10];...
-        'R2',{'B', 'D','C'},[-1e5 1e7 1];...
-        'R3',{'C','D','E','CouplingMet'},[-1, -1e7, 1, -1];...
-        'R4',{'F','G','CouplingMet'},[-1, 1, 1e5]};
-end
+Reactions = {'R1',{'A','B'},[-1 1e10];...
+    'R2',{'B', 'D','C'},[-1e5 1e7 1];...
+    'R3',{'C','D','E','CouplingMet'},[-1, -1e7, 1, -1];...
+    'R4',{'F','G','CouplingMet'},[-1, 1, 1e5]};
+
 ExchangedMets = {'A','E','F','G'};
 %Add Reactions
 for i = 1:size(Reactions,1)
@@ -31,6 +25,10 @@ couplingPos = ismember(model.mets,'CouplingMet');
 model.csense(couplingPos) = 'G'; %(there has to be at least 0 coupling met.)
 %The model can have at most 0.01 units of CouplingMet.
 model = changeRxnBounds(model,'R4',1e-2,'u');
+if ~Coupling
+    removeRxnFlag = 0; % only remove the metabolites but keep the reactions
+    model = removeMetabolites(model,'CouplingMet',removeRxnFlag);
+end
 
 %Add Exchangers
 model = addExchangeRxn(model,ExchangedMets,-1e6*ones(numel(ExchangedMets),1),1e6*ones(numel(ExchangedMets),1));
