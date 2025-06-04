73c4d9fad823cb951d082e9250d0b2d32ab98ab5
farid <farid.zare.edu@gmail.com>
2025-06-04 14:43:06 +0100

    Move testMaps to verified tests

--- test/verifiedTests/visualization/testMaps/testMaps.m
+++ test/verifiedTests/visualization/testMaps/testMaps.m
@@ -1,16 +1,21 @@
-function x = testMaps()
-%tests the functionality of the functions in testMaps especially drawLine,
-%   drawConc, drawFlux
+% The COBRAToolbox: testMaps.m
 %
-%   testMaps
+% Purpose:
+%     - tests the functionality of drawFluxVariability and drawFlux
+%     functions
 %
-%
+% Authors:
+%     - Farid Zare, 04/06/2025: Enhanced formatting and documentation.
 %
 
-oriFolder = pwd;
+% save the current path
+currentDir = pwd;
 
-mFilePath = mfilename('fullpath');
-cd(mFilePath(1:end-length(mfilename)));
+% initialize the test
+fileDir = fileparts(which('testMaps'));
+cd(fileDir);
+
+fprintf('   Testing testMaps ... \n')
 
 mapCoordinateFilename='ecoli_core_map.txt';
 
@@ -18,52 +23,53 @@ map = readCbMap(mapCoordinateFilename);
 load('ecoli_core_model.mat');
 sol = optimizeCbModel(model);
 global CB_MAP_OUTPUT
-if ~strcmp(CB_MAP_OUTPUT,'svg')
-    f = figure;
-end
+CB_MAP_OUTPUT = 'svg';
+
 drawCbMap(map);
 
 %tests drawConc with zero concentrations of metabolites
 disp('Testing drawConc with zero concentrations');
 conc = zeros(size(model.metNames,1), 1);
 drawConc(map, model, conc);
-disp(' ');
-disp(' ');
 
 disp('Testing drawConc with non-zero concentrations');
 conc = rand(size(model.metNames,1),1);
 drawConc(map, model, conc);
-disp(' ');
-disp(' ');
 
 %tests drawFlux with zero fluxes
 disp('Testing drawFlux with zero flux');
 flux = zeros(size(model.rxns,1), 1);
 drawFlux(map, model, flux);
-disp(' ');
-disp(' ');
+% Check and remove 'target.svg' if it exists
+assert(exist('target.svg', 'file') == 2)
+delete('target.svg');
 
 %tests drawFlux with non-zero fluxes w/ reaction with zero flux set to
 %width of 1
 disp('Testing drawFlux with non-zero flux');
 flux = sol.x;
 drawFlux(map, model, flux, [],'zeroFluxWidth',1);
-disp(' ');
-disp(' ');
+% Check and remove 'target.svg' if it exists
+assert(exist('target.svg', 'file') == 2)
+delete('target.svg');
 
 %tests drawFluxVariablity with enlarged arrowheads to denote flux
 %directionality
 load('testMapsData.mat','minFlux','maxFlux');
-display('Testing drawFluxVariability')
+disp('Testing drawFluxVariability')
 drawFluxVariability(map,model,minFlux,maxFlux,[],'rxnDirMultiplier',2.5);
-display(' ');
-display(' ');
-
+% Check and remove 'target.svg' if it exists
+assert(exist('target.svg', 'file') == 2)
+delete('target.svg');
 
 x=1;
 if ~strcmp(CB_MAP_OUTPUT,'svg')
     close(f);
 end
-cd(oriFolder);
 
-end
+% output a success message
+fprintf('Done.\n');
+
+% change the directory
+cd(currentDir)
+
