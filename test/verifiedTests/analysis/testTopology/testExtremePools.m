a797f25eb1f122b7709feb2f74cc802cafb4f324
Farid Zare <farid.zare.edu@gmail.com>
2025-10-20 11:39:17 +0100

    Fix typo in requiredSoftware to requiredSoftwares

--- test/verifiedTests/analysis/testTopology/testExtremePools.m
+++ test/verifiedTests/analysis/testTopology/testExtremePools.m
@@ -10,7 +10,7 @@
 %     Extreme Pathway Lengths and Reaction Participation in Genome-Scale Metabolic Networks
 %     Jason A. Papin, Nathan D. Price and Bernhard Ø. Palsson
 
-solverPkgs = prepareTest('requiredSoftware', {'lrs'});
+solverPkgs = prepareTest('requiredSoftwares', {'lrs'});
 
 [status, result] = system('which lrs');
 
